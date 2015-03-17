efe5cd5dc7b183d573507524c3480ed2d58615d1
pvy <pvy@odoo.com>
2015-03-17 15:11:36 +0100

    [WIP] account, tests : yml tests and some fixes in l10n_be

--- addons/l10n_be/__openerp__.py
+++ addons/l10n_be/__openerp__.py
@@ -87,7 +87,10 @@ Wizards provided by this module:
         '../account/demo/account_invoice_demo.yml',
     ],
     'test': [
-        '../account/test/account_bank_statement.yml',
+        #'../account/test/account_bank_statement.yml',
+        #'../account/test/account_cash_statement.yml',
+        '../account/test/account_invoice_state.yml',
+        '../account/test/account_pre_install.yml',
     ],
     'installable': True,
     'website': 'https://www.odoo.com/page/accounting',
