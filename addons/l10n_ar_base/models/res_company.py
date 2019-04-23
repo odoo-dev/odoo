a9f52989f1689969d2969a8c2c4d69e19fbfb8b4
Katherine Zaoral <kz@adhoc.com.ar>
2019-04-23 16:50:28 -0300

    [REM] l10n_ar_base: not need to overwrite or use onchange in order to set id_number/id_category on partner.

--- addons/l10n_ar_base/models/res_company.py
+++ addons/l10n_ar_base/models/res_company.py
@@ -18,22 +18,3 @@ class ResCompany(models.Model):
     @api.multi
     def cuit_required(self):
         return self.partner_id.cuit_required()
-
-    @api.model
-    def create(self, vals):
-        """
-        On create, we set id number to partner
-        """
-        company = super(ResCompany, self).create(vals)
-        company.change_main_id_category()
-        l10n_ar_id_number = vals.get('l10n_ar_id_number')
-        if l10n_ar_id_number:
-            company.partner_id.l10n_ar_id_number = l10n_ar_id_number
-        return company
-
-    @api.onchange('l10n_ar_id_category_id')
-    def change_main_id_category(self):
-        # we force change on partner to get updated number
-        if self.partner_id:
-            self.partner_id.l10n_ar_id_category_id = self.l10n_ar_id_category_id
-            self.l10n_ar_id_number = self.partner_id.l10n_ar_id_number
