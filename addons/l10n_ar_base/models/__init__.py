865a0b311c22c1d005942b31dee7c5fafd0f5ec2
Katherine Zaoral <kz@adhoc.com.ar>
2019-04-23 18:12:01 -0300

    [FIX] l10n_ar_base: re order import order in order to do define first model and then the related fields.

--- addons/l10n_ar_base/models/__init__.py
+++ addons/l10n_ar_base/models/__init__.py
@@ -3,6 +3,6 @@
 # directory
 ##############################################################################
 from . import res_partner_bank
+from . import res_partner_id_category
 from . import res_company
 from . import res_partner
-from . import res_partner_id_category
