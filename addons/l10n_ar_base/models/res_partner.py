78e4ce3c7d3b5f219515f989403d774de9ef780b
Katherine Zaoral <kz@adhoc.com.ar>
2019-04-24 17:41:20 -0300

    [FIX] l10n_ar: add mising import of ValidationError

--- addons/l10n_ar_base/models/res_partner.py
+++ addons/l10n_ar_base/models/res_partner.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tools.safe_eval import safe_eval
 
 
