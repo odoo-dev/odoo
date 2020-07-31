6ebecdf82bf6070a0e9a80344e02dac6659f09c9
Thibault Delavallée <tde@odoo.com>
2020-07-31 19:58:30 +0200

    mergeup: exhibitor

--- addons/website_event_track_exhibitor/models/event_event.py
+++ addons/website_event_track_exhibitor/models/event_event.py
@@ -20,9 +20,9 @@ class EventEvent(models.Model):
         for event in self:
             if event.event_type_id and event.event_type_id != event._origin.event_type_id:
                 event.exhibitor_menu = event.event_type_id.exhibitor_menu
-            elif event.website_menu and event.website_menu != event._origin.website_menu:
+            elif event.website_menu and event.website_menu != event._origin.website_menu or not event.exhibitor_menu:
                 event.exhibitor_menu = True
-            elif not event.website_menu or not event.exhibitor_menu:
+            elif not event.website_menu:
                 event.exhibitor_menu = False
 
     # ------------------------------------------------------------
