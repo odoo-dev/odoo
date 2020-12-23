96dc5efe7848710040eeb641eb02a5228f0b6d00
Benoit Socias <bso@odoo.com>
2020-12-23 09:29:35 +0100

    [IMP] website_sale: provide an anchor point for updating cart rendering
    Before this commit there was no unique place for modules to participate
    in the cart rendering values.
    
    After this commit modules can update the cart rendering values by
    overloading the controller's _prepare_cart_values(...).
    
    task-2319729
    https://github.com/odoo/odoo/pull/63628
    https://github.com/odoo/enterprise/pull/15375

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -374,6 +374,10 @@ class WebsiteSale(http.Controller):
         request.website.sale_get_order(code=promo)
         return request.redirect(redirect)
 
+    def _prepare_cart_values(self, request, values, **post):
+        """Anchor point for modules that need to update the cart rendered information"""
+        pass
+
     @http.route(['/shop/cart'], type='http', auth="public", website=True, sitemap=False)
     def cart(self, access_token=None, revive='', **post):
         """
@@ -412,6 +416,8 @@ class WebsiteSale(http.Controller):
             if not request.env.context.get('pricelist'):
                 _order = order.with_context(pricelist=order.pricelist_id.id)
             values['suggested_products'] = _order._cart_accessories()
+
+        self._prepare_cart_values(request, values, **post)
 
         if post.get('type') == 'popover':
             # force no-cache so IE11 doesn't cache this XHR
