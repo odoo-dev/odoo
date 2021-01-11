984649ff9949ab5cccd4f09b66d45d46e20d6103
Bruno Boi <boi@odoo.com>
2021-01-11 14:42:50 +0100

    wip poc2

--- addons/wowl/static/src/services/service_registry.ts
+++ addons/wowl/static/src/services/service_registry.ts
@@ -19,7 +19,7 @@ import { downloadService } from "./download";
 import { effectService } from "../effects/effects_service";
 import { localizationService } from "./localization";
 import { currencyService } from "./currency";
-import { keyNavService } from "./keynav";
+import { keyboardManagerService } from "./keyboard_manager";
 
 export const serviceRegistry: Registry<Service<any>> = new Registry();
 
@@ -29,7 +29,7 @@ const services = [
   cookieService,
   currencyService,
   dialogManagerService,
-  keyNavService,
+  keyboardManagerService,
   titleService,
   menusService,
   modelService,
--- addons/wowl/static/src/types.ts
+++ addons/wowl/static/src/types.ts
@@ -29,7 +29,7 @@ import { userMenuRegistry } from "./webclient/user_menu_registry";
 import { debugManagerService } from "./debug_manager/debug_manager_service";
 import { deviceService } from "./services/device";
 import { Currency, currencyService } from "./services/currency";
-import { keyNavService } from "./services/keynav";
+import { keyboardManagerService } from "./services/keyboard_manager";
 
 export interface Registries {
   mainComponentRegistry: typeof mainComponentRegistry;
@@ -144,7 +144,7 @@ export interface Services {
   cookie: ServiceType<typeof cookieService["deploy"]>;
   currency: ServiceType<typeof currencyService["deploy"]>;
   dialog_manager: ServiceType<typeof dialogManagerService["deploy"]>;
-  keynav: ServiceType<typeof keyNavService["deploy"]>;
+  keyboard_manager: ServiceType<typeof keyboardManagerService["deploy"]>;
   localization: ServiceType<typeof localizationService["deploy"]>;
   menus: ServiceType<typeof menusService["deploy"]>;
   model: ServiceType<typeof modelService["deploy"]>;
