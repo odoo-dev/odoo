1057590549880356bd2d1edf4121b65a0d3b12b4
Noe Antoine <nan@odoo.com>
2021-01-20 14:07:13 +0100

    [IMP] crm : develop a smart calendar view from crm opportunity
    Before, when the user clicked on a crm opportunity "meetings" smart button,
    the calendar view was centered on the present day, and with default mode
    (week, month,...) as a scale. This behaviour lead to calendar views that
    were possibly not including those meetings.
    
    This smart calendar improves this by computing the target date and the scale
    to make sure that the user is taken to the useful time period when using the
    smart button from crm opportunity. The meetings considered are only the
    relevant one, that is only the ones not finished yet, if there are some, and
    all (finished) meetings otherwise. The calendar works as follows :
    -If no meetings: show current week
    -If one: show that week
    -If more than one and same week : show that week
    -If more than one and same month : show that month
    -If more than one month, show month of the first meeting.
    
    This is done according to user language week_start and timezone. This way,
    the user always see what is the most relevant.
    
    Also add python tests in class TestCRMLeadSmartCalendar.
    
    * web: This changes calendar_view.js. This small change is in ingoing #63370
    It allows to enter an initial_date via context in calendar view.
    
    Task-ID - 2431245

--- addons/crm/tests/__init__.py
+++ addons/crm/tests/__init__.py
@@ -7,6 +7,7 @@ from . import test_crm_lead_convert
 from . import test_crm_lead_convert_mass
 from . import test_crm_lead_lost
 from . import test_crm_lead_merge
+from . import test_crm_lead_smart_calendar
 from . import test_crm_activity
 from . import test_crm_ui
 from . import test_crm_pls
