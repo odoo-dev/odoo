872c65e051915ab0da3c8b9eceab8382aac5bc91
Didier (did) <did@odoo.com>
2021-01-25 13:55:19 +0100

    CTRL+K to open the switcher

--- addons/mail/static/src/components/switcher/switcher.js
+++ addons/mail/static/src/components/switcher/switcher.js
@@ -5,7 +5,7 @@ const components = {};
 const useShouldUpdateBasedOnProps = require('mail/static/src/component_hooks/use_should_update_based_on_props/use_should_update_based_on_props.js');
 const useStore = require('mail/static/src/component_hooks/use_store/use_store.js');
 
-const { Component } = owl;
+const { Component, useState } = owl;
 
 class Switcher extends Component {
 
@@ -15,16 +15,43 @@ class Switcher extends Component {
     constructor(...args) {
         super(...args);
         useShouldUpdateBasedOnProps();
+
+        this.state = useState({
+            isOpen: false
+        });
+
         // useStore(props => {
         //     const follower = this.env.models['mail.follower'].get(props.followerLocalId);
         //     return [follower ? follower.__state : undefined];
         // });
+
+        document.addEventListener("keydown", event => {
+            // See doc: https://developer.mozilla.org/en-US/docs/Web/API/Document/keydown_event
+            if (event.isComposing || event.keyCode === 229) {
+                return;
+            }
+
+            if (event.ctrlKey && event.keyCode == 75) {
+                event.preventDefault();
+                this._open();
+            }
+        });
     }
 
     //--------------------------------------------------------------------------
     // Public
     //--------------------------------------------------------------------------
 
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+    _open() {
+        this.state.isOpen = true;
+    }
+
+    _close() {
+        this.state.isOpen = false;
+    }
 
     //--------------------------------------------------------------------------
     // Handlers
