4685b792724d42ee615a40253e3a4e0be9e512a6
Gorash <chm@odoo.com>
2021-01-27 17:33:26 +0100

    [IMP] web: add mode qweb in ace, create widget to display profile info

--- addons/web/static/src/js/fields/basic_fields.js
+++ addons/web/static/src/js/fields/basic_fields.js
@@ -3471,7 +3471,8 @@ var AceEditor = DebouncedField.extend({
         '/web/static/lib/ace/ace.js',
         [
             '/web/static/lib/ace/mode-python.js',
-            '/web/static/lib/ace/mode-xml.js'
+            '/web/static/lib/ace/mode-xml.js',
+            '/web/static/lib/ace/mode-qweb.js'
         ]
     ],
     events: {}, // events are triggered manually for this debounced widget
@@ -3564,9 +3565,10 @@ var AceEditor = DebouncedField.extend({
         }
         this.aceEditor.$blockScrolling = true;
         this.aceSession = this.aceEditor.getSession();
+        const mode = this.nodeOptions.mode || 'qweb';
         this.aceSession.setOptions({
             useWorker: false,
-            mode: "ace/mode/" + (this.nodeOptions.mode || 'xml'),
+            mode: "ace/mode/" + (mode === 'xml' ? 'qweb' : mode),
             tabSize: 2,
             useSoftTabs: true,
         });
