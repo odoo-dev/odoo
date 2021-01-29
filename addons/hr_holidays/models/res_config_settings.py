9cb2b49cc842f9137580b77ca96f573394c5a9e5
Arnaud Joset <arj@odoo.com>
2021-01-29 09:33:54 +0100

    WIP_hr_holidays

--- addons/hr_holidays/models/res_config_settings.py
+++ addons/hr_holidays/models/res_config_settings.py
@@ -6,5 +6,5 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    # group_allow_accrual = fields.Boolean("Accrual", implied_group='hr_holidays.group_allow_accrual')
-
+    group_allow_accrual = fields.Boolean("Accrual", implied_group='hr_holidays.group_allow_accrual')
+    module_work_entry_holiday = fields.Boolean(string="Accrual (install work_entry)")
