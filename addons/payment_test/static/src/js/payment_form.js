b1c13a7cb79f552c7accc8f85736beda8628d17a
Victor Feyens <vfe@odoo.com>
2021-02-19 15:38:40 +0100

    [FIX] payment_test: js

--- addons/payment_test/static/src/js/payment_form.js
+++ addons/payment_test/static/src/js/payment_form.js
@@ -11,36 +11,35 @@ odoo.define('payment_test.payment_form', require => {
         //--------------------------------------------------------------------------
 
         /**
-         * Process the payment.
+         * Execute the acquirer-specific implementation of the direct payment flow.
+         *
+         * For an acquirer to redefine the processing of the direct payment flow, it must override
+         * this method.
          *
          * @override method from payment.payment_form_mixin
          * @private
-         * @param {string} provider - The provider of the payment option's acquirer
-         * @param {number} paymentOptionId - The id of the payment option handling the transaction
-         * @param {string} flow - The online payment flow of the transaction
-         * @return {(object|undefined)} The transaction processing values if in direct payment flow
+         * @param {string} provider - The provider of the acquirer
+         * @param {number} acquirerId - The id of the acquirer handling the transaction
+         * @param {object} processingValues - The processing values of the transaction
+         * @return {undefined}
          */
-        _processPayment: function (provider, paymentOptionId, flow) {
-            // Let super create the transaction to work with the processing values
-            return this._super(...arguments).then(result => {
-                if (provider === 'test' && flow === 'direct') {
-                    // Real acquirers don't have access to this information
-                    const customerInput = document.getElementById('customer_input').value;
-                    // Simulate a feedback from our imaginary payment provider
-                    this._rpc({
-                         route: '/payment/test/simulate_payment',
-                         params: {
-                             'reference': result.reference,
-                             'customer_input': customerInput,
-                         },
-                    }).then(() => {
-                        window.location = '/payment/status';
-                    });
-                }
-                return result;
+        _processDirectPayment: function (provider, acquirerId, processingValues) {
+            if (provider !== 'test') {
+                return this._super(...arguments);
+            }
+
+            const customerInput = document.getElementById('customer_input').value;
+            // Simulate a feedback from our imaginary payment provider
+            return this._rpc({
+                route: '/payment/test/simulate_payment',
+                params: {
+                    'reference': processingValues.reference,
+                    'customer_input': customerInput,
+                },
+            }).then(() => {
+                window.location = '/payment/status';
             });
         },
-
 
         /**
          * Prepare the inline form of Test for direct payment.
