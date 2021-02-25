b1113035fe77b691c9bafc276c8fceb9e8fd0511
Victor Feyens <vfe@odoo.com>
2021-02-25 11:29:39 +0100

    [FIX] sale: _get_compatible_acquirers args specification

--- addons/sale/controllers/portal.py
+++ addons/sale/controllers/portal.py
@@ -183,10 +183,9 @@ class CustomerPortal(portal.CustomerPortal):
         if order_sudo.has_to_be_paid():
             logged_in = not request.env.user._is_public()
             acquirers_sudo = request.env['payment.acquirer'].sudo()._get_compatible_acquirers(
-                order_sudo.company_id.id,
-                order_sudo.partner_id.id,
-                order_sudo.currency_id.id,
-                sale_order_id=order_sudo.id,
+                company_id=order_sudo.company_id.id,
+                partner_id=order_sudo.partner_id.id,
+                currency_id=order_sudo.currency_id.id,
             )  # In sudo mode to read the fields of acquirers and partner (if not logged in)
             tokens = request.env['payment.token'].search([
                 ('acquirer_id', 'in', acquirers_sudo.ids),
