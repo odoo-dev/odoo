c7fe28c683ec4493dbc7f73f73c5ddca00c79ba4
Victor Feyens <vfe@odoo.com>
2021-02-25 11:22:45 +0100

    [IMP] payment_paypal: do not pass unnecessary values to form rendering

--- addons/payment_paypal/models/payment_transaction.py
+++ addons/payment_paypal/models/payment_transaction.py
@@ -29,8 +29,8 @@ class PaymentTransaction(models.Model):
         notify_url = self.acquirer_id.paypal_use_ipn \
                      and urls.url_join(base_url, PaypalController._notify_url)
         return {
-            **processing_values,
             'address1': self.partner_address,
+            'amount': processing_values['amount'],
             'business': self.acquirer_id.paypal_email_account,
             'city': self.partner_city,
             'country': self.partner_country_id.code,
