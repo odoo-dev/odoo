55198b89f4e70e6e4356d86524cd04d2af66ab9c
Xavier BOL (xbo) <xbo@odoo.com>
2021-02-26 15:25:53 +0100

    [IMP] sale_project: convert SO field in project into related field
    Before this commit, the user can select the SO in the project to filter
    the SOLs in its tasks and in SOL fields in the project form view. But
    since, the user can now select any SOLs of the customer defined in the
    project or task. Then this field is useless to defined in the project
    form view.
    
    This commit converts the sale_order_id field into a related field of
    the one in the sale_line_id field of the project. Moreover, the
    sale_order_id field is now invisible in the view, the user just need to
    select the default sales order item in the project and the SO of the
    project will be the SO of the default sales order item.
    Then, some unit tests have been updated to essentially remove the
    sale_order_id in the using of create and write methods of
    project.project model.
    Finally, the sale_timesheet_tour has been updated in this commit to
    remove the step when the user selects the default sales order of the
    project.
    
    task-2442683
    
    closes #66181

--- addons/sale_timesheet/models/project.py
+++ addons/sale_timesheet/models/project.py
@@ -49,13 +49,13 @@ class Project(models.Model):
         default=_default_timesheet_product_id)
     warning_employee_rate = fields.Boolean(compute='_compute_warning_employee_rate')
 
-    @api.depends('sale_order_id', 'sale_line_employee_ids', 'allow_billable')
+    @api.depends('sale_line_id', 'sale_line_employee_ids', 'allow_billable')
     def _compute_pricing_type(self):
         billable_projects = self.filtered('allow_billable')
         for project in billable_projects:
             if project.sale_line_employee_ids:
                 project.pricing_type = 'employee_rate'
-            elif project.sale_order_id:
+            elif project.sale_line_id:
                 project.pricing_type = 'fixed_rate'
             else:
                 project.pricing_type = 'task_rate'
@@ -68,21 +68,21 @@ class Project(models.Model):
             return [('allow_billable', operator, value)]
         if operator == '=':
             if value == 'task_rate':
-                return [('sale_line_employee_ids', '=', False), ('sale_order_id', '=', False)]
+                return [('sale_line_employee_ids', '=', False), ('sale_line_id', '=', False)]
             if value == 'fixed_rate':
-                return [('sale_line_employee_ids', '=', False), ('sale_order_id', '!=', False)]
+                return [('sale_line_employee_ids', '=', False), ('sale_line_id', '!=', False)]
             if value == 'employee_rate':
-                return [('sale_line_employee_ids', '!=', False), ('sale_order_id', '!=', False)]
+                return [('sale_line_employee_ids', '!=', False), ('sale_line_id', '!=', False)]
         else:
             if value == 'task_rate':
-                return ['|', ('sale_line_employee_ids', '!=', False), ('sale_order_id', '!=', False)]
+                return ['|', ('sale_line_employee_ids', '!=', False), ('sale_line_id', '!=', False)]
             if value == 'fixed_rate':
-                return ['|', ('sale_line_employee_ids', '!=', False), ('sale_order_id', '=', False)]
+                return ['|', ('sale_line_employee_ids', '!=', False), ('sale_line_id', '=', False)]
             if value == 'employee_rate':
                 return [('sale_line_employee_ids', '=', False)]
         return UserError(_('Value does not exist in the pricing type'))
 
-    @api.depends('sale_order_id', 'partner_id', 'pricing_type')
+    @api.depends('partner_id', 'pricing_type')
     def _compute_display_create_order(self):
         for project in self:
             project.display_create_order = project.partner_id and project.pricing_type == 'task_rate'
