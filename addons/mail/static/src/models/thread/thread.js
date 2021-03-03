c7948ed06ce0ee46bcf504f8b321d96feb5665c2
Julien Giannone <jgi@odoo.com>
2021-03-03 15:18:58 +0100

    [IMP] mail: group DM
    This commit adds feature to create group chat channels
    
    task-id: 2360934

--- addons/mail/static/src/models/thread/thread.js
+++ addons/mail/static/src/models/thread/thread.js
@@ -520,6 +520,39 @@ function factory(dependencies) {
         }
 
         /**
+         * Performs the `group_chat_create` RPC on `mail.channel`.
+         *
+         *
+         * @static
+         * @param {Object} param0
+         * @param {integer[]} param0.partnerIds
+         * @param {boolean} [param0.pinForCurrentPartner]
+         * @returns {mail.thread|undefined} the created or existing chat
+         */
+        static async performRpcCreateGroupChat({ partnerIds, pinForCurrentPartner }) {
+            const device = this.env.messaging.device;
+            const data = await this.env.services.rpc({
+                model: 'mail.channel',
+                method: 'group_chat_create',
+                kwargs: {
+                    context: Object.assign({}, this.env.session.user_content, {
+                        // optimize the return value by avoiding useless queries
+                        // in non-mobile devices
+                        isMobile: device.isMobile,
+                    }),
+                    partners_to: partnerIds,
+                    pin: pinForCurrentPartner,
+                },
+            });
+            if (!data) {
+                return;
+            }
+            return this.env.models['mail.thread'].insert(
+                this.env.models['mail.thread'].convertData(data)
+            );
+        }
+
+        /**
          * Performs the `execute_command` RPC on `mail.channel`.
          *
          * @static
@@ -1166,6 +1199,9 @@ function factory(dependencies) {
         _computeDisplayName() {
             if (this.channel_type === 'chat' && this.correspondent) {
                 return this.custom_channel_name || this.correspondent.nameOrDisplayName;
+            }
+            if (this.channel_type === 'group') {
+                return this.custom_channel_name || this.name;
             }
             return this.name;
         }
