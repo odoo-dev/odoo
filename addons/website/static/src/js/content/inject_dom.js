3d12db6c5bfbe3a69614dd219c3a0bbadb770321
Arthur Detroux (ard) <ard@odoo.com>
2021-03-11 10:04:39 +0100

    fixed cookie funciton for compatibility

--- addons/website/static/src/js/content/inject_dom.js
+++ addons/website/static/src/js/content/inject_dom.js
@@ -1,14 +1,11 @@
 (function () {
-    'use strict';
-    const getCookieValue = (name) => (
-        document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop().replace(/"/g, '') || ''
-    );
     document.addEventListener('DOMContentLoaded', () => {
         const htmlEl = document.getElementsByTagName('html')[0];
         const cookieNames = ['utm_source', 'utm_medium', 'utm_campaign'];
+        const cookies = document.cookie.split('; ').map(cookie => cookie.split('=')).reduce((acc, cookie) => ({ ...acc, [cookie[0]]: cookie[1].replace(/"/g, '') }), {});
         for (const name of cookieNames) {
-            const cookie = getCookieValue(`odoo_${name}`);
-            if (cookie !== '') {
+            const cookie = cookies[`odoo_${name}`];
+            if (cookie) {
                 htmlEl.setAttribute(`data-${name.replace('_', '-')}`, cookie);
             }
         }
