eefdb9f3b7c26d4b4f20455abcde819cc2c47984
Francois (fge) <fge@odoo.com>
2021-03-11 11:41:47 +0100

    WIP

--- addons/wowl/static/src/command_palette/command_palette_dialog.js
+++ addons/wowl/static/src/command_palette/command_palette_dialog.js
@@ -1,12 +1,33 @@
 /** @odoo-module **/
 import { Dialog } from "../components/dialog/dialog";
 const { Component, hooks } = owl;
-const { useExternalListener } = hooks;
+const { useExternalListener, useState } = hooks;
 
 export class CommandPaletteDialog extends Component {
   setup() {
     useExternalListener(window, "keydown", this._onKeydown);
+    this.state = useState({
+      searchValue: "",
+      filterCategories: this.props.categories,
+    });
   }
+
+  _search(ev) {
+    console.log(this.state.searchValue);
+    console.log(this.state.filterCategories);
+    //this.state.filterCategories = {};
+    const searchValue = this.state.searchValue.toLowerCase();
+    for (const key in this.props.categories) {
+      if (Object.hasOwnProperty.call(this.props.categories, key)) {
+        let commands = this.props.categories[key].items.filter((command) => fuzzysearch(searchValue, command.name.toLowerCase()));
+
+          this.state.filterCategories[key].items = commands;
+      }
+    }
+    console.log(this.state.filterCategories);
+    //this.state.filterCategories = result;
+  }
+
   _onKeydown(ev) {
     switch (ev.key) {
       case "Enter":
@@ -28,3 +49,25 @@ export class CommandPaletteDialog extends Component {
 }
 CommandPaletteDialog.template = "wowl.CommandPaletteDialog";
 CommandPaletteDialog.components = { Dialog };
+
+
+function fuzzysearch (needle, haystack) {
+  var hlen = haystack.length;
+  var nlen = needle.length;
+  if (nlen > hlen) {
+    return false;
+  }
+  if (nlen === hlen) {
+    return needle === haystack;
+  }
+  outer: for (var i = 0, j = 0; i < nlen; i++) {
+    var nch = needle.charCodeAt(i);
+    while (j < hlen) {
+      if (haystack.charCodeAt(j++) === nch) {
+        continue outer;
+      }
+    }
+    return false;
+  }
+  return true;
+}
