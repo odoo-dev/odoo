b485b8604a0609dc926fa9030e3ccdf1001d7d68
Matthieu Stockbauer <tsm@odoo.com>
2021-03-29 16:27:37 +0200

    [IMP] website : better after survey tours

--- addons/website/static/src/js/tours/survey_tour.js
+++ addons/website/static/src/js/tours/survey_tour.js
@@ -1,27 +1,36 @@
 odoo.define("website.survey.tour", function (require) {
 "use strict";
-        
+
 const wTourUtils = require("website.tour_utils");
 const core = require("web.core");
 const _t = core._t;
 const snippets = require('tour_survey.snippets');
-        
-const steps = [
-    wTourUtils.clickOnText(snippets.title.snippet, snippets.title.element),
-    wTourUtils.clickOnSnippet(snippets.background),
-    wTourUtils.changeBackgroundColor(),
-    wTourUtils.changeImage(snippets.image),
-    wTourUtils.clickOnSnippet(snippets.padded ? snippets.padded : snippets.title.snippet),
-    wTourUtils.changePaddingSize('bottom'),
-];
-        
+
+const imageStep = snippets.title.snippet.id === 's_text_image' ?
+    wTourUtils.changeImage(snippets.title.snippet) : wTourUtils.changeBackground();
+
+const backgroundColorStep = [wTourUtils.changeBackgroundColor()];
+if (snippets.background)
+    backgroundColorStep.unshift(wTourUtils.clickOnSnippet(snippets.background));
+
+const shapeStep = [];
 if (snippets.shape) {
-    steps.push(wTourUtils.clickOnSnippet(snippets.shape));
-    steps.push(wTourUtils.changeOption('BackgroundShape', 'we-toggler', _t('Background Shape')));
-    steps.push(wTourUtils.selectNested('we-select-page', 'BackgroundShape', ':not(.o_we_pager_controls)', _t('Background Shape')));
+    const previousID = snippets.background ? snippets.background.id : snippets.title.snippet.id;
+    if (snippets.shape.id !== previousID)
+       shapeStep.push(wTourUtils.clickOnSnippet(snippets.shape));
+    shapeStep.push(wTourUtils.changeOption('BackgroundShape', 'we-toggler', _t('Background Shape')));
+    shapeStep.push(wTourUtils.selectNested('we-select-page', 'BackgroundShape', ':not(.o_we_pager_controls', _t('Background Shape'))); 
 }
 
-steps.push(wTourUtils.clickOnSave());
-wTourUtils.registerThemeHomepageTour('survey_tour', steps);    
+const steps = [
+    wTourUtils.clickOnText(snippets.title.snippet, snippets.title.element),
+    imageStep,
+    ...backgroundColorStep,
+    ...shapeStep,
+    wTourUtils.changePaddingSize('top'),
+    wTourUtils.clickOnSave(),
+];
 
-});    
+wTourUtils.registerThemeHomepageTour('survey_tour', steps);
+
+});
