97821b4be3fe2464e98067b941d98e3164864d11
Sébastien Theys <seb@odoo.com>
2021-07-07 18:39:00 +0200

    [IMP] mail: allow click on member avatar or name in member list

--- addons/mail/static/src/models/thread/thread.js
+++ addons/mail/static/src/models/thread/thread.js
@@ -947,6 +947,26 @@ function factory(dependencies) {
         }
 
         /**
+         * Handles click on the avatar of the given member in the member list of
+         * this channel.
+         *
+         * @param {mail.partner} member
+         */
+        onClickMemberAvatar(member) {
+            member.openChat();
+        }
+
+        /**
+         * Handles click on the name of the given member in the member list of
+         * this channel.
+         *
+         * @param {mail.partner} member
+         */
+        onClickMemberName(member) {
+            member.openProfile();
+        }
+
+        /**
          * Opens this thread either as form view, in discuss app, or as a chat
          * window. The thread will be opened in an "active" matter, which will
          * interrupt current user flow.
