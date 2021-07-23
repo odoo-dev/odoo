3d8ad500e09031ced3dff4183dbe541e2b14c205
Antoine Guenet <age@odoo.com>
2021-07-23 15:42:39 +0200

    [IMP] mass_mailing: replace text_image with website's text_image snippet
    This replaces mass_mailing's text_image snippet with website's
    text_image snippet.
    
    Task-2554899

--- addons/mass_mailing/__manifest__.py
+++ addons/mass_mailing/__manifest__.py
@@ -50,6 +50,7 @@
         'views/snippets/s_image_text.xml',
         'views/snippets/s_numbers.xml',
         'views/snippets/s_text_block.xml',
+        'views/snippets/s_text_image.xml',
         'views/snippets/s_three_columns.xml',
         'views/snippets/s_title.xml',
     ],
