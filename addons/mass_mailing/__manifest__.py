7e766bec0d72533a7a0f53e7837f987a4920a910
Antoine Guenet <age@odoo.com>
2021-07-26 11:49:36 +0200

    [ADD] mass_mailing: add website's features_grid snippet to mass_mailing
    This duplicates and adapts the "Features Grid" snippet from website in
    order to use it in mass_mailing as well.
    
    Task-255489

--- addons/mass_mailing/__manifest__.py
+++ addons/mass_mailing/__manifest__.py
@@ -50,6 +50,7 @@
         'views/snippets/s_company_team.xml',
         'views/snippets/s_comparisons.xml',
         'views/snippets/s_features.xml',
+        'views/snippets/s_features_grid.xml',
         'views/snippets/s_image_text.xml',
         'views/snippets/s_masonry_block.xml',
         'views/snippets/s_media_list.xml',
