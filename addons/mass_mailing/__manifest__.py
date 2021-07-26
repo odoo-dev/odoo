f29b39e6a8943df2461b677eb34908cd3bfaa32e
Antoine Guenet <age@odoo.com>
2021-07-26 11:05:57 +0200

    [IMP] mass_mailing: replace steps with website's process_steps snippet
    This replaces mass_mailing's steps snippet with website's process_steps
    snippet.
    
    Task-2554899

--- addons/mass_mailing/__manifest__.py
+++ addons/mass_mailing/__manifest__.py
@@ -55,6 +55,7 @@
         'views/snippets/s_media_list.xml',
         'views/snippets/s_numbers.xml',
         'views/snippets/s_picture.xml',
+        'views/snippets/s_process_steps.xml',
         'views/snippets/s_references.xml',
         'views/snippets/s_showcase.xml',
         'views/snippets/s_text_block.xml',
