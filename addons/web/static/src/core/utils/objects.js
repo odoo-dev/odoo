de8714f9627dd6848f9f4ae76c6a5dee0dcdb3f8
Lucas Perais (lpe) <lpe@odoo.com>
2021-09-06 10:55:34 +0200

    fixup! [IMP] web: core: deepCopy function for Objects

--- addons/web/static/src/core/utils/objects.js
+++ addons/web/static/src/core/utils/objects.js
@@ -11,6 +11,13 @@ export function shallowEqual(obj1, obj2) {
     );
 }
 
+/**
+ * Deep copies an object. As it relies on JSON this function as some limitations
+ * - no support for circular objects
+ * - no support for specific classes, that will at best be lost and at worst crash (Map, Set etc...)
+ * @param  {Object} An object that is fully JSON stringifiable
+ * @return {Object}
+ */
 export function deepCopy(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
