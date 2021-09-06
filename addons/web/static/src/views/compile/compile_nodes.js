db64775af53bd1ed589fd08545f1f9a3ac6131ce
Lucas Perais (lpe) <lpe@odoo.com>
2021-09-06 11:41:28 +0200

    fixup! [REF] web: introduce compilation building-blocks for views processing

--- addons/web/static/src/views/compile/compile_nodes.js
+++ addons/web/static/src/views/compile/compile_nodes.js
@@ -1,6 +1,17 @@
 /* @odoo-module */
 import { appendTo, addLegacyNodeInfo } from "./compile_lib";
 
+/**
+ * An object containing various information about the current
+ * compilation from an Arch to a owl template.
+ * @typedef {Object} CompilationContext
+ */
+
+/**
+ * If a group node has a string, compile a title div for it
+ * @param  {Element} node an arch's node
+ * @return {Element}
+ */
 function makeGroupTitleRow(node) {
     const titleDiv = this.document.createElement("div");
     titleDiv.classList.add("o_horizontal_separator");
@@ -8,7 +19,21 @@ function makeGroupTitleRow(node) {
     return titleDiv;
 }
 
-export function compileGroup({ document, compileNode, outerGroupCol }, { node, params }) {
+/**
+ * Compiles a template node for a `<group>`arch's node. Only for first level
+ * @param {Object} config
+ * @param  {Document} config.document The document from which we can create elements
+ * @param  {Function} config.compileNode   A function to compile children nodes
+ * @param  {number} [config.outerGroupCol] the default group column
+ * @param {Object} params The execution parameters
+ * @param  {Element} params.node An arch's node
+ * @param  {CompilationContext} params.compilationContext
+ * @return {Element} The compiled group node
+ */
+export function compileGroup(
+    { document, compileNode, outerGroupCol },
+    { node, compilationContext }
+) {
     outerGroupCol = outerGroupCol || 2;
 
     const group = document.createElement("div");
@@ -22,14 +47,14 @@ export function compileGroup({ document, compileNode, outerGroupCol }, { node, p
         "col" in node.attributes ? parseInt(node.getAttribute("col"), 10) : outerGroupCol;
     const colSize = Math.max(1, Math.round(12 / nbCols));
 
-    params = Object.create(params);
-    params.groupLevel = (params.groupLevel || 1) + 1;
+    compilationContext = Object.create(compilationContext);
+    compilationContext.groupLevel = (compilationContext.groupLevel || 1) + 1;
     for (let child of node.children) {
         if (child.tagName === "newline") {
             appendTo(group, this.doc.createElement("br"));
             continue;
         }
-        const compiled = compileNode(child, params);
+        const compiled = compileNode(child, compilationContext);
         if (!compiled) {
             continue;
         }
@@ -42,6 +67,14 @@ export function compileGroup({ document, compileNode, outerGroupCol }, { node, p
     return group;
 }
 
+/**
+ * Compiles a template node for a `<widget>`arch's node
+ * @param {Object} config
+ * @param  {Document} config.document The document from which we can create elements
+ * @param {Object} params The execution parameters
+ * @param  {Element} params.node An arch's node
+ * @return {Element} The compiled ViewWidget node
+ */
 export function compileWidget({ document }, { node }) {
     const viewWidget = document.createElement("ViewWidget");
     viewWidget.setAttribute("model", "model");
@@ -53,71 +86,3 @@ export function compileWidget({ document }, { node }) {
 
     return viewWidget;
 }
-
-// LPE KEEP SOMEWHERE
-/*    compileInnerGroup(node, params) {
-        const table = this.doc.createElement("table");
-        table.setAttribute("class", "o_group o_inner_group o_group_col_6");
-        const tbody = this.doc.createElement("tbody");
-        table.appendChild(tbody);
-
-        const colAttr = node.hasAttribute("col")
-            ? parseInt(node.getAttribute("col"), 10)
-            : this.constructor.INNER_GROUP_COL;
-
-        if (node.hasAttribute("string")) {
-            const td = this.doc.createElement("td");
-            td.setAttribute("colspan", colAttr);
-            td.setAttribute("style", "width: 100%");
-            appendTo(td, makeGroupTitleRow(node));
-            appendTo(tbody, td);
-        }
-
-        const rows = [];
-        let currentColspan = 0;
-        let currentRow = this.doc.createElement("tr");
-        for (let child of node.children) {
-            if (child.tagName === "newline") {
-                rows.push(currentRow);
-                currentRow = this.doc.createElement("tr");
-                currentColspan = 0;
-                continue;
-            }
-
-            let colspan = "colspan" in child.attributes
-                ? parseInt(child.getAttribute("colspan"), 10)
-                : 0;
-
-            const isLabeledField =
-                child.tagName === "field" &&
-                !evalIrUiViewModifier(child.getAttribute("nolabel"));
-
-            if (!colspan) {
-                if (isLabeledField) {
-                    colspan = 2;
-                } else {
-                    colspan = 1;
-                }
-            }
-            const finalColspan = colspan - (isLabeledField ? 1 : 0);
-            currentColspan += colspan;
-
-            if (currentColspan > colAttr) {
-                rows.push(currentRow);
-                currentRow = this.doc.createElement("tr");
-                currentColspan = colspan;
-            }
-
-
-            const tds = [];
-            tds.push(...this.compileInnerGroupChild(child, params));
-            appendTo(currentRow, tds);
-        }
-
-        if (currentRow.childNodes.length) {
-            rows.push(currentRow);
-        }
-
-        appendTo(tbody, rows);
-        return table;
-    }*/
