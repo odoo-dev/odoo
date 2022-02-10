dc1e8fe7d71f2d831dd51ef826dde97e02209af9
Jorge Pinna Puissant <jpp@odoo.com>
2022-02-10 12:18:00 +0100

    unskip

--- addons/web/static/tests/webclient/actions/window_action_tests.js
+++ addons/web/static/tests/webclient/actions/window_action_tests.js
@@ -516,8 +516,7 @@ QUnit.module("ActionManager", (hooks) => {
         assert.containsN(target, ".o_data_row", 5);
     });
 
-    // OWL-NEW-RENDERING
-    QUnit.skip("there is no flickering when switching between views", async function (assert) {
+    QUnit.test("there is no flickering when switching between views", async function (assert) {
         assert.expect(20);
         let def;
         const mockRPC = async (route, args) => {
