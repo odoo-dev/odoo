4009ccfe3fae9716dd5ec0369e990afce709bdc5
Jorge Pinna Puissant <jpp@odoo.com>
2022-03-17 10:38:18 +0100

    subType

--- addons/web/static/tests/views/view_tests.js
+++ addons/web/static/tests/views/view_tests.js
@@ -1226,7 +1226,7 @@ QUnit.module("Views", (hooks) => {
         );
     });
 
-    QUnit.skipWOWL("rendering with loaded arch attribute 'js_class'", async function (assert) {
+    QUnit.test("rendering with loaded arch attribute 'js_class'", async function (assert) {
         assert.expect(4);
         const mockRPC = (_, args) => {
             assert.deepEqual(args.kwargs.views, [[2, "toy"]]);
@@ -1250,7 +1250,7 @@ QUnit.module("Views", (hooks) => {
         );
     });
 
-    QUnit.skipWOWL("rendering with given arch attribute 'js_class'", async function (assert) {
+    QUnit.test("rendering with given arch attribute 'js_class'", async function (assert) {
         const mockRPC = () => {
             throw new Error("no RPC expected");
         };
@@ -1269,7 +1269,7 @@ QUnit.module("Views", (hooks) => {
         );
     });
 
-    QUnit.skipWOWL(
+    QUnit.test(
         "rendering with loaded arch attribute 'js_class' and given jsClass",
         async function (assert) {
             assert.expect(3);
@@ -1298,7 +1298,7 @@ QUnit.module("Views", (hooks) => {
         }
     );
 
-    QUnit.skipWOWL(
+    QUnit.test(
         "rendering with given arch attribute 'js_class' and given jsClass",
         async function (assert) {
             class ToyView2 extends Component {}
