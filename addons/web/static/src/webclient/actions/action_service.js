86f2e68cfbd7c9a73c349576ec2baa219aa27379
Aaron Bohy <aab@odoo.com>
2022-03-23 14:57:07 +0100

    [FIX] web: action service: concurrency issue
    There is a concurrency issue in the action service:
     1) be in an action with several views
     2) execute another action (e.g. by clicking on a menu)
      -> [calls the route "/web/action/load", and it takes a while]
     3) meanwhile, click on another view in the view switcher
    
    If the call to "/web/action/load" is long enough, it may happen
    that the view requested in step 3 is briefly shown before the
    action associated with the menu clicked in step 2.
    
    In this scenario, the action of step 2 should never be displayed,
    because the user requested something else afterwards (in this
    case a switch view).
    
    One can produce a similar issue when restoring a previous action
    of the breadcrumbs instead of switching view.
    
    This commit fixes those two issues by registering a resolved
    promise in the keepLast (concurrency utils), s.t. it "cancels"
    the potential current operation (in this case, the call to
    "/web/load/action").

--- addons/web/static/src/webclient/actions/action_service.js
+++ addons/web/static/src/webclient/actions/action_service.js
@@ -1279,6 +1279,7 @@ function makeActionManager(env) {
      * @returns {Promise<Number>}
      */
     async function switchView(viewType, props = {}) {
+        await keepLast.add(Promise.resolve());
         const controller = controllerStack[controllerStack.length - 1];
         const view = _getView(viewType);
         if (!view) {
@@ -1338,6 +1339,7 @@ function makeActionManager(env) {
      * @param {string} jsId
      */
     async function restore(jsId) {
+        await keepLast.add(Promise.resolve());
         let index;
         if (!jsId) {
             index = controllerStack.length - 2;
