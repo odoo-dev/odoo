177ea2a5940f5e8919c694087a6683042ae21519
Aaron Bohy <aab@odoo.com>
2022-05-03 16:28:58 +0200

    unskip some form tests

--- addons/web/static/src/views/form/form_view.js
+++ addons/web/static/src/views/form/form_view.js
@@ -317,12 +317,12 @@ export class FormView extends Component {
         this.enableButtons(disabledButtons);
     }
 
-    discard() {
+    async discard() {
         if (this.props.discardRecord) {
             this.props.discardRecord(this.model.root);
             return;
         }
-        this.model.root.discard();
+        await this.model.root.discard();
         if (this.model.root.isVirtual) {
             this.env.config.historyBack();
         }
