dc1c18fb5bb517a7d4940c6b58eb9146d22d25e6
Thibault Delavallée <tde@odoo.com>
2022-05-12 20:39:30 +0200

    lint: remove tools (LEVEL now used in a single file) + various lint

--- addons/knowledge/models/__init__.py
+++ addons/knowledge/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from . import tools
 from . import knowledge_article_favorite
 from . import knowledge_article_member
 from . import knowledge_article
