08a56bf2281a2cc9e82e20756a7ad88fd31ef5cf
Géry Debongnie <ged@odoo.com>
2022-11-06 10:54:00 +0100

    improve chatter handling of new records

--- addons/mail/static/src/activity/activity.js
+++ addons/mail/static/src/activity/activity.js
@@ -19,7 +19,7 @@ export class Activity extends Component {
             showDetails: false,
         });
         this.delay = computeDelay(this.props.data.date_deadline);
-        onWillUpdateProps(nextProps => {
+        onWillUpdateProps((nextProps) => {
             this.delay = computeDelay(nextProps.data.date_deadline);
         });
     }
