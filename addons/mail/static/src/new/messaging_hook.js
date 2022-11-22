3ef629e0546658b16c3a20d0c4857f402324c887
Didier (did) <did@odoo.com>
2022-11-22 10:05:19 +0100

    [IMP] mail: add typedef for useMessaging hook.
    This allow tooling to work better

--- addons/mail/static/src/new/messaging_hook.js
+++ addons/mail/static/src/new/messaging_hook.js
@@ -3,6 +3,13 @@
 import { useState } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 
+/**
+ * @typedef {import("@mail/new/messaging").Messaging} Messaging
+ */
+
+/**
+ *  @returns {Messaging} messaging
+ */
 export function useMessaging() {
     return useState(useService("mail.messaging"));
 }
