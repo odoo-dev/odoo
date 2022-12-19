d278102480e71e8b50ebb0ff0943f7302f7957d3
Géry Debongnie <ged@odoo.com>
2022-12-19 10:41:02 +0100

    [REF] refactor dropzone again
    this should also make it more robust to target being mounted/unmounted
    while a drag and drop operation is going on

--- addons/mail/static/src/new/dropzone/dropzone.js
+++ addons/mail/static/src/new/dropzone/dropzone.js
@@ -1,6 +1,6 @@
 /* @odoo-module */
 
-import { Component, useExternalListener, useRef, useState } from "@odoo/owl";
+import { Component, useRef, useState, useEffect } from "@odoo/owl";
 
 export class Dropzone extends Component {
     static props = { ref: Object, onDrop: { type: Function, optional: true } };
@@ -8,56 +8,12 @@ export class Dropzone extends Component {
 
     setup() {
         this.root = useRef("root");
-        this.dragCount = 0;
         this.state = useState({
-            isDraggingFile: false,
             isDraggingInside: false,
-            top: 0,
-            left: 0,
-            width: 0,
-            height: 0,
         });
-        useExternalListener(document, "dragenter", this.startDrag);
-        useExternalListener(document, "dragleave", this.stopDrag);
-
-        // Prevents the browser to open or download the file when it is dropped
-        // outside of the dropzone.
-        useExternalListener(window, "dragover", (ev) => ev.preventDefault());
-        useExternalListener(window, "drop", (ev) => {
-            ev.preventDefault();
-            this.dragCount = 0;
+        useEffect(() => {
+            const { top, left, width, height } = this.props.ref.el.getBoundingClientRect();
+            this.root.el.style = `top:${top}px;left:${left}px;width:${width}px;height:${height}px;`;
         });
-    }
-
-    startDrag(ev) {
-        if (this.dragCount === 0) {
-            const el = this.props.ref.el;
-            if (el && ev.dataTransfer && ev.dataTransfer.types.includes("Files")) {
-                const { top, left, width, height } = this.props.ref.el.getBoundingClientRect();
-                Object.assign(this.state, { top, left, width, height });
-                this.state.isDraggingFile = true;
-            }
-        }
-        this.dragCount++;
-    }
-
-    stopDrag(ev) {
-        this.dragCount--;
-        if (this.dragCount === 0) {
-            this.state.isDraggingFile = false;
-        }
-    }
-
-    onDragEnter() {
-        this.state.isDraggingInside = true;
-    }
-
-    onDragLeave() {
-        this.state.isDraggingInside = false;
-    }
-
-    onDrop(ev) {
-        this.stopDrag();
-        this.props.onDrop(ev);
     }
 }
--- addons/mail/static/src/new/dropzone/dropzone_hook.js
+++ addons/mail/static/src/new/dropzone/dropzone_hook.js
@@ -2,16 +2,60 @@
 
 import { registry } from "@web/core/registry";
 import { Dropzone } from "./dropzone";
-import { onWillDestroy } from "@odoo/owl";
+import { useExternalListener, useEffect } from "@odoo/owl";
 
 const componentRegistry = registry.category("main_components");
 
 let id = 1;
 export function useDropzone(targetRef, onDrop) {
     const dropzoneId = `mail.dropzone_${id++}`;
-    componentRegistry.add(dropzoneId, {
-        Component: Dropzone,
-        props: { onDrop, ref: targetRef },
+    let dragCount = 0;
+    let hasTarget = false;
+
+    useExternalListener(document, "dragenter", onDragEnter);
+    useExternalListener(document, "dragleave", onDragLeave);
+    // Prevents the browser to open or download the file when it is dropped
+    // outside of the dropzone.
+    useExternalListener(window, "dragover", (ev) => ev.preventDefault());
+    useExternalListener(window, "drop", (ev) => {
+        ev.preventDefault();
+        dragCount = 0;
+        updateDropzone();
     });
-    onWillDestroy(() => componentRegistry.remove(dropzoneId));
+
+    function updateDropzone() {
+        const shouldDisplayDropzone = dragCount && hasTarget;
+        const hasDropzone = componentRegistry.contains(dropzoneId);
+        if (shouldDisplayDropzone && !hasDropzone) {
+            componentRegistry.add(dropzoneId, {
+                Component: Dropzone,
+                props: { onDrop, ref: targetRef },
+            });
+        }
+        if (!shouldDisplayDropzone && hasDropzone) {
+            componentRegistry.remove(dropzoneId);
+        }
+    }
+
+    function onDragEnter(ev) {
+        if (dragCount || (ev.dataTransfer && ev.dataTransfer.types.includes("Files"))) {
+            dragCount++;
+            updateDropzone();
+        }
+    }
+
+    function onDragLeave() {
+        if (dragCount) {
+            dragCount--;
+            updateDropzone();
+        }
+    }
+
+    useEffect(
+        (el) => {
+            hasTarget = !!el;
+            updateDropzone();
+        },
+        () => [targetRef.el]
+    );
 }
