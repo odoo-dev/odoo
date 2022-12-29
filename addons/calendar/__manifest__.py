6be2e4b7ce76a404b1994b00a823b3066ef02a91
Sébastien Theys <seb@odoo.com>
2022-12-29 13:44:42 +0100

    [REF] mail, *: introduce new code
    Co-authored-by: Alexandre Kühn <aku@odoo.com>
    Co-authored-by: Didier (did) <did@odoo.com>
    Co-authored-by: Géry Debongnie <ged@odoo.com>
    Co-authored-by: Louis Wicket (wil) <wil@odoo.com>
    Co-authored-by: Nemael <mapa@odoo.com>
    Co-authored-by: odoo <maki@odoo.com>
    Co-authored-by: Sébastien Theys <seb@odoo.com>
    Co-authored-by: Thanh Dodeur <tso@odoo.com>
    Co-authored-by: tsm-odoo <tsm@odoo.com>
    Co-authored-by: Zelong Lin <zel@odoo.com>

--- addons/calendar/__manifest__.py
+++ addons/calendar/__manifest__.py
@@ -39,8 +39,11 @@ If you need to manage your meetings, you should install the CRM module.
     'installable': True,
     'application': True,
     'assets': {
+        'mail.assets_backend': [
+            'calendar/static/src/new/**/*',
+        ],
         'mail.assets_messaging': [
-            'calendar/static/src/models/*.js',
+            'calendar/static/src/legacy/models/**/*',
         ],
         'web.assets_backend': [
             'calendar/static/src/scss/calendar.scss',
@@ -48,6 +51,7 @@ If you need to manage your meetings, you should install the CRM module.
             'calendar/static/src/js/services/calendar_notification_service.js',
             'calendar/static/src/views/**/*',
             'calendar/static/src/components/**/*',
+            'calendar/static/src/legacy/**/*',
         ],
         'web.qunit_suite_tests': [
             'calendar/static/tests/**/*',
