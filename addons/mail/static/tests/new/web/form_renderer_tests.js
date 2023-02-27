e272beb412aa541b344922378e01ad888f0879fe
Didier (did) <did@odoo.com>
2023-02-27 11:37:28 +0100

    [REF] mail: backport read more/read less on message content

--- addons/mail/static/tests/new/web/form_renderer_tests.js
+++ addons/mail/static/tests/new/web/form_renderer_tests.js
@@ -146,3 +146,155 @@ QUnit.test(
         assert.containsOnce(target, ".o-mail-chatter button:contains(1)");
     }
 );
+
+QUnit.test(
+    "read more/less links are not duplicated when switching from read to edit mode",
+    async function (assert) {
+        const pyEnv = await startServer();
+        const partnerId = pyEnv["res.partner"].create({});
+        pyEnv["mail.message"].create({
+            author_id: partnerId,
+            // "data-o-mail-quote" added by server is intended to be compacted in read more/less blocks
+            body: `
+                <div>
+                    Dear Joel Willis,<br>
+                    Thank you for your enquiry.<br>
+                    If you have any questions, please let us know.
+                    <br><br>
+                    Thank you,<br>
+                    <span data-o-mail-quote="1">-- <br data-o-mail-quote="1">
+                        System
+                    </span>
+                </div>`,
+            model: "res.partner",
+            res_id: partnerId,
+        });
+        const views = {
+            "res.partner,false,form": `
+                <form string="Partners">
+                    <sheet>
+                        <field name="name"/>
+                    </sheet>
+                    <div class="oe_chatter">
+                        <field name="message_ids"/>
+                    </div>
+                </form>`,
+        };
+        const { openView } = await start({ serverData: { views } });
+        const openViewAction = {
+            res_model: "res.partner",
+            res_id: partnerId,
+            views: [[false, "form"]],
+        };
+        await openView(openViewAction);
+        assert.containsOnce(target, ".o-mail-chatter");
+        assert.containsOnce(target, ".o-mail-message");
+        assert.containsOnce(target, ".o-mail-read-more-less");
+    }
+);
+
+QUnit.test("read more links becomes read less after being clicked", async function (assert) {
+    const pyEnv = await startServer();
+    const partnerId = pyEnv["res.partner"].create({});
+    pyEnv["mail.message"].create([
+        {
+            author_id: partnerId,
+            // "data-o-mail-quote" added by server is intended to be compacted in read more/less blocks
+            body: `
+                    <div>
+                        Dear Joel Willis,<br>
+                        Thank you for your enquiry.<br>
+                        If you have any questions, please let us know.
+                        <br><br>
+                        Thank you,<br>
+                        <span data-o-mail-quote="1">-- <br data-o-mail-quote="1">
+                            System
+                        </span>
+                    </div>`,
+            model: "res.partner",
+            res_id: partnerId,
+        },
+    ]);
+    const views = {
+        "res.partner,false,form": `
+                <form string="Partners">
+                    <sheet>
+                        <field name="name"/>
+                    </sheet>
+                    <div class="oe_chatter">
+                        <field name="message_ids"/>
+                    </div>
+                </form>`,
+    };
+    const { openView } = await start({ serverData: { views } });
+    const openViewAction = {
+        res_model: "res.partner",
+        res_id: partnerId,
+        views: [[false, "form"]],
+    };
+    await openView(openViewAction);
+    assert.containsOnce(target, ".o-mail-chatter");
+    assert.containsOnce(target, ".o-mail-message");
+    assert.containsOnce(target, ".o-mail-read-more-less:contains(Read More)");
+
+    await click(".o-mail-read-more-less");
+    assert.containsOnce(target, ".o-mail-read-more-less:contains(Read Less)");
+});
+
+QUnit.test(
+    "[TECHNICAL] unfolded read more/less links should not fold on message click besides those button links",
+    async function (assert) {
+        // message click triggers a re-render. Before writing of this test, the
+        // insertion of read more/less links were done during render. This meant
+        // any re-render would re-insert the read more/less links. If some button
+        // links were unfolded, any re-render would fold them again.
+        //
+        // This previous behavior is undesirable, and results to bothersome UX
+        // such as inability to copy/paste unfolded message content due to click
+        // from text selection automatically folding all read more/less links.
+        const pyEnv = await startServer();
+        const partnerId = pyEnv["res.partner"].create({ display_name: "Someone" });
+        pyEnv["mail.message"].create({
+            author_id: partnerId,
+            // "data-o-mail-quote" added by server is intended to be compacted in read more/less blocks
+            body: `
+                <div>
+                    Dear Joel Willis,<br>
+                    Thank you for your enquiry.<br>
+                    If you have any questions, please let us know.
+                    <br><br>
+                    Thank you,<br>
+                    <span data-o-mail-quote="1">-- <br data-o-mail-quote="1">
+                        System
+                    </span>
+                </div>
+            `,
+            model: "res.partner",
+            res_id: partnerId,
+        });
+        const views = {
+            "res.partner,false,form": `
+                <form string="Partners">
+                    <sheet>
+                        <field name="name"/>
+                    </sheet>
+                    <div class="oe_chatter">
+                        <field name="message_ids"/>
+                    </div>
+                </form>`,
+        };
+        const { openView } = await start({ serverData: { views } });
+        await openView({
+            res_model: "res.partner",
+            res_id: partnerId,
+            views: [[false, "form"]],
+        });
+        assert.containsOnce(target, ".o-mail-read-more-less:contains(Read More)");
+
+        await click(".o-mail-read-more-less");
+        assert.containsOnce(target, ".o-mail-read-more-less:contains(Read Less)");
+
+        await click(".o-mail-message");
+        assert.containsOnce(target, ".o-mail-read-more-less:contains(Read Less)");
+    }
+);
