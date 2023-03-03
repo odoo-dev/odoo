7ba38972e7f91978e4edc41b3b4a8ffd029c8276
vlst <vlst@odoo.com>
2023-03-03 16:01:56 +0100

    refactoring controllers

--- addons/pos_self_order/controllers/controllers.py
+++ addons/pos_self_order/controllers/controllers.py
@@ -19,28 +19,14 @@ class PosSelfOrder(http.Controller):
         And it will pass some generic variabiles to the template: pos_id, table_id, pos_name, currency
 
         We get some details about this POS from the model "pos.config"
-
         """
-        if not pos_id:
-            raise werkzeug.exceptions.NotFound()
-        pos_sudo = request.env['pos.config'].sudo().search(
-            [('id', '=', pos_id)])
-
-        if not pos_sudo or not pos_sudo.self_order_allow_view_menu():
-            raise werkzeug.exceptions.NotFound()
-
-        # On the landing page of the app we can have a number of custom links
-        # they are defined by the restaurant employee in the backend
-        custom_links_sudo = request.env['pos_self_order.custom_link'].sudo().search([
-        ], order='sequence')
-        custom_links_list = custom_links_sudo.filtered(lambda link: int(pos_id) in [
-                                                       pos.id for pos in link.pos_config_id] or not link.pos_config_id).read(['name', 'url', 'style'])
+        pos_sudo = find_pos_config(pos_id)
         context = {
             'pos_id': pos_id,
             'pos_name': pos_sudo.name,
             'currency_id': pos_sudo.currency_id.id,
             'show_prices_with_tax_included': True,
-            'custom_links': custom_links_list,
+            'custom_links': get_custom_links_list(pos_id),
             'attributes_by_ptal_id': request.env['pos.session'].sudo()._get_attributes_by_ptal_id(),
         }
         # TODO: make sure it is ok to send session_info to frontend
@@ -62,46 +48,107 @@ class PosSelfOrder(http.Controller):
         :return: the menu
         :rtype: list of dict
         """
-        if not pos_id:
-            raise werkzeug.exceptions.NotFound()
-        pos_sudo = request.env['pos.config'].sudo().search(
-            [('id', '=', pos_id)])
-        if not pos_sudo or not pos_sudo.self_order_allow_view_menu():
-            raise werkzeug.exceptions.NotFound()
+        pos_sudo = find_pos_config(pos_id) 
         # we only get the products that are available in THIS POS
-        products_sudo = request.env['product.product'].sudo().search(
-                            [('available_in_pos', '=', True)], 
-                            order='pos_categ_id').filtered(
-                                    lambda product: 
-                                            product.pos_categ_id.id in [category['id'] for category in 
-                                                            pos_sudo.iface_available_categ_ids.read(['id'])]
-                                            or not pos_sudo.iface_available_categ_ids
-                            )
+        products_sudo = get_products_that_are_available_in_this_pos(pos_sudo)
         # for each of the items in products_sudo, we get the price info and the attribute line ids
-        menu= [{
-            **{
-                'price_info': product.get_product_info_pos(product.list_price, 1, int(pos_id))['all_prices'],
-                'attribute_line_ids': product.read(['attribute_line_ids'])[0].get('attribute_line_ids'),
-            },
-            **product.read(['id', 'name', 'description_sale', 'pos_categ_id'])[0],
-        } for product in products_sudo]
-        return menu
+        return add_price_and_attribute_info_to_products(products_sudo, pos_id)
 
     # TODO: right now this route will return the image to whoever calls it; is there any reason to not make it public?
-    @ http.route('/pos-self-order/get-images/<int:product_id>', methods=['GET'], type='http', auth='public')
+    @http.route('/pos-self-order/get-images/<int:product_id>', methods=['GET'], type='http', auth='public')
     def pos_self_order_get_images(self, product_id):
         """
         This is the route that the POS Self Order App uses to GET THE PRODUCT IMAGES
-
+        If the product does not have an image, the function _get_image_stream_from will return the default image
         :param product_id: the id of the product
         :type product_id: int
-
+        :return: the image of the product
+        :rtype: binary
         """
-        # We get the product with the specific id from the database
         product_sudo= request.env['product.product'].sudo().browse(product_id)
-        # We return the image of the product in binary format
-        # 'image_1920' is the name of the field that contains the image
-        # If the product does not have an image, the function _get_image_stream_from will return the default image
         return request.env['ir.binary']._get_image_stream_from(product_sudo, field_name='image_1920').get_response()
+    # def find_pos_config(self, pos_id):
+    #     """ 
+    #     This function checks that the pos_id exists, and that the pos is configured to allow the menu to be viewed online
+        
+    #     :param pos_id: the id of the POS
+    #     :type pos_id: int
+    #     :return: the pos config object, if the pos_id is valid
+    #     """
+    #     print("pos_id", pos_id)
+    #     if not pos_id:
+    #         raise werkzeug.exceptions.NotFound()
+    #     pos_sudo = request.env['pos.config'].sudo().search(
+    #         [('id', '=', pos_id)])
+    #     if not pos_sudo or not pos_sudo.self_order_allow_view_menu():
+    #         raise werkzeug.exceptions.NotFound()
+    #     return pos_sudo
+def find_pos_config(pos_id):
+    """ 
+    This function checks that the pos_id exists, and that the pos is configured to allow the menu to be viewed online
+    
+    :param pos_id: the id of the POS
+    :type pos_id: int
+    :return: the pos config object, if the pos_id is valid
+    """
+    if not pos_id:
+        raise werkzeug.exceptions.NotFound()
+    pos_sudo = request.env['pos.config'].sudo().search(
+        [('id', '=', pos_id)])
+    if not pos_sudo or not pos_sudo.self_order_allow_view_menu():
+        raise werkzeug.exceptions.NotFound()
+    return pos_sudo
 
 
+def get_custom_links_list(pos_id):
+    """
+    On the landing page of the app we can have a number of custom links
+    that are defined by the restaurant employee in the backend.
+    This function returns a list of dictionaries with the name, url and style of each link
+    that is available for the POS with id pos_id
+    :param pos_id: the id of the POS
+    :type pos_id: int
+    :return: a list of dictionaries with the name, url and style of each link
+    :rtype: list of dict
+    """
+    custom_links_sudo = request.env['pos_self_order.custom_link'].sudo().search([
+    ], order='sequence')
+    return custom_links_sudo.filtered(lambda link: 
+                            int(pos_id) in [pos.id for pos in link.pos_config_id] 
+                            or not link.pos_config_id).read(['name', 'url', 'style']
+                            )
+def get_products_that_are_available_in_this_pos(pos_sudo):
+    """
+    This function returns the products that are available in the POS with id pos_id
+    :param pos_sudo: the POS config object
+    :type pos_sudo: pos.config object
+    :return: the products that are available in the POS with id pos_id
+    :rtype: list of product.product objects
+    """
+    return request.env['product.product'].sudo().search(
+                        [('available_in_pos', '=', True)], 
+                        order='pos_categ_id').filtered(
+                                lambda product: 
+                                        product.pos_categ_id.id in [category['id'] for category in 
+                                                        pos_sudo.iface_available_categ_ids.read(['id'])]
+                                        or not pos_sudo.iface_available_categ_ids
+                        )
+
+
+def add_price_and_attribute_info_to_products(products_sudo, pos_id):
+    """
+    This function adds the price info and the attribute line ids to each product in the list products_sudo
+    :param products_sudo: the list of products
+    :type products_sudo: list of product.product objects
+    :param pos_id: the id of the POS
+    :type pos_id: int
+    :return: the list of products with the price info and the attribute line ids
+    :rtype: list of dict
+    """
+    return [{
+        **{
+            'price_info': product.get_product_info_pos(product.list_price, 1, int(pos_id))['all_prices'],
+            'attribute_line_ids': product.read(['attribute_line_ids'])[0].get('attribute_line_ids'),
+        },
+        **product.read(['id', 'name', 'description_sale', 'pos_categ_id'])[0],
+    } for product in products_sudo]
