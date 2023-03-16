a4a09831e93443808113cec3d71af08f36960e7e
vlst <vlst@odoo.com>
2023-03-16 14:03:24 +0100

    qr code page should now work correctly

--- addons/pos_self_order/models/res_config_settings.py
+++ addons/pos_self_order/models/res_config_settings.py
@@ -12,14 +12,23 @@ class ResConfigSettings(models.TransientModel):
         Generate the data needed to print the QR codes page
         """
         business_url = self.env['ir.config_parameter'].get_param('web.base.url')
-        no_of_qr_codes_per_page = 9
+        no_of_qr_codes_per_page = 16
         qr_codes_to_print = [{
             'id': 0,
             'url': url_quote(f"{business_url}/menu?pos_id={self.pos_config_id.id}"),
         } for i in range(0, no_of_qr_codes_per_page)]
         data = {
             'pos_name': self.pos_config_id.name,
-            'tables': qr_codes_to_print,
+            'groups_of_tables': splitListIntoNLists(qr_codes_to_print, 4),
         }
-        print(data)
         return self.env.ref('pos_self_order.report_self_order_qr_codes_page').report_action([], data=data)
+def splitListIntoNLists(l, n):
+    """
+    Split a list into n lists
+    :param l: list to split
+    :type l: list
+    :param n: number of lists to split into
+    :type n: int
+    :return: list of lists
+    """
+    return [l[i:i + n] for i in range(0, len(l), n)]
