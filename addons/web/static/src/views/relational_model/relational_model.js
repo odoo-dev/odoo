fa492f6efd09dd52f9b632e2cc1be20fece97fa5
Pierre Rousseau <pro@odoo.com>
2023-04-14 08:55:48 +0200

    move to getNextConfig

--- addons/web/static/src/views/relational_model/relational_model.js
+++ addons/web/static/src/views/relational_model/relational_model.js
@@ -134,10 +134,6 @@ export class RelationalModel extends Model {
      */
     async load(params = {}) {
         const config = this._getNextConfig(this.config, params);
-        if (!config.isMonoRecord && this.root) {
-            // always reset the offset to 0 when reloading from above
-            config.offset = 0;
-        }
         const data = await this.keepLast.add(this._loadData(config));
         this.root = this._createRoot(config, data);
         this.config = config;
@@ -262,6 +258,10 @@ export class RelationalModel extends Model {
             if (!shallowEqual(config.groupBy || [], currentGroupBy || [])) {
                 delete config.groups;
             }
+        }
+        if (!config.isMonoRecord && this.root) {
+            // always reset the offset to 0 when reloading from above
+            config.offset = 0;
         }
 
         return config;
