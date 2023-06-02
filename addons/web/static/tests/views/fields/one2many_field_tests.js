8fa073fadf7b6424f4128fbb534498c7aa74bc28
FrancoisGe <fge@odoo.com>
2023-06-02 13:52:11 +0200

    new evalContext

--- addons/web/static/tests/views/fields/one2many_field_tests.js
+++ addons/web/static/tests/views/fields/one2many_field_tests.js
@@ -4984,7 +4984,7 @@ QUnit.module("Fields", (hooks) => {
         }
     );
 
-    QUnit.tttt("one2many list, editable, with a date in the context", async function (assert) {
+    QUnit.test("one2many list, editable, with a date in the context", async function (assert) {
         assert.expect(1);
 
         serverData.models.partner.records[0].p = [2];
@@ -5039,7 +5039,7 @@ QUnit.module("Fields", (hooks) => {
                 </form>`,
             resId: 1,
             mockRPC(route, args) {
-                if (args.method === "onchange") {
+                if (args.method === "onchange2") {
                     assert.deepEqual(
                         args.kwargs.context.turtles,
                         [2],
@@ -5094,7 +5094,7 @@ QUnit.module("Fields", (hooks) => {
         );
     });
 
-    QUnit.tttt(
+    QUnit.test(
         "one2many list, the context is properly evaluated and sent",
         async function (assert) {
             assert.expect(2);
@@ -5114,7 +5114,7 @@ QUnit.module("Fields", (hooks) => {
                     </form>`,
                 resId: 1,
                 mockRPC(route, args) {
-                    if (args.method === "onchange") {
+                    if (args.method === "onchange2") {
                         var context = args.kwargs.context;
                         assert.strictEqual(context.hello, "world");
                         assert.strictEqual(context.abc, 10);
@@ -5355,7 +5355,7 @@ QUnit.module("Fields", (hooks) => {
         await clickSave(target);
     });
 
-    QUnit.tttt("new record, the context is properly evaluated and sent", async function (assert) {
+    QUnit.test("new record, the context is properly evaluated and sent", async function (assert) {
         assert.expect(2);
 
         serverData.models.partner.fields.int_field.default = 17;
@@ -5375,7 +5375,7 @@ QUnit.module("Fields", (hooks) => {
                         </field>
                     </form>`,
             mockRPC(route, args) {
-                if (args.method === "onchange") {
+                if (args.method === "onchange2") {
                     n++;
                     if (n === 2) {
                         var context = args.kwargs.context;
