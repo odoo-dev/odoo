5ec0ad79c293cb0c0b6c1ce1dd613a253c8747b8
Jorge Pinna Puissant <jpp@odoo.com>
2023-06-15 10:48:19 +0200

    forgot this one

--- addons/web/static/tests/views/form/form_view_tests.js
+++ addons/web/static/tests/views/form/form_view_tests.js
@@ -8933,7 +8933,7 @@ QUnit.module("Views", (hooks) => {
         assert.strictEqual(target.querySelector(".modal-title").textContent, "Translate: foo");
     });
 
-    QUnit.tttt("save new record before opening translate dialog", async function (assert) {
+    QUnit.test("save new record before opening translate dialog", async function (assert) {
         serverData.models.partner.fields.foo.translate = true;
 
         patchWithCleanup(localization, {
@@ -8974,7 +8974,7 @@ QUnit.module("Views", (hooks) => {
         assert.verifySteps(["get_views", "onchange2"]);
         assert.containsOnce(target, ".o_form_editable");
         await click(target, ".o_field_translate.btn-link");
-        assert.verifySteps(["create", "read", "get_installed", "get_field_translations"]);
+        assert.verifySteps(["create", "web_read", "get_installed", "get_field_translations"]);
         assert.containsOnce(target, ".modal");
         assert.strictEqual(target.querySelector(".modal-title").textContent, "Translate: foo");
     });
