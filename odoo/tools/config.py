9d40167fb8e9bbeca1351ec80ac7106c68070405
Julien Castiaux <juc@odoo.com>
2023-06-26 22:33:23 +0200

    [REF] core: ConfigCleaner(7) separate sources in a chainmap
    This commit is part of a larger refactor, see associated PR.
    
    Store all option values defined at a same level in a dedicate read-only
    dictionnary, compose the various dictionnaries inside a chainmap for
    automatic failover.
    
    The order is as follow (most prioritary source first):
    
    1. Runtime, options set via computation at runtime
    2. CLI, options set via sys.argv
    3. Configuration file, options found in -c/--config/.odoorc
    4. Default, options hardcoded in the source code
    
    The 3 later sources: cli, config file and default, may still contain
    unparsed value, like comma-separated-list strings. It it the job of the
    runtime dictionnary to parse and re-expose those options inside its own
    dictionnary. This ease the cognitive complexity of `_parse_config`
    because it makes the order in which the various sources are loaded a bit
    less relevant. The CLI must still be loaded first for `--config` but the
    actual injection of values inside the `_file_config` and the
    `_cli_config` dictionnary is now irrevelant.

--- odoo/tools/config.py
+++ odoo/tools/config.py
@@ -1,5 +1,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+import collections
 import configparser as ConfigParser
 import errno
 import logging
@@ -83,7 +84,16 @@ def _deduplicate_loggers(loggers):
 
 class configmanager:
     def __init__(self):
-        self.options = {}
+        self._default_options = {}
+        self._file_options = {}
+        self._cli_options = {}
+        self._runtime_options = {}
+        self.options = collections.ChainMap(
+            self._runtime_options,
+            self._cli_options,
+            self._file_options,
+            self._default_options,
+        )
 
         # dictionary mapping option destination (keys in self.options) to MyOptions.
         self.options_index = {}
@@ -362,7 +372,8 @@ class configmanager:
             parser.add_option_group(group)
 
     def _load_default_options(self):
-        self.options.update({
+        self._default_options.clear()
+        self._default_options.update({
             option_name: option.my_default
             for option_name, option in self.options_index.items()
         })
@@ -469,43 +480,38 @@ class configmanager:
             opt.config or os.environ.get('ODOO_RC') or os.environ.get('OPENERP_SERVER') or rcfilepath)
         self.load()
 
-        # Verify that we want to log or not, if not the output will go to stdout
-        if self.options['logfile'] in ('None', 'False'):
-            self.options['logfile'] = False
-        # the same for the pidfile
-        if self.options['pidfile'] in ('None', 'False'):
-            self.options['pidfile'] = False
-        # the same for the test_tags
-        if self.options['test_tags'] == 'None':
-            self.options['test_tags'] = None
-        # and the server_wide_modules
-        if self.options['server_wide_modules'] in ('', 'None', 'False'):
-            self.options['server_wide_modules'] = 'base,web'
+        addons_path = self._cli_options.pop('addons_path', None)
+        self._cli_options.clear()
+        if addons_path is not None:
+            self._cli_options['addons_path'] = addons_path
 
         keys = [
             option_name for option_name, option
             in self.options_index.items()
-            if option.cli_loadable and option.file_loadable
+            if option.cli_loadable
             if option.action != 'append'
         ]
 
         for arg in keys:
-            # Copy the command-line argument (except the special case for log_handler, due to
-            # action=append requiring a real default, so we cannot use the my_default workaround)
             if getattr(opt, arg, None) is not None:
-                self.options[arg] = getattr(opt, arg)
-            # ... or keep, but cast, the config file value.
-            elif isinstance(self.options[arg], str) and self.casts[arg].type in optparse.Option.TYPE_CHECKER:
-                self.options[arg] = optparse.Option.TYPE_CHECKER[self.casts[arg].type](self.casts[arg], arg, self.options[arg])
+                self._cli_options[arg] = getattr(opt, arg)
 
-        if isinstance(self.options['log_handler'], str):
-            self.options['log_handler'] = self.options['log_handler'].split(',')
-        self.options['log_handler'].extend(opt.log_handler)
+        if opt.log_handler:
+            self._cli_options['log_handler'] = opt.log_handler
 
         ismultidb = ',' in (self.options.get('db_name') or '')
         die(ismultidb and (opt.init or opt.update), "Cannot use -i/--init or -u/--update with multiple databases in the -d/--database/db_name")
 
-        if not self.options['addons_path'] or self.options['addons_path']=='None':
+        # Load the various coma-separated options and the special ones
+        self._runtime_options.clear()
+
+        if not self['server_wide_modules']:
+            self._runtime_options['server_wide_modules'] = 'base,web'
+
+        if isinstance(self['log_handler'], str):
+            self._runtime_options['log_handler'] = self['log_handler'].split(',')
+
+        if not self['addons_path']:
             default_addons = []
             base_addons = os.path.join(self.root_path, 'addons')
             if os.path.exists(base_addons):
@@ -513,49 +519,52 @@ class configmanager:
             main_addons = os.path.abspath(os.path.join(self.root_path, '../addons'))
             if os.path.exists(main_addons):
                 default_addons.append(main_addons)
-            self.options['addons_path'] = ','.join(default_addons)
+            self._runtime_options['addons_path'] = ','.join(default_addons)
         else:
-            self.options['addons_path'] = ",".join(
+            self._runtime_options['addons_path'] = ",".join(
                 self._normalize(x)
-                for x in self.options['addons_path'].split(','))
+                for x in self['addons_path'].split(','))
 
-        self.options["upgrade_path"] = (
+        self._runtime_options["upgrade_path"] = (
             ",".join(self._normalize(x)
-                for x in self.options['upgrade_path'].split(','))
-            if self.options['upgrade_path']
+                for x in self['upgrade_path'].split(','))
+            if self['upgrade_path']
             else ""
         )
 
-        self.options['init'] = opt.init and dict.fromkeys(opt.init.split(','), 1) or {}
-        self.options['demo'] = (dict(self.options['init'])
-                                if not self.options['without_demo'] else {})
-        self.options['update'] = opt.update and dict.fromkeys(opt.update.split(','), 1) or {}
-        self.options['translate_modules'] = [m.strip() for m in opt.translate_modules.split(',')]
-        self.options['translate_modules'].sort()
+        self._runtime_options['init'] = opt.init and dict.fromkeys(opt.init.split(','), 1) or {}
+        self._runtime_options['demo'] = (dict(self['init'])
+                                if not self['without_demo'] else {})
+        self._runtime_options['update'] = opt.update and dict.fromkeys(opt.update.split(','), 1) or {}
+        self._runtime_options['translate_modules'] = [m.strip() for m in opt.translate_modules.split(',')]
+        self._runtime_options['translate_modules'].sort()
 
         dev_split = [s.strip() for s in opt.dev_mode.split(',')] if opt.dev_mode else []
-        self.options['dev_mode'] = dev_split + (['reload', 'qweb', 'xml'] if 'all' in dev_split else [])
+        self._runtime_options['dev_mode'] = dev_split + (['reload', 'qweb', 'xml'] if 'all' in dev_split else [])
 
-        self.options['test_enable'] = bool(self.options['test_tags'])
+        self._runtime_options['test_enable'] = bool(self['test_tags'])
 
-        if self.options['longpolling_port']:
+        if self['longpolling_port']:
             self._warn(
                 "The longpolling-port is a deprecated alias to "
                 "the gevent-port option, please use the latter.",
                 DeprecationWarning)
-            self.options['gevent_port'] = self.options.pop('longpolling_port')
+            if not self['gevent_port']:
+                self._runtime_options['gevent_port'] = self['longpolling_port']
+            for map_ in self.options.maps:
+                map_.pop('lonqgpolling_port', None)
 
         if opt.save:
             self.save()
 
         # normalize path options
         for key in ['data_dir', 'logfile', 'pidfile', 'test_file', 'screencasts', 'screenshots', 'pg_path', 'translate_out', 'translate_in', 'geoip_city_db', 'geoip_country_db']:
-            self.options[key] = self._normalize(self.options[key])
+            self._runtime_options[key] = self._normalize(self[key])
 
-        conf.addons_paths = self.options['addons_path'].split(',')
+        conf.addons_paths = self['addons_path'].split(',')
 
         conf.server_wide_modules = [
-            m.strip() for m in self.options['server_wide_modules'].split(',') if m.strip()
+            m.strip() for m in self['server_wide_modules'].split(',') if m.strip()
         ]
         return opt
 
@@ -607,6 +616,7 @@ class configmanager:
             parser.values.test_tags = "+standard"
 
     def load(self):
+        self._file_options.clear()
         outdated_options_map = {
             'xmlrpc_port': 'http_port',
             'xmlrpc_interface': 'http_interface',
@@ -622,9 +632,13 @@ class configmanager:
                     continue
                 if value=='True' or value=='true':
                     value = True
-                if value=='False' or value=='false':
+                elif value=='False' or value=='false':
                     value = False
-                self.options[name] = value
+                elif value=='None':
+                    value = None
+                elif option and option.type in optparse.Option.TYPE_CHECKER and isinstance(value, str):
+                    value = optparse.Option.TYPE_CHECKER[option.type](option, name, value)
+                self._file_options[name] = value
             #parse the other sections, as well
             for sec in p.sections():
                 if sec == 'options':
