39377280c8b04002458cb4013aca4feac89f39c6
Aaron Bohy <aab@odoo.com>
2023-07-10 10:30:35 +0200

    remove unittest import

--- addons/test_sale_product_configurators/tests/test_event_sale_with_product_configurator.py
+++ addons/test_sale_product_configurators/tests/test_event_sale_with_product_configurator.py
@@ -1,7 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from datetime import datetime, timedelta
-import unittest
+
 from odoo import fields
 from odoo.tests import HttpCase, tagged
 
