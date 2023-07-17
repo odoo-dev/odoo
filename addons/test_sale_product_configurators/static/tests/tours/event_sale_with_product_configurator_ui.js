7be0464baed8ecb284bc18bd6a317b3ec120c1c0
Aaron Bohy <aab@odoo.com>
2023-07-17 15:35:28 +0200

    fix event sale tour

--- addons/test_sale_product_configurators/static/tests/tours/event_sale_with_product_configurator_ui.js
+++ addons/test_sale_product_configurators/static/tests/tours/event_sale_with_product_configurator_ui.js
@@ -46,10 +46,10 @@ registry.category("web_tour.tours").add('event_sale_with_product_configurator_to
     trigger: '.o_event_sale_js_event_configurator_ok'
 }, {
     trigger: 'a:contains("Add a product")',
-    extra_trigger: '.o_data_row:nth-child(2)',  // wait for the optional product line
+    extra_trigger: 'td[name="price_subtotal"]:contains("16.50")',  // wait for the optional product line
 }, {
     trigger: 'div[name="product_template_id"] input',
-    extra_trigger: '[name="product_template_id"] .o_dropdown_button',
+    extra_trigger: '.o_data_row:nth-child(3)',  // wait for the new row to be created
     run: 'text event (',
 }, {
     trigger: 'ul.ui-autocomplete a:contains("Registration Event (TEST variants)")',
@@ -80,10 +80,10 @@ registry.category("web_tour.tours").add('event_sale_with_product_configurator_to
     trigger: '.o_event_sale_js_event_configurator_ok'
 }, {
     trigger: 'a:contains("Add a product")',
-    extra_trigger: '.o_data_row:nth-child(3)',  // wait for the adult tickets line
+    extra_trigger: 'td[name="price_subtotal"]:contains("150.00")',  // wait for the adult tickets line
 }, {
     trigger: 'div[name="product_template_id"] input',
-    extra_trigger: '[name="product_template_id"] .o_dropdown_button',
+    extra_trigger: '.o_data_row:nth-child(4)',  // wait for the new row to be created
     run: 'text event (',
 }, {
     trigger: 'ul.ui-autocomplete a:contains("Registration Event (TEST variants)")',
@@ -109,5 +109,5 @@ registry.category("web_tour.tours").add('event_sale_with_product_configurator_to
     in_modal: false,
 }, {
     trigger: '.o_event_sale_js_event_configurator_ok',
-}, ...stepUtils.saveForm({ extra_trigger: '.o_data_row:nth-child(4)' }),
+}, ...stepUtils.saveForm({ extra_trigger: '.o_field_cell.o_data_cell.o_list_number:contains("60.00")' }),
 ]});
