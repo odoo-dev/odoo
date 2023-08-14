c0f664f3632c00566a5b4db45216fa6d4ad45055
Pierre-Yves Dufays <pydu@odoo.com>
2023-08-14 10:58:13 +0200

    [IMP] hr(_contract/_fleet): convert hr.plan to mail.activity.plan
    We convert the custom plan implementation to use the generic one. As the
    generic one can define plans for multiple model. We use the check
    dedicated_to_res_model == 'hr.employee' to activate the specific feature for
    hr.employee. Indeed, that field contains the model name when the plan is
    applicable only for one model.
    
    We also remove the 'launch plan' button as we can now launch a plan directly
    from the activities button in the chatter.
    
    Technical notes:
    For the activity schedule wizard, we add the support for active_ids and
    active_model as default values for res_ids and res_model because it is used as
    link in the chatter to launch the wizard and we want to avoid a big migration
    by keeping the link identical (and there are probably no simple solution to
    keep the same behavior).
    
    Task-3390865

--- addons/hr/models/mail_activity_plan.py
+++ addons/hr/models/mail_activity_plan.py
@@ -1,27 +1,34 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
-class HrPlan(models.Model):
-    _name = 'hr.plan'
-    _description = 'plan'
+class MailActivityPlan(models.Model):
+    _inherit = 'mail.activity.plan'
 
-    name = fields.Char('Name', required=True)
-    company_id = fields.Many2one(
-        'res.company', default=lambda self: self.env.company)
-    department_id = fields.Many2one('hr.department', check_company=True)
-    plan_activity_type_ids = fields.One2many(
-        'hr.plan.activity.type', 'plan_id',
-        string='Activities',
-        check_company=True)
-    active = fields.Boolean(default=True)
-    steps_count = fields.Integer(compute='_compute_steps_count')
+    department_id = fields.Many2one('hr.department', check_company=True, ondelete='cascade')
 
-    @api.depends('plan_activity_type_ids')
-    def _compute_steps_count(self):
-        activity_type_data = self.env['hr.plan.activity.type']._read_group([('plan_id', 'in', self.ids)], ['plan_id'], ['__count'])
-        steps_count = {plan.id: count for plan, count in activity_type_data}
-        for plan in self:
-            plan.steps_count = steps_count.get(plan.id, 0)
+    @api.constrains('res_model')
+    def _check_compatibility_with_model(self):
+        """ Check that when the model is updated to a model different from employee,
+        there are no remaining specific values to employee. """
+        template_ids_to_check = set()
+        error_department = False
+        for plan in self.filtered(lambda plan: plan.res_model != 'hr.employee'):
+            template_ids_to_check.update(plan.template_ids.ids)
+            if plan.department_id:
+                error_department = True
+        errors = [_('Department can only be set with employee plan.')] if error_department else []
+        if template_ids_to_check and self.env['mail.activity.plan.template'].search(
+                [('id', 'in', tuple(template_ids_to_check)),
+                 ('responsible_type', 'in', ('coach', 'manager', 'employee'))], limit=1):
+            errors.append('Coach, manager or employee can only be chosen as template responsible with employee plan.')
+        if errors:
+            raise UserError('\n'.join(errors))
+
+    @api.onchange('res_model')
+    def _onchange_res_model(self):
+        if self.res_model != 'hr.employee':
+            self.department_id = False
