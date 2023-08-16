39453e666f910e4fc4a50761f7d53dadd886349a
Rahul Prajapati <rapr@odoo.com>
2023-08-16 12:22:51 +0530

    [IMP] mail, {crm,im}_livechat: add /bot command for livechat channels
    PURPOSE:
    
    Allow operators to start chatbot conversations from the list of available
    chatbots with visitor.
    
    SPECIFICATIONS:
    
    - add a /bot command for operators in livechat channels.
    - ENTER: open the list of existing chatbots for selection
    - ENTER: select a chatbot and start chatbot conversation for visitor
    
    task-3373161

--- addons/mail/models/discuss/discuss_channel.py
+++ addons/mail/models/discuss/discuss_channel.py
@@ -368,8 +368,7 @@ class Channel(models.Model):
     def action_unfollow(self):
         self._action_unfollow(self.env.user.partner_id)
 
-    def _action_unfollow(self, partner=None, guest=None):
-        self.ensure_one()
+    def _action_unfollow(self, partner=None, guest=None, post_left_message=True):
         self.message_unsubscribe(partner.ids)
         custom_store = Store(self, {"is_pinned": False, "isLocallyPinned": False})
         member = self.env["discuss.channel.member"].search(
@@ -382,13 +381,14 @@ class Channel(models.Model):
             target = partner or guest
             target._bus_send_store(custom_store, notification_type="discuss.channel/leave")
             return
-        notification = Markup('<div class="o_mail_notification">%s</div>') % _(
-            "left the channel"
-        )
-        # sudo: mail.message - post as sudo since the user just unsubscribed from the channel
-        member.channel_id.sudo().message_post(
-            body=notification, subtype_xmlid="mail.mt_comment", author_id=partner.id
-        )
+        if post_left_message:
+            notification = Markup('<div class="o_mail_notification">%s</div>') % _(
+                "left the channel"
+            )
+            # sudo: mail.message - post as sudo since the user just unsubscribed from the channel
+            member.channel_id.sudo().message_post(
+                body=notification, subtype_xmlid="mail.mt_comment", author_id=partner.id
+            )
         # send custom store after message_post to avoid is_pinned reset to True
         member._bus_send_store(custom_store, notification_type="discuss.channel/leave")
         member.unlink()
