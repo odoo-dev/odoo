791d0aa93d038167b0190b5aa91a3b164ca9c0d2
Rahul Reddiar <rare@odoo.com>
2023-09-04 15:30:07 +0530

    [IMP] sale_mrp: refactor kit_price distribution

--- addons/sale_mrp/models/stock_move.py
+++ addons/sale_mrp/models/stock_move.py
@@ -22,11 +22,17 @@ class StockMoveLine(models.Model):
         kit_lines = self.filtered(lambda move_line: move_line.move_id.bom_line_id.bom_id.type == 'phantom')
         for move_line in kit_lines:
             bom_id = move_line.move_id.bom_line_id.bom_id
-            product_id = bom_id.product_id or bom_id.product_tmpl_id.product_variant_id
-            bom_product_price = product_id._compute_bom_price(bom_id)
             global_discount = move_line.move_id.sale_line_id.get_global_discount()
             sale_unit_price = move_line.move_id.sale_line_id.price_reduce_taxinc - global_discount
-            unit_price = sale_unit_price * (move_line.product_id.standard_price / bom_product_price)
-            qty = move_line.product_uom_id._compute_quantity(move_line.quantity, move_line.product_id.uom_id)
+            move_line_qty = move_line.quantity
+            total_list_price = sum(bom_id.bom_line_ids.mapped("product_id.list_price"))
+            bom_components_total_qty = sum(bom_id.bom_line_ids.mapped("product_qty"))
+            bom_line_product_qty = sum(bom_id.bom_line_ids.filtered(lambda line: line.product_id == move_line.product_id).mapped('product_qty'))
+            if total_list_price:
+                list_price_ratio = move_line.product_id.list_price / total_list_price
+                unit_price = ((sale_unit_price * list_price_ratio) / move_line_qty) * ((1 / bom_line_product_qty) * move_line_qty)
+            else:
+                unit_price = (sale_unit_price * (bom_line_product_qty / bom_components_total_qty)) / bom_line_product_qty
+            qty = move_line.product_uom_id._compute_quantity(move_line_qty, move_line.product_id.uom_id)
             move_line.sale_price = unit_price * qty
         super(StockMoveLine, self - kit_lines)._compute_sale_price()
