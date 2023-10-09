58715789495040980700af6cc75043c288bed2db
Thibault Delavallée <tde@odoo.com>
2023-10-09 16:38:14 +0200

    [IMP] mail.activity.plan: lint code
      * rewrite computed fields, simplified

--- addons/mail/models/mail_activity_plan.py
+++ addons/mail/models/mail_activity_plan.py
@@ -2,7 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-from odoo.tools.misc import groupby as tools_groupby
 
 
 class MailActivityPlan(models.Model):
@@ -33,7 +32,7 @@ class MailActivityPlan(models.Model):
               ' and not available when managing activities for other models.')
     steps_count = fields.Integer(compute='_compute_steps_count')
     assignation_summary = fields.Html('Assignation summary', compute='_compute_assignation')
-    has_user_on_demand = fields.Boolean('Has on demand responsible', compute='_compute_assignation')
+    has_user_on_demand = fields.Boolean('Has on demand responsible', compute='_compute_has_user_on_demand')
 
     @api.depends('res_model')
     def _compute_res_model_id(self):
@@ -46,35 +45,24 @@ class MailActivityPlan(models.Model):
 
     @api.depends('template_ids')
     def _compute_steps_count(self):
-        activity_template_data = self.env['mail.activity.plan.template']._read_group(
-            [('plan_id', 'in', self.ids)],
-            ['plan_id'],
-            ['__count'],
-        )
-        steps_count = {plan.id: count for plan, count in activity_template_data}
         for plan in self:
-            plan.steps_count = steps_count.get(plan.id, 0)
+            plan.steps_count = len(plan.template_ids)
 
-    @api.depends('template_ids.responsible_type', 'template_ids.summary')
+    @api.depends('template_ids.summary')
     def _compute_assignation(self):
-        templates_data_by_plan = dict(tools_groupby(self.env['mail.activity.plan.template'].search_read(
-            [('plan_id', 'in', self.ids)],
-            ['activity_type_id', 'plan_id', 'responsible_type', 'sequence', 'summary']
-        ), key=lambda r: r['plan_id'][0]))
-        for plan in self:
-            templates_data = templates_data_by_plan.get(plan.id, False)
-            if templates_data:
-                formatted = ['<ul>']
-                has_user_on_demand = False
-                for template_data in sorted(templates_data, key=lambda d: d["sequence"]):
-                    formatted.append(
-                        f"<li>{template_data['activity_type_id'][1]} - {template_data['responsible_type']}" +
-                        (f": {template_data['summary']}" if template_data['summary'] else '') +
-                        "</li>")
-                    has_user_on_demand |= template_data['responsible_type'] == 'on_demand'
-                formatted.append('</ul>')
-                plan.assignation_summary = ''.join(formatted)
-                plan.has_user_on_demand = has_user_on_demand
-            else:
-                plan.assignation_summary = ''
-                plan.has_user_on_demand = False
+        self.assignation_summary = ''
+        for plan in self.filtered('template_ids'):
+            formatted = ['<ul>']
+            for template in plan.template_ids:
+                formatted.append(
+                    f"<li>{template.activity_type_id.name} - {template.responsible_type}" +
+                    (f": {template.summary}" if template.summary else '') +
+                    "</li>")
+            formatted.append('</ul>')
+            plan.assignation_summary = ''.join(formatted)
+
+    @api.depends('template_ids.responsible_type')
+    def _compute_has_user_on_demand(self):
+        self.has_user_on_demand = False
+        for plan in self.filtered('template_ids'):
+            plan.has_user_on_demand = any(template.responsible_type == 'on_demand' for template in plan.template_ids)
