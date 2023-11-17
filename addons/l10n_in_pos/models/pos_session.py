89cea14fd0fa29aefb1fea2b17d7581443059cfb
David Monnom (moda) <moda@odoo.com>
2023-11-17 17:11:42 +0100

    adapt override part 1

--- addons/l10n_in_pos/models/pos_session.py
+++ addons/l10n_in_pos/models/pos_session.py
@@ -7,6 +7,14 @@ from odoo import models
 class PosSession(models.Model):
     _inherit = 'pos.session'
 
+    def load_data_params(self):
+        params = super().load_data_params()
+
+        if self.company_id.country_code == 'IN':
+            params['search_read']['product.product']['fields'] += ['l10n_in_hsn_code']
+
+        return params
+
     def _loader_params_product_product(self):
         result = super()._loader_params_product_product()
         result['search_params']['fields'].append('l10n_in_hsn_code')
