5ef5813e8289b5e2a1f00564eb0a4df8076e9d70
David Monnom (moda) <moda@odoo.com>
2023-11-21 10:02:21 +0100

    m

--- addons/point_of_sale/static/src/app/models/data_service.js
+++ addons/point_of_sale/static/src/app/models/data_service.js
@@ -1,10 +1,13 @@
 /** @odoo-module */
 
-import { registry } from "@web/core/registry";
 import { Reactive } from "@web/core/utils/reactive";
-import { clone } from "../utils/clone";
 import { reactive } from "@odoo/owl";
 import { createRelatedModels } from "@point_of_sale/app/models/related_models";
+import { registry } from "@web/core/registry";
+
+const INDEXED_DB_NAME = {
+    "product.product": ["barcode", "pos_categ_ids"],
+};
 
 export class PosData extends Reactive {
     // if empty, all python listed models will be loaded
@@ -15,6 +18,7 @@ export class PosData extends Reactive {
         super();
         this.ready = this.setup(...arguments).then(() => this);
         this.custom = {};
+        this.idMap = {};
     }
 
     async setup(env, { orm }) {
@@ -30,48 +34,32 @@ export class PosData extends Reactive {
     }
 
     async initData(env) {
+        const modelOverride = {};
         const response = await this.orm.call("pos.session", "load_data", [
             odoo.pos_session_id,
             PosData.modelToLoad,
         ]);
 
-        for (const [model, data] of Object.entries(response.data)) {
-            if (!data) {
-                continue;
-            }
-
-            if (registry.category("pos_available_models").contains(model)) {
-                const jsModel = registry.category("pos_available_models").get(model);
-                this[model.replaceAll(".", "_")] = data.map((p) => new jsModel(p));
-            } else {
-                this[model.replaceAll(".", "_")] = data;
-            }
+        for (const posModel of registry.category("pos_available_models").getAll()) {
+            modelOverride[posModel.pythonModel] = posModel;
         }
 
-        for (const [key, value] of Object.entries(response.custom)) {
-            this.custom[key] = value;
-        }
+        // need model override to be able to use the correct mod
+        const [models, records] = createRelatedModels(
+            response.relations,
+            env,
+            reactive,
+            modelOverride
+        );
 
         this.relations = response.relations;
-        this.createRelation();
-        this.createIdMap();
-
-        this.models = createRelatedModels(this.relations, env, reactive);
+        this.models = models;
         this.models.loadData(response.data);
 
-        // TODO JCB: Remove the following lines. FTM, they are used for testing.
-        const order1 = this.models["pos.order"].readAll()[0];
-        const line1 = [...order1.lines][0];
-        // think of `serialize` as `export_as_JSON`
-        const line1_json_str = JSON.stringify(line1.serialize());
-        line1.delete();
-        console.log([...order1.lines]);
-        // think of `deserialize` as `init_from_JSON`
-        const recreated_line1 = this.models["pos.order.line"].deserialize(
-            JSON.parse(line1_json_str)
-        );
-        console.log(recreated_line1);
-        console.log([...order1.lines]);
+        for (const [name, model] of Object.entries(records)) {
+            this[name.replaceAll(".", "_")] = Object.values(model);
+            this.idMap[name.replaceAll(".", "_")] = model;
+        }
     }
 
     async loadMissingData(model, ids) {
@@ -91,142 +79,7 @@ export class PosData extends Reactive {
         }
     }
 
-    createIdMap() {
-        this.idMap = {};
-
-        for (const model of Object.keys(this.relations)) {
-            const modelName = model.replaceAll(".", "_");
-            const modelData = this[modelName];
-
-            if (!modelData || !modelData.length === 0) {
-                continue;
-            }
-
-            this.idMap[modelName] = {};
-
-            for (const data of modelData) {
-                if (!data.id) {
-                    continue;
-                }
-
-                this.idMap[modelName][data.id] = data;
-            }
-        }
-    }
-
-    createRelation() {
-        const relations = [];
-
-        for (const models of Object.values(this.relations)) {
-            for (const field of Object.values(models)) {
-                if (!field.relation) {
-                    continue;
-                }
-                relations.push(field);
-            }
-        }
-
-        for (const { name, model, relation, type } of relations) {
-            const relationModels = relation.replaceAll(".", "_");
-            const currentModel = model.replaceAll(".", "_");
-
-            if (!this[relationModels] || !this[currentModel]) {
-                continue;
-            }
-
-            for (const modelData of this[currentModel]) {
-                const currentValue = modelData[name];
-
-                if (!currentValue) {
-                    continue;
-                }
-
-                if (type === "many2many" || type === "one2many") {
-                    modelData[name] = this[relationModels].filter((rel) => {
-                        return currentValue.includes(rel.id);
-                    });
-                } else if (type === "many2one") {
-                    const rel = this[relationModels].find((rel) => rel.id === currentValue);
-                    modelData[name] = rel ? rel : currentValue;
-                }
-            }
-        }
-        this.saveToLocalStorage();
-    }
-
-    // needed to save in localstorage
-    deleteRelation(copy = false) {
-        const data = copy ? copy : this.pos_order;
-        const relations = [];
-
-        for (const models of Object.values(this.relations)) {
-            for (const field of Object.values(models)) {
-                if (!field.relation) {
-                    continue;
-                }
-                relations.push(field);
-            }
-        }
-
-        for (const { name, model, type } of relations) {
-            const currentModel = model.replaceAll(".", "_");
-
-            if (!data[currentModel]) {
-                continue;
-            }
-
-            for (const modelData of data[currentModel]) {
-                const currentValue = modelData[name];
-
-                if (!currentValue) {
-                    continue;
-                }
-
-                if (type === "many2many" || type === "one2many") {
-                    modelData[name] = currentValue.map((rel) => rel.id);
-                } else if (type === "many2one") {
-                    modelData[name] = currentValue.id;
-                }
-            }
-        }
-
-        return data;
-    }
-
-    saveToLocalStorage() {
-        const dataToSave = this.deleteRelation({
-            pos_order: clone(this.pos_order),
-            pos_order_line: clone(this.pos_order_line),
-        });
-
-        localStorage.setItem(
-            `pos_order-pos_session${odoo.pos_session_id}`,
-            JSON.stringify(dataToSave)
-        );
-    }
-
-    parseFromLocalStorage() {
-        const data = JSON.parse(
-            localStorage.getItem(`pos_order-pos_session${odoo.pos_session_id}`)
-        );
-
-        if (!data) {
-            return;
-        }
-
-        for (const [model, data] of Object.entries(data)) {
-            if (!data) {
-                continue;
-            }
-
-            if (registry.category("pos_available_models").contains(model)) {
-                const jsModel = registry.category("pos_available_models").get(model);
-                this[model.replaceAll(".", "_")] = data.map((p) => new jsModel(p));
-            }
-        }
-
-        this.createRelation();
-    }
+    createIndex() {}
 }
 
 export const PosDataService = {
