d53b0b9b00fdd3694f9c15831c64a1fd333ce16e
Géry Debongnie <ged@odoo.com>
2024-01-16 11:48:23 +0100

    wip

--- addons/html_editor/static/src/editor/core/utils.js
+++ addons/html_editor/static/src/editor/core/utils.js
@@ -27,14 +27,9 @@ import {
 import {
     DIRECTIONS,
     boundariesOut,
-    childNodeIndex,
-    endPos,
-    leftPos,
-    nodeSize,
-    rightPos,
-    startPos,
+    nodeSize
 } from "../utils/position";
-import { getCursorDirection } from "../utils/selection";
+import { getCursorDirection, getNormalizedCursorPosition } from "../utils/selection";
 
 
 /**
@@ -196,89 +191,6 @@ export function isSelectionFormat(editable, format) {
 // Cursor management
 //------------------------------------------------------------------------------
 
-
-/**
- * From a given position, returns the normalized version.
- *
- * E.g. <b>abc</b>[]def -> <b>abc[]</b>def
- *
- * @param {Node} node
- * @param {number} offset
- * @param {boolean} [full=true] (if not full, it means we only normalize
- *     positions which are not possible, like the cursor inside an image).
- */
-export function getNormalizedCursorPosition(node, offset, full = true) {
-    const editable = closestElement(node, '.odoo-editor-editable');
-    let closest = closestElement(node);
-    while (
-        closest &&
-        closest !== editable &&
-        (isSelfClosingElement(node) || !closest.isContentEditable)
-    ) {
-        // Cannot put the cursor inside those elements, put it before if the
-        // offset is 0 and the node is not empty, else after instead.
-        [node, offset] = offset || !nodeSize(node) ? rightPos(node) : leftPos(node);
-        closest = closestElement(node);
-    }
-
-    // Be permissive about the received offset.
-    offset = Math.min(Math.max(offset, 0), nodeSize(node));
-
-    if (full) {
-        // Put the cursor in deepest inline node around the given position if
-        // possible.
-        let el;
-        let elOffset;
-        if (node.nodeType === Node.ELEMENT_NODE) {
-            el = node;
-            elOffset = offset;
-        } else if (node.nodeType === Node.TEXT_NODE) {
-            if (offset === 0) {
-                el = node.parentNode;
-                elOffset = childNodeIndex(node);
-            } else if (offset === node.length) {
-                el = node.parentNode;
-                elOffset = childNodeIndex(node) + 1;
-            }
-        }
-        if (el) {
-            const leftInlineNode = leftLeafOnlyInScopeNotBlockEditablePath(el, elOffset).next().value;
-            let leftVisibleEmpty = false;
-            if (leftInlineNode) {
-                leftVisibleEmpty =
-                    isSelfClosingElement(leftInlineNode) ||
-                    !closestElement(leftInlineNode).isContentEditable;
-                [node, offset] = leftVisibleEmpty
-                    ? rightPos(leftInlineNode)
-                    : endPos(leftInlineNode);
-            }
-            if (!leftInlineNode || leftVisibleEmpty) {
-                const rightInlineNode = rightLeafOnlyInScopeNotBlockEditablePath(el, elOffset).next().value;
-                if (rightInlineNode) {
-                    const closest = closestElement(rightInlineNode);
-                    const rightVisibleEmpty =
-                        isSelfClosingElement(rightInlineNode) ||
-                        !closest ||
-                        !closest.isContentEditable;
-                    if (!(leftVisibleEmpty && rightVisibleEmpty)) {
-                        [node, offset] = rightVisibleEmpty
-                            ? leftPos(rightInlineNode)
-                            : startPos(rightInlineNode);
-                    }
-                }
-            }
-        }
-    }
-
-    const prevNode = node.nodeType === Node.ELEMENT_NODE && node.childNodes[offset - 1];
-    if (prevNode && prevNode.nodeName === 'BR' && isFakeLineBreak(prevNode)) {
-        // If trying to put the cursor on the right of a fake line break, put
-        // it before instead.
-        offset--;
-    }
-
-    return [node, offset];
-}
 
 /**
  * @param {Node} anchorNode
--- addons/html_editor/static/src/editor/utils/selection.js
+++ addons/html_editor/static/src/editor/utils/selection.js
@@ -1,6 +1,18 @@
 /** @odoo-module */
 
-import { DIRECTIONS } from "./position";
+import { isBlock } from "./blocks";
+import { isNotEditableNode, isSelfClosingElement } from "./dom_info";
+import { isFakeLineBreak } from "./dom_state";
+import { closestElement, createDOMPathGenerator } from "./dom_traversal";
+import {
+    DIRECTIONS,
+    childNodeIndex,
+    endPos,
+    leftPos,
+    nodeSize,
+    rightPos,
+    startPos,
+} from "./position";
 
 /**
  * From selection position, checks if it is left-to-right or right-to-left.
@@ -21,4 +33,105 @@ export function getCursorDirection(anchorNode, anchorOffset, focusNode, focusOff
     return anchorNode.compareDocumentPosition(focusNode) & Node.DOCUMENT_POSITION_FOLLOWING
         ? DIRECTIONS.RIGHT
         : DIRECTIONS.LEFT;
+}
+
+const leftLeafOnlyInScopeNotBlockEditablePath = createDOMPathGenerator(DIRECTIONS.LEFT, {
+    leafOnly: true,
+    inScope: true,
+    stopTraverseFunction: (node) => isNotEditableNode(node) || isBlock(node),
+    stopFunction: (node) => isNotEditableNode(node) || isBlock(node),
+});
+
+const rightLeafOnlyInScopeNotBlockEditablePath = createDOMPathGenerator(DIRECTIONS.RIGHT, {
+    leafOnly: true,
+    inScope: true,
+    stopTraverseFunction: (node) => isNotEditableNode(node) || isBlock(node),
+    stopFunction: (node) => isNotEditableNode(node) || isBlock(node),
+});
+
+/**
+ * From a given position, returns the normalized version.
+ *
+ * E.g. <b>abc</b>[]def -> <b>abc[]</b>def
+ *
+ * @param {Node} node
+ * @param {number} offset
+ * @param {boolean} [full=true] (if not full, it means we only normalize
+ *     positions which are not possible, like the cursor inside an image).
+ */
+export function getNormalizedCursorPosition(node, offset, full = true) {
+    const editable = closestElement(node, ".odoo-editor-editable");
+    let closest = closestElement(node);
+    while (
+        closest &&
+        closest !== editable &&
+        (isSelfClosingElement(node) || !closest.isContentEditable)
+    ) {
+        // Cannot put the cursor inside those elements, put it before if the
+        // offset is 0 and the node is not empty, else after instead.
+        [node, offset] = offset || !nodeSize(node) ? rightPos(node) : leftPos(node);
+        closest = closestElement(node);
+    }
+
+    // Be permissive about the received offset.
+    offset = Math.min(Math.max(offset, 0), nodeSize(node));
+
+    if (full) {
+        // Put the cursor in deepest inline node around the given position if
+        // possible.
+        let el;
+        let elOffset;
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            el = node;
+            elOffset = offset;
+        } else if (node.nodeType === Node.TEXT_NODE) {
+            if (offset === 0) {
+                el = node.parentNode;
+                elOffset = childNodeIndex(node);
+            } else if (offset === node.length) {
+                el = node.parentNode;
+                elOffset = childNodeIndex(node) + 1;
+            }
+        }
+        if (el) {
+            const leftInlineNode = leftLeafOnlyInScopeNotBlockEditablePath(el, elOffset).next()
+                .value;
+            let leftVisibleEmpty = false;
+            if (leftInlineNode) {
+                leftVisibleEmpty =
+                    isSelfClosingElement(leftInlineNode) ||
+                    !closestElement(leftInlineNode).isContentEditable;
+                [node, offset] = leftVisibleEmpty
+                    ? rightPos(leftInlineNode)
+                    : endPos(leftInlineNode);
+            }
+            if (!leftInlineNode || leftVisibleEmpty) {
+                const rightInlineNode = rightLeafOnlyInScopeNotBlockEditablePath(
+                    el,
+                    elOffset
+                ).next().value;
+                if (rightInlineNode) {
+                    const closest = closestElement(rightInlineNode);
+                    const rightVisibleEmpty =
+                        isSelfClosingElement(rightInlineNode) ||
+                        !closest ||
+                        !closest.isContentEditable;
+                    if (!(leftVisibleEmpty && rightVisibleEmpty)) {
+                        [node, offset] = rightVisibleEmpty
+                            ? leftPos(rightInlineNode)
+                            : startPos(rightInlineNode);
+                    }
+                }
+            }
+        }
+    }
+
+    const prevNode = node.nodeType === Node.ELEMENT_NODE && node.childNodes[offset - 1];
+    if (prevNode && prevNode.nodeName === "BR" && isFakeLineBreak(prevNode)) {
+        // If trying to put the cursor on the right of a fake line break, put
+        // it before instead.
+        offset--;
+    }
+
+    return [node, offset];
 }
