e2e4d96375c00f79c36a641830af77ca43f84612
Deependra Solanki <deso@odoo.com>
2024-07-02 12:04:52 +0530

    [FIX] web_editor: close link popover when moving outside of link
    This commit ensures that the link popover closes when the cursor moves outside
    the link.
    
    task-3631910

--- addons/website/static/tests/tours/edit_link_popover.js
+++ addons/website/static/tests/tours/edit_link_popover.js
@@ -167,6 +167,15 @@ tour.register('edit_link_popover', {
     {
         content: "Click 'Home' link in footer",
         trigger: 'footer a[href="/"]',
+        run: function (actions) {
+            // Simulate a real click, as the current step does not properly set
+            // focus on the link.
+            actions.click();
+            const sel = document.getSelection();
+            const el = this.$anchor[0];
+            sel.collapse(el, 0);
+            el.focus();
+        },
     },
     {
         content: "Popover should be shown (4)",
