8effd42b50ae557fd20187c8779180ab6ae5bbe3
kusi-odoo <kusi@odoo.com>
2024-07-29 19:05:55 +0530

    [IMP] hr_timesheet: improve gantt view with progress bar in task rows
    This commit improve progress bar to the task rows in the gantt view when tasks
    are grouped by assignee. this improvement improves the clearer overview of task
    progress for users.
    
    task-3976614

--- addons/hr_timesheet/models/project_task.py
+++ addons/hr_timesheet/models/project_task.py
@@ -51,6 +51,7 @@ class ProjectTask(models.Model):
         ! Set the task a high priority\n
         Make sure to use the right format and order e.g. Improve the configuration screen 5h #feature #v16 @Mitchell !""",
     )
+    recorded_hours = fields.Float(compute='_compute_recorded_hours', store=True)
     @property
     def SELF_READABLE_FIELDS(self):
         return super().SELF_READABLE_FIELDS | PROJECT_TASK_READABLE_FIELDS
@@ -77,6 +78,17 @@ class ProjectTask(models.Model):
             ('allow_timesheets', operator, value),
         ])
         return [('project_id', 'in', query)]
+
+    @api.depends('timesheet_ids.unit_amount')
+    def _compute_recorded_hours(self):
+        if not any(self._ids):
+            for task in self:
+                task.recorded_hours = sum(task.timesheet_ids.mapped('unit_amount'))
+            return
+        timesheet_read_group = self.env['account.analytic.line']._read_group([('task_id', 'in', self.ids)], ['task_id'], ['unit_amount:sum'])
+        timesheets_per_task = {task.id: amount for task, amount in timesheet_read_group}
+        for task in self:
+            task.recorded_hours = timesheets_per_task.get(task.id, 0.0)
 
     @api.depends('timesheet_ids.unit_amount')
     def _compute_effective_hours(self):
