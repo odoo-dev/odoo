f7ac84f6026e6f1b96ef9105676ae0005e325d36
Ali (alhy) <alhy@odoo.com>
2024-08-02 20:41:19 +0200

    [FIX] point_of_sale,*: fix failing tests
    *: pos_restaurant, pos_hr, pos_loyalty, pos_sale

--- addons/point_of_sale/static/tests/tours/utils/product_screen_util.js
+++ addons/point_of_sale/static/tests/tours/utils/product_screen_util.js
@@ -162,35 +162,54 @@ export function customerIsSelected(name) {
     ];
 }
 export function clickRefund() {
-    return [clickReview(), clickControlButtonMore(), clickControlButton("Refund")];
+    return [clickReview(), ...clickControlButton("Refund")];
 }
 export function controlButtonTrigger(name = "") {
     return `.control-buttons button:contains("${name}")`;
 }
 export function clickControlButton(name) {
-    return {
-        content: `click ${name} button`,
-        trigger: controlButtonTrigger(name),
-        run: "click",
-    };
+    return [
+        ...clickControlButtonMore(),
+        {
+            content: `click ${name} button`,
+            trigger: controlButtonTrigger(name),
+            run: "click",
+        },
+    ];
 }
 
 export function clickControlButtonMore() {
     return [
         {
-            isActive: ["mobile"],
-            content: "click more button",
-            trigger: ".mobile-more-button",
-            run: "click",
-        },
-        {
-            isActive: ["desktop"],
-            content: "click more button",
-            trigger: controlButtonTrigger("More..."),
+            content: "click Actions button",
+            trigger: controlButtonTrigger("Actions"),
             run: "click",
         },
     ];
 }
+
+export function clickInternalNoteButton() {
+    return [
+        {
+            isActive: ["mobile"],
+            content: "click Actions button",
+            trigger: controlButtonTrigger("Actions"),
+            run: "click",
+        },
+        {
+            isActive: ["mobile"],
+            trigger: controlButtonTrigger("Internal Note"),
+            run: "click",
+        },
+        {
+            isActive: ["desktop"],
+            content: "click Internal Note button",
+            trigger: controlButtonTrigger("Internal Note"),
+            run: "click",
+        },
+    ];
+}
+
 /**
  * Selects a given price list in the user interface. This function is designed to be used to select a specific price list.
  *
@@ -265,12 +284,7 @@ export function enterOpeningAmount(amount) {
 export function clickFiscalPosition(name, checkIsNeeded = false) {
     const step = [
         clickReview(),
-        {
-            isActive: ["mobile"],
-            content: "click more button",
-            trigger: ".mobile-more-button",
-            run: "click",
-        },
+        ...clickControlButtonMore(),
         {
             content: "click fiscal position button",
             trigger: ".o_fiscal_position_button",
@@ -285,15 +299,10 @@ export function clickFiscalPosition(name, checkIsNeeded = false) {
 
     if (checkIsNeeded) {
         step.push(
-            {
-                isActive: ["mobile"],
-                content: "click more button",
-                trigger: ".mobile-more-button",
-                run: "click",
-            },
+            ...clickControlButtonMore(),
             {
                 content: "the fiscal position " + name + " has been set to the order",
-                trigger: `.control-buttons button.o_fiscal_position_button:contains("${name}")`,
+                trigger: `.o_fiscal_position_button:contains("${name}")`,
             },
             {
                 content: "cancel dialog",
@@ -466,12 +475,6 @@ export function addOrderline(productName, quantity = 1, unitPrice, expectedTotal
 export function addCustomerNote(note) {
     return inLeftSide(
         [
-            {
-                isActive: ["mobile"],
-                content: "click more button",
-                trigger: ".mobile-more-button",
-                run: "click",
-            },
             clickControlButton("Customer Note"),
             TextInputPopup.inputText(note),
             Dialog.confirm(),
@@ -481,17 +484,7 @@ export function addCustomerNote(note) {
 
 export function addInternalNote(note) {
     return inLeftSide(
-        [
-            {
-                isActive: ["mobile"],
-                content: "click more button",
-                trigger: ".mobile-more-button",
-                run: "click",
-            },
-            clickControlButton("Internal Note"),
-            TextInputPopup.inputText(note),
-            Dialog.confirm(),
-        ].flat()
+        [clickInternalNoteButton(), TextInputPopup.inputText(note), Dialog.confirm()].flat()
     );
 }
 
