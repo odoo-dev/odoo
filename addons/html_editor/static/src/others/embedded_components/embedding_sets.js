da1e7a118875caaf22679d7f7a6512d6788cd27d
Thomas Josse (THJO) <thjo@odoo.com>
2024-08-08 10:05:04 +0200

    [IMP] html_editor: add /video

--- addons/html_editor/static/src/others/embedded_components/embedding_sets.js
+++ addons/html_editor/static/src/others/embedded_components/embedding_sets.js
@@ -4,9 +4,15 @@ import {
     readonlyTableOfContentEmbeddedComponent,
     tableOfContentEmbeddedComponent,
 } from "@html_editor/others/embedded_components/table_of_content/component/table_of_content";
+import { videoEmbedding } from "@html_editor/others/embedded_components/video/component/video";
 
-export const MAIN_EMBEDDINGS = [embeddedExcalidraw, tableOfContentEmbeddedComponent];
+export const MAIN_EMBEDDINGS = [
+    embeddedExcalidraw,
+    tableOfContentEmbeddedComponent,
+    videoEmbedding,
+];
 export const READONLY_MAIN_EMBEDDINGS = [
     readonlyEmbeddedExcalidraw,
     readonlyTableOfContentEmbeddedComponent,
+    videoEmbedding,
 ];
