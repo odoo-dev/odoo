d4e1658a7eec5279219ecb71c11b4e70b2d9a3f9
Xavier Morel <xmo@odoo.com>
2024-08-19 13:26:23 +0200

    [IMP] *: move requirements over to pyproject

--- odoo/release.py
+++ odoo/release.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import warnings
 
 RELEASE_LEVELS = [ALPHA, BETA, RELEASE_CANDIDATE, FINAL] = ['alpha', 'beta', 'candidate', 'final']
 RELEASE_LEVELS_DISPLAY = {ALPHA: ALPHA,
@@ -12,26 +13,24 @@ RELEASE_LEVELS_DISPLAY = {ALPHA: ALPHA,
 # properly comparable using normal operators, for example:
 #  (6,1,0,'beta',0) < (6,1,0,'candidate',1) < (6,1,0,'candidate',2)
 #  (6,1,0,'candidate',2) < (6,1,0,'final',0) < (6,1,2,'final',0)
+# NOTE: serial hasn't been used since 11.0 or so
 version_info = (17, 5, 0, ALPHA, 1, '')
-version = '.'.join(str(s) for s in version_info[:2]) + RELEASE_LEVELS_DISPLAY[version_info[3]] + str(version_info[4] or '') + version_info[5]
-series = serie = major_version = '.'.join(str(s) for s in version_info[:2])
+major_version = '.'.join(str(s) for s in version_info[:2])
+version = major_version + RELEASE_LEVELS_DISPLAY[version_info[3]] + str(version_info[4] or '') + version_info[5]
 
 product_name = 'Odoo'
 description = 'Odoo Server'
-long_desc = '''Odoo is a complete ERP and CRM. The main features are accounting (analytic
-and financial), stock management, sales and purchases management, tasks
-automation, marketing campaigns, help desk, POS, etc. Technical features include
-a distributed server, an object database, a dynamic GUI,
-customizable reports, and XML-RPC interfaces.
-'''
-classifiers = """Development Status :: 5 - Production/Stable
-License :: OSI Approved :: GNU Lesser General Public License v3
-
-Programming Language :: Python
-"""
 url = 'https://www.odoo.com'
 author = 'OpenERP S.A.'
-author_email = 'info@odoo.com'
-license = 'LGPL-3'
 
-nt_service_name = "odoo-server-" + series.replace('~','-')
+nt_service_name = f"odoo-server-{major_version}"
+
+def __getattr__(name):
+    if name in ('series', 'serie'):
+        warnings.warn(
+            "odoo.release.serie and odoo.release.series are deprecated, use `major_version`",
+            category=PendingDeprecationWarning,
+            stacklevel=2,
+        )
+        return major_version
+    raise AttributeError(f"module {__name__} has no attribute {name}")
