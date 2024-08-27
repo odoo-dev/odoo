ceec5af0caff762d22d5a136753b329e4c0d55cf
Gauthier Wala (gawa) <gawa@odoo.com>
2024-08-27 17:46:51 +0200

    hmmm

--- addons/l10n_dk_nemhandel/models/account_edi_xml_oioubl_201.py
+++ addons/l10n_dk_nemhandel/models/account_edi_xml_oioubl_201.py
@@ -6,8 +6,20 @@ class AccountEdiXmlOIOUBL201(models.AbstractModel):
         # EXTENDS account.edi.xml.oioubl_201
         vals = super()._get_partner_party_vals(partner, role)
         if partner.l10n_dk_nemhandel_identifier_type and partner.l10n_dk_nemhandel_identifier_value:
+            prefix = 'DK%s' if partner.l10n_dk_nemhandel_identifier_type == 'DK:CVR' else '%s'
             vals.update({
-                'endpoint_id': partner.l10n_dk_nemhandel_identifier_type,
-                'endpoint_id_attrs': f'DK{partner.l10n_dk_nemhandel_identifier_value}',
+                'endpoint_id': prefix % partner.l10n_dk_nemhandel_identifier_value,
+                'endpoint_id_attrs': {'scheme_ID': partner.l10n_dk_nemhandel_identifier_type},
             })
         return vals
+
+
+    def _get_partner_party_identification_vals_list(self, partner):
+        vals = super()._get_partner_party_identification_vals_list(partner)
+        if partner.l10n_dk_nemhandel_identifier_type and partner.l10n_dk_nemhandel_identifier_value:
+            prefix = 'DK%s' if partner.l10n_dk_nemhandel_identifier_type == 'DK:CVR' else '%s'
+            return vals.append({
+                'id': prefix % partner.l10n_dk_nemhandel_identifier_value,
+                'id_attrs': {'scheme_ID': partner.l10n_dk_nemhandel_identifier_type},
+            })
+
