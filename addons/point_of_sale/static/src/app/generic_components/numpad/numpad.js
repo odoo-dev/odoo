12076150e0b3c1f84d318475023b2d18d882c498
vlst-odoo <vlst@odoo.com>
2024-08-28 16:18:00 +0200

    [REF] point_of_sale: remove number_buffer

--- addons/point_of_sale/static/src/app/generic_components/numpad/numpad.js
+++ addons/point_of_sale/static/src/app/generic_components/numpad/numpad.js
@@ -1,6 +1,6 @@
-import { useService } from "@web/core/utils/hooks";
-import { Component } from "@odoo/owl";
+import { Component, useExternalListener, useState } from "@odoo/owl";
 import { localization } from "@web/core/l10n/localization";
+import { barcodeService } from "@barcodes/barcode_service";
 
 export const buttonsType = {
     type: Array,
@@ -12,6 +12,8 @@ export const buttonsType = {
                 text: { type: String, optional: true },
                 class: { type: String, optional: true },
                 disabled: { type: Boolean, optional: true },
+                // this function has to take a string and return a string
+                modifier: { type: Function, optional: true },
             },
         },
         Number,
@@ -23,10 +25,28 @@ export const DECIMAL = {
     get value() {
         return localization.decimalPoint;
     },
+    modifier: (value) => {
+        if (!value) {
+            return "0" + localization.decimalPoint;
+        }
+        if (value.includes(localization.decimalPoint)) {
+            return value;
+        }
+        return value + localization.decimalPoint;
+    },
 };
-export const BACKSPACE = { value: "Backspace", text: "⌫" };
+export const BACKSPACE = {
+    value: "Backspace",
+    text: "⌫",
+    modifier: (value) =>
+        !value || (value.length === 2 && value.startsWith("-")) ? "" : value.slice(0, -1),
+};
 export const ZERO = { value: "0" };
-export const DEFAULT_LAST_ROW = [{ value: "-", text: "+/-" }, ZERO, DECIMAL];
+export const DEFAULT_LAST_ROW = [
+    { value: "-", text: "+/-", modifier: (value) => (value ? -parseFloat(value).toString() : "-") },
+    ZERO,
+    DECIMAL,
+];
 export const EMPTY = { value: "" };
 
 export function getButtons(lastRow, rightColumn) {
@@ -50,9 +70,9 @@ export function getButtons(lastRow, rightColumn) {
 
 export function enhancedButtons() {
     return getButtons(DEFAULT_LAST_ROW, [
-        { value: "+10" },
-        { value: "+20" },
-        { value: "+50" },
+        { value: "+10", modifier: (value) => (parseFloat(value || 0) + 10).toString() },
+        { value: "+20", modifier: (value) => (parseFloat(value || 0) + 20).toString() },
+        { value: "+50", modifier: (value) => (parseFloat(value || 0) + 50).toString() },
         BACKSPACE,
     ]);
 }
@@ -63,19 +83,59 @@ export class Numpad extends Component {
         class: { type: String, optional: true },
         onClick: { type: Function, optional: true },
         buttons: { type: buttonsType, optional: true },
+        getResetter: { type: Function, optional: true },
     };
     static defaultProps = {
         class: "numpad",
+        buttons: getButtons([DECIMAL, ZERO, BACKSPACE]),
     };
-    get buttons() {
-        return this.props.buttons || getButtons([DECIMAL, ZERO, BACKSPACE]);
-    }
     setup() {
-        if (!this.props.onClick) {
-            this.numberBuffer = useService("number_buffer");
-            this.onClick = (buttonValue) => this.numberBuffer.sendKey(buttonValue);
-        } else {
-            this.onClick = this.props.onClick;
-        }
+        this.state = useState({ value: "", bufferedKeys: [] });
+        this.props.getResetter?.(() => (this.state.value = ""));
+        // We use heuristics to determine if the input is coming from a barcode scanner or a user.
+        useExternalListener(window, "keydown", (event) => {
+            if (
+                ["INPUT", "TEXTAREA"].includes(event.target.tagName) ||
+                (document.body.classList.contains("modal-open") && !this.env.inDialog)
+            ) {
+                return;
+            }
+            const button = this.props.buttons.find((button) => button.value === event.key);
+            if (!button) {
+                return;
+            }
+            this.state.bufferedKeys.push(button);
+            clearTimeout(this.lastTimeout);
+            // Barcode Assumptions:
+            const isBarcode = (keys) => keys.length > 4;
+            const nonBarcodeCharacters = ["-", "Backspace", DECIMAL.value];
+            // When detecting a non barcode key we know for sure that it's the user that is actually typing
+            // and not a barcode scanner sending the keys, so we can safely trigger the action.
+            if (nonBarcodeCharacters.includes(event.key)) {
+                this.onClick(...this.state.bufferedKeys);
+                this.state.bufferedKeys = [];
+                return;
+            }
+            this.lastTimeout = setTimeout(() => {
+                if (!isBarcode(this.state.bufferedKeys)) {
+                    this.onClick(...this.state.bufferedKeys);
+                }
+                this.state.bufferedKeys = [];
+            }, barcodeService.maxTimeBetweenKeysInMs);
+        });
+    }
+    onClick(...buttons) {
+        buttons.forEach((button) => {
+            const modifier = button.modifier || ((value) => value + button.value);
+            this.state.value = modifier(this.state.value);
+            this.props.onClick?.({
+                buffer: this.state.value || "0",
+                key: button.value,
+                button: {
+                    ...button,
+                    modifier,
+                },
+            });
+        });
     }
 }
