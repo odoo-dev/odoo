feba3e2cfdfb08957c93bcddf1da498e13738a31
vlst-odoo <vlst@odoo.com>
2024-08-28 16:18:00 +0200

    [REF] point_of_sale: remove number_buffer

--- addons/point_of_sale/static/src/app/utils/input_popups/number_popup.js
+++ addons/point_of_sale/static/src/app/utils/input_popups/number_popup.js
@@ -1,5 +1,4 @@
 import { _t } from "@web/core/l10n/translation";
-import { useBus, useService } from "@web/core/utils/hooks";
 import { Component, useState } from "@odoo/owl";
 import { Dialog } from "@web/core/dialog/dialog";
 import { Numpad, buttonsType } from "@point_of_sale/app/generic_components/numpad/numpad";
@@ -29,17 +28,10 @@ export class NumberPopup extends Component {
     };
 
     setup() {
-        this.numberBuffer = useService("number_buffer");
-        this.numberBuffer.use({
-            triggerAtEnter: () => this.confirm(),
-            triggerAtEscape: () => this.cancel(),
-        });
         this.state = useState({
-            buffer: this.props.startingValue,
+            buffer: "",
         });
-        useBus(this.numberBuffer, "buffer-update", ({ detail: value }) => {
-            this.state.buffer = value;
-        });
+        this.props.placeholder = this.props.startingValue;
     }
     confirm() {
         this.props.getPayload(this.state.buffer);
