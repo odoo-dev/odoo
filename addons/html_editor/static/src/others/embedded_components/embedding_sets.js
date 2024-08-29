f38556763be9e56b3c04069fc8bb5f81e792a13e
abd-msyukyu-odoo <abd@odoo.com>
2024-08-29 14:42:32 +0200

    [IMP] html_editor: convert table of content to an embedded component

--- addons/html_editor/static/src/others/embedded_components/embedding_sets.js
+++ addons/html_editor/static/src/others/embedded_components/embedding_sets.js
@@ -1,2 +1,7 @@
-export const MAIN_EMBEDDINGS = [];
-export const READONLY_MAIN_EMBEDDINGS = [];
+import {
+    readonlyTableOfContentEmbeddedComponent,
+    tableOfContentEmbeddedComponent,
+} from "@html_editor/others/embedded_components/table_of_content/component/table_of_content";
+
+export const MAIN_EMBEDDINGS = [tableOfContentEmbeddedComponent];
+export const READONLY_MAIN_EMBEDDINGS = [readonlyTableOfContentEmbeddedComponent];
