adb9e66dc3ce2ad05be0c37cacab10b8e72cb36f
Maryam Kia <maki@odoo.com>
2024-09-30 15:53:46 +0200

    [IMP] mail, portal, *: replace the portal chatter fetch route
    This commit replaces `/mail/chatter_fetch` with `/mail/thread/messages` in order
    to reuse the base mail code in portal as much as possible.

--- addons/portal_rating/static/src/chatter/frontend/message_patch.js
+++ addons/portal_rating/static/src/chatter/frontend/message_patch.js
@@ -14,15 +14,18 @@ patch(Message.prototype, {
         return !this.state.editRating && super.isEditing;
     },
 
-    get ratingValue() {
-        return this.message.rating_id?.rating || this.message.rating_value;
+    get commentPublishDate() {
+        return luxon.DateTime.fromFormat(
+            this.message.rating_id.publisher_datetime,
+            "yyyy-MM-dd HH:mm:ss"
+        );
     },
 
     onClikEditComment() {
         this.state.editRating = !this.state.editRating;
         if (this.state.editRating) {
             const messageContent = convertBrToLineBreak(
-                this.props.message.rating_id.publisher_comment
+                this.props.message.rating_id.publisher_comment || ""
             );
             this.props.message.composer = {
                 message: this.props.message,
@@ -38,8 +41,7 @@ patch(Message.prototype, {
     },
 
     exitEditCommentMode() {
-        this.props.message.composer.clear();
-        this.message.composer = null;
+        this.message.composer.clear();
         this.state.editRating = false;
     },
 
@@ -48,6 +50,6 @@ patch(Message.prototype, {
             rating_id: this.message.rating_id.id,
             publisher_comment: "",
         });
-        this.message.rating_id = data;
+        this.store.insert(data);
     },
 });
