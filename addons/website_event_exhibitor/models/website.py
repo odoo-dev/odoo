keyboard-slayer/odoo-eval

[IMP] website_event_(track/exhibitor): use website search bar
For the exhibitors and tracks a custom search bar is made in the website_event
by inheriting the website search bar, Due to this some of the website search bar
functionality is not working properly, So rather than fixing the custom
search bar, we will use the website search bar.

Specifications
===============
1) Now in website_event_track we will be able to search the track by
the speaker's name.

2) Now the cross button in the event track and exhibitors search bar will
work properly.

3) As we are using the website search bar now the suggestions are enabled in
the track and exhibitor.

Task-3758369

addons/website_event_exhibitor/models/website.py
@@ -1,0 +1,11 @@
+from odoo import models
+
+
+class Website(models.Model):
+    _inherit = "website"
+
+    def _search_get_details(self, search_type, order, options):
+        result = super()._search_get_details(search_type, order, options)
+        if search_type in ['sponser', 'all']:
+            result.append(self.env['event.sponsor']._search_get_detail(self, order, options))
+        return result

odoo-dev/odoo

addons/website_event_exhibitor/models/website.py
@@ -1,0 +1,11 @@
+from odoo import models
+
+
+class Website(models.Model):
+    _inherit = "website"
+
+    def _search_get_details(self, search_type, order, options):
+        result = super()._search_get_details(search_type, order, options)
+        if search_type in ['sponsor', 'all']:
+            result.append(self.env['event.sponsor']._search_get_detail(self, order, options))
+        return result