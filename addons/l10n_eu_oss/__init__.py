11054c00c8d89489f965e5da5c22bbf57601615f
Sven Fuehr <svfu@odoo.com>
2024-10-14 13:20:13 +0200

    [FIX] l10n_eu_oss: refresh tax mappings on install
    Currently after installing the l10n_eu_oss module
    the oss tax mappings are not automatically created / refreshed.
    
    After this commit the oss tax mappings are created for all
    companies.
    This also makes the OSS reports from enterprise
    (l10n_eu_oss_reports) visible.
    
    This restores the behavior from 16.0 that was removed in
    cc2d5173f2d3729b792b65d156a53707478251df .
    
    task-4247724

--- addons/l10n_eu_oss/__init__.py
+++ addons/l10n_eu_oss/__init__.py
@@ -3,5 +3,11 @@
 
 from . import models
 
+
+def l10n_eu_oss_post_init(env):
+    companies = env['res.company'].search([])
+    for company in companies:
+        company._map_eu_taxes()
+
 def l10n_eu_oss_uninstall(env):
     env.cr.execute("DELETE FROM ir_model_data WHERE module = 'l10n_eu_oss' and model in ('account.tax.group', 'account.account');")
