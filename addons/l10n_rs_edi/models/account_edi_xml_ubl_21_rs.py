keyboard-slayer/odoo-eval

[ADD] l10n_rs_edi: Add Serbian E-Invoice
Serbia has implemented a mandatory eInvoicing system, named eFaktura, This module utilizes the eFaktura API to enable users to submit their e-invoices.

task-4004913

addons/l10n_rs_edi/models/account_edi_xml_ubl_21_rs.py
@@ -1,0 +1,81 @@
+from odoo import api, models
+
+
+class AccountEdiXmlUBL21RS(models.AbstractModel):
+    _name = "account.edi.xml.ubl.rs"
+    _inherit = 'account.edi.xml.ubl_21'
+    _description = "UBL 2.1 (RS eFaktura)"
+
+    @api.model
+    def _get_customization_ids(self):
+        vals = super()._get_customization_ids()
+        vals['efaktura_rs'] = 'urn:cen.eu:en16931:2017#compliant#urn:mfin.gov.rs:srbdt:2022#conformant#urn:mfin.gov.rs:srbdtext:2022'
+        return vals
+
+    def _export_invoice_vals(self, invoice):
+        # EXTENDS 'account_edi_ubl_cii'
+        vals = super()._export_invoice_vals(invoice)
+
+        vals['vals'].update({
+            'customization_id': self._get_customization_ids()['efaktura_rs'],
+            'InvoiceType_template': 'l10n_rs_edi.ubl_rs_Invoice_Type',
+            'CreditNoteType_template': 'l10n_rs_edi.ubl_rs_CreditNoteType',
+        })
+
+        return vals
+
+    def _get_invoice_period_vals_list(self, invoice):
+        # EXTENDS account_edi_ubl_cii
+        vals_list = super()._get_invoice_period_vals_list(invoice)
+        vals_list.append({
+            'description_code': invoice.l10n_rs_tax_date_obligations_code,
+        })
+        return vals_list
+
+    def _get_partner_party_vals(self, partner, role):
+        vals = super()._get_partner_party_vals(partner, role)
+        vat_country, vat_number = partner._split_vat(partner.vat)
+        if vat_country.isnumeric():
+            vat_number = partner.vat
+        vals.update({
+            'endpoint_id': vat_number,
+            'endpoint_id_attrs': {
+                'schema_id': '9948',
+            },
+        })
+        return vals
+
+    def _get_partner_party_legal_entity_vals_list(self, partner):
+        # EXTENDS 'account_edi_ubl_cii'
+        vals_list = super()._get_partner_party_legal_entity_vals_list(partner)
+        for vals in vals_list:
+            vals['company_id'] = partner.company_registry
+        return vals_list
+
+    def _get_partner_party_tax_scheme_vals_list(self, partner, role):
+        # EXTENDS 'account_edi_ubl_cii'
+        vals_list = super()._get_partner_party_tax_scheme_vals_list(partner, role)
+
+        for vals in vals_list:
+            vat_country, vat_number = partner._split_vat(partner.vat)
+            if vat_country.isnumeric():
+                vat_country = 'RS'
+                vat_number = partner.vat
+            if vat_country == 'RS' and partner.simple_vat_check(vat_country, vat_number):
+                vals['company_id'] = vat_country + vat_number
+        return vals_list
+
+    def _get_delivery_vals_list(self, invoice):
+        # EXTENDS 'account_edi_ubl_cii'
+        vals_list = super()._get_delivery_vals_list(invoice)
+        for vals in vals_list:
+            vals['actual_delivery_date'] = invoice.delivery_date or None
+        return vals_list
+
+    def _get_partner_party_identification_vals_list(self, partner):
+        vals_list = super()._get_partner_party_identification_vals_list()
+        if partner.country_code == 'RS' and partner.l10n_rs_edi_public_funds_id:
+            vals_list.append({
+                'id': ('JBKJS: %s', partner.l10n_rs_edi_public_funds_id),
+            })
+        return vals_list

addons/l10n_rs_edi/models/account_move.py
@@ -1,0 +1,151 @@
+import uuid
+import requests
+import json
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+EFAKTURA_URL = 'https://efakturadev.mfin.gov.rs/api/publicApi/sales-invoice/ubl'
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    l10n_rs_edi_uuid = fields.Char("Invoice UUID", copy=False, help="Unique Identifier for an invoice used as request id")
+
+    l10n_rs_edi_is_eligible = fields.Boolean(
+        compute="_compute_l10n_rs_edi_is_eligible",
+        help="Serbia: technical field to determine if this invoice is eligible to be e-invoiced.",
+    )
+
+    l10n_rs_edi_attachment_file = fields.Binary(
+        string="Serbian E-Invoice XML File",
+        copy=False,
+        attachment=True,
+        help="Serbia: technical field holding the e-invoice XML data.",
+    )
+
+    l10n_rs_edi_attachment_id = fields.Many2one(
+        comodel_name='ir.attachment',
+        string="eFaktura XML Attachment",
+        compute=lambda self: self._compute_linked_attachment_id('l10n_rs_edi_attachment_id', 'l10n_rs_edi_attachment_file'),
+        depends=['l10n_rs_edi_attachment_file'],
+    )
+
+    l10n_rs_edi_state = fields.Selection(
+        string="Serbia E-Invoice state",
+        selection=[
+            ('to_send', 'Sending'),
+            ('sent', 'sent'),
+            ('sending_failed', 'Error')
+        ],
+        tracking=True,
+        readonly=True,
+        copy=False,
+    )
+
+    l10n_rs_edi_error = fields.Text(
+        string="Serbia E-Invoice error",
+        copy=False,
+        readonly=True,
+    )
+
+    l10n_rs_tax_date_obligations_code = fields.Selection(
+        string="Tax Date Obligations",
+        selection=[
+            ('35', 'By Delivery Date'),
+            ('3', 'By Issuance Date'),
+            ('432', 'By Billing System'),
+        ],
+        # compute="_compute_l10n_rs_tax_date_obligations_code",
+    )
+
+    # @api.depends('move_type')
+    # def _compute_l10n_rs_tax_date_obligations_code(self):
+    #     for move in self:
+    #         if move.move_type == 'out_invoice':
+    #             move.l10n_rs_tax_date_obligations_code = '0'
+
+    @api.depends('country_code', 'move_type')
+    def _compute_show_delivery_date(self):
+        # EXTENDS 'account'
+        super()._compute_show_delivery_date()
+        for move in self:
+            if move.country_code == 'RS':
+                move.show_delivery_date = move.is_sale_document()
+
+    @api.depends("country_code", "move_type")
+    def _compute_l10n_rs_edi_is_eligible(self):
+        for move in self:
+            move.l10n_rs_edi_is_eligible = move.country_code == 'RS' and move.is_sale_document()
+
+    def _post(self, soft=True):
+        posted = super()._post(soft)
+        posted.l10n_rs_edi_state = 'to_send'
+        return posted
+
+    @api.depends('l10n_rs_edi_state')
+    def _compute_show_reset_to_draft_button(self):
+        # EXTENDS 'account'
+        super()._compute_show_reset_to_draft_button()
+        for move in self:
+            move.show_reset_to_draft_button = not move.l10n_rs_edi_state in ('sent', 'to_send') and move.show_reset_to_draft_button
+
+    def button_draft(self):
+        # EXTENDS 'account'
+        self.write(
+            {
+                "l10n_rs_edi_error": False,
+                "l10n_rs_edi_attachment_file": False,
+                "l10n_rs_edi_attachment_id": False,
+            }
+        )
+        return super().button_draft()
+
+    def _l10n_rs_edi_send(self, xml):
+        # sends the invoice to the eFaktura
+        self.ensure_one()
+        # attachment_values = self._l10n_rs_edi_get_attachment_values(xml)
+        # self.env['ir.attachment'].create(attachment_values)
+        # self.invalidate_recordset(fnames=['l10n_rs_edi_attachment_id', 'l10n_rs_edi_attachment_file'])
+        # self.with_context(no_new_invoice=True).message_post(attachment_ids=self.l10n_rs_edi_attachment_id.ids)
+        params = {'request_id': self.l10n_rs_edi_uuid}
+        if not self.company_id.l10n_rs_edi_api_key:
+            raise ValidationError('eFaktura API Key is missing')
+        headers = {
+            'content-type': 'application/xml',
+            'ApiKey': self.company_id.l10n_rs_edi_api_key,
+        }
+        try:
+            response = requests.post(EFAKTURA_URL, params=params, headers=headers, data=xml)
+        except Exception as e:
+            self.l10n_rs_edi_state = 'sending_failed'
+            self.l10n_rs_edi_error = e
+            self.message_post(body=e)
+        if not response.ok:
+            self.l10n_rs_edi_state = 'sending_failed'
+            raise ValidationError(response.content)
+        dict_response = json.loads(response.content.decode('utf-8'))
+
+    def _l10n_rs_edi_get_attachment_values(self, xml):
+        return {
+            'name': self._l10n_rs_edi_get_xml_attachment_name(),
+            'mimetype': 'application/xml',
+            'description': _('RS EDI e-move: %s', self.move_type),
+            'company_id': self.company_id.id,
+            'res_id': self.id,
+            'res_model': self._name,
+            'res_field': 'l10n_rs_edi_attachment_file',
+            'raw': xml,
+        }
+
+    def _l10n_rs_edi_get_xml_attachment_name(self):
+        return f"{self.name.replace('/', '_')}_edi.xml"
+
+    @api.model
+    def create(self, vals):
+        records = super().create(vals)
+        for record in records:
+            if record.l10n_rs_edi_is_eligible:
+                record.l10n_rs_edi_uuid = uuid.uuid4()
+        return records

odoo-dev/odoo

addons/l10n_rs_edi/models/account_edi_xml_ubl_21_rs.py
@@ -1,0 +1,82 @@
+from odoo import api, models
+
+
+class AccountEdiXmlUBL21RS(models.AbstractModel):
+    _name = "account.edi.xml.ubl.rs"
+    _inherit = 'account.edi.xml.ubl_21'
+    _description = "UBL 2.1 (RS eFaktura)"
+
+    @api.model
+    def _get_customization_ids(self):
+        vals = super()._get_customization_ids()
+        vals['efaktura_rs'] = 'urn:cen.eu:en16931:2017#compliant#urn:mfin.gov.rs:srbdt:2022#conformant#urn:mfin.gov.rs:srbdtext:2022'
+        return vals
+
+    def _export_invoice_vals(self, invoice):
+        # EXTENDS 'account_edi_ubl_cii'
+        vals = super()._export_invoice_vals(invoice)
+
+        vals.update({
+            'InvoiceType_template': 'l10n_rs_edi.ubl_rs_Invoice_Type',
+            'CreditNoteType_template': 'l10n_rs_edi.ubl_rs_CreditNoteType',
+        })
+        vals['vals'].update({
+            'customization_id': self._get_customization_ids()['efaktura_rs'],
+        })
+        return vals
+
+    def _get_invoice_period_vals_list(self, invoice):
+        # EXTENDS account_edi_ubl_cii
+        vals_list = super()._get_invoice_period_vals_list(invoice)
+        vals_list.append({
+            'description_code': '0' if invoice.move_type == 'out_refund' else invoice.l10n_rs_tax_date_obligations_code,
+        })
+        return vals_list
+
+    def _get_partner_party_vals(self, partner, role):
+        vals = super()._get_partner_party_vals(partner, role)
+        vat_country, vat_number = partner._split_vat(partner.vat)
+        if vat_country.isnumeric():
+            vat_number = partner.vat
+        vals.update({
+            'endpoint_id': vat_number,
+            'endpoint_id_attrs': {
+                'schemeID': '9948',
+            },
+        })
+        return vals
+
+    def _get_partner_party_legal_entity_vals_list(self, partner):
+        # EXTENDS 'account_edi_ubl_cii'
+        vals_list = super()._get_partner_party_legal_entity_vals_list(partner)
+        for vals in vals_list:
+            vals['company_id'] = partner.l10n_rs_edi_customer_id
+        return vals_list
+
+    def _get_partner_party_tax_scheme_vals_list(self, partner, role):
+        # EXTENDS 'account_edi_ubl_cii'
+        vals_list = super()._get_partner_party_tax_scheme_vals_list(partner, role)
+
+        for vals in vals_list:
+            vat_country, vat_number = partner._split_vat(partner.vat)
+            if vat_country.isnumeric():
+                vat_country = 'RS'
+                vat_number = partner.vat
+            if vat_country == 'RS' and partner.simple_vat_check(vat_country, vat_number):
+                vals['company_id'] = vat_country + vat_number
+        return vals_list
+
+    def _get_delivery_vals_list(self, invoice):
+        # EXTENDS 'account_edi_ubl_cii'
+        vals_list = super()._get_delivery_vals_list(invoice)
+        for vals in vals_list:
+            vals['actual_delivery_date'] = invoice.delivery_date or None
+        return vals_list
+
+    def _get_partner_party_identification_vals_list(self, partner):
+        vals_list = super()._get_partner_party_identification_vals_list(partner)
+        if partner.country_code == 'RS' and partner.l10n_rs_edi_public_funds_id:
+            vals_list.append({
+                'id': ('JBKJS: %s', partner.l10n_rs_edi_public_funds_id),
+            })
+        return vals_list

addons/l10n_rs_edi/models/account_move.py
@@ -1,0 +1,151 @@
+import uuid
+import requests
+import json
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+EFAKTURA_URL = 'https://efakturadev.mfin.gov.rs/api/publicApi/sales-invoice/ubl'
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    l10n_rs_edi_uuid = fields.Char("Invoice UUID", copy=False, help="Unique Identifier for an invoice used as request id")
+
+    l10n_rs_edi_is_eligible = fields.Boolean(
+        compute="_compute_l10n_rs_edi_is_eligible",
+        help="Serbia: technical field to determine if this invoice is eligible to be e-invoiced.",
+    )
+
+    l10n_rs_edi_attachment_file = fields.Binary(
+        string="Serbian E-Invoice XML File",
+        copy=False,
+        attachment=True,
+        help="Serbia: technical field holding the e-invoice XML data.",
+    )
+
+    l10n_rs_edi_attachment_id = fields.Many2one(
+        comodel_name='ir.attachment',
+        string="eFaktura XML Attachment",
+        compute=lambda self: self._compute_linked_attachment_id('l10n_rs_edi_attachment_id', 'l10n_rs_edi_attachment_file'),
+        depends=['l10n_rs_edi_attachment_file'],
+    )
+
+    l10n_rs_edi_state = fields.Selection(
+        string="Serbia E-Invoice state",
+        selection=[
+            ('to_send', 'Sending'),
+            ('sent', 'sent'),
+            ('sending_failed', 'Error')
+        ],
+        tracking=True,
+        readonly=True,
+        copy=False,
+    )
+
+    l10n_rs_edi_error = fields.Text(
+        string="Serbia E-Invoice error",
+        copy=False,
+        readonly=True,
+    )
+
+    l10n_rs_tax_date_obligations_code = fields.Selection(
+        string="Tax Date Obligations",
+        selection=[
+            ('35', 'By Delivery Date'),
+            ('3', 'By Issuance Date'),
+            ('432', 'By Billing System'),
+        ],
+        # compute="_compute_l10n_rs_tax_date_obligations_code",
+    )
+
+    # @api.depends('move_type')
+    # def _compute_l10n_rs_tax_date_obligations_code(self):
+    #     for move in self:
+    #         if move.move_type == 'out_invoice':
+    #             move.l10n_rs_tax_date_obligations_code = '0'
+
+    @api.depends('country_code', 'move_type')
+    def _compute_show_delivery_date(self):
+        # EXTENDS 'account'
+        super()._compute_show_delivery_date()
+        for move in self:
+            if move.country_code == 'RS':
+                move.show_delivery_date = move.is_sale_document()
+
+    @api.depends("country_code", "move_type")
+    def _compute_l10n_rs_edi_is_eligible(self):
+        for move in self:
+            move.l10n_rs_edi_is_eligible = move.country_code == 'RS' and move.is_sale_document()
+
+    def _post(self, soft=True):
+        posted = super()._post(soft)
+        posted.l10n_rs_edi_state = 'to_send'
+        return posted
+
+    @api.depends('l10n_rs_edi_state')
+    def _compute_show_reset_to_draft_button(self):
+        # EXTENDS 'account'
+        super()._compute_show_reset_to_draft_button()
+        for move in self:
+            move.show_reset_to_draft_button = not move.l10n_rs_edi_state in ('sent', 'to_send') and move.show_reset_to_draft_button
+
+    def button_draft(self):
+        # EXTENDS 'account'
+        self.write(
+            {
+                "l10n_rs_edi_error": False,
+                "l10n_rs_edi_attachment_file": False,
+                "l10n_rs_edi_attachment_id": False,
+            }
+        )
+        return super().button_draft()
+
+    def _l10n_rs_edi_send(self, xml):
+        # sends the invoice to the eFaktura
+        self.ensure_one()
+        # attachment_values = self._l10n_rs_edi_get_attachment_values(xml)
+        # self.env['ir.attachment'].create(attachment_values)
+        # self.invalidate_recordset(fnames=['l10n_rs_edi_attachment_id', 'l10n_rs_edi_attachment_file'])
+        # self.with_context(no_new_invoice=True).message_post(attachment_ids=self.l10n_rs_edi_attachment_id.ids)
+        params = {'request_id': self.l10n_rs_edi_uuid}
+        if not self.company_id.l10n_rs_edi_api_key:
+            raise ValidationError('eFaktura API Key is missing')
+        headers = {
+            'content-type': 'application/xml',
+            'ApiKey': self.company_id.l10n_rs_edi_api_key,
+        }
+        try:
+            response = requests.post(EFAKTURA_URL, params=params, headers=headers, data=xml)
+        except Exception as e:
+            self.l10n_rs_edi_state = 'sending_failed'
+            self.l10n_rs_edi_error = e
+            self.message_post(body=e)
+        if not response.ok:
+            self.l10n_rs_edi_state = 'sending_failed'
+            raise ValidationError(response.content)
+        dict_response = json.loads(response.content.decode('utf-8'))
+
+    def _l10n_rs_edi_get_attachment_values(self, xml):
+        return {
+            'name': self._l10n_rs_edi_get_xml_attachment_name(),
+            'mimetype': 'application/xml',
+            'description': _('RS EDI e-move: %s', self.move_type),
+            'company_id': self.company_id.id,
+            'res_id': self.id,
+            'res_model': self._name,
+            'res_field': 'l10n_rs_edi_attachment_file',
+            'raw': xml,
+        }
+
+    def _l10n_rs_edi_get_xml_attachment_name(self):
+        return f"{self.name.replace('/', '_')}_edi.xml"
+
+    @api.model_create_multi
+    def create(self, vals):
+        records = super().create(vals)
+        for record in records:
+            if record.l10n_rs_edi_is_eligible:
+                record.l10n_rs_edi_uuid = uuid.uuid4()
+        return records