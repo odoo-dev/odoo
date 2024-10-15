11dc7e66a68e904d91963b25d2dbc8e4aa1a3ebf
Chong Wang (cwg) <cwg@odoo.com>
2024-10-15 21:31:30 +0200

    [REF] node graph

--- addons/graphview/models/graphview_graph.py
+++ addons/graphview/models/graphview_graph.py
@@ -1,76 +1,98 @@
 from collections import defaultdict
 
 from odoo import models, fields, api
-from odoo.tools import lazy_property
+from odoo.tools import lazy_property, OrderedSet
 
 from typing import Set, Dict, Tuple, Iterable, Iterator
 
-
-class lazy_recursive_property(lazy_property):
+class Node:
     """
-    a non-thread-safe lazy recursive property
+    Base class for a node in a graph
     """
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        _visited = f'_{self.fget.__name__}_visited'
-        if getattr(obj, _visited, False):
-            raise RecursionError()
-        setattr(obj, _visited, True)
-        value = self.fget(obj)
-        delattr(obj, _visited)
-        setattr(obj, self.fget.__name__, value)
-        return value
-
-
-class Package:
-    """
-    Python package for the Odoo module with the same name
-    """
-    def __init__(self, name: str, package_graph: 'PackageGraph') -> None:
-        # manifest data
+    def __init__(self, name: str) -> None:
         self.name = name
+        self.edges: Set[Node] = set()
 
-        # dependency
-        self.depends: Set[Package] = set()
-        self.package_graph = package_graph
-
-    @lazy_recursive_property
-    def depth(self) -> int:
-        """ Return the longest distance from self to module 'base' along dependencies. """
-        return max(package.depth for package in self.depends) + 1 if self.depends else 0
-
-    @property
-    def loading_sort_key(self) -> Tuple[int, str]:
-        return self.depth, self.name
+    def add_edge(self, node: 'Node') -> None:
+        self.edges.add(node)
 
 
-class PackageGraph:
+class Graph:
     """
-    Sorted Python packages for Odoo modules with the same names ordered by (package.phase, package.depth, package.name)
+    Base class for a graph
     """
+    def __init__(self) -> None:
+        self._nodes: Dict[str, Node] = {}
 
-    def __init__(self, dependencies) -> None:
-        self._packages: Dict[str, Package] = {}
-        self.dependencies = dependencies
-        self._sort_key = lambda package: package.loading_sort_key
+    def add_node(self, name: str) -> Node:
+        if name not in self._nodes:
+            self._nodes[name] = Node(name)
+        return self._nodes[name]
 
-        for name in dependencies:
-            self._packages[name] = Package(name, self)
+    def add_edge(self, source: str, target: str) -> None:
+        source_node = self.add_node(source)
+        target_node = self.add_node(target)
+        source_node.add_edge(target_node)
 
-        for name in dependencies:
-            package = self._packages[name]
-            package.depends = set(self._packages[dep] for dep in self.dependencies[package.name])
+    def __getitem__(self, name: str) -> Node:
+        return self._nodes[name]
 
-    def __getitem__(self, name: str) -> Package:
-        return self._packages[name]
-
-    def __iter__(self) -> Iterator[Package]:
-        return iter(sorted(self._packages.values(), key=self._sort_key))
+    def __iter__(self) -> Iterator[Node]:
+        return iter(self._nodes.values())
 
     def __len__(self) -> int:
-        return len(self._packages)
+        return len(self._nodes)
+
+
+class DAGNode(Node):
+    """
+    Represents a node in a Directed Acyclic Graph (DAG)
+    """
+    @lazy_property
+    def depth(self) -> int:
+        """ Return the longest distance from self to a leaf node. """
+        return max(node.depth for node in self.edges) + 1 if self.edges else 0
+
+
+class DirectedAcyclicGraph(Graph):
+    """
+    Directed Acyclic Graph (DAG) implementation
+    """
+
+    def __init__(self, dependencies: Dict[str, Set[str]]) -> None:
+        super().__init__()
+
+        for name, deps in dependencies.items():
+            for dep in deps:
+                self.add_edge(name, dep)
+        self.validate()
+
+    def add_node(self, name: str) -> DAGNode:
+        if name not in self._nodes:
+            self._nodes[name] = DAGNode(name)
+        return self._nodes[name]
+
+    def validate(self):
+        """
+        Detect cycles in the graph using iterative depth-first search with StopIteration
+        """
+        visited = set()
+        
+        for start_node in self._nodes.values():
+            if start_node in visited:
+                continue
+
+            path = {start_node: iter(start_node.edges)}
+            while path:
+                node, edge_iter = next(reversed(path.items()))
+                if next_node := next(edge_iter, None):
+                    if next_node.name in path:
+                        raise ValueError(f"Cycle detected: {' -> '.join(n.name for n in path)} -> {next_node.name}")
+                    if next_node not in visited:
+                        path[next_node] = iter(next_node.edges)
+                else:
+                    visited.add(node)
+                    del path[node]
 
 
 class GraphviewGraph(models.Model):
@@ -108,21 +130,21 @@ class GraphviewGraph(models.Model):
         dependencies = {k: v for k, v in dependencies.items() if not k.startswith('test_')}
         dependencies = {k: v if v or k == 'base' else {'base'} for k, v in dependencies.items()}
 
-        g = PackageGraph(dependencies)
+        dag = DirectedAcyclicGraph(dependencies)
 
         # Create Cytoscape elements
         nodes = []
         edges = []
         depth_count = defaultdict(int)
-        for package in g:
-            depth_count[package.depth] += 1
+        for node in sorted(list(dag), key=lambda n: (n.depth, n.name)):
+            depth_count[node.depth] += 1
             nodes.append({
-                'data': {'id': package.name, 'label': package.name},
-                'position': {'x': depth_count[package.depth] * 150, 'y': package.depth * 100},
+                'data': {'id': node.name, 'label': node.name},
+                'position': {'x': depth_count[node.depth] * 150, 'y': node.depth * 100},
             })
-            for dep in package.depends:
+            for edge in node.edges:
                 edges.append({
-                    'data': {'source': package.name, 'target': dep.name}
+                    'data': {'source': node.name, 'target': edge.name}
                 })
 
         stylesheet = [
