keyboard-slayer/odoo-eval

[IMP] project,_*: prevent drag-drop, access error & hide fields for project user
_*=hr_timesheet,project_hr_expense,project_sms,sale_project

Purpose:
Enhance user experience and security by preventing access errors, and hiding
specific fields and actions for project users.

Changes:
Hide the following filter, actions and fields for project user.
 -Timesheet >100%
 -Archive All
 -Unarchive All
 -Timesheets
 -Archive
 -Unarchive
 -Delete
 -Send Email
 -Send Sms Text Message
 -set a rating email template on stages

Prevent access error for project user
 -Click on milestone
 -Create a milestone on fly
 -Create a task from calendar view
 -On open project update

Test case added for the point implemented in this task

-Hde the following actions for project user in form,kanban and list view.
 -Archive All
 -Unarchive All
 -Archive in the form view.

added test case for manager in form view to check implementation working
for manager.

task-3814575

addons/project/static/tests/project_project_form_view.test.js
@@ -1,5 +1,10 @@
 import { expect, test } from "@odoo/hoot";
-import { mountView } from "@web/../tests/web_test_helpers";
+import {
+    onRpc,
+    mountView,
+    contains,
+    toggleMenuItem,
+    toggleActionMenu, } from "@web/../tests/web_test_helpers";
 
 import { defineProjectModels } from "./project_models";
 
@@ -17,4 +22,29 @@ test("project.project (form)", async () => {
         `,
     });
     expect(".o_form_view").toHaveCount(1);
+});
+
+test("project.project (form) show archive action for project manager", async () => {
+    onRpc("has_group", () => true);
+
+    await mountView({
+        resModel: "project.project",
+        type: "form",
+        actionMenus: {},
+        arch: `
+            <form js_class="project_form">
+                <field name="active"/>
+                <field name="name"/>
+            </form>
+        `,
+    });
+
+    await toggleActionMenu();
+    expect(`.o-dropdown--menu span:contains(Archive)`).toHaveCount(1);
+    await toggleMenuItem("Archive");
+    expect(`.modal`).toHaveCount(1);
+    await contains(`.modal-footer .btn-primary`).click();
+    await toggleActionMenu();
+    expect(`.o-dropdown--menu span:contains(Unarchive)`).toHaveCount(1);
+    await toggleMenuItem("UnArchive");
 });

odoo-dev/odoo

addons/project/static/tests/project_project_form_view.test.js
@@ -1,5 +1,11 @@
 import { expect, test } from "@odoo/hoot";
-import { mountView } from "@web/../tests/web_test_helpers";
+import {
+    onRpc,
+    mountView,
+    contains,
+    toggleMenuItem,
+    toggleActionMenu,
+} from "@web/../tests/web_test_helpers";
 
 import { defineProjectModels } from "./project_models";
 
@@ -17,4 +23,29 @@ test("project.project (form)", async () => {
         `,
     });
     expect(".o_form_view").toHaveCount(1);
+});
+
+test("project.project (form) show archive action for project manager", async () => {
+    onRpc("has_group", () => true);
+
+    await mountView({
+        resModel: "project.project",
+        type: "form",
+        actionMenus: {},
+        arch: `
+            <form js_class="project_project_form">
+                <field name="active"/>
+                <field name="name"/>
+            </form>
+        `,
+    });
+
+    await toggleActionMenu();
+    expect(`.o-dropdown--menu span:contains(Archive)`).toHaveCount(1);
+    await toggleMenuItem("Archive");
+    expect(`.modal`).toHaveCount(1);
+    await contains(`.modal-footer .btn-primary`).click();
+    await toggleActionMenu();
+    expect(`.o-dropdown--menu span:contains(Unarchive)`).toHaveCount(1);
+    await toggleMenuItem("UnArchive");
 });