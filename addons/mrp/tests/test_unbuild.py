christian-rb/odoo-dev

[REF] stock: use only local data in stock tests WIP

addons/mrp/tests/test_unbuild.py
@@ -11,10 +11,8 @@ class TestUnbuild(TestMrpCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.stock_location = cls.env.ref('stock.stock_location_stock')
-        cls.env.ref('base.group_user').write({
-            'implied_ids': [(4, cls.env.ref('stock.group_production_lot').id)]
-        })
+        # TODO: remove this after fixing stock_location and stock_location
+        cls.stock_location = cls.stock_location
 
     def test_unbuild_standart(self):
         """ This test creates a MO and then creates 3 unbuild
@@ -465,22 +463,21 @@ class TestUnbuild(TestMrpCommon):
         StockQuant = self.env['stock.quant']
         ProductObj = self.env['product.product']
         # Create new QC/Unbuild location
-        warehouse = self.env.ref('stock.warehouse0')
         unbuild_location = self.env['stock.location'].create({
             'name': 'QC/Unbuild',
             'usage': 'internal',
-            'location_id': warehouse.view_location_id.id
+            'location_id': self.warehouse_1.view_location_id.id
         })
 
         # Create a product route containing a stock rule that will move product from QC/Unbuild location to stock
         self.env['stock.route'].create({
             'name': 'QC/Unbuild -> Stock',
             'warehouse_selectable': True,
-            'warehouse_ids': [(4, warehouse.id)],
+            'warehouse_ids': [(4, self.warehouse_1.id)],
             'rule_ids': [(0, 0, {
                 'name': 'Send Matrial QC/Unbuild -> Stock',
                 'action': 'push',
-                'picking_type_id': self.ref('stock.picking_type_internal'),
+                'picking_type_id': self.picking_type_int.id,
                 'location_src_id': unbuild_location.id,
                 'location_dest_id': self.stock_location.id,
             })],
@@ -580,7 +577,8 @@ class TestUnbuild(TestMrpCommon):
         - decimal accuracy of Product UoM > decimal accuracy of Units
         - unbuild a product with a decimal quantity of component
         """
-        self.env['decimal.precision'].search([('name', '=', 'Product Unit of Measure')]).digits = 4
+        with self.sudo():
+            self.env['decimal.precision'].search([('name', '=', 'Product Unit of Measure')]).digits = 4
         self.uom_unit.rounding = 0.001
 
         self.bom_1.product_qty = 3
@@ -670,9 +668,7 @@ class TestUnbuild(TestMrpCommon):
         unbuild order are applied on the stock moves
         """
         grp_multi_loc = self.env.ref('stock.group_stock_multi_locations')
-        self.env.user.write({'groups_id': [(4, grp_multi_loc.id, 0)]})
-        warehouse = self.env['stock.warehouse'].search([('company_id', '=', self.env.user.id)], limit=1)
-        prod_location = self.env['stock.location'].search([('usage', '=', 'production'), ('company_id', '=', self.env.user.id)])
+        self.env.user.sudo().sudo().write({'groups_id': [(4, grp_multi_loc.id, 0)]})
         subloc01, subloc02, = self.stock_location.child_ids[:2]
 
         mo, _, p_final, p1, p2 = self.generate_mo(qty_final=1, qty_base_1=1, qty_base_2=1)
@@ -688,7 +684,7 @@ class TestUnbuild(TestMrpCommon):
 
         # Transfer the finished product from WH/Stock to `subloc01`
         internal_form = Form(self.env['stock.picking'])
-        internal_form.picking_type_id = warehouse.int_type_id
+        internal_form.picking_type_id = self.warehouse_1.int_type_id
         internal_form.location_id = self.stock_location
         internal_form.location_dest_id = subloc01
         with internal_form.move_ids_without_package.new() as move:
@@ -706,10 +702,19 @@ class TestUnbuild(TestMrpCommon):
         unbuild_order.action_unbuild()
 
         self.assertRecordValues(unbuild_order.produce_line_ids, [
-            # pylint: disable=bad-whitespace
-            {'product_id': p_final.id,  'location_id': subloc01.id,         'location_dest_id': prod_location.id},
-            {'product_id': p2.id,       'location_id': prod_location.id,    'location_dest_id': subloc02.id},
-            {'product_id': p1.id,       'location_id': prod_location.id,    'location_dest_id': subloc02.id},
+            {
+                'product_id': p_final.id,
+                'location_id': subloc01.id,
+                'location_dest_id': self.production_location_id
+            }, {
+                'product_id': p2.id,
+                'location_id': self.production_location_id,
+                'location_dest_id': subloc02.id
+            }, {
+                'product_id': p1.id,
+                'location_id': self.production_location_id,
+                'location_dest_id': subloc02.id
+            },
         ])
 
     def test_compute_product_uom_id(self):
@@ -722,9 +727,8 @@ class TestUnbuild(TestMrpCommon):
         order = self.env['mrp.unbuild'].create({
             'product_id': self.product_4.id,
         })
-        warehouse = self.env.ref('stock.warehouse0')
-        self.assertEqual(order.location_id, warehouse.lot_stock_id)
-        self.assertEqual(order.location_dest_id, warehouse.lot_stock_id)
+        self.assertEqual(order.location_id, self.warehouse_1.lot_stock_id)
+        self.assertEqual(order.location_dest_id, self.warehouse_1.lot_stock_id)
 
     def test_use_unbuilt_sn_in_mo(self):
         """

addons/purchase_stock/tests/common.py
@@ -24,6 +24,8 @@ class PurchaseTestCommon(TestStockCommon):
     @classmethod
     def setUpClass(cls):
         super(PurchaseTestCommon, cls).setUpClass()
+        with cls.sudo():
+            cls.user.groups_id += cls.env.ref('purchase.group_purchase_user')
         cls.env.ref('stock.route_warehouse0_mto').active = True
 
         cls.route_buy = cls.warehouse_1.buy_pull_id.route_id.id
@@ -48,8 +50,8 @@ class PurchaseTestCommon(TestStockCommon):
             'route_ids': [(6, 0, [cls.route_buy, cls.route_mto])],
             'seller_ids': [(0, 0, {'partner_id': cls.partner_1.id, 'delay': 2})]})
 
-        cls.res_users_purchase_user = cls.env['res.users'].create({
-            'company_id': cls.env.ref('base.main_company').id,
+        cls.res_users_purchase_user = cls.env['res.users'].sudo().create({
+            'company_id': cls.company.id,
             'name': "Purchase User",
             'login': "pu",
             'email': "purchaseuser@yourcompany.com",

addons/purchase_stock/tests/test_old_rules.py
@@ -9,10 +9,9 @@ from odoo.addons.purchase_stock.tests.common import PurchaseTestCommon
 class TestPurchaseOldRules(PurchaseTestCommon):
 
     def create_picking_out(self, warehouse):
-        customer_location = self.env.ref('stock.stock_location_customers')
         picking_out = self.env['stock.picking'].create({
             'location_id': warehouse.out_type_id.default_location_src_id.id,
-            'location_dest_id': customer_location.id,
+            'location_dest_id': self.customer_location.id,
             'partner_id': self.customer.id,
             'group_id': self.group.id,
             'picking_type_id': warehouse.out_type_id.id,
@@ -25,7 +24,7 @@ class TestPurchaseOldRules(PurchaseTestCommon):
             'picking_id': picking_out.id,
             'group_id': self.group.id,
             'location_id': warehouse.out_type_id.default_location_src_id.id,
-            'location_dest_id': customer_location.id,
+            'location_dest_id': self.customer_location.id,
             'procure_method': 'make_to_order',
         })
         return picking_out
@@ -238,8 +237,8 @@ class TestPurchaseOldRules(PurchaseTestCommon):
         """ In order to check dates, set product's Delivery Lead Time
             and warehouse route's delay."""
 
-        company = self.env.ref('base.main_company')
-        company.write({'po_lead': 1.00})
+        with self.sudo():
+            self.company.write({'po_lead': 1.00})
 
         warehouse = self.warehouse_3_steps
         # Set delay on push rule

addons/sale_mrp/tests/test_sale_mrp_lead_time.py
@@ -4,16 +4,20 @@
 from datetime import timedelta
 
 from odoo import fields
-from odoo.addons.stock.tests.common import TestStockCommon
+from odoo.addons.mrp.tests.common import TestMrpCommon
 
 from odoo.tests import Form
 
 
-class TestSaleMrpLeadTime(TestStockCommon):
+class TestSaleMrpLeadTime(TestMrpCommon):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        with cls.sudo():
+            # Required to confirm a sale order
+            cls.user.groups_id += cls.env.ref('sales_team.group_sale_salesman')
+
         cls.env.ref('stock.route_warehouse0_mto').active = True
         # Update the product_1 with type, route, Manufacturing Lead Time and Customer Lead Time
         with Form(cls.product_1) as p1:
@@ -60,11 +64,12 @@ class TestSaleMrpLeadTime(TestStockCommon):
             and Customer Lead Time and also set company's Manufacturing Lead Time
             and Sales Safety Days."""
 
-        company = self.env.ref('base.main_company')
-
         # Update company with Manufacturing Lead Time and Sales Safety Days
-        company.write({'manufacturing_lead': 3.0,
-                       'security_lead': 3.0})
+        with self.sudo():
+            self.company.write({
+                'manufacturing_lead': 3.0,
+                'security_lead': 3.0
+            })
 
         # Create sale order of product_1
         order_form = Form(self.env['sale.order'])
@@ -82,7 +87,7 @@ class TestSaleMrpLeadTime(TestStockCommon):
 
         # Check schedule date of picking
         deadline_picking = fields.Datetime.from_string(order.date_order) + timedelta(days=self.product_1.sale_delay)
-        out_date = deadline_picking - timedelta(days=company.security_lead)
+        out_date = deadline_picking - timedelta(days=self.company.security_lead)
         self.assertAlmostEqual(
             order.picking_ids[0].scheduled_date, out_date,
             delta=timedelta(seconds=1),
@@ -95,7 +100,7 @@ class TestSaleMrpLeadTime(TestStockCommon):
         )
 
         # Check schedule date and deadline of manufacturing order
-        mo_date_start = out_date - timedelta(days=manufacturing_order.bom_id.produce_delay) - timedelta(days=company.manufacturing_lead)
+        mo_date_start = out_date - timedelta(days=manufacturing_order.bom_id.produce_delay) - timedelta(days=self.company.manufacturing_lead)
         self.assertAlmostEqual(
             fields.Datetime.from_string(manufacturing_order.date_start), mo_date_start,
             delta=timedelta(seconds=1),
@@ -168,7 +173,7 @@ class TestSaleMrpLeadTime(TestStockCommon):
         )
 
         # Check schedule date and deadline date of manufacturing order
-        mo_date_start = out_date - timedelta(days=manufacturing_order.bom_id.produce_delay) - timedelta(days=warehouse.delivery_route_id.rule_ids[0].delay) - timedelta(days=self.env.ref('base.main_company').manufacturing_lead)
+        mo_date_start = out_date - timedelta(days=manufacturing_order.bom_id.produce_delay) - timedelta(days=warehouse.delivery_route_id.rule_ids[0].delay) - timedelta(days=self.company.manufacturing_lead)
         self.assertAlmostEqual(
             fields.Datetime.from_string(manufacturing_order.date_start), mo_date_start,
             delta=timedelta(seconds=1),

odoo-dev/odoo

addons/mrp/tests/test_unbuild.py
@@ -8,13 +8,6 @@ from odoo.exceptions import UserError
 
 
 class TestUnbuild(TestMrpCommon):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.stock_location = cls.env.ref('stock.stock_location_stock')
-        cls.env.ref('base.group_user').write({
-            'implied_ids': [(4, cls.env.ref('stock.group_production_lot').id)]
-        })
 
     def test_unbuild_standart(self):
         """ This test creates a MO and then creates 3 unbuild
@@ -463,22 +456,21 @@ class TestUnbuild(TestMrpCommon):
         StockQuant = self.env['stock.quant']
         ProductObj = self.env['product.product']
         # Create new QC/Unbuild location
-        warehouse = self.env.ref('stock.warehouse0')
         unbuild_location = self.env['stock.location'].create({
             'name': 'QC/Unbuild',
             'usage': 'internal',
-            'location_id': warehouse.view_location_id.id
+            'location_id': self.warehouse_1.view_location_id.id
         })
 
         # Create a product route containing a stock rule that will move product from QC/Unbuild location to stock
         self.env['stock.route'].create({
             'name': 'QC/Unbuild -> Stock',
             'warehouse_selectable': True,
-            'warehouse_ids': [(4, warehouse.id)],
+            'warehouse_ids': [(4, self.warehouse_1.id)],
             'rule_ids': [(0, 0, {
                 'name': 'Send Matrial QC/Unbuild -> Stock',
                 'action': 'push',
-                'picking_type_id': self.ref('stock.picking_type_internal'),
+                'picking_type_id': self.picking_type_int.id,
                 'location_src_id': unbuild_location.id,
                 'location_dest_id': self.stock_location.id,
             })],
@@ -578,7 +570,8 @@ class TestUnbuild(TestMrpCommon):
         - decimal accuracy of Product UoM > decimal accuracy of Units
         - unbuild a product with a decimal quantity of component
         """
-        self.env['decimal.precision'].search([('name', '=', 'Product Unit of Measure')]).digits = 4
+        with self.admin_permissions():
+            self.env['decimal.precision'].search([('name', '=', 'Product Unit of Measure')]).digits = 4
         self.uom_unit.rounding = 0.001
 
         self.bom_1.product_qty = 3
@@ -668,9 +661,7 @@ class TestUnbuild(TestMrpCommon):
         unbuild order are applied on the stock moves
         """
         grp_multi_loc = self.env.ref('stock.group_stock_multi_locations')
-        self.env.user.write({'groups_id': [(4, grp_multi_loc.id, 0)]})
-        warehouse = self.env['stock.warehouse'].search([('company_id', '=', self.env.user.id)], limit=1)
-        prod_location = self.env['stock.location'].search([('usage', '=', 'production'), ('company_id', '=', self.env.user.id)])
+        self.env.user.sudo().sudo().write({'groups_id': [(4, grp_multi_loc.id, 0)]})
         subloc01, subloc02, = self.stock_location.child_ids[:2]
 
         mo, _, p_final, p1, p2 = self.generate_mo(qty_final=1, qty_base_1=1, qty_base_2=1)
@@ -686,7 +677,7 @@ class TestUnbuild(TestMrpCommon):
 
         # Transfer the finished product from WH/Stock to `subloc01`
         internal_form = Form(self.env['stock.picking'])
-        internal_form.picking_type_id = warehouse.int_type_id
+        internal_form.picking_type_id = self.warehouse_1.int_type_id
         internal_form.location_id = self.stock_location
         internal_form.location_dest_id = subloc01
         with internal_form.move_ids_without_package.new() as move:
@@ -704,10 +695,19 @@ class TestUnbuild(TestMrpCommon):
         unbuild_order.action_unbuild()
 
         self.assertRecordValues(unbuild_order.produce_line_ids, [
-            # pylint: disable=bad-whitespace
-            {'product_id': p_final.id,  'location_id': subloc01.id,         'location_dest_id': prod_location.id},
-            {'product_id': p2.id,       'location_id': prod_location.id,    'location_dest_id': subloc02.id},
-            {'product_id': p1.id,       'location_id': prod_location.id,    'location_dest_id': subloc02.id},
+            {
+                'product_id': p_final.id,
+                'location_id': subloc01.id,
+                'location_dest_id': self.production_location_id
+            }, {
+                'product_id': p2.id,
+                'location_id': self.production_location_id,
+                'location_dest_id': subloc02.id
+            }, {
+                'product_id': p1.id,
+                'location_id': self.production_location_id,
+                'location_dest_id': subloc02.id
+            },
         ])
 
     def test_compute_product_uom_id(self):
@@ -720,9 +720,8 @@ class TestUnbuild(TestMrpCommon):
         order = self.env['mrp.unbuild'].create({
             'product_id': self.product_4.id,
         })
-        warehouse = self.env.ref('stock.warehouse0')
-        self.assertEqual(order.location_id, warehouse.lot_stock_id)
-        self.assertEqual(order.location_dest_id, warehouse.lot_stock_id)
+        self.assertEqual(order.location_id, self.warehouse_1.lot_stock_id)
+        self.assertEqual(order.location_dest_id, self.warehouse_1.lot_stock_id)
 
     def test_use_unbuilt_sn_in_mo(self):
         """

addons/purchase_stock/tests/common.py
@@ -24,6 +24,8 @@ class PurchaseTestCommon(TestStockCommon):
     @classmethod
     def setUpClass(cls):
         super(PurchaseTestCommon, cls).setUpClass()
+        with cls.admin_permissions():
+            cls.user.groups_id += cls.env.ref('purchase.group_purchase_user')
         cls.env.ref('stock.route_warehouse0_mto').active = True
 
         cls.route_buy = cls.warehouse_1.buy_pull_id.route_id.id
@@ -48,8 +50,8 @@ class PurchaseTestCommon(TestStockCommon):
             'route_ids': [(6, 0, [cls.route_buy, cls.route_mto])],
             'seller_ids': [(0, 0, {'partner_id': cls.partner_1.id, 'delay': 2})]})
 
-        cls.res_users_purchase_user = cls.env['res.users'].create({
-            'company_id': cls.env.ref('base.main_company').id,
+        cls.res_users_purchase_user = cls.env['res.users'].sudo().create({
+            'company_id': cls.company.id,
             'name': "Purchase User",
             'login': "pu",
             'email': "purchaseuser@yourcompany.com",

addons/purchase_stock/tests/test_old_rules.py
@@ -10,10 +10,9 @@ from odoo.addons.purchase_stock.tests.common import PurchaseTestCommon
 class TestPurchaseOldRules(PurchaseTestCommon):
 
     def create_picking_out(self, warehouse):
-        customer_location = self.env.ref('stock.stock_location_customers')
         picking_out = self.env['stock.picking'].create({
             'location_id': warehouse.out_type_id.default_location_src_id.id,
-            'location_dest_id': customer_location.id,
+            'location_dest_id': self.customer_location.id,
             'partner_id': self.customer.id,
             'group_id': self.group.id,
             'picking_type_id': warehouse.out_type_id.id,
@@ -26,7 +25,7 @@ class TestPurchaseOldRules(PurchaseTestCommon):
             'picking_id': picking_out.id,
             'group_id': self.group.id,
             'location_id': warehouse.out_type_id.default_location_src_id.id,
-            'location_dest_id': customer_location.id,
+            'location_dest_id': self.customer_location.id,
             'procure_method': 'make_to_order',
         })
         return picking_out
@@ -237,8 +236,8 @@ class TestPurchaseOldRules(PurchaseTestCommon):
         """ In order to check dates, set product's Delivery Lead Time
             and warehouse route's delay."""
 
-        company = self.env.ref('base.main_company')
-        company.write({'po_lead': 1.00})
+        with self.admin_permissions():
+            self.company.write({'po_lead': 1.00})
 
         warehouse = self.warehouse_3_steps
         # Set delay on push rule

addons/sale_mrp/tests/test_sale_mrp_lead_time.py
@@ -4,16 +4,20 @@
 from datetime import timedelta
 
 from odoo import fields
-from odoo.addons.stock.tests.common import TestStockCommon
+from odoo.addons.mrp.tests.common import TestMrpCommon
 
 from odoo.tests import Form
 
 
-class TestSaleMrpLeadTime(TestStockCommon):
+class TestSaleMrpLeadTime(TestMrpCommon):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        with cls.admin_permissions():
+            # Required to confirm a sale order
+            cls.user.groups_id += cls.env.ref('sales_team.group_sale_salesman')
+
         cls.env.ref('stock.route_warehouse0_mto').active = True
         # Update the product_1 with type, route, Manufacturing Lead Time and Customer Lead Time
         with Form(cls.product_1) as p1:
@@ -60,11 +64,12 @@ class TestSaleMrpLeadTime(TestStockCommon):
             and Customer Lead Time and also set company's Manufacturing Lead Time
             and Sales Safety Days."""
 
-        company = self.env.ref('base.main_company')
-
         # Update company with Manufacturing Lead Time and Sales Safety Days
-        company.write({'manufacturing_lead': 3.0,
-                       'security_lead': 3.0})
+        with self.admin_permissions():
+            self.company.write({
+                'manufacturing_lead': 3.0,
+                'security_lead': 3.0
+            })
 
         # Create sale order of product_1
         order_form = Form(self.env['sale.order'])
@@ -82,7 +87,7 @@ class TestSaleMrpLeadTime(TestStockCommon):
 
         # Check schedule date of picking
         deadline_picking = fields.Datetime.from_string(order.date_order) + timedelta(days=self.product_1.sale_delay)
-        out_date = deadline_picking - timedelta(days=company.security_lead)
+        out_date = deadline_picking - timedelta(days=self.company.security_lead)
         self.assertAlmostEqual(
             order.picking_ids[0].scheduled_date, out_date,
             delta=timedelta(seconds=1),
@@ -95,7 +100,7 @@ class TestSaleMrpLeadTime(TestStockCommon):
         )
 
         # Check schedule date and deadline of manufacturing order
-        mo_date_start = out_date - timedelta(days=manufacturing_order.bom_id.produce_delay) - timedelta(days=company.manufacturing_lead)
+        mo_date_start = out_date - timedelta(days=manufacturing_order.bom_id.produce_delay) - timedelta(days=self.company.manufacturing_lead)
         self.assertAlmostEqual(
             fields.Datetime.from_string(manufacturing_order.date_start), mo_date_start,
             delta=timedelta(seconds=1),
@@ -168,7 +173,7 @@ class TestSaleMrpLeadTime(TestStockCommon):
         )
 
         # Check schedule date and deadline date of manufacturing order
-        mo_date_start = out_date - timedelta(days=manufacturing_order.bom_id.produce_delay) - timedelta(days=warehouse.delivery_route_id.rule_ids[0].delay) - timedelta(days=self.env.ref('base.main_company').manufacturing_lead)
+        mo_date_start = out_date - timedelta(days=manufacturing_order.bom_id.produce_delay) - timedelta(days=warehouse.delivery_route_id.rule_ids[0].delay) - timedelta(days=self.company.manufacturing_lead)
         self.assertAlmostEqual(
             fields.Datetime.from_string(manufacturing_order.date_start), mo_date_start,
             delta=timedelta(seconds=1),