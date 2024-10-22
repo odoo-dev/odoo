keyboard-slayer/odoo-eval

[FIX] mail: public channel chatwindow name and new member joined notification
Before this Commit:

The public channel chat window displayed "New message" as the channel name for
non-joined members. When a new user joined a channel via the chat window,
no notification message was posted.

After this Commit:

The public channel chat window now displays the actual channel name.
Whenever any user joins a channel through the chat window, a notification
message is posted.

Task:3899453

addons/mail/static/src/core/common/store_service.js
@@ -186,6 +186,9 @@ export class Store extends BaseStore {
         if (ev.target.closest(".o_channel_redirect") && model && id) {
             ev.preventDefault();
             const thread = this.Thread.insert({ model, id });
+            if (!thread.type) {
+                this.env.services["mail.thread"].fetchChannel(id);
+            }
             this.env.services["mail.thread"].open(thread);
             return true;
         } else if (ev.target.closest(".o_mail_redirect") && id) {

odoo-dev/odoo

addons/mail/static/src/core/common/store_service.js
@@ -186,6 +186,12 @@ export class Store extends BaseStore {
         if (ev.target.closest(".o_channel_redirect") && model && id) {
             ev.preventDefault();
             const thread = this.Thread.insert({ model, id });
+            if (!thread.is_pinned) {
+                this.env.services["mail.thread"].fetchChannel(id).then(() => {
+                    this.env.services["mail.thread"].open(thread);
+                });
+                return true;
+            }
             this.env.services["mail.thread"].open(thread);
             return true;
         } else if (ev.target.closest(".o_mail_redirect") && id) {