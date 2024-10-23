keyboard-slayer/odoo-eval

[IMP] website_event,*: event website facelift
*=website,website_event_booth,website_event_track,website_event_exhibitor,website_event_booth_exhibitor,website_event_booth_sale,test_event_full

Before this Commit:
- User can't edit 'Social' Block under Event Registration through website editor
- Ungroup event menus
- Can't create event page that have event's submenus
- User have to manually add event's url path

After this Commit:
- Removed Location page as it's covered in '/register' page
- User can edit Social Share Block using website editor
- Changed 'Introduction' page to 'Home' page
- Changed 'Info' page to 'Practical' page
- On Event Name click user will redirect to '/register' page
- Regrouped event menus inside parent menus for ease of use
- User can create Event pages with submenus
- User don't have to enter Event's url path manually

Note- Some test cases were changed accordingly to work with this PR's flow!

Task-3661323

addons/test_event_full/static/src/js/tours/wevent_register_tour.js
@@ -158,13 +158,18 @@ var initTourSteps = function (eventName) {
 };
 
 var browseTalksSteps = [{
-    content: 'Browse Talks',
-    trigger: 'a:contains("Talks")',
+    content: 'Browse Talks Menu',
+    trigger: 'a[href*="#"]:contains("Talks")',
+    run: "click",
+}, {
+    content: 'Browse Talks Submenu',
+    trigger: 'a.dropdown-item span:contains("Talks")',
     run: "click",
 }, {
     content: 'Check we are on the talk list page',
     trigger: 'h5:contains("Book your talks")',
 }];
+
 
 var browseBackSteps = [{
     content: 'Browse Back',
@@ -176,8 +181,12 @@ var browseBackSteps = [{
 }];
 
 var browseMeetSteps = [{
-    content: 'Browse Meet',
+    content: 'Browse Community Menu',
     trigger: 'a:contains("Community")',
+    run: "click",
+}, {
+    content: 'Browse Rooms',
+    trigger: 'a:contains("Rooms")',
     run: "click",
 }, {
     content: 'Check we are on the community page',
@@ -192,9 +201,9 @@ registry.category("web_tour.tours").add('wevent_register', {
         initTourSteps('Online Reveal'),
         browseTalksSteps,
         discoverTalkSteps('What This Event Is All About', true, true),
-        browseTalksSteps,
+        browseBackSteps,
         discoverTalkSteps('Live Testimonial', false, false, false),
-        browseTalksSteps,
+        browseBackSteps,
         discoverTalkSteps('Our Last Day Together!', true, false, true),
         browseBackSteps,
         browseMeetSteps,

odoo-dev/odoo

addons/test_event_full/static/src/js/tours/wevent_register_tour.js
@@ -158,13 +158,18 @@ var initTourSteps = function (eventName) {
 };
 
 var browseTalksSteps = [{
-    content: 'Browse Talks',
-    trigger: 'a:contains("Talks")',
+    content: 'Browse Talks Menu',
+    trigger: 'a[href*="#"]:contains("Talks")',
+    run: "click",
+}, {
+    content: 'Browse Talks Submenu',
+    trigger: 'a.dropdown-item span:contains("Talks")',
     run: "click",
 }, {
     content: 'Check we are on the talk list page',
     trigger: 'h5:contains("Book your talks")',
 }];
+
 
 var browseBackSteps = [{
     content: 'Browse Back',
@@ -176,8 +181,8 @@ var browseBackSteps = [{
 }];
 
 var browseMeetSteps = [{
-    content: 'Browse Meet',
-    trigger: 'a:contains("Community")',
+    content: 'Browse Rooms Menu',
+    trigger: 'a:contains("Rooms")',
     run: "click",
 }, {
     content: 'Check we are on the community page',
@@ -191,9 +196,9 @@ registry.category("web_tour.tours").add('wevent_register', {
         initTourSteps('Online Reveal'),
         browseTalksSteps,
         discoverTalkSteps('What This Event Is All About', true, true),
-        browseTalksSteps,
+        browseBackSteps,
         discoverTalkSteps('Live Testimonial', false, false, false),
-        browseTalksSteps,
+        browseBackSteps,
         discoverTalkSteps('Our Last Day Together!', true, false, true),
         browseBackSteps,
         browseMeetSteps,