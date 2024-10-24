01fca9545dc4cb976d540c874aa11a06c04a3b34
Géry Debongnie <ged@odoo.com>
2024-10-24 12:59:19 +0200

    wip

--- addons/loyalty/static/src/js/portal/loyalty_card.js
+++ addons/loyalty/static/src/js/portal/loyalty_card.js
@@ -16,3 +16,61 @@ publicWidget.registry.PortalLoyaltyWidget = publicWidget.Widget.extend({
     },
 
 });
+
+// -----------------------------------------------------------------------------
+// Version 1
+// -----------------------------------------------------------------------------
+import { registry } from '@web/core/registry';
+import { Component } from '@odoo/owl';
+import { useService } from '@web/core/utils/hooks';
+import { rpc } from '@web/core/network/rpc';
+import { PortalLoyaltyCardDialog } from './loyalty_card_dialog/loyalty_card_dialog'
+
+class PortalLoyalty extends Component {
+    static selector = ".o_loyalty_container";
+
+    setup() {
+        this.dialogService = useService("dialog");
+        useDelegatedEvents({
+            "click .o_loyalty_card": "onClickLoyaltyCard",
+        });
+    }
+
+    async onClickLoyaltyCard(ev) {
+        const cardId = ev.currentTarget.dataset.card_id;
+        let data = await rpc(`/my/loyalty_card/${cardId}/values`);
+        this.dialogService.add(PortalLoyaltyCardDialog, data);
+    }
+}
+
+registry.category("public_components").add("loyalty.card", PortalLoyalty);
+
+
+// -----------------------------------------------------------------------------
+// Version 2
+// -----------------------------------------------------------------------------
+import { registry } from '@web/core/registry';
+import { Component } from '@odoo/owl';
+import { useService } from '@web/core/utils/hooks';
+import { rpc } from '@web/core/network/rpc';
+import { PortalLoyaltyCardDialog } from './loyalty_card_dialog/loyalty_card_dialog'
+
+class PortalLoyalty extends Component {
+    static selector = ".o_loyalty_container";
+    static template = xml`
+        <xpath expr="//div[hasclass('o_loyalty_card')]" position="attributes">
+            <attribute name="t-on-click">onClickLoyaltyCard</attribute>
+        </xpath>`;
+
+    setup() {
+        this.dialogService = useService("dialog");
+    }
+
+    async onClickLoyaltyCard(ev) {
+        const cardId = ev.currentTarget.dataset.card_id;
+        let data = await rpc(`/my/loyalty_card/${cardId}/values`);
+        this.dialogService.add(PortalLoyaltyCardDialog, data);
+    }
+}
+
+registry.category("public_components").add("loyalty.card", PortalLoyalty);
