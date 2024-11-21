06571c620b697e0530986ee3374fec0d4e912bdd
Romaric MOYEUVRE <romo@odoo.com>
2024-11-21 12:27:27 +0100

    Fix Countdown Test

--- addons/website/static/tests/interactions/snippets/countdown.test.js
+++ addons/website/static/tests/interactions/snippets/countdown.test.js
@@ -1,6 +1,7 @@
 import { expect, test } from "@odoo/hoot";
 
 import { startInteractions, setupInteractionWhiteList } from "../../core/helpers";
+import { advanceTime } from "@odoo/hoot-mock";
 
 setupInteractionWhiteList("website.countdown");
 
@@ -40,7 +41,6 @@ test("countdown interaction activate with a .s_countdown", async () => {
     expect(core.interactions.length).toBe(1);
 });
 
-
 test("countdown interaction update the canvas for seconds correctly", async () => {
     const endTime = 12345678900;
     const { core, el } = await startInteractions(`
@@ -69,53 +69,82 @@ test("countdown interaction update the canvas for seconds correctly", async () =
             </section>
         </div>
     `);
-    function delay(time) {
-        return new Promise(resolve => setTimeout(resolve, time));
-    }
 
     // time T
 
-    const canvass1 = el.querySelectorAll('canvas');
-    const canvasHours1 = canvass1[1];
-    const dataHours1 = canvasHours1.getContext('2d').getImageData(0, 0, canvasHours1.width, canvasHours1.height).data;
-    const canvasSeconds1 = canvass1[3];
-    const dataSeconds1 = canvasSeconds1.getContext('2d').getImageData(0, 0, canvasSeconds1.width, canvasSeconds1.height).data;
+    const canvas1Els = el.querySelectorAll('canvas');
+    const canvas1Hours = canvas1Els[1];
+    const data1Hours = canvas1Hours.getContext('2d').getImageData(0, 0, canvas1Hours.width, canvas1Hours.height).data;
+    const canvas1Seconds = canvas1Els[3];
+    const data1Seconds = canvas1Seconds.getContext('2d').getImageData(0, 0, canvas1Seconds.width, canvas1Seconds.height).data;
 
-    await delay(1000);
+    await advanceTime(1000);
 
     // time T + 1s
 
-    const canvass2 = el.querySelectorAll('canvas');
-    const canvasHours2 = canvass2[1];
-    const dataHours2 = canvasHours2.getContext('2d').getImageData(0, 0, canvasHours2.width, canvasHours2.height).data;
-    const canvasSeconds2 = canvass2[3];
-    const dataSeconds2 = canvasSeconds2.getContext('2d').getImageData(0, 0, canvasSeconds2.width, canvasSeconds2.height).data;
+    const canvas2Els = el.querySelectorAll('canvas');
+    const canvas2Hours = canvas2Els[1];
+    const data2Hours = canvas2Hours.getContext('2d').getImageData(0, 0, canvas2Hours.width, canvas2Hours.height).data;
+    const canvas2Seconds = canvas2Els[3];
+    const data2Seconds = canvas2Seconds.getContext('2d').getImageData(0, 0, canvas2Seconds.width, canvas2Seconds.height).data;
 
-    const datalengthHours1 = dataHours1.length;
-    const datalengthHours2 = dataHours2.length;
+    await advanceTime(1000);
 
-    const datalengthSeconds1 = dataSeconds1.length;
-    const datalengthSeconds2 = dataSeconds2.length;
+    // time T + 2s
 
-    expect(datalengthHours1).toBe(datalengthHours2)
-    expect(datalengthSeconds1).toBe(datalengthSeconds2)
+    const canvas3Els = el.querySelectorAll('canvas');
+    const canvas3Hours = canvas3Els[1];
+    const data3Hours = canvas3Hours.getContext('2d').getImageData(0, 0, canvas3Hours.width, canvas3Hours.height).data;
+    const canvas3Seconds = canvas3Els[3];
+    const data3Seconds = canvas3Seconds.getContext('2d').getImageData(0, 0, canvas3Seconds.width, canvas3Seconds.height).data;
 
-    let wasHourCanvasChanged = false;
-    for (let i = 0; i < datalengthHours1; i++) {
-        if (dataHours1[i] != dataHours2[i]) {
-            wasHourCanvasChanged = true;
+    // Compare canvases
+
+    const data1HoursLength = data1Hours.length;
+    const data2HoursLength = data2Hours.length;
+    const data3HoursLength = data3Hours.length;
+
+    const data1SecondsLength = data1Seconds.length;
+    const data2SecondsLength = data2Seconds.length;
+    const data3SecondsLength = data3Seconds.length;
+
+    expect(data1HoursLength).toBe(data2HoursLength)
+    expect(data1SecondsLength).toBe(data2SecondsLength)
+    expect(data2HoursLength).toBe(data3HoursLength)
+    expect(data2SecondsLength).toBe(data3SecondsLength)
+
+    let wasHourCanvasChanged12 = false;
+    for (let i = 0; i < data1HoursLength; i++) {
+        if (Math.abs(data1Hours[i] - data2Hours[i]) > 1) {
+            wasHourCanvasChanged12 = true;
             break;
         }
     }
 
-    let wasSecondCanvasChanged = false;
-    for (let i = 0; i < datalengthSeconds1; i++) {
-        if (dataSeconds1[i] != dataSeconds2[i]) {
-            wasSecondCanvasChanged = true;
+    let wasHourCanvasChanged23 = false;
+    for (let i = 0; i < data2HoursLength; i++) {
+        if (Math.abs(data2Hours[i] - data3Hours[i]) > 1) {
+            wasHourCanvasChanged23 = true;
             break;
         }
     }
 
-    expect(wasHourCanvasChanged).toBe(false);
-    expect(wasSecondCanvasChanged).toBe(true);
+    let wasSecondCanvasChanged12 = false;
+    for (let i = 0; i < data1SecondsLength; i++) {
+        if (data1Seconds[i] != data2Seconds[i]) {
+            wasSecondCanvasChanged12 = true;
+            break;
+        }
+    }
+
+    let wasSecondCanvasChanged23 = false;
+    for (let i = 0; i < data2SecondsLength; i++) {
+        if (data2Seconds[i] != data3Seconds[i]) {
+            wasSecondCanvasChanged23 = true;
+            break;
+        }
+    }
+
+    expect(wasHourCanvasChanged12 && wasHourCanvasChanged23).toBe(false);
+    expect(wasSecondCanvasChanged12 && wasSecondCanvasChanged23).toBe(true);
 });
