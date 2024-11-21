a7b06787c81bfb5f5963b52d6d5d0af096aaff38
Romaric MOYEUVRE <romo@odoo.com>
2024-11-21 16:05:42 +0100

    TextHightlight

---
+++ addons/website/static/tests/interactions/text_highlight.test.js
@@ -0,0 +1,13 @@
+import { expect, test } from "@odoo/hoot";
+
+import {
+    startInteractions,
+    setupInteractionWhiteList,
+} from "../core/helpers";
+
+setupInteractionWhiteList("website.text_highlight");
+
+test("text_highlight activate when there is a #wrapwrap", async () => {
+    const { core } = await startInteractions(``);
+    expect(core.interactions.length).toBe(1);
+});
