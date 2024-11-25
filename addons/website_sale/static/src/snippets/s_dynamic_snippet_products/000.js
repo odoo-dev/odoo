d555b4fed178cedec588592e1f1146cbecd937e6
Benoit Socias <bso@odoo.com>
2024-11-25 10:13:59 +0100

    website_sale: main product snippet in 000

--- addons/website_sale/static/src/snippets/s_dynamic_snippet_products/000.js
+++ addons/website_sale/static/src/snippets/s_dynamic_snippet_products/000.js
@@ -1,89 +1,103 @@
 import { registry } from "@web/core/registry";
 import { DynamicSnippetCarousel } from "@website/snippets/s_dynamic_snippet_carousel/000";
-import publicWidget from "@web/legacy/js/public/public_widget";
-import { rpc } from "@web/core/network/rpc";
-import wSaleUtils from "@website_sale/js/website_sale_utils";
-import { WebsiteSale } from "../../js/website_sale";
 
-const DynamicSnippetProductsCard = WebsiteSale.extend({
-    selector: '.o_carousel_product_card',
-    read_events: {
-        'click .js_add_cart': '_onClickAddToCart',
-        'click .js_remove': '_onRemoveFromRecentlyViewed',
-    },
-
-    init(root, options) {
-        const parent = options.parent || root;
-        this._super(parent, options);
-    },
-
-    start() {
-        this.add2cartRerender = this.el.dataset.add2cartRerender === 'True';
-    },
-
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
+export class DynamicSnippetProducts extends DynamicSnippetCarousel {
+    static selector = ".s_dynamic_snippet_products";
 
     /**
-     * Event triggered by a click on the Add to cart button
-     *
-     * @param {OdooEvent} ev
+     * Gets the category search domain
      */
-    async _onClickAddToCart(ev) {
-        const button = ev.currentTarget
-        if (!button.dataset.productSelected || button.dataset.isCombo) {
-            const dummy_form = document.createElement('form');
-            dummy_form.setAttribute('method', 'post');
-            dummy_form.setAttribute('action', '/shop/cart/update');
-
-            const inputPT = document.createElement('input');
-            inputPT.setAttribute('name', 'product_template_id');
-            inputPT.setAttribute('type', 'hidden');
-            inputPT.setAttribute('value', button.dataset.productTemplateId);
-            dummy_form.appendChild(inputPT);
-
-            const inputPP = document.createElement('input');
-            inputPP.setAttribute('name', 'product_id');
-            inputPP.setAttribute('type', 'hidden');
-            inputPP.setAttribute('value', button.dataset.productId);
-            dummy_form.appendChild(inputPP);
-
-            return this._handleAdd($(dummy_form));  // existing logic expects jquery form
+    getCategorySearchDomain() {
+        const searchDomain = [];
+        let productCategoryId = this.el.dataset.productCategoryId;
+        if (productCategoryId && productCategoryId !== 'all') {
+            if (productCategoryId === 'current') {
+                productCategoryId = undefined;
+                const productCategoryFieldEl = this.el.closest("body").querySelector("#product_details .product_category_id");
+                if (productCategoryFieldEl) {
+                    productCategoryId = parseInt(productCategoryFieldEl.value);
+                }
+                if (!productCategoryId) {
+                    const mainObject = this.services.website_page.mainObject;
+                    if (mainObject.model === "product.public.category") {
+                        productCategoryId = mainObject.id;
+                    }
+                }
+                if (!productCategoryId) {
+                    // Try with categories from product, unfortunately the category hierarchy is not matched with this approach
+                    const productTemplateIdEl = this.el.closest("body").querySelector("#product_details .product_category_id");
+                    if (productTemplateIdEl) {
+                        searchDomain.push(['public_categ_ids.product_tmpl_ids', '=', parseInt(productTemplateIdEl.value)]);
+                    }
+                }
+            }
+            if (productCategoryId) {
+                searchDomain.push(['public_categ_ids', 'child_of', parseInt(productCategoryId)]);
+            }
         }
-        else {
-            const data = await rpc("/shop/cart/update_json", {
-                product_id: parseInt(ev.currentTarget.dataset.productId),
-                add_qty: 1,
-                display: false,
-            });
-            wSaleUtils.updateCartNavBar(data);
-            wSaleUtils.showCartNotification(this.call.bind(this), data.notification_info);
-        }
-        if (this.add2cartRerender) {
-            this.trigger_up('widgets_start_request', {
-                $target: this.$el.closest('.s_dynamic'),
-            });
-        }
-    },
+        return searchDomain;
+    }
     /**
-     * Event triggered by a click on the remove button on a "recently viewed"
-     * template.
-     *
-     * @param {OdooEvent} ev
+     * Gets the tag search domain
      */
-    async _onRemoveFromRecentlyViewed(ev) {
-        const rpcParams = {}
-        if (ev.currentTarget.dataset.productSelected) {
-            rpcParams.product_id = ev.currentTarget.dataset.productId;
-        } else {
-            rpcParams.product_template_id = ev.currentTarget.dataset.productTemplateId;
+    getTagSearchDomain() {
+        const searchDomain = [];
+        let productTagIds = this.el.dataset.productTagIds;
+        productTagIds = productTagIds ? JSON.parse(productTagIds) : [];
+        if (productTagIds.length) {
+            searchDomain.push(['all_product_tag_ids', 'in', productTagIds.map(productTag => productTag.id)]);
         }
-        await rpc("/shop/products/recently_viewed_delete", rpcParams);
-        this.trigger_up('widgets_start_request', {
-            $target: this.$el.closest('.s_dynamic'),
+        return searchDomain;
+    }
+    /**
+     * Method to be overridden in child components in order to provide a search
+     * domain if needed.
+     * @override
+     */
+    getSearchDomain() {
+        const searchDomain = super.getSearchDomain(...arguments);
+        searchDomain.push(...this.getCategorySearchDomain());
+        searchDomain.push(...this.getTagSearchDomain());
+        const productNames = this.el.dataset.productNames;
+        if (productNames) {
+            const nameDomain = [];
+            for (const productName of productNames.split(',')) {
+                // Ignore empty names
+                if (!productName.length) {
+                    continue;
+                }
+                // Search on name, internal reference and barcode.
+                if (nameDomain.length) {
+                    nameDomain.unshift('|');
+                }
+                nameDomain.push(...[
+                    '|', '|', ['name', 'ilike', productName],
+                              ['default_code', '=', productName],
+                              ['barcode', '=', productName],
+                ]);
+            }
+            searchDomain.push(...nameDomain);
+        }
+        if (!this.el.dataset.showVariants) {
+            searchDomain.push('hide_variants');
+        }
+        return searchDomain;
+    }
+    /**
+     * @override
+     */
+    getRpcParameters() {
+        const productTemplateIdEl = this.el.closest("body").querySelector("#product_details .product_category_id");
+        return Object.assign(super.getRpcParameters(...arguments), {
+            productTemplateId: productTemplateIdEl ? productTemplateIdEl.value : undefined,
         });
-    },
-});
+    }
+    /**
+     * @override
+     */
+    getMainPageUrl() {
+        return "/shop";
+    }
+}
 
-publicWidget.registry.dynamic_snippet_products_cta = DynamicSnippetProductsCard;
+registry.category("website.active_elements").add("website_sale.dynamic_snippet_products", DynamicSnippetProducts);
