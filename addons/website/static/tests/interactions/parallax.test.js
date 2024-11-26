d90008f1be4228c4e30ffbea126c9baa279be9f8
Romaric MOYEUVRE <romo@odoo.com>
2024-11-26 09:41:50 +0100

    Set parallax test size to "px" instead of "vh"

--- addons/website/static/tests/interactions/parallax.test.js
+++ addons/website/static/tests/interactions/parallax.test.js
@@ -32,8 +32,8 @@ test("parallax activate when there is a .parallax", async () => {
 
 test.tags("desktop")("s_parallax_bg move up with positive speed", async () => {
     const { core, el } = await startInteractions(`
-        <section style="height: 110vh; background-color: blue;"></section>
-        <section class="parallax" data-scroll-background-ratio="2" style="min-height: 20vh; z-index: -1000;">
+        <section style="height: 1000px; background-color: blue;"></section>
+        <section class="parallax" data-scroll-background-ratio="2" style="min-height: 200px; z-index: -1000;">
             <div class="s_parallax_bg" style="background-color: green; height: 600px; overflow: hidden;">
                 <div style="height: 200px;">
                     <h1>TOP</h1>
@@ -46,14 +46,14 @@ test.tags("desktop")("s_parallax_bg move up with positive speed", async () => {
                 </div>
             </div>
         </section>
-        <section style="height: 100vh; background-color: red;"></section>
+        <section style="height: 1000px; background-color: red;"></section>
     `);
     el.style.overflow = "scroll";
     const parallaxBg = el.querySelector(".s_parallax_bg");
     const sectionTop = el.querySelector("section")
     await customScroll(el, { y: 0 });
     const spacing1 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
-    await customScroll(el, { y: 1000 });
+    await customScroll(el, { y: 1100 });
     const spacing2 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
     await customScroll(el, { y: 1500 });
     const spacing3 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
@@ -64,8 +64,8 @@ test.tags("desktop")("s_parallax_bg move up with positive speed", async () => {
 
 test.tags("desktop")("s_parallax_bg move down with negative speed", async () => {
     const { core, el } = await startInteractions(`
-        <section style="height: 110vh; background-color: blue;"></section>
-        <section class="parallax" data-scroll-background-ratio="-2" style="min-height: 20vh; z-index: -1000;">
+        <section style="height: 1000px; background-color: blue;"></section>
+        <section class="parallax" data-scroll-background-ratio="-2" style="min-height: 200px; z-index: -1000;">
             <div class="s_parallax_bg" style="background-color: green; height: 600px; overflow: hidden;">
                 <div style="height: 200px;">
                     <h1>TOP</h1>
@@ -78,14 +78,14 @@ test.tags("desktop")("s_parallax_bg move down with negative speed", async () => 
                 </div>
             </div>
         </section>
-        <section style="height: 100vh; background-color: red;"></section>
+        <section style="height: 1000px; background-color: red;"></section>
     `);
     el.style.overflow = "scroll";
     const parallaxBg = el.querySelector(".s_parallax_bg");
     const sectionTop = el.querySelector("section")
     await customScroll(el, { y: 0 });
     const spacing1 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
-    await customScroll(el, { y: 1000 });
+    await customScroll(el, { y: 1100 });
     const spacing2 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
     await customScroll(el, { y: 1500 });
     const spacing3 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
@@ -95,8 +95,8 @@ test.tags("desktop")("s_parallax_bg move down with negative speed", async () => 
 
 test.tags("desktop")("s_parallax_bg doesn't move with no speed", async () => {
     const { core, el } = await startInteractions(`
-        <section style="height: 110vh; background-color: blue;"></section>
-        <section class="parallax" data-scroll-background-ratio="0" style="min-height: 20vh; z-index: -1000;">
+        <section style="height: 1000px; background-color: blue;"></section>
+        <section class="parallax" data-scroll-background-ratio="0" style="min-height: 200px; z-index: -1000;">
             <div class="s_parallax_bg" style="background-color: green; height: 600px; overflow: hidden;">
                 <div style="height: 200px;">
                     <h1>TOP</h1>
@@ -109,14 +109,14 @@ test.tags("desktop")("s_parallax_bg doesn't move with no speed", async () => {
                 </div>
             </div>
         </section>
-        <section style="height: 100vh; background-color: red;"></section>
+        <section style="height: 1000px; background-color: red;"></section>
     `);
     el.style.overflow = "scroll";
     const parallaxBg = el.querySelector(".s_parallax_bg");
     const sectionTop = el.querySelector("section")
     await customScroll(el, { y: 0 });
     const spacing1 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
-    await customScroll(el, { y: 1000 });
+    await customScroll(el, { y: 1100 });
     const spacing2 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
     await customScroll(el, { y: 1500 });
     const spacing3 = parallaxBg.getBoundingClientRect().bottom - sectionTop.getBoundingClientRect().bottom;
