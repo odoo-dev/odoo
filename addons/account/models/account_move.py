907dedef4302374c154b71824353e8b56b26bae3
Vivek Pathak <vivp@odoo.com>
2024-12-09 16:14:01 +0530

    [IMP] account: tax details JSON POC-3 vivp

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -3271,7 +3271,7 @@ class AccountMove(models.Model):
                             'base_amount': tax['base'],
                             'tax_amount': tax_amount,
                             'tax_id': tax['id'],
-                            'base_tag_ids': list(map(str, tax['tag_ids'])),
+                            'base_tag_ids': list(map(int, tax['tag_ids'])),
                             'tax_repartition_line_id': tax['tax_repartition_line_id'],
                         }
                         group_by_repartition_key[tax['tax_repartition_line_id']] -= tax_amount
---
+++ addons/account/static/src/components/account_tax_info/account_tax_info.js
@@ -0,0 +1,93 @@
+import { onWillStart, useState } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+import { useService } from "@web/core/utils/hooks";
+import { usePopover } from "@web/core/popover/popover_hook";
+import { AccountTaxPopup } from "./account_tax_info_popup";
+import { Many2ManyTaxTagsField } from "@account/components/many2x_tax_tags/many2x_tax_tags";
+
+export class AccountTaxInfo extends Many2ManyTaxTagsField {
+    static template = "account.AccountTaxInfo";
+    static props = {
+        ...Many2ManyTaxTagsField.props,
+    };
+
+    setup() {
+        super.setup();
+        this.accountTaxPopup = usePopover(AccountTaxPopup);
+        this.orm = useService("orm");
+        this.state = useState({
+            allTaxes: [],
+        });
+        onWillStart(async () => {
+            await this.fetchAllTaxes();
+        });
+    }
+
+    async fetchAllTaxes() {
+        if (this.props.record.evalContext.id) {
+            const result = await this.orm.read(
+                "account.move.line",
+                [this.props.record.evalContext.id],
+                ["tax_ids_json"]
+            );
+            if (result && result.length) {
+                const taxInfo = result[0].tax_ids_json;
+                if (taxInfo) {
+                    const allTaxes = await this.processTaxData(taxInfo);
+                    this.state.allTaxes = allTaxes;
+                }
+            }
+        }
+    }
+
+    async processTaxData(taxInfo) {
+        if (Object.values(taxInfo)[0].tax_tag_ids) {
+            const tagIds = [...new Set(Object.values(taxInfo).flatMap((tax) => tax.tax_tag_ids))];
+
+            const taxTagDetails = await this.orm.read("account.account.tag", tagIds, ["name"]);
+            const taxTagMap = Object.fromEntries(taxTagDetails.map((tag) => [tag.id, tag.name]));
+            return Object.values(taxInfo).map((tax) => ({
+                ...tax,
+                tag_names: tax.tax_tag_ids.map((tagId) => taxTagMap[tagId] || ""),
+            }));
+        }
+        const taxIds = Object.values(taxInfo).map((tax) => tax.tax_id);
+        const tagIds = [
+            ...new Set(
+                Object.values(taxInfo).flatMap((tax) =>
+                    tax.base_tag_ids.map((tagId) => parseInt(tagId))
+                )
+            ),
+        ];
+
+        const taxDetails = await this.orm.read("account.tax", taxIds, ["name"]);
+        const tagDetails = await this.orm.read("account.account.tag", tagIds, ["name"]);
+
+        const taxMap = Object.fromEntries(taxDetails.map((t) => [t.id, t.name]));
+        const tagMap = Object.fromEntries(tagDetails.map((tag) => [tag.id, tag.name]));
+
+        return Object.values(taxInfo).map((tax) => ({
+            ...tax,
+            name: taxMap[tax.tax_id] || "",
+            tag_names: tax.base_tag_ids.map((tagId) => tagMap[tagId] || ""),
+        }));
+    }
+
+    getAccountTaxProps() {
+        return {
+            id: this.props.record.evalContext.id,
+            allTaxes: this.state.allTaxes,
+        };
+    }
+
+    openTaxPopupComponent(ev) {
+        const target = ev.currentTarget;
+        this.accountTaxPopup.open(target, this.getAccountTaxProps());
+    }
+}
+
+export const accountTaxInfo = {
+    component: AccountTaxInfo,
+};
+
+registry.category("fields").add("tax_ids_info", accountTaxInfo);
