62c4ed4c727960adf1810295c99e54fbde71700c
Romaric MOYEUVRE <romo@odoo.com>
2024-12-23 15:21:41 +0100

    PurchaseDatetimePicker

--- addons/purchase/__manifest__.py
+++ addons/purchase/__manifest__.py
@@ -50,7 +50,7 @@
             'purchase/static/src/**/*.xml',
         ],
         'web.assets_frontend': [
-            'purchase/static/src/js/purchase_datetimepicker.js',
+            'purchase/static/src/interactions/*',
             'purchase/static/src/js/purchase_portal_sidebar.js',
         ],
     },
