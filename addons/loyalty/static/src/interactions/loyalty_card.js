9e2b63cd825a215f9eb45384d19e985cfe33e634
Romaric MOYEUVRE <romo@odoo.com>
2024-12-23 13:59:51 +0100

    LoyaltyCard

---
+++ addons/loyalty/static/src/interactions/loyalty_card.js
@@ -0,0 +1,21 @@
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
+
+import { rpc } from '@web/core/network/rpc';
+import { PortalLoyaltyCardDialog } from '../js/portal/loyalty_card_dialog/loyalty_card_dialog';
+
+export class LoyaltyCard extends Interaction {
+    static selector = ".o_loyalty_container";
+    dynamicContent = {
+        ".o_loyalty_card": { "t-on-click": (ev) => this.onClickLoyaltyCard(ev.currentTarget.dataset.card_id) },
+    };
+
+    async onClickLoyaltyCard(cardId) {
+        const data = await this.waitFor(rpc(`/my/loyalty_card/${cardId}/values`));
+        this.services.dialog.add(PortalLoyaltyCardDialog, data);
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("loyalty.loyalty_card", LoyaltyCard);
