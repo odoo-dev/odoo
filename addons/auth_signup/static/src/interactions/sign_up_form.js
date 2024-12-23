bd339cf6b82d5d1a5786c1b3a7f0c0b75b363c32
Romaric MOYEUVRE <romo@odoo.com>
2024-12-23 14:09:32 +0100

    SignUpForm

---
+++ addons/auth_signup/static/src/interactions/sign_up_form.js
@@ -0,0 +1,28 @@
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
+
+export class SignUpForm extends Interaction {
+    static selector = ".oe_signup_form";
+    dynamicContent = {
+        _root: { "t-on-submit": this.onSubmit },
+        ".oe_login_buttons > button[type='submit']": { "t-att-disabled": this.submitElStatus },
+    };
+
+    setup() {
+        this.submitElStatus = null;
+    }
+
+    onSubmit() {
+        const submitEl = document.querySelector(".oe_login_buttons > button[type='submit']");
+        if (!this.submitElStatus) {
+            this.submitElStatus = "disabled";
+            const refreshEl = document.createElement("i");
+            refreshEl.classList.add("fa fa-refresh fa-spin");
+            this.insert(refreshEl, submitEl, "beforebegin");
+        }
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("auth_signup.sign_up_form", SignUpForm);
