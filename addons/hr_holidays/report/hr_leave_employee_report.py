afa086fdbdd2f8912c3fc9dd5edf5641aecee172
akha <akha@odoo.com>
2024-12-24 09:53:51 +0100

    Simplify the sql query given that the timezone is only adjusted in the frontend. Also, remove .sudo() as it is not needed and update the dependencies in @api.depends.

--- addons/hr_holidays/report/hr_leave_employee_report.py
+++ addons/hr_holidays/report/hr_leave_employee_report.py
@@ -20,25 +20,17 @@ class HrLeaveEmployeeReport(models.Model):
 
     def init(self):
         # 1. Assume a leave request that spans multiple months, for example, From 15/10 to 13/12. This query will return 3
-        # records instead of 1. The records will have the following boundaries: date_from 15/10, date_to 31/10,
-        # date_from 1/11 date_to 30/11 and date_from 1/12, date_to 13/12.
+        #    records instead of 1. The records will have the following boundaries: date_from 15/10, date_to 31/10,
+        #    date_from 1/11 date_to 30/11 and date_from 1/12, date_to 13/12.
         # 2. The number of days for each record will be computed using _compute_number_of_days.
         # 3. To get the end of a month with HH:mm:ss set as 23:59:59 => month_start + INTERVAL '1 month' - INTERVAL '1 second'.
-        #    However, Odoo the timestamps in the database is UTC. So, the timestamps are adjusted according to the timezone
-        #    of the current user. For example, if the current user has EUROPE/BRUSSELS timezone, 23:59:59 becomes 00:59:59 which
-        #    isn't correct. So, (`AT TIME ZONE {self.env.user.tz}` AT TIME ZONE 'UTC'):: timestamp is used to correct this.
-        #    `AT TIME ZONE {self.env.user.tz}` ##sets## the timezone to that of the current user. ` AT TIME ZONE 'UTC'` will
-        #    ##convert## the timezone to `UTC` so that when Odoo retrieves the timestamps and adjusts the timezone,
-        #    the returned value is as expected. Finally, ::timestamp is used to remove timezone information as required by
-        #    the ORM framework that the timestamps are naive.
         tools.drop_view_if_exists(self._cr, 'hr_leave_employee_report')
         self._cr.execute(f"""
             CREATE or REPLACE view hr_leave_employee_report as (
                 SELECT
                     id, leave_id, employee_id,
-                    CASE WHEN date_from > month THEN date_from ELSE (month AT TIME ZONE '{self.env.user.tz}' AT TIME ZONE 'UTC')::timestamp END AS month_aligned_date_from,
-                    CASE WHEN date_to < (month + INTERVAL '1 month' - INTERVAL '1 second') THEN date_to
-                    ELSE ((month + INTERVAL '1 month' - INTERVAL '1 second') AT TIME ZONE '{self.env.user.tz}' AT TIME ZONE 'UTC')::timestamp END AS month_aligned_date_to
+                    GREATEST(date_from, month) AS month_aligned_date_from,
+                    LEAST(date_to, (month + INTERVAL '1 month' - INTERVAL '1 second')) AS month_aligned_date_to
                 FROM (
                     SELECT
                         ROW_NUMBER() OVER(ORDER BY employee_id) AS id,
@@ -55,23 +47,23 @@ class HrLeaveEmployeeReport(models.Model):
             ); 
         """)
 
-    @api.depends('month_aligned_date_from', 'month_aligned_date_to', 'leave_id', 'leave_id.resource_calendar_id')
+    @api.depends('month_aligned_date_from', 'month_aligned_date_to', 'leave_id.resource_calendar_id')
     def _compute_working_schedule_aligned_dates(self):
         for leave in self:
             start_date = leave.month_aligned_date_from.replace(tzinfo=utc)
             end_date = leave.month_aligned_date_to.replace(tzinfo=utc)
-            work_intervals = leave.leave_id.sudo().resource_calendar_id._work_intervals_batch(start_date, end_date, compute_leaves=False)[False].items()
+            work_intervals = leave.leave_id.resource_calendar_id._work_intervals_batch(start_date, end_date, compute_leaves=False)[False].items()
             leave.working_schedule_aligned_date_from = work_intervals[0][0].astimezone(utc).replace(tzinfo=None)
             leave.working_schedule_aligned_date_to = work_intervals[-1][1].astimezone(utc).replace(tzinfo=None)
 
-    @api.depends('month_aligned_date_from', 'month_aligned_date_to', 'leave_id', 'leave_id.holidays_status_id')
+    @api.depends('month_aligned_date_from', 'month_aligned_date_to', 'leave_id.employee_id', 'leave_id.holiday_status_id')
     def _compute_number_of_days(self):
         for leave in self:
             virtual_leave = self.env['hr.leave'].new({
                 'date_from': leave.month_aligned_date_from,
                 'date_to': leave.month_aligned_date_to,
-                'employee_id': leave.leave_id.sudo().employee_id.id,
-                'holiday_status_id': leave.leave_id.sudo().holiday_status_id.id
+                'employee_id': leave.leave_id.employee_id.id,
+                'holiday_status_id': leave.leave_id.holiday_status_id.id
             })
             leave.number_of_days = virtual_leave._get_durations(additional_domain = [('holiday_id', '!=', leave.leave_id.id)])[virtual_leave.id][0]
 
