cb9b12f880a7cb4d4e90fc52de1ceda40a2e0a8d
Romaric MOYEUVRE <romo@odoo.com>
2024-12-27 13:34:38 +0100

    carousel (folder)

--- addons/website/static/src/interactions/carousel/carousel_bootstrap_upgrade_fix.edit.js
+++ addons/website/static/src/interactions/carousel/carousel_bootstrap_upgrade_fix.edit.js
@@ -1,14 +1,14 @@
+import { CarouselBootstrapUpgradeFix } from "@website/interactions/carousel/carousel_bootstrap_upgrade_fix";
 import { registry } from "@web/core/registry";
-import { CarouselBootstrapUpgradeFix } from "@website/interactions/carousel_bootstrap_upgrade_fix";
 
 const CarouselBootstrapUpgradeFixEdit = I => class extends I {
     // Suspend ride in edit mode.
-    carouselOptions = {ride: false, pause: true};
+    carouselOptions = { ride: false, pause: true };
 };
 
 registry
     .category("public.interactions.edit")
     .add("website.carousel_bootstrap_upgrade_fix", {
         Interaction: CarouselBootstrapUpgradeFix,
-        mixin: CarouselBootstrapUpgradeFixEdit
+        mixin: CarouselBootstrapUpgradeFixEdit,
     });
--- addons/website/static/src/interactions/carousel/carousel_slider.edit.js
+++ addons/website/static/src/interactions/carousel/carousel_slider.edit.js
@@ -1,14 +1,13 @@
+import { CarouselSlider } from "@website/interactions/carousel/carousel_slider";
 import { registry } from "@web/core/registry";
-import { CarouselSlider } from "@website/interactions/carousel_slider";
 
 const CarouselSliderEdit = I => class extends I {
-    dynamicContent = Object.assign(this.dynamicContent, {
-        _root: {
-            "t-on-content_changed": this.onContentChanged,
-        },
-    });
+    dynamicContent = {
+        ...this.dynamicContent,
+        _root: { "t-on-content_changed": this.onContentChanged },
+    };
     // Pause carousel in edit mode.
-    carouselOptions = {ride: false, pause: true};
+    carouselOptions = { ride: false, pause: true };
 
     onContentChanged() {
         this.computeMaxHeight();
@@ -19,5 +18,5 @@ registry
     .category("public.interactions.edit")
     .add("website.carousel_slider", {
         Interaction: CarouselSlider,
-        mixin: CarouselSliderEdit
+        mixin: CarouselSliderEdit,
     });
