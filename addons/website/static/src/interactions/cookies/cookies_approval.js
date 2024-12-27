05e10e05dd37b8ce0513f2bf69876db29bf538aa
Romaric MOYEUVRE <romo@odoo.com>
2024-12-27 14:05:58 +0100

    cookies (folder)

--- addons/website/static/src/interactions/cookies/cookies_approval.js
+++ addons/website/static/src/interactions/cookies/cookies_approval.js
@@ -1,6 +1,7 @@
-import { registry } from "@web/core/registry";
-import { MEDIAS_BREAKPOINTS, SIZES } from "@web/core/ui/ui_service";
 import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
+
+import { MEDIAS_BREAKPOINTS, SIZES } from "@web/core/ui/ui_service";
 
 export class CookiesApproval extends Interaction {
     static selector = "[data-need-cookies-approval]";
@@ -10,10 +11,8 @@ export class CookiesApproval extends Interaction {
     }
 
     start() {
-        if (this.iframeEl) {
-            if (!this.cookiesWarningEl) {
-                this.addOptionalCookiesWarning();
-            }
+        if (this.iframeEl && !this.getCookiesWarningEl()) {
+            this.addOptionalCookiesWarning();
         }
         this.addListener(
             document,
@@ -23,7 +22,7 @@ export class CookiesApproval extends Interaction {
         );
     }
 
-    get cookiesWarningEl() {
+    getCookiesWarningEl() {
         if (this.iframeEl.nextElementSibling?.classList.contains("o_no_optional_cookie")) {
             return this.iframeEl.nextElementSibling;
         }
@@ -36,8 +35,11 @@ export class CookiesApproval extends Interaction {
                 ? `aspect-ratio: 16/9; max-width: ${MEDIAS_BREAKPOINTS[SIZES.SM].maxWidth}px;`
                 : "",
             extraClasses: getComputedStyle(this.iframeEl.parentElement).position === "absolute"
-                ? "" : "my-3",
-        }, this.iframeEl, "afterend");
+                ? ""
+                : "my-3",
+        });
+        this.insert(optionalCookiesWarningEl, this.iframeEl, "afterend");
+        this.services["public.interactions"].startInteractions(optionalCookiesWarningEl);
     }
 
     onOptionalCookiesAccepted() {
@@ -49,4 +51,6 @@ export class CookiesApproval extends Interaction {
     }
 }
 
-registry.category("public.interactions").add("website.cookies_approval", CookiesApproval);
+registry
+    .category("public.interactions")
+    .add("website.cookies_approval", CookiesApproval);
