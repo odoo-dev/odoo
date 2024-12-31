6b971ee9ed656a1a71e7dd22d9e50bf1f938403d
Benoit Socias <bso@odoo.com>
2024-12-31 15:08:08 +0100

    Handle old trigger_up gmap_api_request

--- addons/website/static/src/snippets/s_google_map/google_map.edit.js
+++ addons/website/static/src/snippets/s_google_map/google_map.edit.js
@@ -1,25 +1,15 @@
-/* GLOBAL VARIABLE : google */
-
 import { GoogleMap } from "./google_map";
 import { registry } from "@web/core/registry";
 
 const GoogleMapEdit = I => class extends I {
-    async willStart() {
-        if (typeof google !== 'object' || typeof google.maps !== 'object') {
-            await new Promise(resolve => {
-                this.trigger_up('gmap_api_request', {
-                    editableMode: true,
-                    onSuccess: () => resolve(),
-                });
-            });
-            return;
-        }
-        this.canStart = true
+    setup() {
+        super.setup();
+        this.canSpecifyKey = true;
     }
 }
 
 registry
-    .category("website.editable_active_elements_builders")
+    .category("public.interactions.edit")
     .add("website.google_map", {
         Interaction: GoogleMap,
         mixin: GoogleMapEdit,
