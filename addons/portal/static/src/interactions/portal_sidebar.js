27d54014f35d5fa51dfec7e9977beba7a252f238
Romaric MOYEUVRE <romo@odoo.com>
2025-01-02 14:35:14 +0100

    [...]PortalSidebar + PortalPrepayment

---
+++ addons/portal/static/src/interactions/portal_sidebar.js
@@ -0,0 +1,69 @@
+import { Interaction } from "@web/public/interaction";
+
+import { _t } from "@web/core/l10n/translation";
+import { deserializeDateTime } from "@web/core/l10n/dates";
+
+const { DateTime } = luxon;
+
+export class PortalSidebar extends Interaction {
+
+    setup() {
+        this.printContent = undefined;
+    }
+
+    start() {
+        this.setDelayLabel();
+    }
+
+    /**
+     * Set the due/delay information according to the given date
+     * like : <span class="o_portal_sidebar_timeago" t-att-datetime="invoice.date_due"/>
+     */
+    setDelayLabel() {
+        const timeagoEls = this.el.querySelectorAll(".o_portal_sidebar_timeago");
+        for (const timeagoEl of timeagoEls) {
+            const dateTime = deserializeDateTime(timeagoEl.getAttribute("datetime")).startOf("day");
+            const today = DateTime.now().startOf("day");
+            const diff = dateTime.diff(today).as("days");
+            if (diff === 0) {
+                this.el.innerText = _t('Due today');
+            } else if (diff > 0) {
+                this.el.innerText = _t('Due in %s days', Math.abs(diff).toFixed());
+            } else {
+                this.el.innerText = _t('%s days overdue', Math.abs(diff).toFixed());
+            }
+        }
+    }
+
+    /**
+     * @param {string} href
+     */
+    printIframeContent(href) {
+        if (!this.printContent) {
+            const iframeEl = document.createElement("iframe");
+            iframeEl.setAttribute("id", "print_iframe_content");
+            iframeEl.setAttribute("href", href);
+            iframeEl.style.display = "none";
+            this.printContent = iframeEl;
+            this.insert(this.printContent, this.el);
+            this.addListener(this.printContent, "load", () => this.printContent.contentWindow.print());
+        } else {
+            this.printContent.contentWindow.print()
+        }
+    }
+
+    /**
+     * @param {HTMLElement} quoteHeaderEl
+     */
+    extractText(quoteHeaderEl) {
+        const rawText = [];
+        for (const el of quoteHeaderEl.children) {
+            const text = el.innerText.trim();
+            const tagName = el.tagName.toLowerCase();
+            if (text && this.authorizedTextTag.includes(tagName.toLowerCase())) {
+                rawText.push(text);
+            }
+        }
+        return rawText.join(" ");
+    }
+}
