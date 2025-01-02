1a721f2f69e1509d7a7b254dc0eb4ee3ae11f7ac
Romaric MOYEUVRE <romo@odoo.com>
2025-01-02 14:35:14 +0100

    [...]PortalSidebar + PortalPrepayment

---
+++ addons/account/static/src/interactions/account_portal_sidebar.js
@@ -0,0 +1,58 @@
+import { PortalSidebar } from "@portal/src/interaction";
+import { registry } from "@web/core/registry";
+
+import { scrollTo } from "@web/core/utils/scrolling";
+
+export class AccountPortalSidebar extends PortalSidebar {
+    static selector = ".o_portal_invoice_sidebar";
+    dynamicContent = {
+        _window: { "t-on-resize": this.updateIframeSize },
+        ".o_portal_invoice_print": { "t-on-click.prevent.withTarget": this.onClickPrint },
+    };
+
+    setup() {
+        super.setup();
+        this.invoiceHTMLEl = this.el.querySelector("iframe#invoice_html");
+    }
+
+    start() {
+        super.start();
+        const iframeDoc = this.invoiceHTMLEl.contentDocument || this.invoiceHTMLEl.contentWindow.document;
+        if (iframeDoc.readyState === 'complete') {
+            this.updateIframeSize();
+        } else {
+            this.addListener(this.invoiceHTMLEl, "load", this.updateIframeSize);
+        }
+    }
+
+    /**
+     * Called when the iframe is loaded or the window is resized on customer portal.
+     * The goal is to expand the iframe height to display the full report without scrollbar.
+     */
+    updateIframeSize() {
+        const wrapwrapEl = this.invoiceHTMLEl.contentDocument.querySelector("div#wrapwrap");
+        this.invoiceHTMLEl.style.height = 0;
+        this.invoiceHTMLEl.style.height = wrapwrapEl.scrollHeight;
+        // scroll to the right place after iframe resize
+        const isAnchor = /^#[\w-]+$/.test(window.location.hash)
+        if (!isAnchor) {
+            return;
+        }
+        const targetEl = document.querySelector(`#${window.location.hash}`);
+        if (!targetEl.length) {
+            return;
+        }
+        scrollTo(targetEl, { behavior: "instant" });
+    }
+
+    /**
+     * @param {MouseEvent} ev
+     */
+    onClickPrint(ev, currentTargetEl) {
+        this.printIframeContent(currentTargetEl.getAttribute("href"));
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("account.account_portal_sidebar", AccountPortalSidebar);
---
+++ addons/purchase/static/src/interactions/purchase_portal_sidebar.js
@@ -0,0 +1,108 @@
+import { PortalSidebar } from "@portal/src/interaction";
+import { registry } from "@web/core/registry";
+
+import { uniqueId } from "@web/core/utils/functions";
+
+export class PurchasePortalSidebar extends PortalSidebar {
+    static selector = ".o_portal_purchase_sidebar";
+
+    setup() {
+        super.setup();
+        this.authorizedTextTag = ["em", "b", "i", "u"];
+        this.spyWatched = document.querySelector("body[data-target='.navspy']");
+    }
+
+    start() {
+        super.start();
+        const spyWatcheElement = this.el.querySelector("[data-id='portal_sidebar']");
+        this.setElementId(spyWatcheElement);
+        // Nav Menu ScrollSpy
+        this.generateMenu();
+    }
+
+    /**
+     * create an unique id and added as a attribute of spyWatched element
+     *
+     * @param {string} prefix
+     * @param {HTMLElement} el
+     */
+    setElementId(prefix, el) {
+        const id = uniqueId(prefix);
+        el.setAttribute("id", id);
+        return id;
+    }
+
+    generateMenu() {
+        const lastLI = false;
+        const lastUL = null;
+        const bsSidenavEl = this.el.querySelector(".bs-sidenav");
+
+        const quoteEls = document.querySelectorAll("#quote_content [id^=quote_header_], #quote_content [id^=quote_]");
+        for (const quoteEl of quoteEls) {
+            quoteEl.removeAttribute("id");
+        };
+        this.spyWatched.removeAttribute("id");
+
+        const quoteHeaderEls = this.spyWatched.querySelectorAll("#quote_content h2, #quote_content h3");
+        for (const quoteHeaderEl of quoteHeaderEls) {
+            let id = null;
+            let text = null;
+            switch (quoteHeaderEl.tagName.toLowerCase()) {
+                case "h2":
+                    id = this.setElementId("quote_header_", quoteHeaderEl);
+                    text = this.extractText(quoteHeaderEl);
+                    if (!text) {
+                        break;
+                    }
+                    const linkEl = document.createElement("a");
+                    linkEl.classList.add("nav-link", "p-0");
+                    linkEl.href = `#${id}`;
+                    linkEl.style.maxWidth = "200px";
+                    linkEl.innerText = text;
+
+                    const liEl = document.createElement("li");
+                    liEl.classList.add("nav-item");
+
+                    liEl.appendChild(linkEl);
+                    this.insert(liEl, bsSidenavEl);
+
+                    lastLI = liEl;
+                    lastUL = false;
+                    break;
+                case "h3":
+                    id = this.setElementId("quote_", quoteHeaderEl);
+                    text = this.extractText(quoteHeaderEl);
+                    if (!text) {
+                        break;
+                    }
+                    if (lastLI) {
+                        if (!lastUL) {
+                            const ulEl = document.createElement("ul");
+                            ulEl.classList.add("nav", "flex-column");
+
+                            this.insert(ulEl, liEl);
+
+                            lastUL = ulEl;
+                        }
+                        const linkEl = document.createElement("a");
+                        linkEl.classList.add("nav-link", "p-0");
+                        linkEl.href = `#${id}`;
+                        linkEl.style.maxWidth = "200px";
+                        linkEl.innerText = text;
+
+                        const liEl = document.createElement("li");
+                        liEl.classList.add("nav-item");
+
+                        liEl.appendChild(linkEl);
+                        this.insert(liEl, lastUL);
+                    }
+                    break;
+            }
+            quoteHeaderEl.setAttribute("data-anchor", true);
+        }
+    }
+}
+
+registry
+    .category("public.interactions")
+    .add("purchase.purchase_portal_sidebar", PurchasePortalSidebar);
