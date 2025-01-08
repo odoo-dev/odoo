8e4b0675d76b15263819edae14fcb7e9596a9d44
Géry Debongnie <ged@odoo.com>
2025-01-08 15:08:43 +0100

    fixup: fix prob with website sale rebase

--- addons/website_sale/static/src/snippets/s_dynamic_snippet_products/dynamic_snippet_products.js
+++ addons/website_sale/static/src/snippets/s_dynamic_snippet_products/dynamic_snippet_products.js
@@ -1,6 +1,5 @@
-import { rpc } from "@web/core/network/rpc";
-import publicWidget from "@web/legacy/js/public/public_widget";
-import DynamicSnippetCarousel from "@website/snippets/s_dynamic_snippet_carousel/dynamic_snippet_carousel";
+import { DynamicSnippetCarousel } from "@website/snippets/s_dynamic_snippet_carousel/dynamic_snippet_carousel";
+import { registry } from "@web/core/registry";
 
 export class DynamicSnippetProducts extends DynamicSnippetCarousel {
     static selector = ".s_dynamic_snippet_products";
@@ -100,71 +99,12 @@ export class DynamicSnippetProducts extends DynamicSnippetCarousel {
     }
 }
 
-const DynamicSnippetProductsCard = publicWidget.Widget.extend({
-    selector: '.o_carousel_product_card',
-    read_events: {
-        'click .js_add_cart': '_onClickAddToCart',
-        'click .js_remove': '_onRemoveFromRecentlyViewed',
-    },
+registry
+    .category("public.interactions")
+    .add("website_sale.dynamic_snippet_products", DynamicSnippetProducts);
 
-    init(root, options) {
-        const parent = options.parent || root;
-        this._super(parent, options);
-    },
-
-    start() {
-        this.add2cartRerender = this.el.dataset.add2cartRerender === 'True';
-    },
-
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
-
-    /**
-     * Event triggered by a click on the Add to cart button
-     *
-     * @param {OdooEvent} ev
-     */
-    async _onClickAddToCart(ev) {
-        const dataset = ev.currentTarget.dataset;
-
-        const productTemplateId = parseInt(dataset.productTemplateId);
-        const productId = parseInt(dataset.productId);
-        const isCombo = dataset.productType === 'combo';
-
-        await this.call('websiteSale', 'addToCart', {
-            productTemplateId: productTemplateId,
-            productId: productId,
-            isCombo: isCombo,
-        });
-
-        if (this.add2cartRerender) {
-            this.trigger_up('widgets_start_request', {
-                $target: this.$el.closest('.s_dynamic'),
-            });
-        }
-    },
-    /**
-     * Event triggered by a click on the remove button on a "recently viewed"
-     * template.
-     *
-     * @param {OdooEvent} ev
-     */
-    async _onRemoveFromRecentlyViewed(ev) {
-        const rpcParams = {}
-        if (ev.currentTarget.dataset.productSelected) {
-            rpcParams.product_id = ev.currentTarget.dataset.productId;
-        } else {
-            rpcParams.product_template_id = ev.currentTarget.dataset.productTemplateId;
-        }
-        await rpc("/shop/products/recently_viewed_delete", rpcParams);
-        this.trigger_up('widgets_start_request', {
-            $target: this.$el.closest('.s_dynamic'),
-        });
-    },
-});
-
-publicWidget.registry.dynamic_snippet_products_cta = DynamicSnippetProductsCard;
-publicWidget.registry.dynamic_snippet_products = DynamicSnippetProducts;
-
-export default DynamicSnippetProducts;
+registry
+    .category("public.interactions.edit")
+    .add("website_sale.dynamic_snippet_products", {
+        Interaction: DynamicSnippetProducts,
+    });
