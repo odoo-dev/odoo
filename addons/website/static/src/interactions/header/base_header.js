2809c04e29b949582ff3eb32b900a8a068f5d4e5
Robin Lejeune (role) <role@odoo.com>
2025-01-17 16:53:30 +0100

    [REF] *: rewrite many public widgets as Interactions
    This commit adapts the code of many (but not all) public widgets as
    Interactions.
    
    Co-authored-by: Géry Debongnie <ged@odoo.com>
    Co-authored-by: Benoit Socias <bso@odoo.com>
    Co-authored-by: Romaric Moyeuvre <romo@odoo.com>
    Co-authored-by: Robin Lejeune <role@odoo.com>

--- addons/website/static/src/interactions/header/base_header.js
+++ addons/website/static/src/interactions/header/base_header.js
@@ -131,6 +131,9 @@ export class BaseHeader extends Interaction {
         this.adjustMainPadding();
     }
 
+    /**
+     * @param {number} [addCount=0]
+     */
     adaptToHeaderChangeLoop(addCount = 0) {
         this.adaptToHeaderChange();
         this.transitionCount = Math.max(0, this.transitionCount + addCount);
@@ -210,6 +213,9 @@ export class BaseHeader extends Interaction {
         return this.el.getBoundingClientRect().height;
     }
 
+    /**
+     * @param {boolean} useAffixed
+     */
     toggleCSSAffixed(useAffixed) {
         this.cssAffixed = useAffixed;
         this.adaptToHeaderChange();
