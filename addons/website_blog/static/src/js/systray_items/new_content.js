b64d079a13d3f2a6489f34105e180c5b84c8126e
ksbh-odoo <ksbh@odoo.com>
2025-01-21 12:48:19 +0530

    [IMP] website_blog: blog context

--- addons/website_blog/static/src/js/systray_items/new_content.js
+++ addons/website_blog/static/src/js/systray_items/new_content.js
@@ -6,7 +6,9 @@ patch(NewContentModal.prototype, {
         super.setup();
 
         const newBlogElement = this.state.newContentElements.find(element => element.moduleXmlId === 'base.module_website_blog');
-        newBlogElement.createNewContent = () => this.onAddContent('website_blog.blog_post_action_add', true);
+        const newBlogPostContext = parseInt(document.querySelector("iframe.o_iframe").contentDocument.querySelector("main #wrap.website_blog [data-oe-model='blog.blog']")?.dataset.oeId);
+        const context = newBlogPostContext ? { default_blog_id: newBlogPostContext } : undefined;
+        newBlogElement.createNewContent = () => this.onAddContent('website_blog.blog_post_action_add', true, context);
         newBlogElement.status = MODULE_STATUS.INSTALLED;
         newBlogElement.model = 'blog.post';
     },
