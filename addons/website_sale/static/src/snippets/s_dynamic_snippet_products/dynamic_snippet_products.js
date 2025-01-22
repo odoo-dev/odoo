985f81c3dbe179f84334428dadbb8f3a39c02d17
Romaric MOYEUVRE <romo@odoo.com>
2025-01-22 14:15:59 +0100

    !fixup [REF] *: rewrite many public widgets as Interactions

--- addons/website_sale/static/src/snippets/s_dynamic_snippet_products/dynamic_snippet_products.js
+++ addons/website_sale/static/src/snippets/s_dynamic_snippet_products/dynamic_snippet_products.js
@@ -85,7 +85,7 @@ export class DynamicSnippetProducts extends DynamicSnippetCarousel {
      * @override
      */
     getRpcParameters() {
-        const productTemplateIdEl = this.el.closest("body").querySelector("#product_details .product_category_id");
+        const productTemplateIdEl = document.body.querySelector("#product_details .product_category_id");
         return Object.assign(super.getRpcParameters(...arguments), {
             productTemplateId: productTemplateIdEl ? productTemplateIdEl.value : undefined,
         });
