4faf415b45b9957e1fd37321978482953fccee03
Pierre Rousseau <pro@odoo.com>
2025-01-22 10:28:49 +0100

    Revert "[IMP] spreadsheet: Remove dispatch from CoreViewPlugin"
    This reverts commit 265672b03c8ae498ba24a6e9b28b6676116b8b21.

--- addons/spreadsheet/static/src/global_filters/index.js
+++ addons/spreadsheet/static/src/global_filters/index.js
@@ -2,7 +2,6 @@ import * as spreadsheet from "@odoo/o-spreadsheet";
 
 import { GlobalFiltersUIPlugin } from "./plugins/global_filters_ui_plugin";
 import { GlobalFiltersCorePlugin } from "./plugins/global_filters_core_plugin";
-import { GlobalFiltersCoreViewPlugin } from "./plugins/global_filters_core_view_plugin";
 const { inverseCommandRegistry } = spreadsheet.registries;
 
 function identity(cmd) {
@@ -55,4 +54,4 @@ inverseCommandRegistry
         ];
     });
 
-export { GlobalFiltersCorePlugin, GlobalFiltersCoreViewPlugin, GlobalFiltersUIPlugin };
+export { GlobalFiltersCorePlugin, GlobalFiltersUIPlugin };
