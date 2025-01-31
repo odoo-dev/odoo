a254b01b654050bf70e5a242f7bbf82bc3418bbd
Vincent Larcin <vila@odoo.com>
2025-01-31 14:35:15 +0100

    fix dashboard

--- addons/esg/static/src/dashboard.js
+++ addons/esg/static/src/dashboard.js
@@ -1,11 +1,14 @@
 import { Component } from "@odoo/owl";
 import { registry } from "@web/core/registry";
 import { Layout } from "@web/search/layout";
+import { standardActionServiceProps } from "@web/webclient/actions/action_service";
 
 class EsgDashboard extends Component {
     static template = "esg.Dashboard";
     static components = { Layout };
-    static props = {}
+    static props = {
+        ...standardActionServiceProps,
+    };
 
     setup() {
         this.display = {
