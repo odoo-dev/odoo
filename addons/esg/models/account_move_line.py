d3e900b535e8f6e16b3e22dce1f6fdb5e9c8789f
Maxime de Neuville <mane@odoo.com>
2025-02-03 11:40:22 +0100

    Add _compute_emission_factor_id mechanism
    Co-authored-by: RaoufGhrissi <RaoufGhrissi@users.noreply.github.com>

--- addons/esg/models/account_move_line.py
+++ addons/esg/models/account_move_line.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from odoo import fields, models, api
 
 
@@ -15,26 +17,47 @@ class AccountMoveLine(models.Model):
 
     @api.depends('product_id', 'account_id', 'move_id.partner_id')
     def _compute_emission_factor_id(self):
-        assignation_lines_per_product = dict(self.env['esg.emission.factor.line.assignation']._read_group(
-            domain=[('product_id', 'in', [False] + self.product_id.ids)],
-            groupby=['product_id'],
-            aggregates=['id:recordset'],
-            order='product_id desc',
-        ))
+        Assignation = self.env['esg.emission.factor.line.assignation']
+        assignation_lines = Assignation.search([
+            '|',
+            '|',
+            ('product_id', 'in', self.product_id.ids),
+            ('vendor_id', 'in', self.move_id.partner_id.ids),
+            ('account_id', 'in', self.account_id.ids)
+        ])
+
+        assignation_lines_per_product, assignation_lines_per_vendor, assignation_lines_per_account = defaultdict(Assignation.browse), defaultdict(Assignation.browse), defaultdict(Assignation.browse)
+        for line in assignation_lines:
+            if line.product_id:
+                assignation_lines_per_product[line.product_id] |= line
+            if line.vendor_id:
+                assignation_lines_per_vendor[line.vendor_id] |= line
+            if line.account_id:
+                assignation_lines_per_account[line.account_id] |= line
 
         for line in self:
-            assignation_lines = assignation_lines_per_product.get(line.product_id)
-            if not assignation_lines:
-                continue # TODO: use bayes based on vendor and description (like account)
-
-            same_vendor_assignation_lines = assignation_lines.filtered(lambda l: l.vendor_id == line.move_id.partner_id)
-            if same_vendor_assignation_lines:
-                same_account_assignation_lines = same_vendor_assignation_lines.filtered(lambda l: l.account_id == line.account_id)
-                if same_account_assignation_lines:
-                    line.emission_factor_id = same_account_assignation_lines[0].emission_factor_id
+            same_product_assignation_lines = assignation_lines_per_product.get(line.product_id)
+            if same_product_assignation_lines:
+                same_vendor_assignation_lines = same_product_assignation_lines.filtered(lambda l: l.vendor_id == line.move_id.partner_id)
+                if same_vendor_assignation_lines:
+                    same_account_assignation_lines = same_vendor_assignation_lines.filtered(lambda l: l.account_id == line.account_id)
+                    if same_account_assignation_lines:
+                        line.emission_factor_id = same_account_assignation_lines[0].emission_factor_id
+                    else:
+                        line.emission_factor_id = same_vendor_assignation_lines.emission_factor_id.sorted('sequence')[0]
                 else:
-                    line.emission_factor_id = same_vendor_assignation_lines.emission_factor_id.sorted('sequence')[0]
+                    same_account_assignation_lines = same_product_assignation_lines.filtered(lambda l: l.account_id == line.account_id)
+                    if same_account_assignation_lines:
+                        line.emission_factor_id = same_account_assignation_lines[0].emission_factor_id
+                    else:
+                        line.emission_factor_id = same_product_assignation_lines.emission_factor_id.sorted('sequence')[0]
             else:
-                line.emission_factor_id = assignation_lines.emission_factor_id.sorted('sequence')[0]
-
-
+                same_vendor_assignation_lines = assignation_lines_per_vendor.get(line.product_id)
+                if same_vendor_assignation_lines:
+                    same_account_assignation_lines = same_vendor_assignation_lines.filtered(lambda l: l.account_id == line.account_id)
+                    if same_account_assignation_lines:
+                        line.emission_factor_id = same_account_assignation_lines[0].emission_factor_id
+                    else:
+                        line.emission_factor_id = same_vendor_assignation_lines.emission_factor_id.sorted('sequence')[0]
+                else:
+                    line.emission_factor_id = assignation_lines_per_account.get(line.account_id, Assignation).emission_factor_id.sorted('sequence')[:1]
