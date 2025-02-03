417bc0ed6c40919a23f4e16013de16e0ce036671
ramh <ramh@odoo.com>
2025-02-03 17:06:37 +0530

    [FIX] mail: instantly display channel created from kanban  view
    Before this commit:
    
    When a channel was created from the kanban view, it was not visible
    in the discuss app until the user refreshed the page.
    
    After this commit:
    
    The newly created channel is immediately added to the channel list.
    
    Task-4510421

--- addons/mail/models/discuss/discuss_channel.py
+++ addons/mail/models/discuss/discuss_channel.py
@@ -329,6 +329,7 @@ class DiscussChannel(models.Model):
         # pop the mail_create_bypass_create_check key to avoid leaking it outside of create)
         channels = channels.with_context(mail_create_bypass_create_check=None)
         channels._subscribe_users_automatically()
+        self.env.user._bus_send_store(channels)
 
         return channels
 
@@ -1195,7 +1196,6 @@ class DiscussChannel(models.Model):
         new_channel.group_public_id = group.id if group else None
         notification = Markup('<div class="o_mail_notification">%s</div>') % _("created this channel.")
         new_channel.message_post(body=notification, message_type="notification", subtype_xmlid="mail.mt_comment")
-        self.env.user._bus_send_store(new_channel)
         return new_channel
 
     @api.model
