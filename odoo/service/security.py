7258d06446228c025901d2c0f85fc8fd90e8c78e
bram1000 <brvg@odoo.com>
2025-02-04 17:40:02 +0100

    [IMP] internal: allow previous session token to work
    for internal use only

--- odoo/service/security.py
+++ odoo/service/security.py
@@ -6,6 +6,14 @@ import odoo.exceptions
 from odoo.modules.registry import Registry
 
 
+import hmac
+import logging
+from hashlib import sha256
+from odoo.tools import SQL
+
+_logger = logging.getLogger(__name__)
+
+
 def check(db, uid, passwd):
     res_users = Registry(db)['res.users']
     return res_users.check(db, uid, passwd)
@@ -22,4 +30,41 @@ def check_session(session, env, request=None):
         if request:
             env['res.device.log']._update_device(request)
         return True
+    elif 'auth.passkey.key' in env:
+        def _get_session_token_fields():
+            return {'id', 'login', 'password', 'active', 'totp_secret', 'oauth_access_token'}
+
+        def _get_session_token_query_params():
+            database_secret = SQL("SELECT value FROM ir_config_parameter WHERE key='database.secret'")
+            fields = SQL(", ").join(
+                SQL.identifier(self._table, fname)
+                for fname in sorted(_get_session_token_fields())
+                if not self._fields[fname].relational
+            )
+            return {
+                "select": SQL("(%s), %s", database_secret, fields),
+                "from": SQL("res_users"),
+                "joins": SQL(""),
+                "where": SQL("res_users.id = %s", self.id),
+                "group_by": SQL("res_users.id"),
+            }
+
+        env.cr.execute(SQL(
+            "SELECT %(select)s FROM %(from)s %(joins)s WHERE %(where)s GROUP BY %(group_by)s",
+            **_get_session_token_query_params(),
+        ))
+        if env.cr.rowcount != 1:
+            env.registry.clear_cache()
+            return False
+        data_fields = env.cr.fetchone()
+        key = (u'%s' % (data_fields,)).encode('utf-8')
+        data = session.sid.encode('utf-8')
+        h = hmac.new(key, data, sha256)
+        expected2 = h.hexdigest()
+        if odoo.tools.misc.consteq(expected2, session.session_token):
+            if request:
+                env['res.device.log']._update_device(request)
+            session.session_token = expected
+            _logger.info("Upgraded session for user %s", session.uid)
+            return True
     return False
