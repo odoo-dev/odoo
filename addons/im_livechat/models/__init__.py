bf2a956d41db133b708194e4fd84ca057d41ba66
Pierre-Yves Dufays <pydu@odoo.com>
2025-02-07 15:36:58 +0100

    [IMP] various: convert KPI
    We convert the kpi.
    
    parent-task-2705154
    task-2647241

--- addons/im_livechat/models/__init__.py
+++ addons/im_livechat/models/__init__.py
@@ -4,6 +4,7 @@ from . import chatbot_message
 from . import chatbot_script
 from . import chatbot_script_answer
 from . import chatbot_script_step
+from . import digest
 from . import res_users
 from . import res_groups
 from . import res_partner
@@ -15,4 +16,3 @@ from . import im_livechat_channel_member_history
 from . import mail_message
 from . import res_users_settings
 from . import rating_rating
-from . import digest
