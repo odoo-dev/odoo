708dfa78c4957a57b69a3dcf0c1a62f7c3bd3398
dhha-odoo <dhha@odoo.com>
2025-02-16 19:29:33 +0530

    [IMP] stock*: product routes improvements
    *: mrp, mrp_subcontracting, mrp_subcontracting_dropshipping, purchase_stock
    
    With this commit
    =======================
    
    - The 'Manufacture' route now has a higher sequence than the 'Buy' route.
    - The 'Buy' route is no longer selected by default for products.
    - If a product has no BOM, no MO will be created from Reordering Rules. It will
      give error for create a bom.
    - In the Replenishment Dashboard, users can now select the 'Warehouse Selectable' route.
    - The 'Buy' route cannot be selected if the product is
      not purchasable (i.e., 'Can be Purchased' must be enabled).
    - The operations section is hidden if no routes on the product.
    - No need to select routes in the following cases:
    
         - If purchase_ok is enabled and the product has a vendor,
           the 'Buy' route is automatically selected.
         - If the product has a BOM, the 'mrp' route is automatically selected.
         - If a product is a component of a subcontracting BOM and has sufficient
           available quantity, the 'Resupply Subcontractor on Order'
           route is automatically selected.
    
    - Now, the 'Dropship' route can also work for 'subcontracting BOMs',
      similar to the 'Dropship Subcontractor on Order' route.
    
    Task - [4385221](https://www.odoo.com/odoo/my-tasks/4385221)

--- addons/purchase_stock/models/stock.py
+++ addons/purchase_stock/models/stock.py
@@ -284,3 +284,10 @@ class ProcurementGroup(models.Model):
                 wh = wh_by_comp[company]
                 procurement.values['route_ids'] |= wh.reception_route_id
         return super().run(procurements, raise_user_error=raise_user_error)
+
+    def _get_product_routes(self, product):
+        route_ids = super()._get_product_routes(product)
+        buy_routes = self.env['stock.rule'].search([('action', '=', 'buy'), ('picking_type_id.code', '=', 'incoming')]).route_id
+        if not product.purchase_ok or not product.seller_ids and not any(route in product.route_ids for route in buy_routes):
+            route_ids -= buy_routes
+        return route_ids
