4ab2154018af684cac7c6981a3c960da20215924
rare-odoo <rare@odoo.com>
2025-02-19 11:37:45 +0530

    [IMP] sale: show recent sold/invoiced product for the customer

--- addons/sale/models/product_template.py
+++ addons/sale/models/product_template.py
@@ -65,6 +65,27 @@ class ProductTemplate(models.Model):
     def _compute_product_tooltip(self):
         super()._compute_product_tooltip()
 
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        """
+        Returns the product templates in a prioritized sequence for the sale order line product selection.
+        First, it lists all products that have been invoiced to the specified customer,
+        sorted from the most recent to the oldest invoice date.
+        Afterward, it includes the remaining products in their default order of display.
+        """
+        args = args or []
+        if not name and self.env.context.get('partner_id') and self.env.context.get('is_sale'):
+            product_templates_id =  [item['product_tmpl_id'] for item in self.get_prioritized_product_and_time('sale')[:limit]]
+            prioritized_product_templates = self.browse(product_templates_id)
+            remaining_product_templates = self.search(
+                [('id', 'not in', prioritized_product_templates.ids)] + args,
+                limit=limit - len(prioritized_product_templates)
+            )
+            product_templates = prioritized_product_templates + remaining_product_templates
+            return [(product_template.id, product_template.display_name) for product_template in product_templates]
+        else:
+            return super().name_search(name, args, operator, limit)
+
     def _prepare_tooltip(self):
         tooltip = super()._prepare_tooltip()
         if not self.sale_ok:
