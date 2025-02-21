5fb8d17e911208baf2ed5937c6da61af5c672caa
tsm-odoo <tsm@odoo.com>
2025-02-21 07:35:21 +0100

    [FIX] im_livechat: only allow one click on chat bot answer
    This PR fixes several issues:
    - Clicking multiple times on a chat bot answer would lead to multiple
    messages being posted which could impact the chat bot flow. Disable the
    buttons once an answer has been selected.
    
    - Messages could be shown in an incorrect order when answering the first
    chat bot question. Ensure thread messages are properly loaded before posting
    the answer.
    
    - Chat window was flickering when persisting a temporary thread
    (replacing the temporary thread with the created one). Ensure the thread
    swap occurs when the created thread is fully ready, thus matching the
    temporary thread visual and avoiding flickering.
    
    task-4589418

--- addons/im_livechat/static/tests/embed/livechat_button.test.js
+++ addons/im_livechat/static/tests/embed/livechat_button.test.js
@@ -1,4 +1,3 @@
-import { waitNotifications } from "@bus/../tests/bus_test_helpers";
 import {
     defineLivechatModels,
     loadDefaultEmbedConfig,
@@ -36,20 +35,17 @@ test("open/close temporary channel", async () => {
 test("open/close persisted channel", async () => {
     await startServer();
     await loadDefaultEmbedConfig();
-    onRpc("/im_livechat/get_session", async (req) => {
-        const { params } = await req.json();
-        if (params.persisted) {
-            asyncStep("persisted");
-        }
-    });
     const env = await start({ authenticateAs: false });
+    env.services.bus_service.subscribe("discuss.channel/new_message", () =>
+        asyncStep("discuss.channel/new_message")
+    );
     await mountWithCleanup(LivechatButton);
     await click(".o-livechat-LivechatButton");
     await insertText(".o-mail-Composer-input", "How can I help?");
     await triggerHotkey("Enter");
-    await waitForSteps(["persisted"]);
+    await contains(".o-mail-Thread:not([data-transient])");
     await contains(".o-mail-Message-content", { text: "How can I help?" });
-    await waitNotifications([env, "discuss.channel/new_message"]);
+    await waitForSteps(["discuss.channel/new_message"]);
     await click("[title*='Close Chat Window']");
     await click(".o-livechat-CloseConfirmation-leave");
     await contains(".o-mail-ChatWindow", { text: "Did we correctly answer your question?" });
