6201daf178464eaa935ea430a321c9502f99dd24
Jitendra Prajapat <jipr@odoo.com>
2025-02-28 15:12:40 +0530

    [IMP] point_of_sale: remove customer display configuration
    Before this commit:
    =====================
    Customer display configuration included three options: local, remote, and proxy,
    making the setup more complex.
    
    After this commit:
    ================
    Customer display is now always enabled by default, removing the need for
    configuration in res config settings. This simplifies the setup and ensures the
    feature is always available.
    
    Task-4604770
    
    Related Enterprise PR:https://github.com/odoo/enterprise/pull/81395
    Related Upgrade PR:https://github.com/odoo/upgrade/pull/7386

--- addons/point_of_sale/static/src/app/components/navbar/navbar.js
+++ addons/point_of_sale/static/src/app/components/navbar/navbar.js
@@ -20,7 +20,7 @@ import { OrderTabs } from "@point_of_sale/app/components/order_tabs/order_tabs";
 import { PresetSlotsPopup } from "@point_of_sale/app/components/popups/preset_slots_popup/preset_slots_popup";
 import { makeAwaitable } from "@point_of_sale/app/utils/make_awaitable_dialog";
 import { _t } from "@web/core/l10n/translation";
-import { openCustomerDisplay } from "@point_of_sale/customer_display/utils";
+import { openProxyCustomerDisplay } from "@point_of_sale/customer_display/utils";
 
 const { DateTime } = luxon;
 
@@ -110,7 +110,7 @@ export class Navbar extends Component {
         this.pos.scanning = !this.pos.scanning;
     }
     get customerFacingDisplayButtonIsShown() {
-        return this.pos.config.customer_display_type !== "none" && !isMobileOS();
+        return !isMobileOS();
     }
     get showCashMoveButton() {
         return this.pos.showCashMoveButton;
@@ -133,25 +133,22 @@ export class Navbar extends Component {
     }
 
     openCustomerDisplay() {
-        if (this.pos.config.customer_display_type === "local") {
-            window.open(
+        const proxyIP = this.pos.getDisplayDeviceIP();
+        if (proxyIP) {
+            openProxyCustomerDisplay(
+                proxyIP,
+                this.pos.config.access_token,
+                this.pos.config.id,
+                this.notification
+            );
+        } else {
+            this.pos.local_customer_display_windowRef = window.open(
                 `/pos_customer_display/${this.pos.config.id}/${this.pos.config.access_token}`,
                 "newWindow",
                 "width=800,height=600,left=200,top=200"
             );
             this.notification.add(_t("PoS Customer Display opened in a new window"));
         }
-        if (this.pos.config.customer_display_type === "remote") {
-            this.notification.add(
-                _t("Navigate to your PoS Customer Display on the other computer")
-            );
-        }
-        openCustomerDisplay(
-            this.pos.getDisplayDeviceIP(),
-            this.pos.config.access_token,
-            this.pos.config.id,
-            this.notification
-        );
     }
 
     get showCreateProductButton() {
