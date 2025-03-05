f126db51cac142eaa103ad2df01a496a1a606c21
rusp-odoo <rusp@odoo.com>
2025-03-05 10:07:37 +0530

    temp

---
+++ addons/pos_simplified_receipt/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Point of Sale - Simplified Receipt',
+    'version': '1.0',
+    'depends': ['point_of_sale'],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_simplified_receipt/static/src/**/*',
+        ],
+    },
+}
