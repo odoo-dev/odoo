2d78c8fe05758245ba336e128cfc7712e0c90b79
Rodolpho Lima <rcdl@odoo.com>
2025-03-13 11:56:04 +0100

    [FIX] html_editor: delete in uneditable zone
    Content in an uneditable zone should not be deleted (per definition).
    On the other hand, a contenteditable=false element (and its children)
    can be removed if it is fully included by the selection.
    
    Before this commit, executing one of the delete_* commands with a
    selection inside a uneditable zone would lead to the removal of the
    whole uneditable subtree.
    
    This happened because the DeletePlugin was performing a normalization
    step (via setSelection) that fully included an originally partially
    selected contenteditable=false element, making it impossible to
    differentiate between a selection inside a non-editable zone and one
    that fully includes it (selection on the outer boundaries of the
    contenteditable=false element).
    
    This commit, by avoiding the call to setSelection and normalizing it
    with the suitable transformations achieves two goals:
    - it fixes the issue above (no deletion of non editable content),
    - it avoids unnecessarily setting the selection before doing the actual
    deletion, which was previously done with the sole purpose of normalizing
    it differently from the one returned by getEditableSelection.
    
    task-4242213

--- addons/html_editor/static/tests/delete/backward.test.js
+++ addons/html_editor/static/tests/delete/backward.test.js
@@ -472,7 +472,7 @@ describe("Selection collapsed", () => {
             });
         });
 
-        test.todo("should not delete in contenteditable=false", async () => {
+        test("should not delete in contenteditable=false", async () => {
             await testEditor({
                 contentBefore: `<p contenteditable="false">ab[]cdef</p>`,
                 stepFunction: deleteBackward,
@@ -2034,7 +2034,7 @@ describe("Selection not collapsed", () => {
             });
         });
 
-        test.todo("should not delete in contenteditable=false 1", async () => {
+        test("should not delete in contenteditable=false 1", async () => {
             await testEditor({
                 contentBefore: `<p contenteditable="false">ab[cd]ef</p>`,
                 stepFunction: deleteBackward,
@@ -2042,7 +2042,7 @@ describe("Selection not collapsed", () => {
             });
         });
 
-        test.todo("should not delete in contenteditable=false 2", async () => {
+        test("should not delete in contenteditable=false 2", async () => {
             await testEditor({
                 contentBefore: `<div contenteditable="false">
                                     <p>a[b</p>
