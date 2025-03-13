89c64b526e3b062da63215d5d2719364dd9cab20
Mohammad Abdulmoneim (abdu) <abdu@odoo.com>
2025-03-13 15:15:04 +0100

    [FIX] product,*: adjust uom and price of generated purchase order
    This commit adjusts setting `product_uom_id` field on the generated
    purchase order line in the 3 main scenarios: manual purchase order,
    procurement from MTO, and procurement from replenishment wizard. The
    intended behavior is as follows:
    
    * Manual PO:
    The selected pricelist must respect the uom of the PO line. Even if the
    quantity of the line matches a pricelist with a cheaper price (but
    different uom), it should not be selected. If there is no pricelist with
    the unit of the line, the pricelist with the product base unit is
    chosen. If it doesn't exist too, no pricelist is chosen, and the price
    is left to the user (default value of 0).
    
    * Procurement from MTO:
    The selected pricelist must be the cheapest pricelist that respects the
    requested quantity, even if that cheaper pricelist has a different uom
    than the source of procurement (SO, MO, etc.). However, the generated PO
    line uses the uom of the pricelist not the source.
    
    * Replenishment Wizard:
    The behavior is similar to manual PO. The selected unit in the wizard
    must be respected, even if there is a cheaper pricelist that matches the
    requested quantity (with different uom). Also now, the selected vendor
    in the wizard doesn't affect the chosen priclist. It's only used to look
    for pricelists with the same partner.
    
    Task-4471379

--- addons/purchase_stock/tests/common.py
+++ addons/purchase_stock/tests/common.py
@@ -57,5 +57,21 @@ class PurchaseTestCommon(TestStockCommon):
             'name': "Purchase User",
             'login': "pu",
             'email': "purchaseuser@yourcompany.com",
-            'group_ids': [Command.set([cls.env.ref('purchase.group_purchase_user').id])],
+            'groups_id': [Command.set([cls.env.ref('purchase.group_purchase_user').id])],
+            })
+
+        cls.fuzzy_drink = cls.env['product.product'].create({
+            'name': 'Fuzzy Drink',
+            'is_storable': True,
+            'route_ids': [Command.set([cls.route_buy, cls.route_mto])],
+            'seller_ids': [Command.create({
+                'partner_id': cls.partner_1.id,
+                'product_uom_id': cls.env.ref('uom.product_uom_unit').id,
+                'price': 1.0,
+            }), Command.create({
+                'partner_id': cls.partner_1.id,
+                'product_uom_id': cls.env.ref('uom.product_uom_pack_6').id,
+                'price': 5.0,
+                'min_qty': 2,
+            })]
         })
