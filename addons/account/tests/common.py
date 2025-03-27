63bb7e7ea8f843a92211d206b31babf5824ddea1
William Braeckman <wbr@odoo.com>
2025-03-27 09:30:11 +0100

    [FIX] account: remove unused groups

--- addons/account/tests/common.py
+++ addons/account/tests/common.py
@@ -281,10 +281,8 @@ class AccountTestInvoicingCommon(ProductCommon):
                     'group_mass_mailing_user', # mass_mailing
                     'group_mrp_manager', # mrp
                     'group_pos_manager', # point_of_sale
-                    'group_product_manager', # product
                     'group_project_manager', # project
                     'group_purchase_manager', # purchase
-                    'group_sale_manager', # sales_team
                     'group_stock_manager', # stock
                     'group_survey_user', # survey
                     'group_website_designer', # website
