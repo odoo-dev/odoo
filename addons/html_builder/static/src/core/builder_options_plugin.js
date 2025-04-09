69da81deef14a4cadf65f6d52e8fd56eb9e0c70c
Soukéina Bojabza <sobo@odoo.com>
2025-04-09 18:08:06 +0200

    Deactivate the options when clicking on the "BLOCKS" and "THEME" tabs

--- addons/html_builder/static/src/core/builder_options_plugin.js
+++ addons/html_builder/static/src/core/builder_options_plugin.js
@@ -18,6 +18,7 @@ export class BuilderOptionsPlugin extends Plugin {
     static shared = [
         "getContainers",
         "updateContainers",
+        "deactivateContainers",
         "getPageContainers",
         "getRemoveDisabledReason",
         "getCloneDisabledReason",
@@ -113,6 +114,12 @@ export class BuilderOptionsPlugin extends Plugin {
 
         this.lastContainers = newContainers;
         this.dependencies.history.setStepExtra("optionSelection", this.target);
+        this.dispatchTo("change_current_options_containers_listeners", this.lastContainers);
+    }
+
+    deactivateContainers() {
+        this.target = null;
+        this.lastContainers = [];
         this.dispatchTo("change_current_options_containers_listeners", this.lastContainers);
     }
 
