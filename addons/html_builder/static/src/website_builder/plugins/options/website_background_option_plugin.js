7949cd089978f81661bb4e9593bc459477cd0bfe
Benoit Socias <bso@odoo.com>
2025-04-15 16:44:41 +0200

    withSequence withoutNumber

--- addons/html_builder/static/src/website_builder/plugins/options/website_background_option_plugin.js
+++ addons/html_builder/static/src/website_builder/plugins/options/website_background_option_plugin.js
@@ -9,12 +9,15 @@ import {
     ONLY_BG_IMAGE_EXLUDE,
     ONLY_BG_IMAGE_SELECTOR,
 } from "./utils";
+import { withSequence } from "@html_editor/utils/resource";
+import { SNIPPET_SPECIFIC_BEFORE } from "@html_builder/utils/option_sequence";
+import { WEBSITE_BACKGROUND_OPTIONS } from "@html_builder/website_builder/option_sequence";
 
 class WebsiteBackgroundOptionPlugin extends Plugin {
     static id = "websiteOption";
     resources = {
         builder_options: [
-            {
+            withSequence(SNIPPET_SPECIFIC_BEFORE, {
                 OptionComponent: WebsiteBackgroundOption,
                 selector: "section",
                 applyTo: ":scope > .carousel:not(.s_carousel_cards)",
@@ -25,8 +28,8 @@ class WebsiteBackgroundOptionPlugin extends Plugin {
                     withShapes: true,
                     withColorCombinations: true,
                 },
-            },
-            {
+            }),
+            withSequence(WEBSITE_BACKGROUND_OPTIONS, {
                 OptionComponent: WebsiteBackgroundOption,
                 selector: BOTH_BG_COLOR_IMAGE_SELECTOR,
                 exclude: BOTH_BG_COLOR_IMAGE_EXCLUDE,
@@ -37,8 +40,8 @@ class WebsiteBackgroundOptionPlugin extends Plugin {
                     withShapes: true,
                     withColorCombinations: true,
                 },
-            },
-            {
+            }),
+            withSequence(WEBSITE_BACKGROUND_OPTIONS, {
                 OptionComponent: WebsiteBackgroundOption,
                 selector: ONLY_BG_COLOR_SELECTOR,
                 exclude: ONLY_BG_COLOR_EXCLUDE,
@@ -47,8 +50,8 @@ class WebsiteBackgroundOptionPlugin extends Plugin {
                     withImages: false,
                     withColorCombinations: true,
                 },
-            },
-            {
+            }),
+            withSequence(WEBSITE_BACKGROUND_OPTIONS, {
                 OptionComponent: WebsiteBackgroundOption,
                 selector: ONLY_BG_IMAGE_SELECTOR,
                 exclude: ONLY_BG_IMAGE_EXLUDE,
@@ -59,7 +62,7 @@ class WebsiteBackgroundOptionPlugin extends Plugin {
                     withShapes: true,
                     withColorCombinations: false,
                 },
-            },
+            }),
         ],
     };
 }
