6468843bd8cf61ac262e458d2f45b1609af3d07a
std-odoo <std@odoo.com>
2025-04-15 14:17:51 +0200

    fixup! [IMP] web: allow reading properties with `web_read`

--- addons/web/models/models.py
+++ addons/web/models/models.py
@@ -254,11 +254,11 @@ class Base(models.AbstractModel):
                         if not property_:
                             continue
 
-                        if property_.get('type') == 'many2one' and property_.get('comodel') and property_['value']:
+                        if property_.get('type') == 'many2one' and property_.get('comodel') and property_.get('value'):
                             record = self.env[property_['comodel']].with_context(field_spec.get('context')).browse(property_['value'][0])
                             property_['value'] = record.web_read(spec['fields']) if 'fields' in spec else property_['value']
 
-                        if property_.get('type') == 'many2many' and property_.get('comodel') and property_['value']:
+                        if property_.get('type') == 'many2many' and property_.get('comodel') and property_.get('value'):
                             records = self.env[property_['comodel']].with_context(field_spec.get('context')).browse([r[0] for r in property_['value']])
                             property_['value'] = records.web_read(spec['fields']) if 'fields' in spec else property_['value']
 
