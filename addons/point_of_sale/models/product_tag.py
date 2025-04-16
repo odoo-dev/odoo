035770e3e84a57b125bd27fa120a559269aba7be
Stephane Vanmeerhaeghe (stva) <stva@odoo.com>
2025-04-16 14:28:54 +0200

    [IMP] point_of_sale: add product tag description
    This commit adds an description field to product tags to allow displaying additional information in Self and Kiosk.
    It also introduces secondary buttons (“Mobile Menu” and “Open Kiosk”) on the dashboard for improved navigation.
    
    Task: 4724527

--- addons/point_of_sale/models/product_tag.py
+++ addons/point_of_sale/models/product_tag.py
@@ -1,11 +1,25 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import models, api
+from odoo import api, fields, models
+from odoo.tools import is_html_empty
 
 
 class ProductTag(models.Model):
     _name = 'product.tag'
     _inherit = ['product.tag', 'pos.load.mixin']
 
+    has_image = fields.Boolean(compute='_compute_has_image')
+    description = fields.Html(string='Description', translate=True)
+
     @api.model
     def _load_pos_data_fields(self, config_id):
-        return ['name']
+        return ['name', 'description', 'color', 'has_image']
+
+    @api.depends('has_image')
+    def _compute_has_image(self):
+        for tag in self:
+            tag.has_image = bool(tag.image)
+
+    def write(self, vals):
+        if vals.get('description') and is_html_empty(vals['description']):
+            vals['description'] = ''
+        return super().write(vals)
