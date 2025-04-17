5c5561596e7218d94b08d75516035a009ecca6ed
panv-odoo <panv@odoo.com>
2025-04-17 17:29:43 +0530

    [IMP] website: adapt and enable website tours
    Enabled following tours:
    - website_seo_notification

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -789,7 +789,6 @@ class TestUi(HttpCaseWithWebsiteUser):
     def test_website_font_family(self):
         self.start_tour("/", "website_font_family", login="admin")
 
-    @unittest.skip
     def test_website_seo_notification(self):
         self.start_tour(self.env['website'].get_client_action_url("/"), "website_seo_notification", login="admin")
 
