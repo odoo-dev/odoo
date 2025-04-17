94339595506f9affd715005ae06d2d5de47ad0a9
Rodolpho Lima <rcdl@odoo.com>
2025-04-17 16:36:34 +0200

    debug tools

--- addons/html_editor/static/src/core/history_plugin.js
+++ addons/html_editor/static/src/core/history_plugin.js
@@ -180,6 +180,8 @@ export class HistoryPlugin extends Plugin {
         this.observer = new MutationObserver(this.handleNewRecords.bind(this));
         this._cleanups.push(() => this.observer.disconnect());
         this.clean();
+        // @todo REMOVE ME
+        window.historyPlugin = this;
     }
 
     clean() {
