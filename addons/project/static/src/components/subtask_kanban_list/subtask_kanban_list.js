46731cb72ae5be2f96d19b5033cabe359056f1c6
rspa-odoo <rspa@odoo.com>
2025-04-18 19:05:54 +0530

    [FIX] project: correct subtask order in kanban and form views
    Issue:
    When creating subtasks from the kanban view of tasks, the order of the subtasks
    is displayed incorrectly. This inconsistency appears both in the kanban view of
    the parent task and in the form view of the parent task when the subtasks are
    created from the kanban view.
    
    Steps to reproduce:
    Create a few subtasks from the task form view, then switch to the kanban view.
    From the kanban card of the same parent task, open the subtask list and create
    a few more subtasks.
    
    Current behaviour:
    Subtasks created from the kanban view do not follow a consistent order (i.e.,
    the order in which they are created). The subtask list on the kanban view
    displays them out of sequence, and this incorrect order is also reflected in
    the form view of the parent task when subtasks are created from the kanban view.
    
    Behaviour after fix:
    Subtasks created from the kanban view of a parent task now maintain the correct
    creation order. This order is reflected consistently in both the kanban view and
    the form view of the parent task. Additionally, when the subtask order is
    updated from the form view, the change is also reflected in the kanban view.
    
    task-4572316

--- addons/project/static/src/components/subtask_kanban_list/subtask_kanban_list.js
+++ addons/project/static/src/components/subtask_kanban_list/subtask_kanban_list.js
@@ -73,7 +73,14 @@ export class SubtaskKanbanList extends Component {
     }
 
     async _onSubtaskCreateNameChanged(name) {
-        await this.orm.create("project.task", [{ display_name: name, parent_id: this.props.record.resId, user_ids: this.props.record.data.user_ids.resIds }]);
+        const child_ids = this.props.record.data.child_ids;
+        const sequence = child_ids.records[child_ids.count - 1].data.sequence + 1
+        await this.orm.create("project.task", [{
+            display_name: name,
+            parent_id: this.props.record.resId,
+            user_ids: this.props.record.data.user_ids.resIds,
+            sequence: sequence,
+        }]);
         this.subtaskCreate.open = false;
         this.subtaskCreate.name = "";
         this.props.record.load();
