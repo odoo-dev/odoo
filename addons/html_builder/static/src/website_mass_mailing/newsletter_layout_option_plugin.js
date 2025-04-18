3d0cf7beba9570d7208babc5ff7b10653b661775
Augustin (duau) <duau@odoo.com>
2025-04-18 11:31:08 +0200

    NewsletterLayoutOptionPlugin extension for sms

--- addons/html_builder/static/src/website_mass_mailing/newsletter_layout_option_plugin.js
+++ addons/html_builder/static/src/website_mass_mailing/newsletter_layout_option_plugin.js
@@ -4,6 +4,8 @@ import { withSequence } from "@html_editor/utils/resource";
 
 export class NewsletterLayoutOptionPlugin extends Plugin {
     static id = "newsletterLayoutOptionPlugin";
+    static dependencies = ["builderActions"];
+
     resources = {
         builder_options: [
             withSequence(1, {
@@ -13,7 +15,32 @@ export class NewsletterLayoutOptionPlugin extends Plugin {
                     ":scope > .container, :scope > .container-fluid, :scope > .o_container_small",
             }),
         ],
+        builder_actions: this.getActions(),
     };
+
+    getActions() {
+        const getAction = this.dependencies.builderActions.getAction;
+        return {
+            selectNewsletterTemplate: {
+                prepare: async ({ actionParam }) => {
+                    await getAction("selectTemplate").prepare({ actionParam: actionParam });
+                },
+                isApplied: ({ editingElement, param: { attribute } }) => {
+                    const parentEl = editingElement.parentElement;
+                    return (
+                        (!parentEl.dataset.newsletterTemplate && attribute === "email") ||
+                        parentEl.dataset.newsletterTemplate === attribute
+                    );
+                },
+                apply: (action) => {
+                    getAction("selectTemplate").apply(action);
+                    const parentEl = action.editingElement.parentElement;
+                    parentEl.dataset.newsletterTemplate = action.param.attribute;
+                },
+                clean: (action) => getAction("selectTemplate").clean(action),
+            },
+        };
+    }
 }
 
 registry
