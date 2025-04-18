a62f09d4ff869de5e742020c396608dc04b9fb3e
Sanjay Sharma <shsa@odoo.com>
2025-04-18 16:46:41 +0530

    Fix issue with s_social_media options where the value for social media in test cases defaults to false when not defined.
    Resolved issue:
    OwlError: Invalid props for component 'BuilderTextInputBase': 'value'
    must be a string or null.
    
    fix tour test_26_website_media_dialog_icons

--- addons/website/tests/test_ui.py
+++ addons/website/tests/test_ui.py
@@ -569,7 +569,6 @@ class TestUi(HttpCaseWithWebsiteUser):
             'websiteIdMapping': json.dumps({'Test Website': website.id})
         })
 
-    @unittest.skip
     def test_26_website_media_dialog_icons(self):
         self.env.ref('website.default_website').write({
             'social_twitter': 'https://twitter.com/Odoo',
@@ -579,6 +578,7 @@ class TestUi(HttpCaseWithWebsiteUser):
             'social_github': 'https://github.com/odoo',
             'social_instagram': 'https://www.instagram.com/explore/tags/odoo/',
             'social_tiktok': 'https://www.tiktok.com/@odoo',
+            'social_discord': 'https://discord.com/servers/discord-town-hall-169256939211980800',
         })
         self.start_tour("/", 'website_media_dialog_icons', login='admin')
 
