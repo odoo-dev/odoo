d31dc64e0527c87ad40305ca40031b3fe942861a
Rémi Rahir (rar) <rar@odoo.com>
2025-04-18 14:25:21 +0200

    wip o-spreadsheet lib update
    ### Contains the following commits:
    
    https://github.com/odoo/o-spreadsheet/commit/cc5a04b05 [FIX] Collaborative: Send client position on activeSheet change [Task: 4736980](https://www.odoo.com/odoo/2328/tasks/4736980)
    https://github.com/odoo/o-spreadsheet/commit/3ab5a96a7 [PERF] collaborative: remove useless destructuring [Task: 4736986](https://www.odoo.com/odoo/2328/tasks/4736986)
    https://github.com/odoo/o-spreadsheet/commit/a98b72949 [FIX] collaborative: check all overlapping zones [Task: 4736986](https://www.odoo.com/odoo/2328/tasks/4736986)
    https://github.com/odoo/o-spreadsheet/commit/1beed801b [FIX] composer: prevent autocomplete on unknown characters in composer [Task: 4652661](https://www.odoo.com/odoo/2328/tasks/4652661)

--- addons/spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js
+++ addons/spreadsheet/static/src/o_spreadsheet/o_spreadsheet.js
@@ -6953,7 +6953,7 @@
         if (executed.type === "ADD_COLUMNS_ROWS") {
             return expandZoneOnInsertion(zone, executed.dimension === "COL" ? "left" : "top", executed.base, executed.position, executed.quantity);
         }
-        return { ...zone };
+        return zone;
     }
 
     /**
@@ -18416,7 +18416,13 @@ day_count_convention (number, default=${DEFAULT_DAY_COUNT_CONVENTION} ) ${_lt("A
         }
         return null;
     }
-    const separatorRegexp = /\w|\.|!|\$/;
+    /**
+      - \p{L} is for any letter (from any language)
+      - \p{N} is for any number
+      - the u flag at the end is for unicode, which enables the `\p{...}` syntax
+     */
+    const unicodeSymbolCharRegexp = /\p{L}|\p{N}|_|\.|!|\$/u;
+    const SYMBOL_CHARS = new Set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_.!$");
     /**
      * A "Symbol" is just basically any word-like element that can appear in a
      * formula, which is not a string. So:
@@ -18456,7 +18462,7 @@ day_count_convention (number, default=${DEFAULT_DAY_COUNT_CONVENTION} ) ${_lt("A
                 };
             }
         }
-        while (chars[0] && chars[0].match(separatorRegexp)) {
+        while (chars[0] && (SYMBOL_CHARS.has(chars[0]) || chars[0].match(unicodeSymbolCharRegexp))) {
             result += chars.shift();
         }
         if (result.length) {
@@ -35252,6 +35258,12 @@ day_count_convention (number, default=${DEFAULT_DAY_COUNT_CONVENTION} ) ${_lt("A
                         const { col, row } = this.getters.getNextVisibleCellPosition(cmd.sheetIdTo, 0, 0);
                         this.selectCell(col, row);
                     }
+                    const { col, row } = this.gridSelection.anchor.cell;
+                    this.moveClient({
+                        sheetId: this.getters.getActiveSheetId(),
+                        col,
+                        row,
+                    });
                     break;
                 }
                 case "REMOVE_COLUMNS_ROWS": {
@@ -36213,10 +36225,8 @@ day_count_convention (number, default=${DEFAULT_DAY_COUNT_CONVENTION} ) ${_lt("A
         }
         const target = [];
         for (const zone1 of cmd.target) {
-            for (const zone2 of executed.target) {
-                if (!overlap(zone1, zone2)) {
-                    target.push({ ...zone1 });
-                }
+            if (executed.target.every((zone2) => !overlap(zone1, zone2))) {
+                target.push(zone1);
             }
         }
         if (target.length) {
@@ -43778,8 +43788,8 @@ day_count_convention (number, default=${DEFAULT_DAY_COUNT_CONVENTION} ) ${_lt("A
 
 
     __info__.version = '16.0.66';
-    __info__.date = '2025-03-26T12:54:26.409Z';
-    __info__.hash = 'e2c1da2';
+    __info__.date = '2025-04-18T12:25:19.516Z';
+    __info__.hash = 'cc5a04b';
 
 
 })(this.o_spreadsheet = this.o_spreadsheet || {}, owl);
