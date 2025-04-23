86e26ad33f13579e9365640926be6302bfbaddff
Nicolas Bayet <nby@odoo.com>
2025-04-23 14:10:24 +0200

    getimageinfos

--- addons/html_builder/static/tests/website_builder/image_gallery.test.js
+++ addons/html_builder/static/tests/website_builder/image_gallery.test.js
@@ -18,20 +18,6 @@ test("Add image in gallery", async () => {
         },
     ]);
 
-    onRpc("/html_editor/get_image_info", () => {
-        expect.step("get_image_info");
-        return {
-            attachment: {
-                id: 1,
-            },
-            original: {
-                id: 1,
-                image_src: "/web/image/hoot.png",
-                mimetype: "image/png",
-            },
-        };
-    });
-
     onRpc(
         "/web/image/hoot.png",
         () => {
@@ -62,6 +48,19 @@ test("Add image in gallery", async () => {
         </section>
         `
     );
+    onRpc("/html_editor/get_image_info", () => {
+        expect.step("get_image_info");
+        return {
+            attachment: {
+                id: 1,
+            },
+            original: {
+                id: 1,
+                image_src: "/web/image/hoot.png",
+                mimetype: "image/png",
+            },
+        };
+    });
     await contains(":iframe .first_img").click();
     expect("[data-action-id='addImage']").toHaveCount(1);
     await contains("[data-action-id='addImage']").click();
@@ -78,7 +77,13 @@ test("Add image in gallery", async () => {
     );
 
     expect(columnImgs).toEqual([["1", "3", "4", "5", "6"], ["2"]]);
-    expect.verifySteps(["get_image_info"]);
+    expect.verifySteps([
+        "get_image_info",
+        "get_image_info",
+        "get_image_info",
+        "get_image_info",
+        "get_image_info",
+    ]);
     expect(":iframe .o_masonry_col img[data-index='6']").toHaveAttribute(
         "data-mimetype",
         "image/webp"
