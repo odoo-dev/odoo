b09ec6033d8fbcd35684b5e33ff2462003800da4
Davide Bonetto <dabo@odoo.com>
2025-04-23 08:52:15 +0200

    [IMP] html_builder, html_editor: Neutralizing Navigation and Menu Data plugin

--- addons/html_editor/static/src/main/link/link_plugin.js
+++ addons/html_editor/static/src/main/link/link_plugin.js
@@ -220,6 +220,15 @@ export class LinkPlugin extends Plugin {
             icon: "fa-square",
         }),
 
+        link_popovers: [
+            withSequence(50, {
+                //Default option
+                PopoverClass: LinkPopover,
+                isAvailable: () => true,
+                getProps: (props) => props,
+            }),
+        ],
+
         /** Handlers */
         beforeinput_handlers: withSequence(5, this.onBeforeInput.bind(this)),
         input_handlers: this.onInputDeleteNormalizeLink.bind(this),
@@ -236,14 +245,10 @@ export class LinkPlugin extends Plugin {
         split_element_block_overrides: this.handleSplitBlock.bind(this),
         insert_line_break_element_overrides: this.handleInsertLineBreak.bind(this),
     };
+
     setup() {
-        this.overlay = this.dependencies.overlay.createOverlay(
-            LinkPopover,
-            {
-                closeOnPointerdown: false,
-            },
-            { sequence: 50 }
-        );
+        this.initializePopovers();
+        this.currentOverlay = this.getActivePopover().overlay;
         this.addDomListener(this.editable, "click", (ev) => {
             const linkEl = ev.target.closest("a");
             if (linkEl) {
@@ -377,7 +382,7 @@ export class LinkPlugin extends Plugin {
      * @param {HTMLElement} [linkElement]
      */
     openLinkTools(linkElement, type) {
-        this.overlay.close();
+        this.currentOverlay.close();
         if (!this.isLinkAllowedOnSelection()) {
             return this.services.notification.add(
                 _t("Unable to create a link on the current selection."),
@@ -507,15 +512,15 @@ export class LinkPlugin extends Plugin {
             onRemove: () => {
                 this.removeLinkInDocument();
                 this.linkInDocument = null;
-                this.overlay.close();
+                this.currentOverlay.close();
             },
             onCopy: () => {
                 this.linkInDocument = null;
-                this.overlay.close();
+                this.currentOverlay.close();
             },
             onClose: () => {
                 this.linkInDocument = null;
-                this.overlay.close();
+                this.currentOverlay.close();
                 this.dependencies.selection.focusEditable();
             },
             getInternalMetaData: this.getInternalMetaData,
@@ -529,13 +534,17 @@ export class LinkPlugin extends Plugin {
             type: this.type || "",
             showReplaceTitleBanner: this.newlyInsertedLinks.has(linkElement),
         };
-        this.overlay.open({ props });
+
+        const popover = this.getActivePopover(linkElement);
+        this.currentOverlay = popover.overlay;
+        this.currentOverlay.open({ props: popover.getProps(props) });
         if (this.linkInDocument) {
             if (this.newlyInsertedLinks.has(this.linkInDocument)) {
                 this.newlyInsertedLinks.delete(this.linkInDocument);
             }
         }
     }
+
     /**
      * close the link tool
      *
@@ -546,8 +555,8 @@ export class LinkPlugin extends Plugin {
         // Some unit tests fail when this.overlay.isOpen but the DOM don't contain the linkPopover yet.
         // Because of some kind of race condition between the hoot mock event and the owl renderer.
         // This is why we check for the popover in the DOM.
-        if (this.overlay.isOpen && document.querySelector(".o-we-linkpopover")) {
-            this.overlay.close();
+        if (this.currentOverlay.isOpen && document.querySelector(".o-we-linkpopover")) {
+            this.currentOverlay.close();
             if (link && link.isConnected) {
                 this.dependencies.selection.setSelection({
                     anchorNode: link,
@@ -665,6 +674,13 @@ export class LinkPlugin extends Plugin {
                 if (closestLinkElement !== this.linkInDocument) {
                     this.openLinkTools(closestLinkElement);
                 }
+            } else if (
+                closestLinkElement &&
+                (closestLinkElement.getAttribute("role") === "menuitem" ||
+                    closestLinkElement.classList.contains("nav-link")) &&
+                !closestLinkElement.dataset.bsToggle
+            ) {
+                this.openLinkTools(closestLinkElement);
             } else {
                 this.linkInDocument = null;
                 this.closeLinkTools();
@@ -901,8 +917,8 @@ export class LinkPlugin extends Plugin {
             if (url && this?.isCurrentLinkInSync) {
                 linkEl.setAttribute("href", url);
                 this.isCurrentLinkInSync = false;
-                if (this.overlay.isOpen) {
-                    this.overlay.close();
+                if (this.currentOverlay.isOpen) {
+                    this.currentOverlay.close();
                 }
             }
         }
@@ -1014,6 +1030,27 @@ export class LinkPlugin extends Plugin {
                 }
             }
         }
+    }
+
+    initializePopovers() {
+        this.overlays = [];
+        this.getResource("link_popovers").map((link_popover) => {
+            this.overlays.push({
+                overlay: this.dependencies.overlay.createOverlay(
+                    link_popover.PopoverClass,
+                    {
+                        closeOnPointerdown: false,
+                    },
+                    { sequence: 50 }
+                ),
+                isAvailable: link_popover.isAvailable,
+                getProps: link_popover.getProps,
+            });
+        });
+    }
+
+    getActivePopover(linkElement) {
+        return this.overlays.find((overlay) => overlay.isAvailable(linkElement));
     }
 }
 
