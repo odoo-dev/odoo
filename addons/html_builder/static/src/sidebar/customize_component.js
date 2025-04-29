b78fc855bd4cd739270eda34b6e7fc227be7a195
FrancoisGe <fge@odoo.com>
2025-04-29 08:32:44 +0200

    Convert img => IMg

--- addons/html_builder/static/src/sidebar/customize_component.js
+++ addons/html_builder/static/src/sidebar/customize_component.js
@@ -1,8 +1,10 @@
 import { Component } from "@odoo/owl";
 import { useOptionsSubEnv } from "@html_builder/utils/utils";
+import { Img } from "@html_builder/core/img";
 
 export class CustomizeComponent extends Component {
     static template = "html_builder.CustomizeComponent";
+    static components = { Img };
     static props = {
         editingElements: { type: Array },
         comp: { type: Function },
