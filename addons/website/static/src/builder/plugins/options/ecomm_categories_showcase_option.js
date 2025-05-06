42f752a8e121ab646e9c72fddc7c0e5e726f062b
Xavier Luyckx (xlu) <xlu@odoo.com>
2025-05-06 16:38:56 +0200

    [IMP] website: add `s_ecomm_categories_showcase` snippet
    This commit introduces a new ecommerce-oriented snippet for showcasing
    product categories, featuring hover-based block expansion, responsive
    horizontal scrolling, and visual editor controls for layout variations.
    
    task-4380924
    Part of task-4252024

---
+++ addons/website/static/src/builder/plugins/options/ecomm_categories_showcase_option.js
@@ -0,0 +1,19 @@
+import { BaseOptionComponent } from "@html_builder/core/utils";
+
+export class EcommCategoriesShowcaseOption extends BaseOptionComponent {
+    static template = "website.EcommCategoriesShowcaseOption";
+    static props = {};
+
+    setup() {
+        super.setup();
+    }
+}
+
+export class EcommCategoriesShowcaseBlocksDesign extends BaseOptionComponent {
+    static template = "website.EcommCategoriesShowcaseBlocksDesign";
+    static props = {};
+
+    setup() {
+        super.setup();
+    }
+} 
