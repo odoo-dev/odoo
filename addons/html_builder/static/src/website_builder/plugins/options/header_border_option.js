6dc01a737efcb1dcf80b0309e9340727c7884a7d
FrancoisGe <fge@odoo.com>
2025-05-06 08:30:15 +0200

    [FIX] web: wip

--- addons/html_builder/static/src/website_builder/plugins/options/header_border_option.js
+++ addons/html_builder/static/src/website_builder/plugins/options/header_border_option.js
@@ -1,6 +1,8 @@
 import { BaseOptionComponent, useDomState } from "@html_builder/core/utils";
 import { BorderConfigurator } from "@html_builder/plugins/border_configurator_option";
 import { ShadowOption } from "@html_builder/plugins/shadow_option";
+import { onWillStart } from "@odoo/owl";
+import { Deferred } from "@web/core/utils/concurrency";
 
 export class HeaderBorderOption extends BaseOptionComponent {
     static template = "website.HeaderBorderOption";
@@ -8,6 +10,16 @@ export class HeaderBorderOption extends BaseOptionComponent {
     static components = { BorderConfigurator, ShadowOption };
 
     setup() {
+        onWillStart(() => {
+            const def = new Deferred();
+            console.log("HeaderBorderOption setup");
+
+            setTimeout(() => {
+                def.resolve();
+            }, 2000);
+            console.log("HeaderBorderOption setup end");
+            return def;
+        });
         super.setup();
         this.domState = useDomState((editingElement) => ({
             withRoundCorner: !editingElement.classList.contains("o_header_force_no_radius"),
