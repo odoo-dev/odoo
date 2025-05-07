b5bbc53cd68a26dbf152c65f99a7021923080a52
Rodolpho Lima <rcdl@odoo.com>
2025-05-07 13:40:22 +0200

    Fix toolbar open in extended mode
    Upon opening, the toolbar should be displayed in its compact form.
    
    Before this commmit, extending the toolbar followed by closing and
    reopening it would (incorrectely) result in its extended form.

--- addons/html_editor/static/src/main/toolbar/toolbar_plugin.js
+++ addons/html_editor/static/src/main/toolbar/toolbar_plugin.js
@@ -134,7 +134,7 @@ export class ToolbarPlugin extends Plugin {
     static shared = ["getToolbarInfo"];
     resources = {
         selectionchange_handlers: this.handleSelectionChange.bind(this),
-        selection_leave_handlers: () => this.overlay.close(),
+        selection_leave_handlers: () => this.closeToolbar(),
         step_added_handlers: () => this.updateToolbar(),
         user_commands: {
             id: "expandToolbar",
@@ -209,10 +209,10 @@ export class ToolbarPlugin extends Plugin {
         } else {
             // Mouse interaction behavior:
             // Close toolbar on mousedown and prevent it from opening until mouseup.
-            this.addGlobalDomListener("mousedown", (ev) => {
+            this.addDomListener(this.editable, "mousedown", (ev) => {
                 // Don't close if the mousedown is on an overlay.
                 if (!ev.target?.closest?.(".o-overlay-item")) {
-                    this.overlay.close();
+                    this.closeToolbar();
                     this.debouncedUpdateToolbar.cancel();
                     this.onSelectionChangeActive = false;
                 }
@@ -238,7 +238,7 @@ export class ToolbarPlugin extends Plugin {
             // sequential keystrokes.
             this.addDomListener(this.editable, "keydown", (ev) => {
                 if (ev.key.startsWith("Arrow")) {
-                    this.overlay.close();
+                    this.closeToolbar();
                     this.onSelectionChangeActive = false;
                 }
             });
@@ -306,10 +306,14 @@ export class ToolbarPlugin extends Plugin {
     }
 
     updateToolbar(selectionData = this.dependencies.selection.getSelectionData()) {
-        this.updateToolbarVisibility(selectionData);
-        if (this.overlay.isOpen || this.config.disableFloatingToolbar) {
-            this.updateButtonsStates(selectionData.editableSelection);
+        this.updateNamespace();
+        if (!this.config.disableFloatingToolbar) {
+            this.updateToolbarVisibility(selectionData);
+            if (!this.overlay.isOpen) {
+                return;
+            }
         }
+        this.updateButtonsStates(selectionData.editableSelection);
     }
 
     getFilterTraverseNodes() {
@@ -319,21 +323,12 @@ export class ToolbarPlugin extends Plugin {
     }
 
     updateToolbarVisibility(selectionData) {
-        this.updateNamespace();
-        if (this.config.disableFloatingToolbar) {
-            return;
-        }
-
         if (this.shouldBeVisible(selectionData)) {
             // Open toolbar or update its position
             const props = { toolbar: this.getToolbarInfo(), class: "shadow rounded my-2" };
-            if (!this.overlay.isOpen) {
-                // Open toolbar in compact mode
-                this.isToolbarExpanded = false;
-            }
             this.overlay.open({ props });
         } else if (this.overlay.isOpen && !this.shouldPreventClosing()) {
-            this.overlay.close();
+            this.closeToolbar();
         }
     }
 
@@ -408,6 +403,11 @@ export class ToolbarPlugin extends Plugin {
             }
         }
         this.updateSelection = null;
+    }
+
+    closeToolbar() {
+        this.overlay.close();
+        this.isToolbarExpanded = false;
     }
 }
 
