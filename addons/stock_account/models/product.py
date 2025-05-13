a408d3de43b5ca8a1dd694508ac67f6fe37ea895
khsr-odoo <khsr@odoo.com>
2025-05-13 12:33:53 +0530

    [FIX]

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -91,12 +91,16 @@ will update the cost of every lot/serial number in stock."),
         res = super(ProductTemplate, self).write(vals)
 
         for product_template, (products, description, products_orig_quantity_svl) in impacted_templates.items():
+            new_products = products
+            if not products.exists():
+                out_svl_vals_list, products_orig_quantity_svl, new_products = Product \
+                    ._svl_empty_stock(description, product_template=product_template)
             # Replenish the stock with the new cost method.
-            in_svl_vals_list = products._svl_replenish_stock(description, products_orig_quantity_svl)
+            in_svl_vals_list = new_products._svl_replenish_stock(description, products_orig_quantity_svl)
             in_stock_valuation_layers = SVL.create(in_svl_vals_list)
             if product_template.valuation == 'real_time':
                 move_vals_list += Product._svl_replenish_stock_am(in_stock_valuation_layers)
-            products._update_lots_standard_price()
+            new_products._update_lots_standard_price()
 
         # Check access right
         if move_vals_list and not self.env['stock.valuation.layer'].has_access('read'):
