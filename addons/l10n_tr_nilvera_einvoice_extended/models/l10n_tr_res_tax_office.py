91dcaeb5a7fa424e11e95875a2a6493a0cc5cac6
Khumam ALKH <alkh@odoo.com>
2025-05-14 10:56:06 +0400

    [ADD] l10n_tr_nilvera_einvoice_extended, l10_tr_nilvera_einvoice: Adding nilvera einvoice extention
    [ADD] l10n_tr_nilvera_einvoice_extended: Adding Tax Offices

---
+++ addons/l10n_tr_nilvera_einvoice_extended/models/l10n_tr_res_tax_office.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class L10nTrAccountTaxCode(models.Model):
+    _name = "l10n_tr.res.tax.office"
+    _description = "Turkish Tax Offices"
+
+    name = fields.Char(translate=True)
+    code = fields.Integer()
+    state_id = fields.Many2one("res.country.state")
+    state_code = fields.Char(related="state_id.code")
