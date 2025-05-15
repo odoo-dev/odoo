67c0e8a62fe032434787149c88fc76e1c3348afb
sako-odoo <sako@odoo.com>
2025-05-15 11:48:18 +0530

    [REV] mrp: restore `mrp.document` and remove BoM document section
    This commit reverts the core change from:
    PR [#152286](https://github.com/odoo/odoo/pull/152286),
    which had removed the `mrp.document` model by merging it with the product
    document model.
    
    At the same time, it removes the document section from the BoM form,
    as product documents are already accessible from the product form itself.
    Showing them again in the BoM is redundant and adds unnecessary complexity to
    the interface.
    
    After this commit, the `mrp.document` model is restored, and the BoM form no
    longer displays product-related documents. This change helps avoid redundancy,
    as product documents remain accessible from the product form, and results in a
    cleaner, more focused BoM interface.
    
    Task ID: 4593207

--- addons/mrp/controller/mrp_document.py
+++ addons/mrp/controller/mrp_document.py
@@ -3,38 +3,35 @@
 import json
 import logging
 
-from odoo import _
-from odoo.http import request, route, Controller
+from odoo import http
+from odoo.http import request
 
 logger = logging.getLogger(__name__)
 
 
-class ProductDocumentController(Controller):
+class MrpDocumentRoute(http.Controller):
 
-    @route('/product/document/upload', type='http', methods=['POST'], auth='user')
+    @http.route('/mrp/document/upload', type='http', methods=['POST'], auth="user")
     def upload_document(self, ufile, res_model, res_id):
-        if res_model not in ('product.product', 'product.template'):
-            return
-
         record = request.env[res_model].browse(int(res_id)).exists()
-
         if not record or not record.browse().has_access('write'):
             return
 
         files = request.httprequest.files.getlist('ufile')
-        result = {'success': _("All files uploaded")}
+        result = {'success': self.env._("All files uploaded")}
         for ufile in files:
             try:
                 mimetype = ufile.content_type
-                request.env['product.document'].create({
+                request.env['mrp.document'].with_context(
+                    disable_product_documents_creation=True
+                ).create({
                     'name': ufile.filename,
                     'res_model': record._name,
                     'res_id': record.id,
-                    'company_id': record.company_id.id,
                     'mimetype': mimetype,
                     'raw': ufile.read(),
                 })
             except Exception as e:
-                logger.exception("Failed to upload document %s", ufile.filename)
+                logger.exception("Fail to upload document %s", ufile.filename)
                 result = {'error': str(e)}
         return json.dumps(result)
