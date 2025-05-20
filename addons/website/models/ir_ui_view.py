25327eb5041b0db253ee80465bc1c762ec082dd5
Gorash <chm@odoo.com>
2025-05-20 16:47:00 +0200

    [IMP] website: Improved performance of `filter_duplicate` O(n²) => O(n)

--- addons/website/models/ir_ui_view.py
+++ addons/website/models/ir_ui_view.py
@@ -293,21 +293,23 @@ class IrUiView(models.Model):
               * In a website context, every view from another website
         """
         current_website_id = self._context.get('website_id')
-        most_specific_views = self.env['ir.ui.view']
         if not current_website_id:
             return self.filtered(lambda view: not view.website_id)
 
+        most_specific_views = {}
         for view in self:
-            # specific view: add it if it's for the current website and ignore
-            # it if it's for another website
-            if view.website_id and view.website_id.id == current_website_id:
-                most_specific_views |= view
-            # generic view: add it only if, for the current website, there is no
-            # specific view for this view (based on the same `key` attribute)
-            elif not view.website_id and not any(view.key == view2.key and view2.website_id and view2.website_id.id == current_website_id for view2 in self):
-                most_specific_views |= view
+            if view.website_id:
+                # specific view: add it if it's for the current website and ignore
+                # it if it's for another website
+                if view.website_id.id != current_website_id:
+                    continue
+                most_specific_views[view.key] = view
+            if view.key not in most_specific_views:
+                # generic view: add it only if, for the current website, there is no
+                # specific view for this view (based on the same `key` attribute)
+                most_specific_views[view.key] = view
 
-        return most_specific_views
+        return self.env['ir.ui.view'].union(*most_specific_views.values())
 
     @api.model
     def _view_get_inherited_children(self, view):
