81fab6c06039e108df6afbfcb2517121412fe9bf
Alessandro Lupo <alup@odoo.com>
2025-05-20 08:06:28 +0200

    [IMP] website: Floating Blocks option

---
+++ addons/website/static/src/builder/plugins/floating_blocks_option_plugin.js
@@ -0,0 +1,126 @@
+import { Plugin } from "@html_editor/plugin";
+import { registry } from "@web/core/registry";
+import { withSequence } from "@html_editor/utils/resource";
+import { after, BEGIN } from "@html_builder/utils/option_sequence";
+import { DEVICE_VISIBILITY, LAYOUT_GRID } from "@website/builder/option_sequence";
+import {
+    FloatingBlocksOption,
+    FloatingBlocksBlockOption,
+    FloatingBlocksBlockMobileOption,
+} from "./floating_blocks_option";
+import { renderToElement } from "@web/core/utils/render";
+
+class FloatingBlocksOptionPlugin extends Plugin {
+    static id = "floatingBlocksOptionPlugin";
+    static dependencies = ["edit_interaction"];
+    resources = {
+        builder_options: [
+            withSequence(after(DEVICE_VISIBILITY), {
+                OptionComponent: FloatingBlocksOption,
+                selector: ".s_floating_blocks",
+            }),
+        ],
+        builder_actions: {
+            floatingBlocksRoundness: {
+                getValue: ({ editingElement }) => {
+                    for (let x = 0; x <= 5; x++) {
+                        if (editingElement.classList.contains(`rounded-${x}`)) {
+                            return x;
+                        }
+                    }
+                    return 0;
+                },
+                apply: ({ editingElement, value }) => {
+                    for (let x = 0; x <= 5; x++) {
+                        editingElement.classList.remove(`rounded-${x}`);
+                    }
+                    editingElement.classList.add(`rounded-${value}`);
+                },
+            },
+            addCard: {
+                apply: ({ editingElement: el }) => {
+                    const newCardEl = renderToElement("website.s_floating_blocks.new_card");
+                    const wrapperEl = el.querySelector(".s_floating_blocks_wrapper");
+                    wrapperEl.appendChild(newCardEl);
+                    newCardEl.scrollIntoView({ behavior: "smooth", block: "center" });
+                    this.validateBoxesNumber(el);
+                    this.dependencies.edit_interaction.restartInteractions(el);
+                },
+            },
+        },
+        clean_for_save_handlers: this.cleanForSave.bind(this),
+        normalize_handlers: this.normalize.bind(this),
+    };
+
+    cleanForSave({ root }) {
+        for (const el of root.querySelectorAll(".s_floating_blocks_wrapper")) {
+            const boxesEls = [...el.querySelectorAll(".s_floating_blocks_block")];
+            const alertEl = el.querySelector(".s_floating_blocks_alert_empty");
+            if (boxesEls.length > 0) {
+                alertEl?.remove();
+            } else {
+                // Special case: by injecting the "No cards" alert ('alertEl'), we
+                // prevent the automatic snippet removal during edition. Still, if
+                // the user intentionally "saves" the snippet empty, we'll emulate
+                // the original editor behavior by removing it here.
+                // See also FloatingBlocksOption.setup().
+                el.closest(".s_floating_blocks").remove();
+            }
+        }
+    }
+
+    normalize(el) {
+        // Counts the blocks and restarts the interaction every time that a
+        // block is added, removed or moved. Note that interaction must
+        // restart on block movement to get the correct visual effect.
+        const wrapperEl = el.closest(".s_floating_blocks_wrapper");
+        if (wrapperEl) {
+            this.validateBoxesNumber(wrapperEl);
+            this.dependencies.edit_interaction.restartInteractions(
+                wrapperEl.closest(".s_floating_blocks")
+            );
+        }
+    }
+
+    validateBoxesNumber(el) {
+        const boxesEls = [...el.querySelectorAll(".s_floating_blocks_block")];
+        el.querySelector(".s_floating_blocks_alert_empty")?.classList.toggle(
+            "d-none",
+            boxesEls.length > 0
+        );
+    }
+}
+
+class FloatingBlocksBlockOptionPlugin extends Plugin {
+    static id = "floatingBlocksBlockOptionPlugin";
+    resources = {
+        builder_options: [
+            withSequence(BEGIN, {
+                OptionComponent: FloatingBlocksBlockMobileOption,
+                selector: ".s_floating_blocks .s_floating_blocks_block",
+                applyTo: ".container-fluid",
+            }),
+            withSequence(LAYOUT_GRID, {
+                OptionComponent: FloatingBlocksBlockOption,
+                selector: ".s_floating_blocks .s_floating_blocks_block",
+            }),
+        ],
+        dropzone_selector: [
+            // Lock grid-items within their grid
+            {
+                selector: ".s_floating_blocks_block_grid .o_grid_item",
+                dropLockWithin: ".s_floating_blocks_block_grid",
+            },
+            // Lock block-items within the snippet
+            {
+                selector: ".s_floating_blocks .s_floating_blocks_block",
+                dropLockWithin: ".s_floating_blocks",
+            },
+        ],
+    };
+}
+
+registry.category("website-plugins").add(FloatingBlocksOptionPlugin.id, FloatingBlocksOptionPlugin);
+registry
+    .category("website-plugins")
+    .add(FloatingBlocksBlockOptionPlugin.id, FloatingBlocksBlockOptionPlugin);
--- addons/website/static/src/builder/plugins/options/background_option.js
+++ addons/website/static/src/builder/plugins/options/background_option.js
@@ -2,6 +2,12 @@ import { BaseOptionComponent, useDomState } from "@html_builder/core/utils";
 import { BackgroundOption } from "@website/builder/plugins/background_option/background_option";
 import { ParallaxOption } from "./parallax_option";
 import { useBackgroundOption } from "@website/builder/plugins/background_option/background_hook";
+import {
+    BOTH_BG_COLOR_IMAGE_EXCLUDE,
+    BOTH_BG_COLOR_IMAGE_SELECTOR,
+    ONLY_BG_IMAGE_EXLUDE,
+    ONLY_BG_IMAGE_SELECTOR,
+} from "@website/builder/plugins/options/utils";
 
 export class WebsiteBackgroundOption extends BaseOptionComponent {
     static template = "website.WebsiteBackgroundOption";
@@ -21,8 +27,34 @@ export class WebsiteBackgroundOption extends BaseOptionComponent {
         super.setup();
         const { showColorFilter } = useBackgroundOption(this.isActiveItem);
         this.showColorFilter = () => showColorFilter() || this.isActiveItem("toggle_bg_video_id");
-        this.websiteBgOptionDomState = useDomState((el) => ({
-            applyTo: el.querySelector(".s_parallax_bg") ? ".s_parallax_bg" : "",
-        }));
+
+        this.colorImageSelectors = BOTH_BG_COLOR_IMAGE_SELECTOR.split(",");
+        this.onlyImageSelectors = ONLY_BG_IMAGE_SELECTOR.split(",");
+        this.colorImageExcludes = BOTH_BG_COLOR_IMAGE_EXCLUDE.split(",");
+        this.onlyImageExcludes = ONLY_BG_IMAGE_EXLUDE.split(",");
+
+        this.websiteBgOptionDomState = useDomState((el) => {
+            const candidate = el.querySelector(".s_parallax_bg");
+            if (!candidate) {
+                return { applyTo: "" };
+            }
+            // The following code prevents the background color option of a
+            // parent from targetting a child that is supposed to have its
+            // own background option.
+            // TODO: maybe find a better way to achieve this?
+            const hasBgImageOption = (node) =>
+                (this.colorImageSelectors.some((selector) => node.matches(selector)) &&
+                    !this.colorImageExcludes.some((exclude) => node.matches(exclude))) ||
+                (this.onlyImageSelectors.some((selector) => node.matches(selector)) &&
+                    !this.onlyImageExcludes.some((exclude) => node.matches(exclude)));
+            let currentNode = candidate;
+            while (currentNode && currentNode !== el) {
+                if (hasBgImageOption(currentNode)) {
+                    return { applyTo: "" };
+                }
+                currentNode = currentNode.parentElement;
+            }
+            return { applyTo: ".s_parallax_bg" };
+        });
     }
 }
