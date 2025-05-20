3720d0d332e38be0c97f655146968de6c67b4f64
Arib Ansari <aans@odoo.com>
2025-05-20 19:07:03 +0530

    [IMP] website: re-enable test_website_form_special_characters tour

--- addons/website/tests/test_website_form_editor.py
+++ addons/website/tests/test_website_form_editor.py
@@ -61,8 +61,6 @@ class TestWebsiteFormEditor(HttpCaseWithUserPortal):
     def test_website_form_editable_content(self):
         self.start_tour('/', 'website_form_editable_content', login="admin")
 
-    # TODO @mysterious-egg: new tour
-    @unittest.skip
     def test_website_form_special_characters(self):
         self.start_tour('/', 'website_form_special_characters', login='admin')
         mail = self.env['mail.mail'].search([], order='id desc', limit=1)
