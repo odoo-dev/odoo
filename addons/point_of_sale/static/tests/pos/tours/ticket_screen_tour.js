5c42e0ab848f3dba6c96772c50318b358937814b
sjai-odoo <sjai@odoo.com>
2025-05-28 16:57:52 +0530

    [FIX] point_of_sale: fix tour
    - removed `remove_from_ui` which is
    unnecessarry.
    - fixed lot refund tour

--- addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
+++ addons/point_of_sale/static/tests/pos/tours/ticket_screen_tour.js
@@ -233,6 +233,7 @@ registry.category("web_tour.tours").add("LotRefundTour", {
             FeedbackScreen.clickNextOrder(),
             ...ProductScreen.clickRefund(),
             TicketScreen.selectOrder("001"),
+            ProductScreen.clickLine("Product A"),
             ProductScreen.clickNumpad("1"),
             TicketScreen.toRefundTextContains("1"),
             TicketScreen.confirmRefund(),
