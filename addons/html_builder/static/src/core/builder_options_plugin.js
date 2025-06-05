62ba59502471ee68759e8cdbeb828c8524c5e462
Géry Debongnie <ged@odoo.com>
2025-06-05 12:43:49 +0200

    [IMP] html_builder: experiment

--- addons/html_builder/static/src/core/builder_options_plugin.js
+++ addons/html_builder/static/src/core/builder_options_plugin.js
@@ -355,6 +355,7 @@ export class BuilderOptionsPlugin extends Plugin {
             .sort(([a], [b]) => (b.contains(a) ? 1 : -1))
             .map(([element, Options]) => ({
                 id: previousElementToIdMap.get(element) || uniqueId(),
+                folded: true,
                 element,
                 options: Options,
                 optionTitleComponents: elementToOptionTitleComponents.get(element) || [],
@@ -375,6 +376,9 @@ export class BuilderOptionsPlugin extends Plugin {
         );
         if (lastValidContainerIdx > 0) {
             containers = containers.slice(lastValidContainerIdx);
+        }
+        if (containers.length) {
+            containers.at(-1).folded = false;
         }
         return containers;
     }
