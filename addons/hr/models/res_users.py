948adcc2a96c8d57b4b7dfcae001e7fe11220b1d
adsh-odoo <adsh@odoo.com>
2025-06-06 13:12:28 +0530

    [WIP] test

--- addons/hr/models/res_users.py
+++ addons/hr/models/res_users.py
@@ -150,7 +150,6 @@ class ResUsers(models.Model):
     employee_type = fields.Selection(related='employee_id.employee_type', readonly=False, related_sudo=False)
     employee_resource_calendar_id = fields.Many2one(related='employee_id.resource_calendar_id', string="Employee's Working Hours", readonly=True)
 
-    create_employee = fields.Boolean(store=False, default=False, copy=False, string="Technical field, whether to create an employee")
     create_employee_id = fields.Many2one('hr.employee', store=False, copy=False, string="Technical field, bind user to this employee on create")
 
     can_edit = fields.Boolean(compute='_compute_can_edit')
@@ -219,16 +218,16 @@ class ResUsers(models.Model):
         res = super().create(vals_list)
         employee_create_vals = []
         for user, vals in zip(res, vals_list):
-            if not vals.get('create_employee') and not vals.get('create_employee_id'):
-                continue
             if vals.get('create_employee_id'):
                 self.env['hr.employee'].browse(vals.get('create_employee_id')).user_id = user
-            else:
+            elif res._is_internal():
                 employee_create_vals.append(dict(
                     name=user.name,
                     company_id=user.env.company.id,
                     **self.env['hr.employee']._sync_user(user)
                 ))
+            else:
+                continue
         if employee_create_vals:
             self.env['hr.employee'].with_context(clean_context(self.env.context)).create(employee_create_vals)
         return res
