001cc484bf2a05c43d0766f13bd74515a94a4242
svs-odoo <svs@odoo.com>
2025-06-12 10:38:32 +0200

    [IMP] sale: duration since last invoice product
    This commit adds two things:
    
    Add `product.product` `last_invoiced_date` compute field
    ========================================================
    This field gets the last time the product was invoiced for a given
    partner (get in the context.)
    If the `partner_id` is not in the context, the compute is skip.
    
    Add last invoiced time period beside the product's name
    =======================================================
    In a Sale Order, when selecting a SO line's product, the time period
    between today and the last invoice for the SO partner will be displayed
    beside the product's name.
    
    For example, let say we have a product named "Tool Box":
    - If the last invoice was done 2 weeks ago, the product's display name
      will be "Tool Box    14d";
    - If the last invoice was done 18 months ago, the product's display name
      will be "Tool Box    1y".
    
    task-4423839

--- addons/sale/models/product_product.py
+++ addons/sale/models/product_product.py
@@ -4,19 +4,56 @@ from datetime import timedelta
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
-from odoo.tools import float_round
 
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     sales_count = fields.Float(compute='_compute_sales_count', string='Sold', digits='Product Unit')
+    last_invoice_date = fields.Date(compute='_compute_last_invoice_date')
 
     # Catalog related fields
     product_catalog_product_is_in_sale_order = fields.Boolean(
         compute='_compute_product_is_in_sale_order',
         search='_search_product_is_in_sale_order',
     )
+
+    @api.depends_context('partner_id')
+    def _compute_last_invoice_date(self):
+        self.last_invoice_date = False
+        partner_id = self.env.context.get('partner_id')
+        if not partner_id:
+            return
+
+        read_group_data = self.env['account.move.line']._read_group(
+            [
+                ('product_id', 'in', self.ids),
+                ('move_id.state', '=', 'posted'),
+                ('partner_id', '=', partner_id),
+            ],
+            ['product_id', 'invoice_date:day']
+        )
+        for product, date in read_group_data:
+            product.last_invoice_date = date
+
+    @api.depends_context('formatted_display_name', 'partner_id', 'prioritize_for')
+    def _compute_display_name(self):
+        super()._compute_display_name()
+
+        # Add last invoiced date beside the product's name.
+        if self.env.context.get('formatted_display_name') and\
+            self.env['product.template']._must_prioritize_invoiced_product():
+            today = fields.Date.today()
+            for product in self:
+                if product.last_invoice_date:
+                    days_count = (today - product.last_invoice_date).days
+                    if days_count > 365:
+                        day_value_str = self.env._('%(years_count)sy', years_count=(days_count // 365))
+                    elif days_count > 30:
+                        day_value_str = self.env._('%(months_count)smo', months_count=(days_count // 30))
+                    else:
+                        day_value_str = self.env._('%(days_count)sd', days_count=days_count)
+                    product.display_name += f'\t--{day_value_str}--'
 
     def _compute_sales_count(self):
         r = {}
@@ -113,7 +150,7 @@ class ProductProduct(models.Model):
             so_lines.product_uom_id = to_uom_id
         return super()._update_uom(to_uom_id)
 
-    def _trigger_uom_warning(self):        
+    def _trigger_uom_warning(self):
         res = super()._trigger_uom_warning()
         if res:
             return res
