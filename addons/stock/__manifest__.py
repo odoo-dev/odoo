4b28f07b8fb5400c2166fe4e824693d176e57993
Adrien Widart (awt) <awt@odoo.com>
2025-06-18 09:36:57 +0200

    [FIX] stock: amazing new dependency

--- addons/stock/__manifest__.py
+++ addons/stock/__manifest__.py
@@ -6,7 +6,7 @@
     'version': '1.1',
     'summary': 'Manage your stock and logistics activities',
     'website': 'https://www.odoo.com/app/inventory',
-    'depends': ['product', 'barcodes_gs1_nomenclature', 'digest'],
+    'depends': ['product', 'barcodes_gs1_nomenclature', 'digest', 'account'],
     'category': 'Inventory/Inventory',
     'sequence': 25,
     'demo': [
