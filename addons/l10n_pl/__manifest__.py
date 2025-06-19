7de1d3b0cc1bd6dd8aee5ccd78ad70915c428799
AhmedElemary57 <aele@odoo.com>
2025-06-19 17:11:51 +0200

    [ADD] l10n_pl_edi: Generate and download FA(3) XML for KSeF
    - Added new fields to account.move to track KSeF status, reference number, attachment, and error logs.
    - Implemented `_l10n_pl_edi_render_fa3_xml()` to generate FA(3)-compliant XML structure using lxml.
    - Created `action_send_to_ksef()` method to generate, store, and log the XML file as an ir.attachment.
    - Integrated download functionality via HTTP controller `/l10n_pl_edi/download_xml/<invoice_id>` to allow direct XML download.
    - Added frontend button in the invoice form view to trigger generation and download in one step.
    - Prepared logic for future KSeF API integration and status handling.
    
    This lays the foundation for full KSeF 2.0 compliance and testing ahead of the official rollout
    
    task-4728713

--- addons/l10n_pl/__manifest__.py
+++ addons/l10n_pl/__manifest__.py
@@ -22,6 +22,7 @@ Wewnętrzny numer wersji OpenGLOBE 1.02
         'base_iban',
         'base_vat',
         'account',
+        'certificate',
     ],
     'auto_install': ['account'],
     'data': [
