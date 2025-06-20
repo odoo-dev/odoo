75087bc1090a453fc70433f430131d08062bc0bd
dhha-odoo <dhha@odoo.com>
2025-06-20 17:42:12 +0530

    [IMP] stock*: allow selection of dropship operation type in rules
    *: stock_dropshipping
    
    With this commit
    ========================
    
    Previously, only 'Receipt' operation types could be selected for rules
    with the 'Buy' action. However, with a dedicated 'Dropship' operation type,
    it's now possible to distinguish between standard receipts and dropship flows.
    
    This commit enhances the route rule configuration by allowing both
    'Receipt' and 'Dropship' operation types to be selectable for 'Buy' actions.
    Now that 'Dropship' is an operation type, allowing it for 'Buy' rules ensures
    correct flow setup for dropshipping scenarios.
    
    task-4353843

--- addons/stock/models/stock_rule.py
+++ addons/stock/models/stock_rule.py
@@ -80,8 +80,8 @@ class StockRule(models.Model):
     picking_type_id = fields.Many2one(
         'stock.picking.type', 'Operation Type',
         required=True, check_company=True,
-        domain="[('code', '=?', picking_type_code_domain)]")
-    picking_type_code_domain = fields.Char(compute='_compute_picking_type_code_domain')
+        domain="[('code', 'in', picking_type_code_domain)] if picking_type_code_domain else []")
+    picking_type_code_domain = fields.Json(compute='_compute_picking_type_code_domain')
     delay = fields.Integer('Lead Time', default=0, help="The expected date of the created transfer will be computed based on this lead time.")
     partner_address_id = fields.Many2one(
         'res.partner', 'Partner Address',
