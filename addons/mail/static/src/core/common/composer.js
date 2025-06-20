16e9052edc421fc748e674c67ce54b379e0736cd
Akash Parmar <akap@odoo.com>
2025-06-20 11:15:33 +0530

    [IMP] mail: replace anchors with buttons in edit-composer
    Before this commit:
    
    "Cancel" and "Save" actions in the edit-composer were implemented using `<a>`
    elements, which is semantically incorrect for button-like behavior.
    
    After this commit:
    
    These actions are now rendered as `<button>` elements,
    improving semantic correctness.
    Since anchor tags were removed, `preventDefault()` is no longer
    needed and has been removed from `onClickCancelOrSaveEditText`.
    Additionally, the `markup()` calls were refactored to use
    markup template literals(`markup\`...\``).
    
    task-4826935

--- addons/mail/static/src/core/common/composer.js
+++ addons/mail/static/src/core/common/composer.js
@@ -32,7 +32,7 @@ import { _t } from "@web/core/l10n/translation";
 import { useService } from "@web/core/utils/hooks";
 import { createElementWithContent } from "@web/core/utils/html";
 import { FileUploader } from "@web/views/fields/file_handler";
-import { escape, isEmail } from "@web/core/utils/strings";
+import { isEmail } from "@web/core/utils/strings";
 import { isDisplayStandalone, isIOS, isMobileOS } from "@web/core/browser/feature_detection";
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
@@ -94,8 +94,9 @@ export class Composer extends Component {
         this.isIosPwa = isIOS() && isDisplayStandalone();
         this.composerActions = useComposerActions();
         this.OR_PRESS_SEND_KEYBIND = _t("or press %(send_keybind)s", {
-            send_keybind: markup(
-                this.sendKeybinds.map((key) => `<samp>${escape(key)}</samp>`).join(" + ")
+            send_keybind: htmlJoin(
+                this.sendKeybinds.map((key) => markup`<samp>${key}</samp>`),
+                " + "
             ),
         });
         this.store = useService("mail.store");
@@ -244,7 +245,6 @@ export class Composer extends Component {
     }
 
     onClickCancelOrSaveEditText(ev) {
-        ev.preventDefault();
         const composer = toRaw(this.props.composer);
         if (composer.message && ev.target.dataset?.type === EDIT_CLICK_TYPE.CANCEL) {
             this.props.onDiscardCallback(ev);
@@ -259,35 +259,23 @@ export class Composer extends Component {
             return _t(
                 "%(open_button)s%(icon)s%(open_em)sDiscard editing%(close_em)s%(close_button)s",
                 {
-                    open_button: markup(
-                        `<button class='btn px-1 py-0' data-type="${escape(
-                            EDIT_CLICK_TYPE.CANCEL
-                        )}">`
-                    ),
-                    close_button: markup("</button>"),
-                    icon: markup(
-                        `<i class='fa fa-times-circle pe-1' data-type="${escape(
-                            EDIT_CLICK_TYPE.CANCEL
-                        )}"></i>`
-                    ),
-                    open_em: markup(`<em data-type="${escape(EDIT_CLICK_TYPE.CANCEL)}">`),
-                    close_em: markup("</em>"),
+                    open_button: markup`<button class='btn px-1 py-0' data-type="${EDIT_CLICK_TYPE.CANCEL}">`,
+                    close_button: markup`</button>`,
+                    icon: markup`<i class='fa fa-times-circle pe-1' data-type="${EDIT_CLICK_TYPE.CANCEL}"></i>`,
+                    open_em: markup`<em data-type="${EDIT_CLICK_TYPE.CANCEL}">`,
+                    close_em: markup`</em>`,
                 }
             );
         } else {
             const tags = {
-                open_samp: markup("<samp>"),
-                close_samp: markup("</samp>"),
-                open_em: markup("<em>"),
-                close_em: markup("</em>"),
-                open_cancel: markup(
-                    `<a role="button" href="#" data-type="${escape(EDIT_CLICK_TYPE.CANCEL)}">`
-                ),
-                close_cancel: markup("</a>"),
-                open_save: markup(
-                    `<a role="button" href="#" data-type="${escape(EDIT_CLICK_TYPE.SAVE)}">`
-                ),
-                close_save: markup("</a>"),
+                open_samp: markup`<samp>`,
+                close_samp: markup`</samp>`,
+                open_em: markup`<em>`,
+                close_em: markup`</em>`,
+                open_cancel: markup`<button class="btn btn-link fst-italic p-0 align-baseline" data-type="${EDIT_CLICK_TYPE.CANCEL}">`,
+                close_cancel: markup`</button>`,
+                open_save: markup`<button class="btn btn-link fst-italic p-0 align-baseline" data-type="${EDIT_CLICK_TYPE.SAVE}">`,
+                close_save: markup`</button>`,
             };
             return this.env.inChatter
                 ? _t(
@@ -635,9 +623,9 @@ export class Composer extends Component {
      */
     formatDefaultBodyForFullComposer(defaultBody, signature = "") {
         if (signature) {
-            defaultBody = htmlJoin(defaultBody, markup("<br>"), signature);
+            defaultBody = htmlJoin(defaultBody, markup`<br>`, signature);
         }
-        return htmlJoin(markup("<div>"), defaultBody, markup("</div>")); // as to not wrap in <p> by html_sanitize
+        return htmlJoin(markup`<div>`, defaultBody, markup`</div>`); // as to not wrap in <p> by html_sanitize
     }
 
     clear() {
