8831f43d9580cbba387306f8c7e66e8a5982b0e5
svs-odoo <svs@odoo.com>
2025-06-27 13:36:08 +0200

    RIP petit ange parti trop tôt :'(

--- addons/purchase/models/product.py
+++ addons/purchase/models/product.py
@@ -1,10 +1,25 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
-from odoo.tools.float_utils import float_round
-from odoo.exceptions import UserError
+from collections import defaultdict
 from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from odoo.tools import SQL
+
+
+def _sort_purchased_products(products, limit=100):
+    # Helper function to sort purchased products (template and variant.)
+    previously_purchased_products = products.filtered('last_purchase_date').sorted(lambda p:
+        (p.last_purchase_date, -p.id), reverse=True
+    )
+    remaining_limit = max(limit - len(previously_purchased_products), 0)
+    if remaining_limit:
+        remaining_products = (products - previously_purchased_products)[:remaining_limit]
+        products = previously_purchased_products + remaining_products.sorted()
+    else:
+        products = previously_purchased_products
+    return products[:limit]
 
 
 class ProductTemplate(models.Model):
@@ -18,6 +33,21 @@ class ProductTemplate(models.Model):
         help="On ordered quantities: Control bills based on ordered quantities.\n"
             "On received quantities: Control bills based on received quantities.")
     purchase_line_warn_msg = fields.Text('Message for Purchase Order Line')
+    last_purchase_date = fields.Date(compute='_compute_last_purchase_date')
+    last_purchase_since = fields.Char(compute='_compute_last_purchase_date')
+
+    @api.depends_context('partner_id', 'prioritize_for')
+    def _compute_last_purchase_date(self):
+        self.last_purchase_date = False
+        self.last_purchase_since = ''
+        if not self._must_prioritize_purchased_product():
+            return
+        for product_tmpl in self:
+            most_recent_purchased_product = max(
+                [(p.last_purchase_date, p) for p in product_tmpl.product_variant_ids if p.last_invoice_date],
+                default=False)
+            product_tmpl.last_invoice_date = most_recent_purchased_product[0]
+            product_tmpl.last_invoice_since = most_recent_purchased_product[1].last_invoice_since
 
     @api.depends('type')
     def _compute_purchase_method(self):
@@ -51,6 +81,10 @@ class ProductTemplate(models.Model):
         action['display_name'] = _("Purchase History for %s", self.display_name)
         return action
 
+    @api.model
+    def _must_prioritize_purchased_product(self):
+        return bool(self.env.context.get('partner_id') and self.env.context.get('prioritize_for') == 'purchase')
+
 
 class ProductProduct(models.Model):
     _inherit = 'product.product'
@@ -62,6 +96,65 @@ class ProductProduct(models.Model):
         compute='_compute_is_in_purchase_order',
         search='_search_is_in_purchase_order',
     )
+    last_purchase_date = fields.Date(compute='_compute_last_purchase_date')
+    last_purchase_since = fields.Char(compute='_compute_last_purchase_date')
+
+    @api.depends_context('formatted_display_name', 'partner_id', 'prioritize_for')
+    def _compute_display_name(self):
+        super()._compute_display_name()
+
+        # Add last invoiced date beside the product's name.
+        if self.env.context.get('formatted_display_name') and\
+            self.env['product.template']._must_prioritize_purchased_product():
+            for product in self:
+                if product.last_purchase_date:
+                    product.display_name = f'`{product.last_purchase_since}` **{product.display_name}**'
+
+    @api.depends_context('partner_id', 'prioritize_for')
+    def _compute_last_purchase_date(self):
+        self.last_purchase_date = False
+        self.last_purchase_since = ''
+        if not self.env['product.template']._must_prioritize_purchased_product():
+            return
+        now = fields.Datetime.now()
+        partner_id = self.env.context['partner_id']
+        products_and_last_purchase_date = self._get_products_and_last_purchase_date(partner_id)
+        for product_and_date in products_and_last_purchase_date:
+            product = self.browse(product_and_date['product_id'])
+            last_purchase_date = product_and_date['last_purchase_date']
+            last_purchase_date = last_purchase_date if last_purchase_date <= now else now
+            product.last_purchase_date = last_purchase_date
+            days_count = (now - last_purchase_date).days
+            product.last_purchase_since = self._get_last_invoice_since(days_count)
+
+    def _get_products_and_last_purchase_date(self, partner_id):
+        print('\n\n1. Search relevant PO.')
+        purchase_order_ids = self.env['purchase.order'].search([
+            ('partner_id', '=', partner_id),
+            ('state', '=', 'purchase'),
+            ('date_approve', '>', (fields.Datetime.now() - relativedelta(years=1))),
+        ]).ids
+        print(f'\n\n2. Relevant PO found: {len(purchase_order_ids)}.')
+        if not purchase_order_ids:
+            # No need to search for PO lines if there is no matching PO.
+            print('\n\n3. No relevant PO found, no second query.')
+            return []
+        print('\n\n3. Search most recent purchase date by product.')
+        sql_query = SQL("""
+            SELECT pol.product_id, MAX(po.date_approve) AS last_purchase_date
+            FROM purchase_order_line pol
+            JOIN purchase_order po ON pol.order_id = po.id
+            WHERE order_id IN %(purchase_order_ids)s
+                AND product_id IN %(product_ids)s
+            GROUP BY pol.product_id
+            """,
+            purchase_order_ids=tuple(purchase_order_ids),
+            product_ids=tuple(self.ids),
+        )
+        self.env.cr.execute(sql_query)
+        res = self.env.cr.dictfetchall()
+        print(f'\n\n4. Result: {res}')
+        return res
 
     def _compute_purchased_product_qty(self):
         date_from = fields.Datetime.to_string(fields.Date.context_today(self) - relativedelta(years=1))
@@ -102,6 +195,15 @@ class ProductProduct(models.Model):
         ]).product_id.ids
         return [('id', 'in', product_ids)]
 
+    @api.model
+    def name_search(self, name='', domain=None, operator='ilike', limit=100):
+        if not name and self.env['product.template']._must_prioritize_purchased_product():
+            products = self.search(domain)
+            products = _sort_purchased_products(products, limit)
+            return [(product.id, product.display_name) for product in products]
+        else:
+            return super().name_search(name, domain, operator, limit)
+
     def action_view_po(self):
         action = self.env["ir.actions.actions"]._for_xml_id("purchase.action_purchase_history")
         action['domain'] = ['&', ('state', '=', 'purchase'), ('product_id', 'in', self.ids)]
@@ -110,6 +212,15 @@ class ProductProduct(models.Model):
 
     def _get_backend_root_menu_ids(self):
         return super()._get_backend_root_menu_ids() + [self.env.ref('purchase.menu_purchase_root').id]
+
+    @api.model
+    def _get_last_invoice_since(self, days_count):
+        if days_count > 365:
+            return self.env._('%(years_count)sy', years_count=(days_count // 365))
+        elif days_count > 30:
+            return self.env._('%(months_count)smo', months_count=(days_count // 30))
+        else:
+            return self.env._('%(days_count)sd', days_count=days_count)
 
     def _update_uom(self, to_uom_id):
         for uom, product, po_lines in self.env['purchase.order.line']._read_group(
