56ecfeba5c8bce486cc32cf241685ed43aacb663
FrancoisGe <fge@odoo.com>
2025-06-30 11:29:22 +0200

    [FIX] website: test warning

--- addons/html_editor/static/src/core/history_plugin.js
+++ addons/html_editor/static/src/core/history_plugin.js
@@ -6,6 +6,27 @@ import { withSequence } from "@html_editor/utils/resource";
 import { Deferred } from "@web/core/utils/concurrency";
 import { toggleClass } from "@html_editor/utils/dom";
 
+function toStringNode(node) {
+    if (node.tagName) {
+        return `\nNode(${node.tagName}, ${JSON.stringify(
+            node.attributes
+        )}") \n Children: ${node.children.map(toStringNode).join(", ")}`;
+    }
+    return `\n Node(${node.textContent}")`;
+}
+function toStringMutation(m) {
+    if (m.type === "characterData") {
+        return `\n CharacterData(${m.id}, "${m.text}", "${m.oldValue}")`;
+    }
+    if (m.type === "add") {
+        return `\n Add(${toStringNode(m.node)}`;
+    }
+    if (m.type === "remove") {
+        return `\n Remove(${toStringNode(m.node)}`;
+    }
+    return `\n ${m.type}(${m.id}, "${m.value}", "${m.oldValue}")`;
+}
+
 /**
  * @typedef { import("./selection_plugin").EditorSelection } EditorSelection
  *
@@ -408,6 +429,7 @@ export class HistoryPlugin extends Plugin {
     handleNewRecords(records) {
         const filteredRecords = this.processNewRecords(records);
         if (filteredRecords.length) {
+            console.trace(filteredRecords);
             // TODO modify `handleMutations` of web_studio to handle
             // `undoOperation`
             const stepState = this.stepsStates.get(this.currentStep.id);
@@ -1374,6 +1396,16 @@ export class HistoryPlugin extends Plugin {
         if (this.ignoreIsCurrentStepModified) {
             return false;
         }
+        const mutations = this.currentStep.mutations.filter((m) =>
+            ["characterData", "remove", "add"].includes(m.type)
+        );
+        console.log(mutations);
+        if (mutations.length) {
+            for (const m of mutations) {
+                console.warn(toStringMutation(m));
+            }
+        }
+
         return this.currentStep.mutations.find((m) =>
             ["characterData", "remove", "add"].includes(m.type)
         );
