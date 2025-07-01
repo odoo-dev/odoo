199d9f33ac631ec4fb6844886c11236571a3e130
Arnold Moyaux <arm@odoo.com>
2025-07-01 16:26:58 +0200

    [REF] stock_account: adapt landed costs
    No more impact on valuation, only used to dispatch the landed cost
    among products value

--- addons/stock_landed_costs/models/account_move.py
+++ addons/stock_landed_costs/models/account_move.py
@@ -31,7 +31,7 @@ class AccountMove(models.Model):
             'cost_lines': [(0, 0, {
                 'product_id': l.product_id.id,
                 'name': l.product_id.name,
-                'account_id': l.product_id.product_tmpl_id.get_product_accounts()['stock_input'].id,
+                'account_id': l.product_id.product_tmpl_id.get_product_accounts()['stock_valuation'].id,
                 'price_unit': sign * l.currency_id._convert(l.price_subtotal, l.company_currency_id, l.company_id, self.invoice_date or fields.Date.context_today(l)),
                 'split_method': l.product_id.split_method_landed_cost or 'equal',
             }) for l in landed_costs_lines],
@@ -46,11 +46,6 @@ class AccountMove(models.Model):
         context = dict(self.env.context, default_vendor_bill_id=self.id)
         views = [(self.env.ref('stock_landed_costs.view_stock_landed_cost_tree2').id, 'list'), (False, 'form'), (False, 'kanban')]
         return dict(action, domain=domain, context=context, views=views)
-
-    def _post(self, soft=True):
-        posted = super()._post(soft)
-        posted.sudo().landed_costs_ids.reconcile_landed_cost()
-        return posted
 
     def _update_order_line_info(self, product_id, quantity, **kwargs):
         price_unit = super()._update_order_line_info(product_id, quantity, **kwargs)
@@ -77,10 +72,6 @@ class AccountMoveLine(models.Model):
     def _onchange_is_landed_costs_line(self):
         if self.is_landed_costs_line and self.product_id and self.product_type != 'service':
             self.is_landed_costs_line = False
-
-    def _get_stock_valuation_layers(self, move):
-        layers = super()._get_stock_valuation_layers(move)
-        return layers.filtered(lambda svl: not svl.stock_landed_cost_id)
 
     def _eligible_for_cogs(self):
         return super()._eligible_for_cogs() or (
