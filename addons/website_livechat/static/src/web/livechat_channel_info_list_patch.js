bf183d912659477818adb8504d4e7c9c83801f7d
Alex Kühn <aku@odoo.com>
2025-07-02 11:48:33 +0200

    wip aku

--- addons/website_livechat/static/src/web/livechat_channel_info_list_patch.js
+++ addons/website_livechat/static/src/web/livechat_channel_info_list_patch.js
@@ -1,4 +1,6 @@
 import { LivechatChannelInfoList } from "@im_livechat/core/web/livechat_channel_info_list";
+import { formatDateTime } from "@web/core/l10n/dates";
+import { _t } from "@web/core/l10n/translation";
 import { patch } from "@web/core/utils/patch";
 
 /** @type {import("models").ChannelMember} */
@@ -9,6 +11,9 @@ const livechatChannelInfoListPatch = {
                 (channel) => channel?.id !== this.props.thread.id
             ) || []
         );
+    },
+    CLOSED_ON_TEXT(thread) {
+        return _t("(closed on: %(date)s)", { date: formatDateTime(thread.livechat_end_dt) });
     },
 };
 
