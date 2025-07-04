4b6d3b6d75066b15e9990bf3258a614b98586b92
Brice bib Bartoletti <bib@odoo.com>
2025-07-04 18:25:17 +0200

    [REF] base: inherit_branding recursive -> iterative
    The aim of this commit is to consume less memory by avoiding to
    instantiate a full python frame on the stack.
    
    Task: None

--- odoo/addons/base/models/ir_ui_view.py
+++ odoo/addons/base/models/ir_ui_view.py
@@ -878,14 +878,17 @@ actual arch.
         return locate_node(arch, spec)
 
     def inherit_branding(self, specs_tree):
-        for node in specs_tree.iterchildren(tag=etree.Element):
-            xpath = node.getroottree().getpath(node)
+        stack = list(specs_tree.iterchildren(tag=etree.Element, reversed=True))
+        while stack:
+            node = stack.pop()
             if node.tag == 'data' or node.tag == 'xpath' or node.get('position'):
-                self.inherit_branding(node)
+                stack.append(node)
             elif node.get('t-field'):
+                xpath = node.getroottree().getpath(node)
                 node.set('data-oe-xpath', xpath)
-                self.inherit_branding(node)
+                stack.append(node)
             else:
+                xpath = node.getroottree().getpath(node)
                 node.set('data-oe-id', str(self.id))
                 node.set('data-oe-xpath', xpath)
                 node.set('data-oe-model', 'ir.ui.view')
