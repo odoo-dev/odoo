16aaacc5c51b746628f5312dd964538ab7c3c50a
khsr-odoo <khsr@odoo.com>
2025-07-10 11:49:15 +0530

    [FIX] base_geolocalize: fix crash when computing geolocation for unnamed partner

--- addons/base_geolocalize/models/res_partner.py
+++ addons/base_geolocalize/models/res_partner.py
@@ -54,9 +54,10 @@ class ResPartner(models.Model):
             else:
                 partners_not_geo_localized |= partner
         if partners_not_geo_localized:
+            names = ', '.join(filter(None, partners_not_geo_localized.mapped('name')))
             self.env['bus.bus']._sendone(self.env.user.partner_id, 'simple_notification', {
                 'type': 'danger',
                 'title': _("Warning"),
-                'message': _('No match found for %(partner_names)s address(es).', partner_names=', '.join(partners_not_geo_localized.mapped('name')))
+                'message': _('No match found for %(partner_names)s address(es).', partner_names=names)
             })
         return True
