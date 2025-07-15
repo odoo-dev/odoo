f2bcfa2e1266696617f3bf766606cad49427251f
rare-odoo <rare@odoo.com>
2025-07-15 12:27:37 +0530

    [IMP] contact_base: enforce mandatory name field in contact & addresses form
    Updated the res.partner form view to make the name field required for all
    contacts added via the Contact & Addresses section.

---
+++ addons/kmed_customization/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contact Base Customization",
+    'version': '1.0',
+    'category': 'Hidden/Tests',
+    'description': """
+This module tests the changes done in standard modules for customization.
+    """,
+    'depends': [
+        'contacts',
+        'base',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
