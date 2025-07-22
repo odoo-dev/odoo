04d5497478795ea4f06b4d6aff96265cf72c4949
svs-odoo <svs@odoo.com>
2025-07-22 15:50:12 +0200

    wip clean report

--- addons/stock_account/static/src/stock_valuation/line/toggle_line.js
+++ addons/stock_account/static/src/stock_valuation/line/toggle_line.js
@@ -1,23 +1,8 @@
-import { useState } from "@odoo/owl";
 import { StockValuationReportLine } from "./line";
 
 
 export class StockValuationReportToggleLine extends StockValuationReportLine {
     static template = "stock_account.StockValuationReport.InventoryValuationToggleLine";
-
-    // setup() {
-    //     super.setup();
-    //     this.state = useState({
-    //         checked: false,
-    //         displaySublines: true,
-    //     });
-    // }
-
-    // On Click Methods --------------------------------------------------------
-    onClickToggle() {
-        super.onClickToggle();
-        this.state.checked = !this.state.checked;
-    }
 }
 
 StockValuationReportToggleLine.components.StockValuationReportToggleLine = StockValuationReportToggleLine;
