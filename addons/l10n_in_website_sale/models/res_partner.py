e3a9a3569e6742be2aff5b5e8ed7a6056eac869b
Manav Shah <sham@odoo.com>
2025-08-06 19:16:39 +0530

    [FIX] l10n_in_website_sale: Update Fiscal only if it's SEZ or empty

--- addons/l10n_in_website_sale/models/res_partner.py
+++ addons/l10n_in_website_sale/models/res_partner.py
@@ -5,33 +5,20 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     def _update_l10n_in_gst_treatment_and_fp_from_iap_autocomplete(self):
+        sez_fp = self.env['account.chart.template'].ref('fiscal_position_in_export_sez_in', raise_if_not_found=False)
+        response = self.env['res.partner'].enrich_by_gst(self.vat) if self.vat else {}
 
-        def _reset_gst_values():
-            self.update({
-                'l10n_in_gst_treatment': 'consumer',
-                'property_account_position_id': False,
-            })
-
-        if not self.vat:
-            _reset_gst_values()
-            return
-
-        response = self.env['res.partner'].enrich_by_gst(self.vat)
-        # Response is empty when invalid gst number is sent.
+        # Response is empty when GSTIN is invalid or removed.
         if not response:
-            _reset_gst_values()
-            return
-        if (
-            response.get('error')
-            or (gst_treatment := response.get('l10n_in_gst_treatment', 'regular')) == self.l10n_in_gst_treatment
-        ):
+            self.l10n_in_gst_treatment = 'consumer'
+            if self.property_account_position_id == sez_fp:
+                self.property_account_position_id = False
             return
 
-        fiscal_position = (
-            gst_treatment == 'special_economic_zone'
-            and self.env['account.chart.template'].ref('fiscal_position_in_export_sez_in', raise_if_not_found=False)
-        )
-        self.update({
-            'l10n_in_gst_treatment': gst_treatment,
-            'property_account_position_id': fiscal_position and fiscal_position.id or False,
-        })
+        if response.get('error'):
+            return
+
+        gst_treatment = response.get('l10n_in_gst_treatment', 'regular')
+        if sez_fp and (not self.property_account_position_id or self.property_account_position_id == sez_fp):
+            self.property_account_position_id = sez_fp.id if gst_treatment == 'special_economic_zone' else False
+        self.l10n_in_gst_treatment = gst_treatment
