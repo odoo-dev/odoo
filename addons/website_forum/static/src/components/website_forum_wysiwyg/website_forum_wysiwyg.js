788924862025da1a37b7ffd508ef26ab030ea2b1
Rodolpho Lima <rcdl@odoo.com>
2025-08-21 16:39:02 +0200

    [REF] use resizer hook

--- addons/website_forum/static/src/components/website_forum_wysiwyg/website_forum_wysiwyg.js
+++ addons/website_forum/static/src/components/website_forum_wysiwyg/website_forum_wysiwyg.js
@@ -1,46 +1,37 @@
 import { removeClass } from "@html_editor/utils/dom";
-import { Component, markup, onMounted, useExternalListener } from "@odoo/owl";
+import { markup, onMounted, useExternalListener } from "@odoo/owl";
 import { BASIC_PLUGINS, FULL_EDIT_PLUGINS } from "../../plugins/plugin_sets";
-import { ResizableWysiwyg } from "../resizable_wysiwyg";
-import { Resizer } from "../resizer/resizer";
+import { useResizer } from "./resizer_hook";
+import { Wysiwyg } from "@html_editor/wysiwyg";
+import { isMobileOS } from "@web/core/browser/feature_detection";
 
-export class WebsiteForumWysiwyg extends Component {
-    static template = "html_editor.WebsiteForumWysiwyg";
-    static components = { ResizableWysiwyg, Resizer };
+export class WebsiteForumWysiwyg extends Wysiwyg {
+    static template = "website_forum.WebsiteForumWysiwyg";
     static props = {
+        ...super.props,
         textareaEl: HTMLElement,
         fullEdit: Boolean,
         getRecordInfo: Function,
+        resizable: { type: Boolean, optional: true },
+        initialHeight: { type: Number, optional: true },
+        minHeight: { type: Number, optional: true },
+    };
+    static defaultProps = {
+        ...super.defaultProps,
+        contentClass: "note-editable",
+        resizable: !isMobileOS(),
+        initialHeight: 350,
+        minHeight: 100,
     };
 
+    /** @override */
     setup() {
+        super.setup();
+        if (this.props.resizable) {
+            // Event listener added on template.
+            this.onResizerMouseDown = useResizer("content", this.props.minHeight);
+        }
         const form = this.props.textareaEl.closest("form");
-        this.editor = null;
-        let contentRef;
-        this.wysiwygProps = {
-            setContentRef: (ref) => (contentRef = ref),
-            onLoad: (editor) => (this.editor = editor),
-            config: {
-                getRecordInfo: this.props.getRecordInfo,
-                Plugins: this.props.fullEdit ? FULL_EDIT_PLUGINS : BASIC_PLUGINS,
-                content: this.getTextAreaContent(),
-                resources: {
-                    start_edition_handlers: () => this.cleanImageClasses(this.editor.editable),
-                    clean_for_save_handlers: ({ root }) => this.cleanImageClasses(root),
-                },
-                defaultLinkAttributes: { rel: "ugc" },
-                // TODO: check if this depends on full edit (it does indirectly, as there's
-                // no media plugin without full edit)
-                dropImageAsAttachment: true,
-                allowImageTransform: this.props.fullEdit,
-            },
-            contentClass: "note-editable",
-        };
-        this.resizerProps = {
-            getTargetRef: () => contentRef,
-            initialHeight: 350,
-            minHeight: 100,
-        };
         // Prevent form submission behavior of buttons inside the form
         onMounted(() =>
             form.querySelectorAll(".o-wysiwyg button").forEach((btn) => (btn.type = "button"))
@@ -48,6 +39,25 @@ export class WebsiteForumWysiwyg extends Component {
         this.submitButton = form.querySelector("button[type=submit]");
         useExternalListener(this.submitButton, "click", this.onSubmitButtonClick.bind(this));
         this.readyForSubmit = false;
+    }
+
+    /** @override */
+    getEditorConfig() {
+        return {
+            getRecordInfo: this.props.getRecordInfo,
+            Plugins: this.props.fullEdit ? FULL_EDIT_PLUGINS : BASIC_PLUGINS,
+            content: this.getTextAreaContent(),
+            resources: {
+                start_edition_handlers: () => this.cleanImageClasses(this.editor.editable),
+                clean_for_save_handlers: ({ root }) => this.cleanImageClasses(root),
+            },
+            defaultLinkAttributes: { rel: "ugc" },
+            // TODO: check if this depends on full edit (it does indirectly, as there's
+            // no media plugin without full edit)
+            dropImageAsAttachment: true,
+            allowImageTransform: this.props.fullEdit,
+            height: `${this.props.initialHeight}px`,
+        };
     }
 
     cleanImageClasses(root) {
