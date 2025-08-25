7f01506c97dee70186ef4f10ed65eaaf23872f59
igbe <igbe@odoo.com>
2025-08-25 17:18:54 +0200

    [FIX] account: improve pdf report layout with sections
    Improve the pdf layout for sections & subsections.
    
    1 - Add a horizontal padding like:
        - Section: padding = 0
        - Product into Section: padding = 2
        - Subsections: padding = 2
        - Product into subsection: padding = 4
    
    2 - Sections & Subsections line are bold if there are not in Hide composition. Otherwise, the line will be displayed as a product line.
    
    3 - Improve the Hide prices. Now, we do the same grouping as the Hide composition, meaning we group lines by group of taxes. We also hide the tax on product line, showing the tax only on the section line.
    
    4 - Remove the "Subtotal: " lines. Now the subtotals will be displayed on section lines.
    
    task-4966574

--- addons/account/models/account_move_line.py
+++ addons/account/models/account_move_line.py
@@ -3389,16 +3389,21 @@ class AccountMoveLine(models.Model):
         self.ensure_one()
         return self.move_id.state == 'posted'
 
-    def get_lines_grouped_by_section(self):
+    def get_lines_grouped_by_section(self, grouped=True):
         """
         Return a tax-wise summary of sale order lines linked to section.
         Groups lines by their tax IDs and computes subtotal and total for each group.
         """
         self.ensure_one()
 
-        section_lines = self.child_ids + self.child_ids.child_ids
+        section_lines = self.move_id.invoice_line_ids.filtered_domain([('id', 'child_of', self.id)]) - self
         result = []
         for taxes, lines in groupby(section_lines, key=lambda l: l.tax_ids):
+            amls = sum(lines, start=self.env['account.move.line'])
+            for aml in amls.sorted('sequence'):
+                if aml.parent_id != self and aml.parent_id not in amls:
+                    amls += aml.parent_id
+
             tax_labels = [tax.tax_label for tax in taxes if tax.tax_label]
             subtotal = sum(l.price_subtotal for l in lines)
             price_total = sum(l.price_total for l in lines)
@@ -3410,7 +3415,25 @@ class AccountMoveLine(models.Model):
                     'ids': [line.id for line in section_lines],
                     'price_subtotal': subtotal,
                     'price_total': price_total,
+                    'display_type': self.display_type if not grouped else 'product',
+                    'quantity': 1,
+                    'discount': self.discount,
                 })
+
+                if not grouped:
+                    for line in amls.sorted('sequence'):
+                        if line != self:
+                            result.append({
+                                'name': line.name,
+                                'ids': [line.id],
+                                'price_subtotal': sum(l.price_subtotal for l in amls.filtered_domain([('id', 'child_of', line.id)])),
+                                'price_total': sum(l.price_total for l in amls.filtered_domain([('id', 'child_of', line.id)])),
+                                'display_type': line.display_type,
+                                'quantity': line.quantity,
+                                'line_uom': line.product_uom_id,
+                                'product_uom': line.product_id.uom_id,
+                                'discount': line.discount,
+                            })
 
         return result or [{
             'name': self.name,
