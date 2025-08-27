6a9fded2fa517e4c7c9204015dd788ac0394b326
srap-odoo <srap@odoo.com>
2025-08-27 14:31:33 +0530

    [FIX] sale_stock: adjust valuation tests after valuationpocalypse
    Updated the reconciliation tests:
    Instead of relying on check_reconciliation in accounts, it now validates
    balanced entries on accounts.

--- addons/sale_stock/tests/test_anglo_saxon_valuation_reconciliation.py
+++ addons/sale_stock/tests/test_anglo_saxon_valuation_reconciliation.py
@@ -1,7 +1,5 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from unittest import skip
-
 from odoo.addons.stock_account.tests.test_anglo_saxon_valuation_reconciliation_common import ValuationReconciliationTestCommon
 from odoo.tests import Form, tagged
 
@@ -69,7 +67,6 @@ class TestValuationReconciliationCommon(ValuationReconciliationTestCommon):
 
 
 @tagged('post_install', '-at_install')
-@skip('Temporary to fast merge new valuation')
 class TestValuationReconciliation(TestValuationReconciliationCommon):
 
     @classmethod
@@ -98,8 +95,13 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
 
         invoice = self._create_invoice_for_so(sale_order, test_product, '2018-02-12')
         invoice.action_post()
-        picking = self.env['stock.picking'].search([('sale_id', '=', sale_order.id)])
-        self.check_reconciliation(invoice, picking, operation='sale')
+        self.assertRecordValues(invoice.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 33.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_tax_sale'].id, 'credit': 4.95, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 0.0, 'debit': 37.95},
+            {'account_id': self.company_data['default_account_stock_valuation'].id, 'credit': 42.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 0.0, 'debit': 42.0},
+        ])
 
     def test_invoice_shipment(self):
         """ Tests the case into which we make the invoice first, and then send
@@ -118,7 +120,13 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
         self._process_pickings(sale_order.picking_ids)
 
         picking = self.env['stock.picking'].search([('sale_id', '=', sale_order.id)])
-        self.check_reconciliation(invoice, picking, operation='sale')
+        self.assertRecordValues(invoice.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 33.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_tax_sale'].id, 'credit': 4.95, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 0.0, 'debit': 37.95},
+            {'account_id': self.company_data['default_account_stock_valuation'].id, 'credit': 13.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 0.0, 'debit': 13.0},
+        ])
 
         #return the goods and refund the invoice
         stock_return_picking_form = Form(self.env['stock.return.picking']
@@ -139,7 +147,13 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
         self.assertEqual(invoice.payment_state, 'reversed', "Invoice should be in 'reversed' state.")
         self.assertEqual(invoice.reversal_move_ids.payment_state, 'paid', "Refund should be in 'paid' state.")
         self.assertEqual(new_invoice.state, 'draft', "New invoice should be in 'draft' state.")
-        self.check_reconciliation(invoice.reversal_move_ids, return_pick, operation='sale')
+        self.assertRecordValues(invoice.reversal_move_ids.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 0.0, 'debit': 33.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 37.95, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_stock_valuation'].id, 'credit': 0.0, 'debit': 13.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 13.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_tax_sale'].id, 'credit': 0.0, 'debit': 4.95},
+        ])
 
     def test_multiple_shipments_invoices(self):
         """ Tests the case into which we deliver part of the goods first, then 2 invoices at different rates, and finally the remaining quantities
@@ -150,23 +164,38 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
         sale_order = self._create_sale(test_product, '2018-01-01', quantity=5)
 
         self._process_pickings(sale_order.picking_ids, quantity=2.0)
-        picking = self.env['stock.picking'].search([('sale_id', '=', sale_order.id)], order="id asc", limit=1)
-
         invoice = self._create_invoice_for_so(sale_order, test_product, '2018-02-03', quantity=3)
         invoice.action_post()
-        self.check_reconciliation(invoice, picking, full_reconcile=False, operation='sale')
+        self.assertRecordValues(invoice.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 99.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_tax_sale'].id, 'credit': 14.85, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 0.0, 'debit': 113.85},
+            {'account_id': self.company_data['default_account_stock_valuation'].id, 'credit': 252.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 0.0, 'debit': 252.0},
+        ])
 
         invoice2 = self._create_invoice_for_so(sale_order, test_product, '2018-03-12', quantity=2)
         invoice2.action_post()
-        self.check_reconciliation(invoice2, picking, full_reconcile=False, operation='sale')
 
+        self.assertRecordValues(invoice2.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 66.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_tax_sale'].id, 'credit': 9.9, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 0.0, 'debit': 75.90},
+            {'account_id': self.company_data['default_account_stock_valuation'].id, 'credit': 168.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 0.0, 'debit': 168.0},
+        ])
         self._process_pickings(sale_order.picking_ids.filtered(lambda x: x.state != 'done'), quantity=3.0)
-        picking = self.env['stock.picking'].search([('sale_id', '=', sale_order.id)], order='id desc', limit=1)
-        self.check_reconciliation(invoice2, picking, operation='sale')
+        self.assertRecordValues(invoice2.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 66.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_tax_sale'].id, 'credit': 9.9, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 0.0, 'debit': 75.90},
+            {'account_id': self.company_data['default_account_stock_valuation'].id, 'credit': 168.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 0.0, 'debit': 168.0},
+        ])
 
     def test_fifo_multiple_products(self):
-        """ Test Automatic Inventory Valuation with FIFO costs method, 3 products,
-            2,3,4 out svls and 2 in moves by product. This tests a more complex use case with anglo-saxon accounting.
+        """ Test Automatic Inventory Valuation with FIFO costs method, 2 products,
+            This tests a more complex use case with anglo-saxon accounting.
         """
         wh = self.env['stock.warehouse'].search([
             ('company_id', '=', self.env.company.id),
@@ -182,12 +211,8 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
         } for i in range(1, 3)]))
         product_1.categ_id.property_valuation = 'real_time'
         product_1.categ_id.property_cost_method = 'fifo'
-        # give another output account to product_2
         categ_2 = product_1.categ_id.copy()
-        account_2 = categ_2.property_stock_account_output_categ_id.copy()
-        categ_2.property_stock_account_output_categ_id = account_2
         product_2.categ_id = categ_2
-        # Create out_svls
         so = self.env['sale.order'].sudo().create({
             'partner_id': self.partner_a.id,
             'currency_id': self.other_currency.id,
@@ -213,7 +238,6 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
             'invoice_date': '2021-01-10',
             'invoice_line_ids': [(0, 0, {
                 'name': 'test line',
-                'account_id': self.company_data['default_account_revenue'].id,
                 'price_unit': 10.0,
                 'quantity': 2,
                 'discount': 0.0,
@@ -224,7 +248,15 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
 
         so.invoice_ids += inv
         inv.action_post()
-        # Create in_moves for P1/P2 such that the first move compensates the out_svls
+        self.assertRecordValues(inv.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 10.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 10.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 10.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_tax_sale'].id, 'credit': 4.5, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 0.0, 'debit': 34.5},
+        ])
+
+        # Create in_moves for P1/P2 such that the first move compensates the out moves
         in_moves = self.env['stock.move'].create([{
             'description_picking': '%s-%s' % (str(quantity), str(product)),  # to not merge the moves
             'product_id': product.id,
@@ -243,34 +275,33 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
             move.quantity = move.product_uom_qty
             move.picked = True
         in_moves._action_done()
-
-        self.assertEqual(product_1.value_svl, -20)
-        self.assertEqual(product_2.value_svl, 0)
-        # Check that the correct number of amls have been created and posted
-        input_aml = self.env['account.move.line'].search([
-            ('account_id', '=', product_1.categ_id.property_stock_account_input_categ_id.id),
-        ], order='date, id')
-        output1_aml = self.env['account.move.line'].search([
-            ('account_id', '=', product_1.categ_id.property_stock_account_output_categ_id.id),
-        ], order='date, id')
-        output2_aml = self.env['account.move.line'].search([
-            ('account_id', '=', product_2.categ_id.property_stock_account_output_categ_id.id),
-        ], order='date, id')
-        valo_aml = self.env['account.move.line'].search([
-            ('account_id', '=', product_1.categ_id.property_stock_valuation_account_id.id),
-        ], order='date, id')
-        self.assertEqual(len(input_aml), 2)
-        self.assertEqual(len(output1_aml), 6)
-        self.assertEqual(len(output2_aml), 4)
-        self.assertEqual(len(valo_aml), 7)
-        # All amls should be reconciled
-        self.assertTrue(all(aml.reconciled for aml in output1_aml + output2_aml))
+        bill = self.env['account.move'].create({
+            'currency_id': self.other_currency.id,
+            'move_type': 'in_invoice',
+            'invoice_date': '2021-01-10',
+            'invoice_line_ids': [(0, 0, {
+                'name': 'bill line',
+                'account_id': self.company_data['default_account_expense'].id,
+                'price_unit': 10.0,
+                'quantity': 2,
+                'discount': 0.0,
+                'product_id': product.id,
+            }) for product in [product_1, product_2]],
+        })
+        self.assertEqual(product_1.total_value, 0)
+        self.assertEqual(product_1.qty_available, -2)
+        self.assertEqual(product_2.total_value, 0)
+        self.assertEqual(product_2.qty_available, 0)
+        self.assertRecordValues(bill.line_ids, [
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 0.0, 'debit': 10.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 0.0, 'debit': 10.0},
+            {'account_id': self.company_data['default_account_tax_purchase'].id, 'credit': 0.0, 'debit': 3.0},
+            {'account_id': self.company_data['default_account_payable'].id, 'credit': 23.0, 'debit': 0.0},
+        ])
 
     def test_anglo_saxon_valuation_reconciliation(self):
-        """In some particular cases, _stock_account_anglo_saxon_reconcile_valuation tries to reconcile the same account_move_line twice.
-        This test checks if there is a step in the method that prevents this.
-        """
 
+        """This test checks anglo saxon accounting flow works well with invoice/credit note."""
         self.env.company.anglo_saxon_accounting = True
 
         products = [self.test_product_delivery, self.test_product_delivery_2]
@@ -289,13 +320,11 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
             line_form.product_id = products[0]
             line_form.price_unit = products[0].standard_price
             line_form.quantity = 1
-            line_form.account_id = self.company_data['default_account_stock_out']
             line_form.tax_ids.clear()
         with move_form.invoice_line_ids.new() as line_form:
             line_form.product_id = products[1]
             line_form.price_unit = products[1].standard_price
             line_form.quantity = 1
-            line_form.account_id = self.company_data['default_account_stock_out']
             line_form.tax_ids.clear()
         invoice_1 = move_form.save()
 
@@ -306,12 +335,9 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
         with move_form.invoice_line_ids.new() as line_form:
             line_form.product_id = products[1]
             line_form.price_unit = products[1].standard_price
-            line_form.account_id = self.company_data['default_account_stock_out']
             line_form.quantity = 1
             line_form.tax_ids.clear()
         invoice_2 = move_form.save()
-
-        invoice_2.action_post()
 
         # Create invoice 3
         move_form = Form(self.env["account.move"].with_context(default_move_type="out_refund"))
@@ -320,12 +346,9 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
         with move_form.invoice_line_ids.new() as line_form:
             line_form.product_id = products[0]
             line_form.price_unit = products[0].standard_price
-            line_form.account_id = self.company_data['default_account_stock_out']
             line_form.quantity = 1
             line_form.tax_ids.clear()
         invoice_3 = move_form.save()
-
-        invoice_3.action_post()
 
         # Creating stock moves and associated sale order lines
         stock_location = self.env['stock.warehouse'].search([
@@ -336,6 +359,7 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
             'location_id': stock_location.id,
             'location_dest_id': self.ref('stock.stock_location_customers'),
             'picking_type_id': stock_location.warehouse_id.out_type_id.id,
+            'sale_id': sale_order.id
         })
 
         sm_1 = self.env['stock.move'].create({
@@ -345,7 +369,6 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
             'location_id': out_picking.location_id.id,
             'location_dest_id': out_picking.location_dest_id.id,
             'picking_id': out_picking.id,
-            'account_move_ids': invoice_1 | invoice_3,
             'state': 'done'
         })
 
@@ -362,7 +385,6 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
             'location_id': out_picking.location_id.id,
             'location_dest_id': out_picking.location_dest_id.id,
             'picking_id': out_picking.id,
-            'account_move_ids': invoice_1 | invoice_2,
             'state': 'done'
         })
 
@@ -372,19 +394,32 @@ class TestValuationReconciliation(TestValuationReconciliationCommon):
             'move_ids': sm_2
         })
 
-        invoice_1.invoice_line_ids.sale_line_ids = sale_order_line_1 | sale_order_line_2
+        sale_order.action_confirm()
+        invoice_2.invoice_line_ids.sale_line_ids = sale_order_line_1
+        invoice_2.action_post()
 
-        # Creating a stock valuation layer for invoice_2 to populate the no_exchange_reconcile_plan
-        svl_vals = {
-            'company_id': self.env.company.id,
-            'product_id': products[1].id,
-            'description': "description",
-            'unit_cost': products[1].standard_price,
-            'quantity': 1,
-        }
-
-        invoice_2.stock_valuation_layer_ids |= self.env['stock.valuation.layer'].create(svl_vals)
-        invoice_2.stock_valuation_layer_ids.stock_valuation_layer_id |= self.env['stock.valuation.layer'].create(svl_vals)
+        invoice_3.invoice_line_ids.sale_line_ids = sale_order_line_2
+        invoice_3.action_post()
+        invoice_1.invoice_line_ids[0].sale_line_ids = sale_order_line_1
+        invoice_1.invoice_line_ids[1].sale_line_ids = sale_order_line_2
 
         invoice_1.action_post()
         self.assertEqual(invoice_1.state, 'posted')
+
+        self.assertRecordValues(invoice_1.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 42.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 42.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 0.0, 'debit': 84.0},
+            {'account_id': self.company_data['default_account_stock_valuation'].id, 'credit': 42.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 0.0, 'debit': 42.0},
+        ])
+        self.assertRecordValues(invoice_2.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 0.0, 'debit': 42.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 42.0, 'debit': 0.0},
+        ])
+        self.assertRecordValues(invoice_3.line_ids, [
+            {'account_id': self.company_data['default_account_revenue'].id, 'credit': 0.0, 'debit': 42.0},
+            {'account_id': self.company_data['default_account_receivable'].id, 'credit': 42.0, 'debit': 0.0},
+            {'account_id': self.company_data['default_account_stock_valuation'].id, 'credit': 0.0, 'debit': 42.0},
+            {'account_id': self.company_data['default_account_expense'].id, 'credit': 42.0, 'debit': 0.0},
+        ])
