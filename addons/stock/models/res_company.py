f7b905742a78d46c75ced83199a91aa76f8cd128
chha-odoo <chha@odoo.com>
2025-08-29 15:07:11 +0530

    [IMP] stock: remove virtual locations from data

--- addons/stock/models/res_company.py
+++ addons/stock/models/res_company.py
@@ -69,34 +69,28 @@ class ResCompany(models.Model):
             })
 
     def _create_inventory_loss_location(self):
-        parent_location = self.env.ref('stock.stock_location_locations_virtual', raise_if_not_found=False)
         for company in self:
             inventory_loss_location = self.env['stock.location'].create({
                 'name': 'Inventory adjustment',
                 'usage': 'inventory',
-                'location_id': parent_location.id,
                 'company_id': company.id,
             })
             self.env['ir.default'].set('product.template', 'property_stock_inventory', inventory_loss_location.id, company_id=company.id)
 
     def _create_production_location(self):
-        parent_location = self.env.ref('stock.stock_location_locations_virtual', raise_if_not_found=False)
         for company in self:
             production_location = self.env['stock.location'].create({
                 'name': 'Production',
                 'usage': 'production',
-                'location_id': parent_location.id,
                 'company_id': company.id,
             })
             self.env['ir.default'].set('product.template', 'property_stock_production', production_location.id, company_id=company.id)
 
     def _create_scrap_location(self):
-        parent_location = self.env.ref('stock.stock_location_locations_virtual', raise_if_not_found=False)
         for company in self:
             scrap_location = self.env['stock.location'].create({
                 'name': 'Scrap',
                 'usage': 'inventory',
-                'location_id': parent_location.id,
                 'company_id': company.id,
                 'scrap_location': True,
             })
