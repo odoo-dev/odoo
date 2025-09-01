2a8e9e891a407cfcf50cc71044b62a87716061cc
Pierre-Yves Dufays <pydu@odoo.com>
2025-09-01 09:13:46 +0200

    [FIX] base: avoid pretending html being pdf content
    WIP
    
    Task-5052041

--- odoo/addons/base/models/ir_actions_report.py
+++ odoo/addons/base/models/ir_actions_report.py
@@ -1015,6 +1015,7 @@ class IrActionsReport(models.Model):
         # In case of test environment without enough workers to perform calls to wkhtmltopdf,
         # fallback to render_html.
         if (modules.module.current_test or tools.config['test_enable']) and not self.env.context.get('force_report_rendering'):
+            data['report_type'] = 'html'
             return self._render_qweb_html(report_ref, res_ids, data=data)
 
         self = self.with_context(webp_as_jpg=True)
