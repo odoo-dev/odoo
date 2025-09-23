31faf32a8ab3a460c72294b0f824640418bc85c3
Tanguy Quéguineur <taqu@odoo.com>
2025-09-23 21:48:42 +0200

    [FIX] project_sms, sale_sms: access templates as project manager
    **Steps to reproduce**
    - Have `project_sms` and `sale_sms` installed.
    - Have a regular user (not admin) with
    
    **Cause**
    
    opw-4908909

---
+++ addons/project_sms/upgrades/1.1/pre-migrate.py
@@ -0,0 +1,12 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+def migrate(cr, version):
+    cr.execute("""
+        UPDATE ir_rule r
+           SET domain_force = '[("model_id.model", "in", ("project.task", "project.project"))]'
+          FROM ir_model_data d
+         WHERE d.res_id = r.id
+           AND d.model = 'ir.rule'
+           AND d.module = 'project_sms'
+           AND d.name = 'ir_rule_sms_template_project_manager'
+    """)
