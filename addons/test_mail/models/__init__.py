8535b3c0fdb7c8c7243fdd91708b84edb04efa2b
Thibault Delavallée <tde@odoo.com>
2025-10-01 11:51:57 +0200

    [IMP] test_mail: quick test models cleanup / move
    Task-

--- addons/test_mail/models/__init__.py
+++ addons/test_mail/models/__init__.py
@@ -2,6 +2,7 @@ from . import mail_test_access
 from . import mail_test_lead
 from . import mail_test_ticket
 from . import test_mail_corner_case_models
+from . import test_mail_compose_models
 from . import test_mail_feature_models
 from . import test_mail_models
 from . import test_mail_thread_models
