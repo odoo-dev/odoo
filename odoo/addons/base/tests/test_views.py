87719dbcdb4eb18e696f9bcc1bbc9ada3cc43c87
Gorash <chm@odoo.com>
2025-10-06 14:10:05 +0200

    [IMP] clean invisible: base_setup

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -4841,7 +4841,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'base_geolocalize',
             'base_import_module',
             'base_install_request',
-            'base_setup',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
