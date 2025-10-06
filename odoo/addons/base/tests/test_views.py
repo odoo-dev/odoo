b25bf95eb447b1dbef0c7c6c4e782c35060f141a
Gorash <chm@odoo.com>
2025-10-06 13:50:27 +0200

    [IMP] clean invisible: sale_purchase

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5123,7 +5123,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'sale_planning',
             'sale_product_matrix',
             'sale_project',
-            'sale_purchase',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
