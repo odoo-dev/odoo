acb698518fbdc16598618f9d2798add2822ef32c
Gorash <chm@odoo.com>
2025-10-06 13:29:31 +0200

    [IMP] clean invisible: website_event_twitter_wall

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5180,7 +5180,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'website_event_track_gantt',
             'website_event_track_quiz',
             'website_event_track_social',
-            'website_event_twitter_wall',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
