ad0f56116daa7df3c5dc257df423b8dab2ccfcae
Gorash <chm@odoo.com>
2025-10-06 13:47:57 +0200

    [IMP] clean invisible: social

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5136,7 +5136,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'sms',
             'snailmail',
             'snailmail_account',
-            'social',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
