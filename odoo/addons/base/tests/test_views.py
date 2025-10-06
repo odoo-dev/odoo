2b7fbc9e1b7534a8a53ce032743ef8de647208cb
Gorash <chm@odoo.com>
2025-10-06 13:40:26 +0200

    [IMP] clean invisible: utm

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5161,7 +5161,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'test_testing_utilities',
             'timesheet_grid',
             'uom',
-            'utm',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
