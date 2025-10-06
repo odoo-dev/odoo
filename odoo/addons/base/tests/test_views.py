5151feb6118b246a446fcbf841bdef65f3e06f7b
Gorash <chm@odoo.com>
2025-10-06 13:26:42 +0200

    [IMP] clean invisible: website_livechat

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5185,7 +5185,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'website_helpdesk_forum',
             'website_hr_recruitment',
             'website_knowledge',
-            'website_livechat',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
