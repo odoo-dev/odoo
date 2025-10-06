82fca0105826e2686f6f0458a7270a49b93228ce
satp-odoo <satp@odoo.com>
2025-10-06 18:13:11 +0530

    [ADD] hackathon_registration: added event details page
    - added event details page

--- addons/hackathon_registration/__manifest__.py
+++ addons/hackathon_registration/__manifest__.py
@@ -6,7 +6,7 @@
     'depends': [
         'website', 'event', 'website_event'
     ],
-    'data': ['views/hackathon_template.xml'],
+    'data': ['views/hackathon_template.xml', 'views/event_template_page_hackathon.xml'],
     'installable': True,
     'application': True,
 }
