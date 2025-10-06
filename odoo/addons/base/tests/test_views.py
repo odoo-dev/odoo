8d3f281075ac017a445fdda0c4a1a0d060ad05ef
Gorash <chm@odoo.com>
2025-10-06 13:40:01 +0200

    [IMP] clean invisible: voip

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5162,7 +5162,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'timesheet_grid',
             'uom',
             'utm',
-            'voip',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
