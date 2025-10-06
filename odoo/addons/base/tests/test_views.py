a20ef9b5230fed00efa7fe5bbf55d53865cdd242
Gorash <chm@odoo.com>
2025-10-06 13:54:41 +0200

    [IMP] clean invisible: room

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5110,7 +5110,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'rating',
             'repair',
             'resource',
-            'room',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
