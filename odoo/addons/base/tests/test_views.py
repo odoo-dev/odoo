c53f3eb8e291d019614c042f367e89ba8c63421a
Gorash <chm@odoo.com>
2025-10-06 13:54:59 +0200

    [IMP] clean invisible: repair

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5108,7 +5108,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'quality_mrp',
             'quality_mrp_workorder',
             'rating',
-            'repair',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
