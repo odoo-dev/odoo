4548dc2fdf7334b2ff8b825839a5616466bd2e48
Gorash <chm@odoo.com>
2025-10-06 13:23:43 +0200

    [IMP] clean invisible: website_sms

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5193,7 +5193,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'website_sale_stock',
             'website_slides',
             'website_slides_survey',
-            'website_sms',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
