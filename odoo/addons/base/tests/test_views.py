e1f9e772e8dab39ccd8e7040ffc068cbe2c860f5
Gorash <chm@odoo.com>
2025-10-06 14:05:44 +0200

    [IMP] clean invisible: gamification

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -4879,7 +4879,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'event_sale',
             'fleet',
             'frontdesk',
-            'gamification',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
