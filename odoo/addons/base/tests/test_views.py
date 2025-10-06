709621d6eea88504334b1c40866deea990fe77e9
Gorash <chm@odoo.com>
2025-10-06 13:37:44 +0200

    [IMP] clean invisible: website_customer

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5170,7 +5170,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'website_blog',
             'website_crm_iap_reveal',
             'website_crm_partner_assign',
-            'website_customer',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
