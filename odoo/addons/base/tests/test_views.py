5f9e115a9adb5ac862258f2e829ee43a02eba95f
Gorash <chm@odoo.com>
2025-10-06 13:48:19 +0200

    [IMP] clean invisible: snailmail

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5134,7 +5134,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'sales_team',
             'sign',
             'sms',
-            'snailmail',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
