1ac8b69113022ae82886d1e73f1eff3e48ffcfc4
Gorash <chm@odoo.com>
2025-10-06 13:24:08 +0200

    [IMP] clean invisible: website_slides

--- odoo/addons/base/tests/test_views.py
+++ odoo/addons/base/tests/test_views.py
@@ -5191,7 +5191,6 @@ class TestInvisibleField(TransactionCaseWithUserDemo):
             'website_sale_loyalty',
             'website_sale_slides',
             'website_sale_stock',
-            'website_slides',
         )
 
         modules_without_error = set(self.env['ir.module.module'].search([('state', '=', 'intalled'), ('name', 'in', only_log_modules)]).mapped('name'))
