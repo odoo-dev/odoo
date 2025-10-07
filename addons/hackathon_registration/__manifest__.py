e85f4a943f148b23acce1af59a78fafdc3937cb9
Madhav Pancholi <pamd@odoo.com>
2025-10-07 13:03:51 +0530

    [IMP] hackathon_registration: fix breadcrum issue in upcoming events
    Added Hackathons as breadcrums instead of default All events breadcrumbs from
    event template

--- addons/hackathon_registration/__manifest__.py
+++ addons/hackathon_registration/__manifest__.py
@@ -6,7 +6,9 @@
     'depends': [
         'website', 'event', 'website_event'
     ],
-    'data': ['views/hackathon_template.xml', 'views/event_template_page_hackathon.xml'],
+    'data': ['views/hackathon_template.xml',
+             'views/event_template_page_hackathon.xml',
+             'views/inherited_navbar_template.xml'],
     'installable': True,
     'application': True,
 }
--- addons/hackathon_registration/controllers/my_hackathons.py
+++ addons/hackathon_registration/controllers/my_hackathons.py
@@ -19,7 +19,8 @@ class MyHackathon(http.Controller):
         new_state = self.env.ref('event.event_stage_new')
         announced_state = self.env.ref('event.event_stage_announced')
         upcoming_hackathons = request.env['event.event'].sudo().search([
-            ('stage_id', 'in', [new_state.id, announced_state.id]), ('is_published', '=', True)
+            ('stage_id', 'in', [
+             new_state.id, announced_state.id]), ('is_published', '=', True)
         ])
         return request.render('hackathon_registration.my_hackathons_page', {
             'my_hackathons': my_hackathons,
@@ -29,8 +30,17 @@ class MyHackathon(http.Controller):
     @http.route(['/hackathon/details/<int:hackathon_id>',
                  ], type='http', auth='user', website=True)
     def hackathon_details(self, hackathon_id):
-        event_registration = request.env['event.registration'].sudo().browse(hackathon_id)
+        event_registration = request.env['event.registration'].sudo().browse(
+            hackathon_id)
         values = {
             'event': event_registration.event_id,
         }
         return request.render('hackathon_registration.hackathon_event_details_page_template', values)
+
+    @http.route(['/hackathon/event/<model("event.event"):event>'], type='http', auth='user', website=True)
+    def hackathon_event_page(self, event):
+        values = {
+            'event': event,
+            'is_hackathon_page': True,
+        }
+        return request.render('website_event.event_description_full', values)
