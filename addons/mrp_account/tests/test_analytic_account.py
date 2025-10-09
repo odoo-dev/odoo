1a9842824da1cfddcde39cc1e9b9833fb9be3ae9
sako-odoo <sako@odoo.com>
2025-10-09 19:26:11 +0530

    [FIX] mrp_account: test_analytic_account

--- addons/mrp_account/tests/test_analytic_account.py
+++ addons/mrp_account/tests/test_analytic_account.py
@@ -1,11 +1,8 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from unittest import skip
-
 from odoo.tests.common import TransactionCase
 
 
-@skip('Temporary to fast merge new valuation')
 class TestMrpAnalyticAccount(TransactionCase):
     @classmethod
     def setUpClass(cls):
@@ -33,7 +30,6 @@ class TestMrpAnalyticAccount(TransactionCase):
         })
 
 
-@skip('Temporary to fast merge new valuation')
 class TestAnalyticAccount(TestMrpAnalyticAccount):
     def test_mandatory_analytic_plan_bom(self):
         """
