aafb62b61b147cfacc25a0fd50a3c2288679f214
assh-odoo <assh@odoo.com>
2025-10-16 18:42:50 +0530

    [FIX] sale_project: prevent error when create sale order

--- addons/sale_project/models/sale_order.py
+++ addons/sale_project/models/sale_order.py
@@ -273,7 +273,7 @@ class SaleOrder(models.Model):
             if project and not project.sale_line_id:
                 project.sale_line_id = service_sol
                 if not project.reinvoiced_sale_order_id:
-                    project.reinvoiced_sale_order_id = service_sol.order_id or created_records[0] if created_records else False
+                    project.reinvoiced_sale_order_id = (service_sol and service_sol.order_id) or created_records[0] if created_records else False
             if task and not task.sale_line_id:
                 created_records.with_context(disable_project_task_generation=True).action_confirm()
                 task.sale_line_id = service_sol
