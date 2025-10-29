7a8dfa66c6e9d2a98de3430ad9fa45ba86bce1b5
h4818 <ayh@odoo.com>
2025-10-29 10:10:58 +0100

    [ADD] l10n_mk: North Macedonia CoA
    Default accounting setup for North Macedonia (mk)
    
    task-5177442

---
+++ addons/l10n_mk/__manifest__.py
@@ -0,0 +1,22 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'North Macedonia - Accounting',
+    'icon': '/account/static/description/l10n.png',
+    'countries': ['mk'],
+    'version': '1.0',
+    'category': 'Accounting/Localizations/Account Charts',
+    'description': """
+    Chart Of Account and Taxes for North Macedonia.
+    """,
+    'website': 'https://www.odoo.com/documentation/latest/applications/finance/fiscal_localizations.html',
+    'depends': [
+        'account',
+    ],
+    'data': [
+    ],
+    'demo': [
+        'demo/demo_company.xml',
+    ],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
