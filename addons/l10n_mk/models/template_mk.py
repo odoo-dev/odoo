592a06f055182ae0547a493cb795d36d409bf73e
h4818 <ayh@odoo.com>
2025-11-06 18:24:49 +0100

    [IMP] l10n_mk: continue

--- addons/l10n_mk/models/template_mk.py
+++ addons/l10n_mk/models/template_mk.py
@@ -9,8 +9,31 @@ class AccountChartTemplate(models.AbstractModel):
     @template('mk')
     def _get_mk_template_data(self):
         return {
-            # 'property_account_receivable_id': 'lu_2011_account_4011',
-            # 'property_account_payable_id': 'lu_2011_account_44111',
-            # 'property_stock_valuation_account_id': 'lu_2020_account_60761',
-            'code_digits': '3',
+            'property_account_receivable_id': 'l10n_mk_account_120',
+            'property_account_payable_id': 'l10n_mk_account_220',
+            'code_digits': '6',
+        }
+
+    @template('mk', 'res.company')
+    def _get_ma_res_company(self):
+        return {
+            self.env.company.id: {
+                'account_fiscal_country_id': 'base.mk',
+                # 'bank_account_code_prefix': '5141',
+                # 'cash_account_code_prefix': '51611',
+                # 'transfer_account_code_prefix': '5115',
+                # 'account_default_pos_receivable_account_id': 'pcg_34218',
+                # 'income_currency_exchange_account_id': 'pcg_7331',
+                # 'expense_currency_exchange_account_id': 'pcg_6331',
+                # 'account_journal_suspense_account_id': 'pcg_3497',
+                # 'default_cash_difference_income_account_id': 'pcg_73861',
+                # 'default_cash_difference_expense_account_id': 'pcg_63861',
+                # 'account_journal_early_pay_discount_gain_account_id': 'pcg_73862',
+                # 'account_journal_early_pay_discount_loss_account_id': 'pcg_63862',
+                # 'account_sale_tax_id': 'vat_out_20_80',
+                # 'account_purchase_tax_id': 'vat_in_20_146',
+                'income_account_id': 'l10n_mk_account_730',
+                'expense_account_id': 'l10n_mk_account_701',
+                # 'tax_exigibility': 'True',
+            },
         }
