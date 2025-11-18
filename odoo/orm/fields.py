e99a17063616ca810121d9e2a11b1ea24fc44a5d
Chong Wang (cwg) <cwg@odoo.com>
2025-11-18 11:00:34 +0100

    [IMP] wip poc

--- odoo/orm/fields.py
+++ odoo/orm/fields.py
@@ -1134,7 +1134,7 @@ class Field(typing.Generic[T]):
         if column['is_nullable'] == 'NO':
             sql.drop_not_null(model.env.cr, model._table, self.name)
         self._convert_db_column(model, column)
-        column.clear()  # remove information, because it may no longer be valid
+        # column.clear()  # remove information, because it may no longer be valid
 
     def _convert_db_column(self, model: BaseModel, column: dict[str, typing.Any]):
         """ Convert the given database column to the type of the field. """
