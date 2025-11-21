0f8a0c90f6532c38788aa590c8dcf0b4e76a43b2
haahi-odoo <haahi@odoo.com>
2025-11-21 12:06:25 +0530

    [IMP] awesome_owl: add counter, card components and todo lists
    -Made  estate module user-friendly UI
    -Added counter and card component and display in awesome owl module
    -Added todo list in awesome_owl

--- awesome_dashboard/static/src/dashboard.js
+++ awesome_dashboard/static/src/dashboard.js
@@ -1,5 +1,8 @@
-import { Component } from "@odoo/owl";
+import { Component, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { useService } from "@web/core/utils/hooks";
+import { DashboardItem } from "./dashboard_item/dashboard_item";
 
 class AwesomeDashboard extends Component {
     static template = "awesome_dashboard.AwesomeDashboard";
