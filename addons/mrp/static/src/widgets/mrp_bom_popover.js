17e0aae3b0299c31038f1fed577b2ce9bfe4768b
hapt-odoo <hapt@odoo.com>
2025-12-16 14:15:51 +0530

    [IMP] mrp: demo
    demo

--- addons/mrp/static/src/widgets/mrp_bom_popover.js
+++ addons/mrp/static/src/widgets/mrp_bom_popover.js
@@ -1,24 +1,49 @@
 import { registry } from "@web/core/registry";
-import {
-    PopoverComponent,
-    PopoverWidgetField,
-    popoverWidgetField,
-} from "@stock/widgets/popover_widget";
+import { usePopover } from "@web/core/popover/popover_hook";
+import { useService } from "@web/core/utils/hooks";
+import { PopoverComponent, PopoverWidgetField, popoverWidgetField } from "@stock/widgets/popover_widget";
 
-class BomPopover extends PopoverComponent {
+class MrpBomPopover extends PopoverComponent {
     static template = "mrp.bomPopover";
+    setup() {
+        super.setup();
+        this.actionService = useService("action");
+    }
+
+    async _openBomOverview() {
+        return this.actionService.doAction("mrp.action_report_mrp_bom", {
+            additionalContext: {
+                active_id: this.props.bom_id,
+                active_product_id: this.props.res_id,
+                active_model: "product.product",
+                mode: "forecast",
+            },
+        });
+    }
+
+    async goToAction(id, model) {
+        return this.actionService.doAction({
+            type: "ir.actions.act_window",
+            res_model: model,
+            res_id: id,
+            views: [[false, "form"]],
+            target: "current",
+            context: { active_id: id },
+        });
+    }
 }
 
-class BomPopoverField extends PopoverWidgetField {
+class MrpBomPopoverField extends PopoverWidgetField {
     static components = {
-        Popover: BomPopover,
+        Popover: MrpBomPopover,
     };
-    setup(){
+    setup() {
         super.setup();
+        this.popover = usePopover(this.constructor.components.Popover, { position: useService('ui').isSmall ? 'top' : 'right' });
     }
 }
 
 registry.category("fields").add("mrp_bom_popover", {
     ...popoverWidgetField,
-    component: BomPopoverField,
+    component: MrpBomPopoverField,
 });
