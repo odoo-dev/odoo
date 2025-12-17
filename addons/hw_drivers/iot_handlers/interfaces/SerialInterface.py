cb28bb10553494228c8709f965d50c65cec98847
yaso <yaso@odoo.com>
2025-12-17 14:20:45 +0100

    m

--- addons/hw_drivers/iot_handlers/interfaces/SerialInterface.py
+++ addons/hw_drivers/iot_handlers/interfaces/SerialInterface.py
@@ -10,9 +10,20 @@ from odoo.addons.hw_drivers.interface import Interface
 class SerialInterface(Interface):
     connection_type = 'serial'
 
+    def _get_serial_identifier(self, port):
+        """
+        Generate a more user-friendly identifier for serial devices
+        based on their manufacturer and serial number when available.
+        Avoids recreating a device when the usb port changes
+        Example: METTLER-TOLEDO-Ser_CDC instead of /dev/ttyUSB0
+        """
+        if port.manufacturer and port.serial_number:
+            return (port.manufacturer + "-" + port.serial_number).replace(' ', '-')
+        return port.device
+
     def get_devices(self):
         serial_devices = {
-            port.device: {'identifier': port.device}
+            port.device: {'identifier': self._get_serial_identifier(port)}
             for port in comports()
             if platform.system() == 'Windows' or port.device != '/dev/ttyAMA10'
             # RPI 5 uses ttyAMA10 as a console serial port for system messages: odoo interprets it as scale -> avoid it
