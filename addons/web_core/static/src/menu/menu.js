0c78009d5e8a4e8e8f00f2f2703cc4b704ba73c8
Michaël Mattiello <mcm@odoo.com>
2025-12-18 09:07:07 +0100

    menu component

---
+++ addons/web_core/static/src/menu/menu.js
@@ -0,0 +1,43 @@
+import { Component, plugin, props, useEffect, xml } from "@odoo/owl";
+import { PopoverPlugin } from "@web_core/popover/popover_plugin";
+
+class MenuPopover extends Component {
+    static template = "web_core.MenuPopover";
+}
+
+export class Menu extends Component {
+    static template = xml``;
+
+    controls = props({
+        target: Function,
+        isOpen: Function,
+    });
+
+    content = props(["slots"]);
+
+    popover = plugin(PopoverPlugin).createPopover(this.controls.target, {
+        component: MenuPopover,
+        props: {
+            slots: this.content.slots,
+        },
+    });
+
+    setup() {
+        useEffect(() => {
+            if (this.controls.isOpen()) {
+                this.popover.open();
+            } else {
+                this.popover.close();
+            }
+        });
+    }
+}
+
+export class MenuItem extends Component {
+    static template = "web_core.MenuItem";
+
+    props = props({
+        isActive: { type: Boolean, optional: true, defaultValue: false },
+        onSelected: Function,
+    });
+}
