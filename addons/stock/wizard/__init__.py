fdc1a4a22275c6050671eeb92564a03384c6bd51
Quentin Wolfs <quwo@odoo.com>
2025-12-30 15:13:33 +0100

    [FIX] stock: handle no longer entire pack more easily
    Steps to reproduce:
    - Have two packages containing:
      - Pack 1: 5 Product A, 5 Product B
      - Pack 2: 10 Product A
    - Create a transfert that moves 5 Product A & 5 Product B
    - Confirm, Pack 1 should be reserved
    - In Product A's move, change the pick from to Pack 2
    
    Issues:
    After the change, Pack 1 remains the destination package of both moves,
    resulting in an error at validation, since part of the package (product
    A) is still at the source location while we try to move Product B into
    another location with the same package.
    
    The solution to this is two-fold:
    - When updating the source of a move line having the same package as
      source and destination, clear its destination package.
    - When validating a picking with packages that are no longer entire,
      display a wizard showing which packages are now non-complete and
      allowing the user to unpack them before validating the transfer.
    
    task-5222941

--- addons/stock/wizard/__init__.py
+++ addons/stock/wizard/__init__.py
@@ -19,3 +19,4 @@ from . import stock_replenishment_info
 from . import stock_quant_relocate
 from . import stock_put_in_pack
 from . import stock_zero_demand_confirmation
+from . import stock_not_entire_pack_warning
