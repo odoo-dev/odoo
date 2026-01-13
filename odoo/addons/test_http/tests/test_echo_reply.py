9fe02e0c433abeddb5b5d09a8f90209025a78e4f
odoo <chm@odoo.com>
2026-01-13 10:03:50 +0100

    [IMP] request.env in xml => env

--- odoo/addons/test_http/tests/test_echo_reply.py
+++ odoo/addons/test_http/tests/test_echo_reply.py
@@ -223,7 +223,7 @@ class TestHttpEchoReplyJsonWithDB(TestHttpBase):
         res = self.db_url_open("/test_http/echo-json-context", data=payload, headers=CT_JSON)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res.text, '{"jsonrpc": "2.0", "id": 0, "result": '
-            f'{{"lang": "en_US", "tz": false, "uid": {self.jackoneill.id}}}'
+            f'{{"lang": "en_US", "tz": false, "uid": {self.jackoneill.id}, "fallback_website_id": 1}}'
             '}')
 
     def test_echojson3_bad_json(self):
