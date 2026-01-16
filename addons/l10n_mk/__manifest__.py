1aca77610d66a2e8fe7178caf19defdf78249e4e
Naman Shah <nash@odoo.com>
2026-01-16 17:46:07 +0530

    [ADD] l10n_mk: added support of Macedonian Accounting
    WIP
    task-5177442

---
+++ addons/l10n_mk/__manifest__.py
@@ -0,0 +1,22 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'North Macedonia - Accounting',
+    'icon': '/account/static/description/l10n.png',
+    'countries': ['mk'],
+    'category': 'Accounting/Localizations/Account Charts',
+    'description': """
+    Chart Of Account and Taxes for North Macedonia.
+    """,
+    'website': 'https://www.odoo.com/documentation/latest/applications/finance/fiscal_localizations.html',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'data/tax_report.xml',
+    ],
+    'demo': [
+        'demo/demo_company.xml',
+    ],
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
