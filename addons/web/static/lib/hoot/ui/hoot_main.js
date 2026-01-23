13049a551035a8a73fb6f7b5908fec7d49641982
stefanorigano (sri) <sri@odoo.com>
2026-01-23 11:25:43 +0100

    [IMP] tmp material symbols

--- addons/web/static/lib/hoot/ui/hoot_main.js
+++ addons/web/static/lib/hoot/ui/hoot_main.js
@@ -93,7 +93,7 @@ export class HootMain extends Component {
                         <HootSearch />
                         <HootDropdown buttonClassName="'bg-btn'">
                             <t t-set-slot="toggler" t-slot-scope="dropdownState">
-                                <i class="fa fa-cog transition" t-att-class="{ 'rotate-90': dropdownState.open }" />
+                                <i class="fa fa-cog mi-filled transition" t-att-class="{ 'rotate-90': dropdownState.open }" />
                             </t>
                             <t t-set-slot="menu">
                                 <HootConfigMenu />
