e90eeb6dc398fff21ec8c0bbdd8961c48f438af0
Paul Morelle <pmo@odoo.com>
2026-01-26 22:01:26 +0100

    fixup! [IMP] web: add controller to get KPIs without a registry

--- addons/web/controllers/kpi_provider.py
+++ addons/web/controllers/kpi_provider.py
@@ -9,7 +9,7 @@ from odoo.addons.base.models.res_users import ResUsersApikeys
 
 class WebKpiProviderController(http.Controller):
     @http.route('/json/2/kpi.provider/get_kpi_summary', type='json2', auth='nodb', csrf=False)
-    def get_kpi_summary(self, database):
+    def get_kpi_summary(self):
         request_headers = http.request.httprequest.headers
         if 'authorization' not in request_headers:
             raise Unauthorized('Missing "Authorization"', www_authenticate=WWWAuthenticate('bearer'))
@@ -19,7 +19,7 @@ class WebKpiProviderController(http.Controller):
             raise Unauthorized('Use an API Key with a Bearer Authorization header.', www_authenticate=WWWAuthenticate('bearer'))
 
         apikey = authorization[7:]
-        with sql_db.db_connect(database).cursor() as cr:
+        with sql_db.db_connect(http.request.db).cursor() as cr:
             uid = self._check_apikey(cr, apikey)
             if not uid:
                 raise Unauthorized('Invalid apikey', www_authenticate=WWWAuthenticate('bearer'))
