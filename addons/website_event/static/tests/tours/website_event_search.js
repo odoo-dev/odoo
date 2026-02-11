01e7fc9929ad34c7beb6af173cfe6bd4cac8a78d
divy-odoo <divy@odoo.com>
2026-02-11 18:04:00 +0530

    [FIX] website: changes during review

--- addons/website_event/static/tests/tours/website_event_search.js
+++ addons/website_event/static/tests/tours/website_event_search.js
@@ -1,4 +1,5 @@
 import { registry } from "@web/core/registry";
+import { delay } from "@web/core/utils/concurrency";
 
 registry.category("web_tour.tours").add("test_website_event_search", {
     steps: () => [
@@ -20,8 +21,23 @@ registry.category("web_tour.tours").add("test_website_event_search", {
             run: 'click'
         },
         {
-            trigger: '.input-group .search-query.form-control',
-            run: 'edit Event 0 && click body',
+            content: "Click on search input",
+            trigger: ".o_searchbar_form input",
+            run: "click",
+        },
+        {
+            trigger: '#o_search_modal_events_list .input-group .search-query.form-control',
+            run: 'edit Event 0',
+        },
+        {
+            trigger: "body",
+            async run() {
+                await delay(2000);
+            }
+        },
+        {
+            trigger: "body",
+            run: "press Escape",
         },
         {
             trigger: '.badge.bg-primary:contains(1)',
