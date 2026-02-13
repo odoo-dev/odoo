dzdzdzdz1010/OdooStandard

[REF] html_editor, *: use `checkPredicates` for all "every" predicates
\*: `html_builder`, `website`.

This replaces all calls to predicates that used `Array.every` so they
use `checkPredicates` instead.

task-5207637

addons/html_editor/static/src/main/power_buttons_plugin.js
@@ -139,9 +139,7 @@ export class PowerButtonsPlugin extends Plugin {
             !this.services.ui.isSmall &&
             !closestElement(editableSelection.anchorNode, "td, th, li") &&
             !block.style.textAlign &&
-            this.getResource("power_buttons_visibility_predicates").every((predicate) =>
-                predicate(editableSelection)
-            )
+            (this.checkPredicates("power_buttons_visibility_predicates", editableSelection) ?? true)
         ) {
             this.powerButtonsContainer.classList.remove("d-none");
             const direction = closestElement(element, "[dir]")?.getAttribute("dir");

odoo-dev/odoo

addons/html_editor/static/src/main/power_buttons_plugin.js
@@ -138,9 +138,7 @@ export class PowerButtonsPlugin extends Plugin {
             !this.services.ui.isSmall &&
             !closestElement(documentSelection.anchorNode, "td, th, li") &&
             !block.style.textAlign &&
-            this.getResource("power_buttons_visibility_predicates").every((predicate) =>
-                predicate(documentSelection)
-            )
+            (this.checkPredicates("power_buttons_visibility_predicates", documentSelection) ?? true)
         ) {
             this.powerButtonsContainer.classList.remove("d-none");
             const direction = closestElement(block, "[dir]")?.getAttribute("dir");