e3ae5631f6d763a7121632920b324a5f2e53ab9c
Federico Falcone <ffed@odoo.com>
2026-02-19 17:04:56 +0100

    [FIX] hr_holidays: fix domain group allocation wizard
    When opening the "Time Off Type" many2one field from the group allocation wizard (hr.leave.allocation.generate.multi.wizard), a traceback was raised because the domain contained ('company_id', 'in', ...), but hr.work.entry.type does not have a company_id field, it uses country_id instead.
    
    task-5952849

--- addons/hr_holidays/wizard/hr_leave_allocation_generate_multi_wizard.py
+++ addons/hr_holidays/wizard/hr_leave_allocation_generate_multi_wizard.py
@@ -24,7 +24,7 @@ class HrLeaveAllocationGenerateMultiWizard(models.TransientModel):
 
     def _domain_work_entry_type_id(self):
         domain = [
-            ('company_id', 'in', self.env.companies.ids + [False]),
+            ('country_id', 'in', self.env.companies.country_id.ids + [False]),
             ('requires_allocation', '=', True),
         ]
         if self.env.user.has_group('hr_holidays.group_hr_holidays_user'):
