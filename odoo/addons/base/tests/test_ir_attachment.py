a8dcce9aeb5d043dae75ebfc8cb434899fa1e53a
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-02-23 02:23:38 +0000

    [IMP] base: security fields for attachments

--- odoo/addons/base/tests/test_ir_attachment.py
+++ odoo/addons/base/tests/test_ir_attachment.py
@@ -327,7 +327,7 @@ class TestPermissions(TransactionCaseWithUserDemo):
             """
             SELECT "ir_attachment"."id"
             FROM "ir_attachment"
-            WHERE ("ir_attachment"."res_field" IN %s AND "ir_attachment"."res_id" IN %s AND "ir_attachment"."res_model" IN %s AND (
+            WHERE ("ir_attachment"."res_field" IN %s AND "ir_attachment"."res_id" IN %s AND "ir_attachment"."res_model" IN %s) AND (
                 "ir_attachment"."public" IS TRUE
                 OR (
                     ("ir_attachment"."res_field" IN %s OR "ir_attachment"."res_field" IS NULL)
@@ -341,7 +341,7 @@ class TestPermissions(TransactionCaseWithUserDemo):
                     )
                     AND "ir_attachment"."res_model" IN %s
                 )
-            ))
+            )
             ORDER BY "ir_attachment"."id" DESC
             """
         ]):
