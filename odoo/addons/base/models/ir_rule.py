f08973f4f9e1cd5f1e4b2fc25b50051816bc06f9
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-03-04 13:21:34 +0000

    [IMP] orm: remove access checks in inherits models

--- odoo/addons/base/models/ir_rule.py
+++ odoo/addons/base/models/ir_rule.py
@@ -140,21 +140,14 @@ class IrRule(models.Model):
     def _compute_domain(self, model_name: str, mode: str = "read") -> Domain:
         model = self.env[model_name]
 
-        # add rules for parent models
-        global_domains: list[Domain] = []
-        for parent_model_name, parent_field_name in model._inherits.items():
-            if not model._fields[parent_field_name].store:
-                continue
-            if domain := self._compute_domain(parent_model_name, mode):
-                global_domains.append(Domain(parent_field_name, 'any', domain))
-
         rules = self._get_rules(model_name, mode=mode)
         if not rules:
-            return Domain.AND(global_domains).optimize(model)
+            return Domain.TRUE
 
         # browse user and rules with sudo to avoid access errors!
         eval_context = self._eval_context()
         user_groups = self.env.user.all_group_ids
+        global_domains: list[Domain] = []
         group_domains: list[Domain] = []
         for rule in rules.sudo():
             if rule.groups and not (rule.groups & user_groups):
