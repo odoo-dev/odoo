fa1ba02e8c32b268b27b2e9a412c1b2f2549a789
Brieuc-brd <brd@odoo.com>
2026-03-05 16:36:56 +0100

    [IMP] html_builder: s_alert

--- addons/html_builder/static/src/plugins/alert_option_plugin.js
+++ addons/html_builder/static/src/plugins/alert_option_plugin.js
@@ -31,24 +31,20 @@ export class AlertIconAction extends BuilderAction {
         if (!icon) {
             return;
         }
-        fonts.computeFonts();
-        const allFaIcons = fonts.fontIcons[0].alias;
-        icon.classList.remove(...allFaIcons);
-        icon.classList.add(className);
+        icon.dataset.icon = className;
     }
     clean({ editingElement, params: { mainParam: className } }) {
         const icon = editingElement.querySelector(".s_alert_icon");
         if (!icon) {
             return;
         }
-        icon.classList.remove(className);
     }
     isApplied({ editingElement, params: { mainParam: className } }) {
         const iconEl = editingElement.querySelector(".s_alert_icon");
         if (!iconEl) {
             return;
         }
-        return iconEl.classList.contains(className);
+        return iconEl.dataset.icon.includes(className);
     }
 }
 registry.category("builder-plugins").add(AlertOptionPlugin.id, AlertOptionPlugin);
