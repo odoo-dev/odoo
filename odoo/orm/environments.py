9388d2c1f0f72a2dd44675a61415e784de2e6819
Chong Wang (cwg) <cwg@odoo.com>
2026-03-10 14:09:50 +0100

    [IMP] core: invalidate non-store fields after inverse
    For non-store fields, the inverse method doesn't promise the written
    value will be the same as the next time computed value. In order to keep
    the cache consistent, we invalidate the field after the inverse method
    is called.

--- odoo/orm/environments.py
+++ odoo/orm/environments.py
@@ -441,7 +441,7 @@ class Environment(Mapping[str, "BaseModel"]):
 
             for field, rec_ids in ids_by_field.items():
                 ids = protected.get(field)
-                protected[field] = ids.union(rec_ids) if ids else frozenset(rec_ids)
+                protected[field] = ids.union(rec_ids) if ids else set(rec_ids)
             yield
         finally:
             protected.popmap()
@@ -596,7 +596,7 @@ class Transaction:
         # x2many fields if they are not in cache yet
         self.field_data_patches = defaultdict["Field", defaultdict["IdType", list["IdType"]]](lambda: defaultdict(list))
         # fields to protect {field: ids}
-        self.protected = StackMap["Field", frozenset["IdType"]]()
+        self.protected = StackMap["Field", set["IdType"]]()
         # pending computations {field: ids}
         self.tocompute = defaultdict["Field", OrderedSet["IdType"]](OrderedSet)
         # backward-compatible view of the cache
