bd91733d6e76c59d1953c2de2ec0fb820dfe6f83
Brieuc-brd <brd@odoo.com>
2026-03-12 12:41:14 +0100

    [IMP] point_of_sale: centered_icon

--- addons/point_of_sale/static/src/app/components/centered_icon/centered_icon.js
+++ addons/point_of_sale/static/src/app/components/centered_icon/centered_icon.js
@@ -3,6 +3,7 @@ import { Component, xml } from "@odoo/owl";
 export class CenteredIcon extends Component {
     static props = {
         icon: String,
+        icon_class: String,
         text: { type: String, optional: true },
         class: { type: String, optional: true },
     };
@@ -11,7 +12,7 @@ export class CenteredIcon extends Component {
     };
     static template = xml`
         <div t-attf-class="{{props.class}} d-flex flex-column align-items-center justify-content-center">
-            <i t-attf-class="fa {{props.icon}}" role="img" />
+            <i t-attf-class="oi {{props.icon_class}}" t-att-data-icon="props.icon" role="img" />
             <h3 t-if="props.text" t-esc="props.text" class="w-75 mt-2 text-center"/>
         </div>
     `;
