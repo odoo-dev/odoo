49d9c7da4e9720e85d90bfddd00b1b3d6d464c0b
jodh-odoo <jodh@odoo.com>
2026-03-16 18:46:38 +0530

    [IMP] stock: support all warehouses in forecast report
    Purpose:
    ========
    In multi-warehouse environments, The forecasted report is currently
    limited to a single warehouse. users must switch between warehouses
    to understand stock availability, making it difficult to get a
    complete and reliable overview.
    
    This limitation reduces efficiency and makes planning more complex,
    especially when stock is distributed across multiple warehouses.
    
    With This Commit:
    =================
    The forecasted report now supports working across multiple warehouses,
    allowing users to get a consolidated view of stock.
    
    - Introduces an `All Warehouses` option in the report,
    - Ensures data is properly aggregated across warehouses,
    - Keeps the report consistent and usable when All Warehouses
      are selected.
    
    This improves visibility of stock and simplifies analysis for
    multi-warehouse setups.
    
    task-4385275

--- addons/stock/static/src/stock_forecasted/forecasted_header.js
+++ addons/stock/static/src/stock_forecasted/forecasted_header.js
@@ -4,7 +4,7 @@ import { Component, markup } from "@odoo/owl";
 
 export class ForecastedHeader extends Component {
     static template = "stock.ForecastedHeader";
-    static props = { docs: Object, openView: Function };
+    static props = { docs: Object, openView: Function, selectedWarehouseIds: Array };
 
     setup(){
         this.orm = useService("orm");
@@ -16,6 +16,10 @@ export class ForecastedHeader extends Component {
     async _onClickInventory(){
         const productIds = this.props.docs.product_variants_ids;
         const action = await this.orm.call('product.product', 'action_open_quants', [productIds]);
+        action.domain = [
+            ...(action.domain || []),
+            ["warehouse_id", "in", this.props.selectedWarehouseIds],
+        ];
         if (action.help) {
             action.help = markup(action.help);
         }
