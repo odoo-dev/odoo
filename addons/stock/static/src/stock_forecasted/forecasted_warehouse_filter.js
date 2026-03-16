864ceb44af6197d3e843dc9bb99e2ada5a37cc08
jodh-odoo <jodh@odoo.com>
2026-03-16 18:46:38 +0530

    [IMP] stock: support all warehouses in forecast report
    Purpose:
    ========
    In multi-warehouse environments, The forecasted report is currently
    limited to a single warehouse. users must switch between warehouses
    to understand stock availability, making it difficult to get a
    complete and reliable overview.
    
    This limitation reduces efficiency and makes planning more complex,
    especially when stock is distributed across multiple warehouses.
    
    With This Commit:
    =================
    The forecasted report now supports working across multiple warehouses,
    allowing users to get a consolidated view of stock.
    
    - Introduces an `All Warehouses` option in the report,
    - Ensures data is properly aggregated across warehouses,
    - Keeps the report consistent and usable when All Warehouses
      are selected.
    
    This improves visibility of stock and simplifies analysis for
    multi-warehouse setups.
    
    task-4385275

--- addons/stock/static/src/stock_forecasted/forecasted_warehouse_filter.js
+++ addons/stock/static/src/stock_forecasted/forecasted_warehouse_filter.js
@@ -1,11 +1,9 @@
 import { Dropdown } from "@web/core/dropdown/dropdown";
-import { DropdownItem } from "@web/core/dropdown/dropdown_item";
-import { useService } from "@web/core/utils/hooks";
-import { Component, onWillStart, t, useProps } from "@odoo/owl";
+import { Component, t, useProps } from "@odoo/owl";
 
 export class ForecastedWarehouseFilter extends Component {
     static template = "stock.ForecastedWarehouseFilter";
-    static components = { Dropdown, DropdownItem };
+    static components = { Dropdown };
     props = useProps({
         action: t.object(),
         setWarehouseInContext: t.function(),
@@ -13,26 +11,22 @@ export class ForecastedWarehouseFilter extends Component {
     });
 
     setup() {
-        this.orm = useService("orm");
         this.context = this.props.action.context;
         this.warehouses = this.props.warehouses;
-        onWillStart(this.onWillStart)
-    }
-
-    async onWillStart() {
-        this.displayWarehouseFilter = (this.warehouses.length > 1);
     }
 
     _onSelected(id){
         this.props.setWarehouseInContext(Number(id));
     }
 
-    get activeWarehouse(){
-        return this.context.warehouse_id ? this.warehouses.find((w) => w.id == this.context.warehouse_id) : this.warehouses[0];
+    get activeWarehouse() {
+        return warehouse = this.context.warehouse_id
+            ? this.warehouses.find((w) => w.id == this.context.warehouse_id)
+            : this.warehouses[0];
     }
 
     get warehousesItems() {
-        return this.warehouses.map(warehouse => ({
+        return this.warehouses.map((warehouse) => ({
             id: warehouse.id,
             label: warehouse.name,
             onSelected: () => this._onSelected(warehouse.id),
