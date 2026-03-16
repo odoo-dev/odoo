3f82b267bd472459e205141458e46e9bab92639b
jodh-odoo <jodh@odoo.com>
2026-03-16 18:46:38 +0530

    [IMP] stock: support all warehouses in forecast report
    Purpose:
    ========
    In multi-warehouse environments, The forecasted report is currently
    limited to a single warehouse. users must switch between warehouses
    to understand stock availability, making it difficult to get a
    complete and reliable overview.
    
    This limitation reduces efficiency and makes planning more complex,
    especially when stock is distributed across multiple warehouses.
    
    With This Commit:
    =================
    The forecasted report now supports working across multiple warehouses,
    allowing users to get a consolidated view of stock.
    
    - Introduces an `All Warehouses` option in the report,
    - Ensures data is properly aggregated across warehouses,
    - Keeps the report consistent and usable when All Warehouses
      are selected.
    
    This improves visibility of stock and simplifies analysis for
    multi-warehouse setups.
    
    task-4385275

--- addons/stock/tests/test_report.py
+++ addons/stock/tests/test_report.py
@@ -748,7 +748,8 @@ class TestReports(TestReportsCommon):
         """ Create some transfer for two different warehouses and check the
         report display the good moves according to the selected warehouse.
         """
-        # Warehouse config.
+        # Warehouses config.
+        wh_1 = self.picking_type_in.warehouse_id
         wh_2 = self.wh_2
         picking_type_out_2 = self.env['stock.picking.type'].search([
             ('code', '=', 'outgoing'),
@@ -765,7 +766,10 @@ class TestReports(TestReportsCommon):
             move_line.product_uom_qty = 5
         delivery = delivery_form.save()
 
-        report_values, docs, lines = self.get_report_forecast(product_template_ids=self.product_template.ids)
+        report_values, docs, lines = self.get_report_forecast(
+            product_template_ids=self.product_template.ids,
+            context={'warehouse_id': wh_1.id},
+        )
         draft_picking_qty = self.sum_dicts(docs['product'], 'draft_picking_qty')
         self.assertEqual(len(lines), 1, "Must have 1 line.")
         self.assertEqual(draft_picking_qty['out'], 5)
@@ -780,7 +784,10 @@ class TestReports(TestReportsCommon):
 
         # Confirm the delivery -> The report still have 1 line.
         delivery.action_confirm()
-        report_values, docs, lines = self.get_report_forecast(product_template_ids=self.product_template.ids)
+        report_values, docs, lines = self.get_report_forecast(
+            product_template_ids=self.product_template.ids,
+            context={'warehouse_id': wh_1.id},
+        )
         draft_picking_qty = self.sum_dicts(docs['product'], 'draft_picking_qty')
         self.assertEqual(len(lines), 1)
         self.assertEqual(draft_picking_qty['out'], 0)
@@ -805,7 +812,10 @@ class TestReports(TestReportsCommon):
             move_line.product_uom_qty = 8
         delivery_2 = delivery_form.save()
 
-        report_values, docs, lines = self.get_report_forecast(product_template_ids=self.product_template.ids)
+        report_values, docs, lines = self.get_report_forecast(
+            product_template_ids=self.product_template.ids,
+            context={'warehouse_id': wh_1.id},
+        )
         draft_picking_qty = self.sum_dicts(docs['product'], 'draft_picking_qty')
         self.assertEqual(len(lines), 1)
         self.assertEqual(draft_picking_qty['out'], 0)
@@ -821,7 +831,10 @@ class TestReports(TestReportsCommon):
         self.assertEqual(draft_picking_qty['out'], 8)
         # Confirm the second delivery -> The report must now have 1 line.
         delivery_2.action_confirm()
-        report_values, docs, lines = self.get_report_forecast(product_template_ids=self.product_template.ids)
+        report_values, docs, lines = self.get_report_forecast(
+            product_template_ids=self.product_template.ids,
+            context={'warehouse_id': wh_1.id},
+        )
         draft_picking_qty = self.sum_dicts(docs['product'], 'draft_picking_qty')
         self.assertEqual(len(lines), 1)
         self.assertEqual(draft_picking_qty['out'], 0)
@@ -895,7 +908,8 @@ class TestReports(TestReportsCommon):
         """ Create transfers for two different companies and check report
         display the right transfers.
         """
-        # Configure second warehouse.
+        # Warehouses config.
+        wh_1 = self.picking_type_in.warehouse_id
         company_2 = self.env['res.company'].create({'name': 'Aperture Science'})
         wh_2 = self.env['stock.warehouse'].search([('company_id', '=', company_2.id)])
         wh_2_picking_type_in = wh_2.in_type_id
@@ -920,7 +934,10 @@ class TestReports(TestReportsCommon):
             move_line.product_uom_qty = 5
         wh_2_receipt = receipt_form.save()
 
-        report_values, docs, lines = self.get_report_forecast(product_template_ids=self.product_template.ids)
+        report_values, docs, lines = self.get_report_forecast(
+            product_template_ids=self.product_template.ids,
+            context={'warehouse_id': wh_1.id},
+        )
         draft_picking_qty = self.sum_dicts(docs['product'], 'draft_picking_qty')
         self.assertEqual(len(lines), 1, "Must have 1 line.")
         self.assertEqual(draft_picking_qty['in'], 2)
@@ -939,7 +956,10 @@ class TestReports(TestReportsCommon):
         wh_1_receipt.action_confirm()
         wh_2_receipt.action_confirm()
 
-        report_values, docs, lines = self.get_report_forecast(product_template_ids=self.product_template.ids)
+        report_values, docs, lines = self.get_report_forecast(
+            product_template_ids=self.product_template.ids,
+            context={'warehouse_id': wh_1.id},
+        )
         self.assertEqual(len(lines), 2, "Must have 2 lines.")
         self.assertEqual(lines[1]['document_in']['id'], wh_1_receipt.id)
         self.assertEqual(lines[1]['quantity'], 2)
