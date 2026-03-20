e3d8836ad504ddb55630fb78fd3ffb7aaa2db6b1
dvsh-odoo <dvsh@odoo.com>
2026-03-20 17:49:16 +0530

    [MERGE] l10n_dk: merge l10n_dk_fik into l10n_dk
    - Merge l10n_dk_fik into l10n_dk and integrate FIK
      payment reference directly into the main module.
    
    - This includes the ability to generate Danish FIK payment references
      from sales journals using a configured 8-digit creditor number.
    
    Related taskID-5401553

--- addons/l10n_dk/models/account_journal.py
+++ addons/l10n_dk/models/account_journal.py
@@ -1,11 +1,58 @@
+import re
+
 from odoo import Command, api, fields, models
+from odoo.exceptions import ValidationError
+
+L10N_DK_FIK_MODELS = ('dk_fik_71', 'dk_fik_75')
 
 
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
+    invoice_reference_model = fields.Selection(
+        selection_add=[
+            ('dk_fik_71', "Denmark FIK Number (+71)"),
+            ('dk_fik_75', "Denmark FIK Number (+75)"),
+        ],
+        ondelete={
+            'dk_fik_71': lambda recs: recs.write({'invoice_reference_model': 'odoo'}),
+            'dk_fik_75': lambda recs: recs.write({'invoice_reference_model': 'odoo'}),
+        },
+    )
+
+    l10n_dk_fik_creditor_number = fields.Char(
+        string="FIK Creditor Number",
+        compute='_compute_l10n_dk_fik_creditor_number',
+        store=True,
+        readonly=False,
+    )
     l10n_dk_nemhandel_proxy_state = fields.Selection(related='company_id.l10n_dk_nemhandel_proxy_state')
     is_nemhandel_journal = fields.Boolean(string='Journal used for Nemhandel')
+
+    @api.depends('invoice_reference_model', 'company_id.bank_ids.account_number')
+    def _compute_l10n_dk_fik_creditor_number(self):
+        for journal in self:
+            if journal.invoice_reference_model not in L10N_DK_FIK_MODELS:
+                journal.l10n_dk_fik_creditor_number = False
+                continue
+
+            bank = journal.company_id.bank_ids[:1]
+            creditor_number = '00000000'
+            if bank and bank.account_number:
+                digits = re.sub(r'\D', '', bank.account_number or '')
+                creditor_number = digits[-8:].zfill(8)
+
+            journal.l10n_dk_fik_creditor_number = creditor_number
+
+    @api.constrains('l10n_dk_fik_creditor_number', 'invoice_reference_model')
+    def _check_fik_creditor_number(self):
+        for record in self:
+            if record.invoice_reference_model not in L10N_DK_FIK_MODELS:
+                continue
+
+            creditor = record.l10n_dk_fik_creditor_number
+            if not creditor or not (creditor.isdigit() and len(creditor) == 8):
+                raise ValidationError(self.env._("FIK Creditor Number must be exactly 8 digits."))
 
     @api.depends('l10n_dk_nemhandel_proxy_state')
     def _compute_show_refresh_out_einvoices_status_button(self):
--- addons/l10n_dk/models/account_move.py
+++ addons/l10n_dk/models/account_move.py
@@ -1,5 +1,8 @@
+import re
+from stdnum import luhn
+
 from odoo import _, api, fields, models
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 
 class AccountMove(models.Model):
@@ -39,6 +42,31 @@ class AccountMove(models.Model):
                 move.nemhandel_move_state = False
             else:
                 move.nemhandel_move_state = move.nemhandel_move_state
+
+    def _get_invoice_reference_dk_fik(self, prefix, max_digits):
+        self.ensure_one()
+        invoice_digits = re.sub(r"\D", "", self.name or "") or str(self.id)
+        if len(invoice_digits) > max_digits:
+            raise ValidationError(
+               self.env._(
+                    "FIK %(prefix)s reference cannot be generated: invoice number '%(invoice)s' has more than %(max_digits)s digits.",
+                ) % {
+                    "prefix": prefix,
+                    "invoice": self.name,
+                    "max_digits": max_digits,
+                },
+            )
+        payment_id = invoice_digits.zfill(max_digits)
+        check_digit = luhn.calc_check_digit(payment_id)
+        creditor_number = self.journal_id.l10n_dk_fik_creditor_number
+
+        return f"+{prefix}<{payment_id}{check_digit}+{creditor_number}<"
+
+    def _get_invoice_reference_dk_fik_71_invoice(self):
+        return self._get_invoice_reference_dk_fik("71", 14)
+
+    def _get_invoice_reference_dk_fik_75_invoice(self):
+        return self._get_invoice_reference_dk_fik("75", 15)
 
     @api.model
     def _get_ubl_cii_builder_from_xml_tree(self, tree):
