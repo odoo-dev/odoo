6dad09c208f6eccc589eb426838b27e34ad22e20
luvi <luvi@odoo.com>
2026-03-25 09:06:30 +0100

    [IMP] web: inline boolean checkbox

--- addons/web/static/src/views/form/form_group/form_group.js
+++ addons/web/static/src/views/form/form_group/form_group.js
@@ -95,4 +95,10 @@ export class InnerGroup extends Group {
 
         return rows;
     }
+    computeCellDisplay(cell) {
+        if(!cell.props.fieldInfo.widget) {
+            cell.props.fieldInfo.widget = "boolean_toggle";
+        }
+        return cell;
+    }
 }
