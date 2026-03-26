2d9fba646555238774811bd2f317a6fa9d1afd44
Géry Debongnie <ged@odoo.com>
2026-03-26 10:46:14 +0100

    prepare standalone owl3 compatibility layer

--- addons/web/__manifest__.py
+++ addons/web/__manifest__.py
@@ -189,6 +189,7 @@ This module provides the core of the Odoo Web Client.
             ('include', 'web.assets_frontend_minimal'),
 
             'web/static/lib/owl/owl.js',
+            'web/static/src/owl2/owl3_compatibility_layer.js',
             'web/static/src/owl2/utils.js',
             'web/static/lib/owl/odoo_module.js',
             'web/static/lib/jquery/jquery.js',
@@ -357,6 +358,7 @@ This module provides the core of the Odoo Web Client.
             'web/static/lib/luxon/luxon.js',
             'web/static/src/libs/luxon.js',
             'web/static/lib/owl/owl.js',
+            'web/static/src/owl2/owl3_compatibility_layer.js',
             'web/static/src/owl2/utils.js',
             'web/static/lib/owl/odoo_module.js',
             # core
@@ -431,6 +433,7 @@ This module provides the core of the Odoo Web Client.
             'web/static/src/module_loader.js',
 
             'web/static/lib/owl/owl.js',
+            'web/static/src/owl2/owl3_compatibility_layer.js',
             'web/static/src/owl2/utils.js',
             'web/static/lib/owl/odoo_module.js',
 
@@ -474,6 +477,7 @@ This module provides the core of the Odoo Web Client.
             'web/static/src/module_loader.js',
 
             'web/static/lib/owl/owl.js',
+            'web/static/src/owl2/owl3_compatibility_layer.js',
             'web/static/src/owl2/utils.js',
             'web/static/lib/owl/odoo_module.js',
             'web/static/lib/luxon/luxon.js',
