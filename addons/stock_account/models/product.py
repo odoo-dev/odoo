a1b1e215205553ca06505bb2b88c7a853e4acf15
Shruti Chourasiya <scho@odoo.com>
2026-03-30 06:06:52 +0000

    [FIX] stock_account: use valuation context when calculating avg_cost

--- addons/stock_account/models/product.py
+++ addons/stock_account/models/product.py
@@ -268,9 +268,13 @@ class ProductProduct(models.Model):
             std_price_by_company_id[company.id] = std_price_by_product_id
             total_value_by_company_id[company.id] = total_value_by_product_id
 
-        for product in self:
+        self_ctx = self._with_valuation_context()
+        if at_date:
+            self_ctx = self_ctx.with_context(at_date=at_date, to_date=at_date)
+
+        for product, product_ctx in zip(self, self_ctx):
             product.total_value = sum(total_value_by_company_id[c.id].get(product.id, 0) for c in self.env.companies)
-            product.avg_cost = product.total_value / product.qty_available if product.qty_available else std_price_by_company_id[self.env.company.id].get(product.id, product.standard_price)
+            product.avg_cost = product.total_value / product_ctx.qty_available if product_ctx.qty_available else std_price_by_company_id[self.env.company.id].get(product.id, product.standard_price)
 
     @api.model_create_multi
     def create(self, vals_list):
