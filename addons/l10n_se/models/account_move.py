11abb22906afce2e0c7d3aeccb2dd8c4b8f36f62
Kyrillos <kymag@odoo.com>
2026-04-01 13:25:22 +0200

    [IMP] l10n_se: add delivery date on the move
    Following new legislation in Sweden, invoices must now display the actual delivery date.
    
    task-6076324

--- addons/l10n_se/models/account_move.py
+++ addons/l10n_se/models/account_move.py
@@ -54,6 +54,22 @@ class AccountMove(models.Model):
         self.ensure_one()
         return self._get_invoice_reference_se_ocr4(self.partner_id.ref if str(self.partner_id.ref).isdecimal() else str(self.partner_id.id))
 
+    def _post(self, soft=True):
+        # EXTENDS 'account.move'
+        to_post = super()._post(soft)
+        for move in self:
+            if move.is_sale_document() and move.country_code == 'SE' and not move.delivery_date:
+                move.delivery_date = move.invoice_date
+        return to_post
+
+    @api.onchange('move_type', 'country_code')
+    def _compute_show_delivery_date(self):
+        # EXTENDS 'account.move'
+        super()._compute_show_delivery_date()
+        for move in self:
+            if move.country_code == 'SE':
+                move.show_delivery_date = move.is_sale_document()
+
     @api.onchange('partner_id')
     def _onchange_partner_id(self):
         """ If Vendor Bill and Vendor OCR is set, add it. """
