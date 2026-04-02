980c0171760077c283afada39ee397b3959b4827
David <dacat@odoo.com>
2026-04-02 11:07:27 +0200

    [IMP] l10n_se: add delivery date on invoices
    New legislation in Sweden to add delivery date on invoices.
    
    Task-6076201

--- addons/l10n_se/models/account_move.py
+++ addons/l10n_se/models/account_move.py
@@ -9,6 +9,22 @@ from stdnum import luhn
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    @api.depends('country_code', 'move_type')
+    def _compute_show_delivery_date(self):
+        # EXTENDS 'account'
+        super()._compute_show_delivery_date()
+        for move in self:
+            if move.country_code == 'SE':
+                move.show_delivery_date = move.is_sale_document()
+
+    def _post(self, soft=True):
+        # EXTENDS 'account'
+        res = super()._post(soft)
+        for move in self:
+            if move.country_code == 'SE' and move.is_sale_document() and not move.delivery_date:
+                move.delivery_date = move.invoice_date or fields.Date.context_today(self)
+        return res
+
     def _get_invoice_reference_se_ocr2(self, reference):
         self.ensure_one()
         return reference + luhn.calc_check_digit(reference)
