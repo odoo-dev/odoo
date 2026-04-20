7a170da561de7f261c561ad56388ff83ece7ff04
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-04-20 15:49:16 +0000

    [IMP] orm: BinaryValue.filename
    Having the file name tied to the value of a binary field allows to
    manage its storage: we can write it in `ir.attachment.name` when a
    binary value is assigned to a binary field stored as attachment.
    This makes additional file name fields obsolete in other models.
    
    For it to work, we need to update the serialization format of binary
    fields over RPC. When reading a binary field, we return now a dictionary
    containing "content" (not included by default, force with
    `include_binary_content` context key), "filename" and "size". When
    writing, we accept such a dictionary or a base64 string for backward
    compatibility.
    
    task-6303991

--- addons/web/static/tests/views/list/list_view.test.js
+++ addons/web/static/tests/views/list/list_view.test.js
@@ -18681,7 +18681,6 @@ test(`context keys not passed down the stack and not to fields`, async () => {
                 tz: "taht",
                 uid: 7,
                 allowed_company_ids: [1],
-                bin_size: true,
                 list_view_ref: "foo_view_ref",
             },
         },
@@ -18715,7 +18714,6 @@ test(`context keys not passed down the stack and not to fields`, async () => {
                 tz: "taht",
                 uid: 7,
                 allowed_company_ids: [1],
-                bin_size: true,
             },
         },
     ]);
