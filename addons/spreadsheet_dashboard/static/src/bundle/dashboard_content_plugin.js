87f6707e572a8f045b398fa9d8a803fb63c5a5a2
Adrien Minne (adrm) <adrm@odoo.com>
2026-04-22 16:13:28 +0200

    wip

--- addons/spreadsheet_dashboard/static/src/bundle/dashboard_content_plugin.js
+++ addons/spreadsheet_dashboard/static/src/bundle/dashboard_content_plugin.js
@@ -3,7 +3,7 @@
 import { OdooCorePlugin } from "@spreadsheet/plugins";
 import { registries, coreTypes } from "@odoo/o-spreadsheet";
 
-const DEFAULT_CONTENT = `<div data-name="Mailing" class="o_layout oe_unremovable oe_unmovable o_empty_theme" style="background-color: #F7F7F7;"><div class="container o_mail_wrapper o_mail_regular oe_unremovable"><div class="row mw-100 mx-0"><div class="col o_mail_no_options o_mail_wrapper_td bg-white oe_structure o_savable oe_empty o_dirty" data-editor-message-default="true" data-editor-message="Drag blocks here" contenteditable="true"><section class="s_three_columns o_mail_snippet_general o_cc pt32 pb32" data-vxml="001" data-snippet="s_three_columns" data-name="Columns" contenteditable="false">
+const DEFAULT_CONTENT = `<div data-name="Dashboard" class="oe_unremovable oe_unmovable""><div class="container oe_unremovable"><div class="row mw-100 mx-0"><div class="col o_mail_no_options o_mail_wrapper_td bg-white oe_structure o_savable oe_empty o_dirty" data-editor-message-default="true" data-editor-message="Drag blocks here" contenteditable="true"><section class="s_three_columns o_mail_snippet_general o_cc pt32 pb32" data-vxml="001" data-snippet="s_three_columns" data-name="Columns" contenteditable="false">
         <div class="container" contenteditable="true">
             <div class="row">
                 <div class="o_mail_no_colorpicker pt16 pb16 col-12 col-md-12 o_draggable">
