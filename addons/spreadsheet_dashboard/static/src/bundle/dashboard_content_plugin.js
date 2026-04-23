5dc45e08a3d20d4e4719ceb78c9055f3e4e3eccd
Adrien Minne (adrm) <adrm@odoo.com>
2026-04-23 16:47:09 +0200

    [TMP] spreadsheet: update o_spreadsheet to latest version

--- addons/spreadsheet_dashboard/static/src/bundle/dashboard_content_plugin.js
+++ addons/spreadsheet_dashboard/static/src/bundle/dashboard_content_plugin.js
@@ -3,10 +3,15 @@
 import { OdooCorePlugin } from "@spreadsheet/plugins";
 import { registries, coreTypes } from "@odoo/o-spreadsheet";
 
-const DEFAULT_CONTENT = `<div data-name="Dashboard" class="oe_unremovable oe_unmovable""><div class="container oe_unremovable"><div class="row mw-100 mx-0"><div class="col o_mail_no_options o_mail_wrapper_td bg-white oe_structure o_savable oe_empty o_dirty" data-editor-message-default="true" data-editor-message="Drag blocks here" contenteditable="true"><section class="s_three_columns o_mail_snippet_general o_cc pt32 pb32" data-vxml="001" data-snippet="s_three_columns" data-name="Columns" contenteditable="false">
-        <div class="container" contenteditable="true">
-            <div class="row">
-                <div class="o_mail_no_colorpicker pt16 pb16 col-12 col-md-12 o_draggable">
+const DEFAULT_CONTENT = `
+<div data-name="Dashboard" class="oe_unremovable oe_unmovable"">
+    <div class="oe_unremovable">
+        <div class="row mw-100 mx-0">
+        <div class="col o_mail_no_options o_mail_wrapper_td bg-white oe_structure o_savable oe_empty o_dirty" data-editor-message-default="true" data-editor-message="Drag blocks here" contenteditable="true">
+            <section class="s_three_columns o_mail_snippet_general o_cc pt32 pb32" data-vxml="001" data-snippet="s_three_columns" data-name="Columns" contenteditable="false">
+                <div class="container" contenteditable="true">
+                    <div class="row">
+                    <div class="o_mail_no_colorpicker pt16 pb16 col-12 col-md-12 o_draggable">
                     <div class="card text-bg-white h-100">
                         <div class="o_not_editable" contenteditable="false">
                             <img class="card-img-top o_editable_media" src="/web/image/mass_mailing.s_three_columns_default_image_1" alt="" data-mimetype="image/jpeg">
