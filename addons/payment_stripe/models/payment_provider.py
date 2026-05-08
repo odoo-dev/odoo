959e9ccf24250a4f5793f28cfe47305b911e65db
Mazen <mamr@odoo.com>
2026-05-08 13:22:13 +0200

    [IMP] payment*: move unit conversion to provider

--- addons/payment_stripe/models/payment_provider.py
+++ addons/payment_stripe/models/payment_provider.py
@@ -8,7 +8,6 @@ from odoo import _, api, fields, models
 from odoo.exceptions import RedirectWarning, UserError, ValidationError
 from odoo.tools.urls import urljoin as url_join
 
-from odoo.addons.payment import utils as payment_utils
 from odoo.addons.payment.logging import get_payment_logger
 from odoo.addons.payment_stripe import const
 from odoo.addons.payment_stripe import utils as stripe_utils
@@ -320,12 +319,7 @@ class PaymentProvider(models.Model):
         inline_form_values = {
             "publishable_key": self._stripe_get_publishable_key(),
             "currency_name": currency_name,
-            "minor_amount": (
-                amount
-                and payment_utils.to_minor_currency_units(
-                    amount, currency, arbitrary_decimal_number=self._get_amount_precision(currency)
-                )
-            ),
+            "minor_amount": amount and self._to_minor_currency_units(amount, currency),
             "capture_method": "manual" if self.capture_manually else "automatic",
             "billing_details": {
                 "name": partner.name or "",
