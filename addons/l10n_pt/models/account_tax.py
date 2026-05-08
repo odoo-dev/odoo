e0d24f9a57ee5e6290bb35b845e717bc15a4d5d2
Ayush Modi <aamo@odoo.com>
2026-05-08 10:22:24 +0530

    [FIX] l10n_pt: prevent error

--- addons/l10n_pt/models/account_tax.py
+++ addons/l10n_pt/models/account_tax.py
@@ -9,7 +9,12 @@ class AccountTax(models.Model):
         # EXTENDS 'account'
         country_code = company.account_fiscal_country_id.code
         if country_code == 'PT':
-            mode = 'included'
+            if base_lines and all(
+                tax_data['price_include']
+                for base_line in base_lines
+                for tax_data in base_line['tax_details']['taxes_data']
+            ):
+                mode = 'included'
         super()._round_tax_details_tax_amounts(base_lines, company, mode=mode)
 
     @api.model
@@ -17,5 +22,10 @@ class AccountTax(models.Model):
         # EXTENDS 'account'
         country_code = company.account_fiscal_country_id.code
         if country_code == 'PT':
-            mode = 'included'
+            if base_lines and all(
+                tax_data['price_include']
+                for base_line in base_lines
+                for tax_data in base_line['tax_details']['taxes_data']
+            ):
+                mode = 'included'
         super()._round_tax_details_base_lines(base_lines, company, mode=mode)
