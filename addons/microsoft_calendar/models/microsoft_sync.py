e5153e10fbec52dbcddce876906344eb5cfdcd5f
Sylvio Poliart (sypol) <sypol@odoo.com>
2026-05-13 13:37:16 +0200

    [FIX] microsoft_calendar: prevent invitation emails on recurrence sync
    When syncing a recurring event from Outlook that contains exception
    occurrences (i.e. modified instances), invitation emails were sent to
    all attendees for each exception, even though the event originated from
    Microsoft and was not created by the user.
    
    Steps to reproduce:
    1. Enable Microsoft Calendar sync for a user who has attendees.
    2. In Outlook, create a recurring event and modify one occurrence to
       create an exception (e.g. change its start time).
    3. Trigger the sync in Odoo (_sync_microsoft_calendar).
    4. Observe: an invitation email is sent for the exception occurrence.
    
    The root cause: _sync_recurrence_microsoft2odoo() called
    _microsoft_to_odoo_values() without with_ids=True for exception
    occurrences. As a result, microsoft_id was never set on the created
    event, causing _skip_send_mail_status_update() to return False and the
    invitation email flow to proceed.
    
    opw-6109770

--- addons/microsoft_calendar/models/microsoft_sync.py
+++ addons/microsoft_calendar/models/microsoft_sync.py
@@ -191,7 +191,7 @@ class MicrosoftCalendarSync(models.AbstractModel):
                 if recurrent_event.type == 'occurrence':
                     value = self.env['calendar.event']._microsoft_to_odoo_recurrence_values(recurrent_event, base_values)
                 else:
-                    value = self.env['calendar.event']._microsoft_to_odoo_values(recurrent_event, default_values)
+                    value = self.env['calendar.event']._microsoft_to_odoo_values(recurrent_event, default_values, with_ids=True)
 
                 to_create_values += [dict(value, need_sync_m=False)]
 
