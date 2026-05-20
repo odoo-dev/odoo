stxavierhighschoolkmd-lab/Odoo-school1

[IMP] *: migrate to Material Symbols icon system
This commit removes the Font Awesome library and introduces Material
Symbols as the main icon system. To cover icons not provided by Material
Symbols, the Odoo UI Icon (OI) library is maintained (e.g., custom
kanban view icons or brand icons such as Facebook or GitHub).
This library is reworked with a new implementation based on the oi
class prefix and a `data-icon` attribute.

For example:
`class="fa fa-check"` becomes `class="oi" data-icon="check"`

In addition to existing controller classes, icons can now be defined as
outlined or filled using the `oi-outlined`` and `oi-filled`` classes, or
by adjusting the `--oi-mi-fill`` CSS variable.

To ease the transition, a temporary mapping is introduced in icons.scss
to allow continued use of the `fa-` classes. This mapping is intended to
be removed once the migration is complete.

This commit also fine-tunes the RTL support for the new icon system.

The `fa-[6-10]x`` size utility classes, previously defined in
`html_editor` but used across multiple modules, are moved to
`webclient/icons.scss` to improve reusability.

Finally, `odoo_ui_icon.css` is renamed to align with the Material
Symbols implementation.

Co-authored-by: stefanorigano (sri) <sri@odoo.com>

task-5901783

addons/web/__manifest__.py
@@ -546,23 +546,23 @@ This module provides the core of the Odoo Web Client.
             '/web/static/lib/fullcalendar/timegrid/index.global.js',
             '/web/static/lib/fullcalendar/list/index.global.js',
         ],
-        # Icons bundles: font-awesome, odoo_ui_icons and both combined. Only the
-        # combination should probably be used. But it is split to allow FA
+        # Icons bundles: material_symbols, odoo_ui_icons and both combined. Only the
+        # combination should probably be used. But it is split to allow MS
         # preload on the frontend.
-        'web.fontawesome': [
-            '/web/static/src/libs/fontawesome/fonts/fontawesome-webfont.woff2',
-            '/web/static/src/libs/fontawesome/fonts/fontawesome-webfont.woff',
-            '/web/static/src/libs/fontawesome/css/font-awesome.css',
+        'web.material_symbols': [
+            '/web/static/src/libs/materialsymbols/MaterialSymbolsRounded_wgth-400_opsz-24.woff2',
+            '/web/static/src/libs/materialsymbols/MaterialSymbolsSharp_wgth-400_opsz-24.woff2',
+            '/web/static/src/libs/materialsymbols/materialsymbols.css',
         ],
         'web.odoo_ui_icons': [
             '/web/static/lib/odoo_ui_icons/fonts/odoo_ui_icons.woff2',
             '/web/static/lib/odoo_ui_icons/fonts/odoo_ui_icons.woff',
-            '/web/static/lib/odoo_ui_icons/style.css',
+            '/web/static/lib/odoo_ui_icons/odoo_ui_icons.css',
         ],
         'web.icons_fonts': [
-            ('include', 'web.fontawesome'),
+            ('include', 'web.material_symbols'),
             ('include', 'web.odoo_ui_icons'),
-            'web/static/src/scss/fontawesome_overridden.scss',  # some are fa classes... but using odoo_ui_icons font
+            'web/static/src/webclient/icons.scss',
         ],
     },
     'bootstrap': True,  # load translations for login screen,

addons/website/tests/test_assets.py
@@ -138,15 +138,12 @@ class TestWebsiteAssets(odoo.tests.HttpCase):
         # Verify that CSS contains link to binary asset
         css = self.url_open('/web/assets/_______/web.assets_frontend.min.css').text
         self.assertNotIn('''/web/static/src/libs/fontawesome/fonts/fontawesome-webfont.woff''', css, "Fonts should have been replaced")
-        fontface = re.findall(r'''@font-face{font-family: 'FontAwesome'; src: url\("/web/assets/\w{7}/web.fontawesome.min.woff2"\) format\('woff2'\), url\("/web/assets/\w{7}/web.fontawesome.min.woff"\) format\('woff'\);''', css)
+        fontface = re.findall(r'''@font-face{font-family: 'material_symbols_rounded'; src: url\("/web/assets/\w{7}/web.material_symbols.min.woff2"\) format\('woff2'\);''', css)
         self.assertTrue(fontface, "Font should have been replaced")
         # Verify that links actually return expected binary
-        font = self.url_open('/web/assets/_______/web.fontawesome.min.woff2')
+        font = self.url_open('/web/assets/_______/web.material_symbols.min.woff2')
         self.assertEqual('font/woff2', font.headers.get('Content-Type'), "Should be woff2")
         self.assertEqual(b'wOF2', font.content[:4])
-        font = self.url_open('/web/assets/_______/web.fontawesome.min.woff')
-        self.assertEqual('font/woff', font.headers.get('Content-Type'), "Should be woff")
-        self.assertEqual(b'wOFF', font.content[:4])
 
     def test_binary_asset_website(self):
         # Make website 1's CSS distinct from base one
@@ -159,15 +156,12 @@ class TestWebsiteAssets(odoo.tests.HttpCase):
         # Verify that CSS contains link to binary asset
         css = self.url_open(f'/web/assets/{website_id}/_______/web.assets_frontend.min.css').text
         self.assertNotIn('''/web/static/src/libs/fontawesome/fonts/fontawesome-webfont.woff''', css, "Fonts should have been replaced")
-        fontface = re.findall(rf'''@font-face{{font-family: 'FontAwesome'; src: url\("/web/assets/{website_id}/\w{{7}}/web.fontawesome.min.woff2"\) format\('woff2'\), url\("/web/assets/{website_id}/\w{{7}}/web.fontawesome.min.woff"\) format\('woff'\);''', css)
+        fontface = re.findall(rf'''@font-face{{font-family: 'material_symbols_rounded'; src: url\("/web/assets/{website_id}/\w{{7}}/web.material_symbols.min.woff2"\) format\('woff2'\);''', css)
         self.assertTrue(fontface, "Font should have been replaced")
         # Verify that links actually return expected binary
-        font = self.url_open(f'/web/assets/{website_id}/_______/web.fontawesome.min.woff2')
+        font = self.url_open(f'/web/assets/{website_id}/_______/web.material_symbols.min.woff2')
         self.assertEqual('font/woff2', font.headers.get('Content-Type'), "Should be woff2")
         self.assertEqual(b'wOF2', font.content[:4])
-        font = self.url_open(f'/web/assets/{website_id}/_______/web.fontawesome.min.woff')
-        self.assertEqual('font/woff', font.headers.get('Content-Type'), "Should be woff")
-        self.assertEqual(b'wOFF', font.content[:4])
 
 
 @odoo.tests.tagged('-at_install', 'post_install')

odoo-dev/odoo

addons/web/__manifest__.py
@@ -526,23 +526,27 @@ This module provides the core of the Odoo Web Client.
             '/web/static/lib/fullcalendar/timegrid/index.global.js',
             '/web/static/lib/fullcalendar/list/index.global.js',
         ],
-        # Icons bundles: font-awesome, odoo_ui_icons and both combined. Only the
-        # combination should probably be used. But it is split to allow FA
+        # Icons bundles: material_symbols, odoo_ui_icons and both combined. Only the
+        # combination should probably be used. But it is split to allow MS
         # preload on the frontend.
-        'web.fontawesome': [
-            '/web/static/src/libs/fontawesome/fonts/fontawesome-webfont.woff2',
-            '/web/static/src/libs/fontawesome/fonts/fontawesome-webfont.woff',
-            '/web/static/src/libs/fontawesome/css/font-awesome.css',
+        'web.material_symbols_outlined': [
+            '/web/static/src/libs/materialsymbols/MaterialSymbolsOutlined_wgth-400_opsz-24.woff2',
+            '/web/static/src/libs/materialsymbols/material_symbols_outlined.css',
+        ],
+        'web.material_symbols_sharp': [
+            '/web/static/src/libs/materialsymbols/MaterialSymbolsSharp_wgth-400_opsz-24.woff2',
+            '/web/static/src/libs/materialsymbols/material_symbols_sharp.css',
         ],
         'web.odoo_ui_icons': [
             '/web/static/lib/odoo_ui_icons/fonts/odoo_ui_icons.woff2',
             '/web/static/lib/odoo_ui_icons/fonts/odoo_ui_icons.woff',
-            '/web/static/lib/odoo_ui_icons/style.css',
+            '/web/static/lib/odoo_ui_icons/odoo_ui_icons.css',
         ],
         'web.icons_fonts': [
-            ('include', 'web.fontawesome'),
+            ('include', 'web.material_symbols_outlined'),
+            ('include', 'web.material_symbols_sharp'),
             ('include', 'web.odoo_ui_icons'),
-            'web/static/src/scss/fontawesome_overridden.scss',  # some are fa classes... but using odoo_ui_icons font
+            'web/static/src/webclient/icons.scss',
         ],
     },
     'bootstrap': True,  # load translations for login screen,

addons/website/tests/test_assets.py
@@ -138,15 +138,12 @@ class TestWebsiteAssets(odoo.tests.HttpCase):
         # Verify that CSS contains link to binary asset
         css = self.url_open('/web/assets/_______/web.assets_frontend.min.css').text
         self.assertNotIn('''/web/static/src/libs/fontawesome/fonts/fontawesome-webfont.woff''', css, "Fonts should have been replaced")
-        fontface = re.findall(r'''@font-face{font-family: 'FontAwesome'; src: url\("/web/assets/\w{7}/web.fontawesome.min.woff2"\) format\('woff2'\), url\("/web/assets/\w{7}/web.fontawesome.min.woff"\) format\('woff'\);''', css)
+        fontface = re.findall(r'''@font-face{font-family: 'material_symbols_outlined'; src: url\("/web/assets/\w{7}/web.material_symbols_outlined.min.woff2"\) format\('woff2'\);''', css)
         self.assertTrue(fontface, "Font should have been replaced")
         # Verify that links actually return expected binary
-        font = self.url_open('/web/assets/_______/web.fontawesome.min.woff2')
+        font = self.url_open('/web/assets/_______/web.material_symbols_outlined.min.woff2')
         self.assertEqual('font/woff2', font.headers.get('Content-Type'), "Should be woff2")
         self.assertEqual(b'wOF2', font.content[:4])
-        font = self.url_open('/web/assets/_______/web.fontawesome.min.woff')
-        self.assertEqual('font/woff', font.headers.get('Content-Type'), "Should be woff")
-        self.assertEqual(b'wOFF', font.content[:4])
 
     def test_binary_asset_website(self):
         # Make website 1's CSS distinct from base one
@@ -159,15 +156,12 @@ class TestWebsiteAssets(odoo.tests.HttpCase):
         # Verify that CSS contains link to binary asset
         css = self.url_open(f'/web/assets/{website_id}/_______/web.assets_frontend.min.css').text
         self.assertNotIn('''/web/static/src/libs/fontawesome/fonts/fontawesome-webfont.woff''', css, "Fonts should have been replaced")
-        fontface = re.findall(rf'''@font-face{{font-family: 'FontAwesome'; src: url\("/web/assets/{website_id}/\w{{7}}/web.fontawesome.min.woff2"\) format\('woff2'\), url\("/web/assets/{website_id}/\w{{7}}/web.fontawesome.min.woff"\) format\('woff'\);''', css)
+        fontface = re.findall(rf'''@font-face{{font-family: 'material_symbols_outlined'; src: url\("/web/assets/{website_id}/\w{{7}}/web.material_symbols_outlined.min.woff2"\) format\('woff2'\);''', css)
         self.assertTrue(fontface, "Font should have been replaced")
         # Verify that links actually return expected binary
-        font = self.url_open(f'/web/assets/{website_id}/_______/web.fontawesome.min.woff2')
+        font = self.url_open(f'/web/assets/{website_id}/_______/web.material_symbols_outlined.min.woff2')
         self.assertEqual('font/woff2', font.headers.get('Content-Type'), "Should be woff2")
         self.assertEqual(b'wOF2', font.content[:4])
-        font = self.url_open(f'/web/assets/{website_id}/_______/web.fontawesome.min.woff')
-        self.assertEqual('font/woff', font.headers.get('Content-Type'), "Should be woff")
-        self.assertEqual(b'wOFF', font.content[:4])
 
 
 @odoo.tests.tagged('-at_install', 'post_install')