20019b0ae1a6309dc7d2809e8146900000ac68db
Aaron Bohy <aab@odoo.com>
2026-05-21 11:44:10 +0200

    [FIX] mail: skip test that makes others fail

--- addons/mail/static/tests/discuss/call/call.test.js
+++ addons/mail/static/tests/discuss/call/call.test.js
@@ -413,7 +413,8 @@ test("'New Meeting' in mobile", async () => {
     await contains(".o-discuss-ChannelMember:text('Partner 2')");
 });
 
-test("Dropzones below fullscreen meeting view are disabled", async () => {
+test.tags("owl3");
+test.skip("Dropzones below fullscreen meeting view are disabled", async () => {
     const popoutIframe = document.createElement("iframe");
     const outsideArea = document.createElement("div");
     getFixture().appendChild(outsideArea);
@@ -493,8 +494,7 @@ test("Dropzones below fullscreen meeting view are disabled", async () => {
     await contains(".o-mail-Discuss .o-mail-AttachmentContainer:not(.o-isUploading)", { count: 2 });
 });
 
-test.tags("owl3");
-test.skip("Systray icon shows latest action", async () => {
+test("Systray icon shows latest action", async () => {
     const pyEnv = await startServer();
     const channelId = pyEnv["discuss.channel"].create({ name: "General" });
     await start();
