29d18a69d1e0f1ef01887abc914b7edc44cb4976
mear-odoo <mear@odoo.com>
2026-05-26 12:19:28 +0530

    [IMP] mail:  make typing indicator background color follow user presence status
    Previously, the typing indicator always displayed a green background, regardless
    of the user's actual presence. This led to inconsistent behavior, as users
    marked as idle, offline, or in DND were still shown typing in green.
    
    This commit ensures that the typing indicator background color reflects the
    user's presence status by displaying the corresponding color for online, idle,
    offline, and DND states.
    
    task-6232735

--- addons/mail/static/src/discuss/typing/common/thread_icon_patch.js
+++ addons/mail/static/src/discuss/typing/common/thread_icon_patch.js
@@ -1,8 +1,9 @@
 import { ThreadIcon } from "@mail/core/common/thread_icon";
-import { Typing } from "@mail/discuss/typing/common/typing";
 
 import { patch } from "@web/core/utils/patch";
 
-patch(ThreadIcon, {
-    components: { ...ThreadIcon.components, Typing },
+patch(ThreadIcon.prototype, {
+    get typingMember() {
+        return this.correspondent || this.channel?.otherTypingMembers[0];
+    },
 });
