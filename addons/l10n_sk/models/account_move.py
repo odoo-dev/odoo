31c57da13e9cc49bf2de3ac664dd81206c568607
Arnaud Sibille <arsi@odoo.com>
2026-05-28 15:33:04 +0200

    [IMP] don't copy sk taxable_supply_date

--- addons/l10n_sk/models/account_move.py
+++ addons/l10n_sk/models/account_move.py
@@ -5,4 +5,4 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    taxable_supply_date = fields.Date()
+    taxable_supply_date = fields.Date(copy=False)
