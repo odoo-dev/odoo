78ac4968873e2821c1a75ba120f5ad420c1ea0f2
Julien Alardot (jual) <jual@odoo.com>
2026-06-02 13:51:22 +0200

    [FIX] uom: Fix conversion if same unit of measure
    Before this fix, when passing an amount from a model to another,
    we would enforce quantity rounding for same unit of measures.

--- addons/uom/models/uom_uom.py
+++ addons/uom/models/uom_uom.py
@@ -159,16 +159,13 @@ class UomUom(models.Model):
                 - if true, raise an exception if the conversion is not possible (different UomUom category),
                 - otherwise, return the initial quantity
         """
-        if not self or not qty:
+        if not self or not qty or self == to_unit:
             return qty
         self.ensure_one()
 
-        if self == to_unit:
-            amount = qty
-        else:
-            amount = qty * self.factor
-            if to_unit:
-                amount = amount / to_unit.factor
+        amount = qty * self.factor
+        if to_unit:
+            amount = amount / to_unit.factor
 
         if to_unit and round:
             amount = tools.float_round(amount, precision_rounding=to_unit.rounding, rounding_method=rounding_method)
