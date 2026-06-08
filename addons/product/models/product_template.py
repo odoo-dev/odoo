7b4e6bf99ce651d74659a74999c685a494e4c861
pusu-odoo <pusu@odoo.com>
2026-06-08 16:27:45 +0530

    [IMP] product: hide fields
    WIP

--- addons/product/models/product_template.py
+++ addons/product/models/product_template.py
@@ -124,7 +124,7 @@ class ProductTemplate(models.Model):
 
     is_storable = fields.Boolean(
         'Track Inventory', store=True, compute='_compute_is_storable', readonly=False,
-        default=False, precompute=True, tracking=True,
+        default=False, tracking=True,
         help='A storable product is a product for which you manage stock.')
     qty_available = fields.Float(
         'Quantity On Hand', compute='_compute_quantities', search='_search_qty_available',
