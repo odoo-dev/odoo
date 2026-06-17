77736b44ad8a4ef414561db077e799c44298f560
Xavier Morel <xmo@odoo.com>
2026-06-17 13:26:44 +0200

    [ADD] core: support for dictConfig
    Allows configuring the entire logging subsystem declaratively. This
    can be a bit complicated to tune, but it avoids the need for bespoke
    code in Odoo for fine tuning, or third party handlers e.g. let's say
    someone wants
    
    - a terser stdout logger, which can be split out using `cut`
    - and a DEBUG JSON-based file logger for long-term info
    - buffered to only dump its content every 10000 records[^1]
    - except for the SQL queries, or werkzeug
    
    then something like this works:
    
        {
          "version": 1,
          "formatters": {
            "singledb": {
              "class": "odoo.netsvc.ColoredFormatter",
              "format": "%(asctime)s\t%(levelname)s\t%(name)s\t%(message)s",
              "datefmt": "%Y-%m-%dT%H:%M:%S"
            },
            "json": {
              "class": "json_log_formatter.VerboseJSONFormatter"
            }
          },
          "handlers": {
            "console": {
              "class": "logging.StreamHandler",
              "formatter": "singledb",
              "level": "INFO",
              "stream": "ext://sys.stderr"
            },
            "json": {
              "class": "logging.handlers.WatchedFileHandler",
              "formatter": "json",
              "filename": "odoo.log"
            },
            "buffer": {
              "class": "logging.handlers.MemoryHandler",
              "target": "json",
              "capacity": 10000
            }
          },
          "root": {
            "handlers": ["console", "buffer"],
            "level": "DEBUG"
          },
          "loggers": {
            "odoo.sql_db": {"level": "INFO"},
          }
        }
    
    (this assumes `json_log_formatter` is installed in the python
    environment).
    
    This addition includes an opt-in extension to dictConfig to allow still
    running Odoo's hard-coded logging configuration, so that log config
    can be used in addition to existing setup, for convenience.
    
    [^1]: in reality that part's not super useful as the underlying
        handlers don't support batching calls but it shows how handlers
        can build on one another
    
    (cherry picked from commit 046d4a82b13fb0e80668b7f41238c62182065b25)

--- odoo/tools/config.py
+++ odoo/tools/config.py
@@ -215,6 +215,9 @@ class configmanager(object):
         group.add_option('--log-sql', action="append_const", dest="log_handler", const="odoo.sql_db:DEBUG", help='shortcut for --log-handler=odoo.sql_db:DEBUG')
         group.add_option('--log-db', dest='log_db', help="Logging database", my_default=False)
         group.add_option('--log-db-level', dest='log_db_level', my_default='warning', help="Logging database level")
+        group.add_option('--log-config', dest='log_config', type='path', my_default='',
+                         help="JSON logging configuration file, in dictconfig format ("
+                              "https://docs.python.org/3/library/logging.config.html#logging-config-dictschema).")
         # For backward-compatibility, map the old log levels to something
         # quite close.
         levels = [
