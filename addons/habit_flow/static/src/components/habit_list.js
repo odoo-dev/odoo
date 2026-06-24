3817a93d77af6c66b852b16061b631e58d712208
Dhrutik Patel <dhrp@odoo.com>
2026-06-24 16:13:34 +0530

    WIP 2

--- addons/habit_flow/static/src/components/habit_list.js
+++ addons/habit_flow/static/src/components/habit_list.js
@@ -6,29 +6,9 @@ export class HabitList extends Component {
     static components = { HabitCard };
     props = props({
         habits: t.array(),
-        activeFilter: t.string(),
-        hasAnyHabits: t.boolean(),
-        onCreate: t.function(),
         onToggleToday: t.function(),
         onEdit: t.function(),
         onArchive: t.function(),
         onUnarchive: t.function(),
     });
-
-    get emptyTitle() {
-        if (!this.props.hasAnyHabits) {
-            return "Create your first habit";
-        }
-        if (this.props.activeFilter === "archived") {
-            return "No archived habits";
-        }
-        return "No habits match this view";
-    }
-
-    get emptyBody() {
-        if (!this.props.hasAnyHabits) {
-            return "HabitFlow keeps your habits in this browser only. Add a habit to start tracking.";
-        }
-        return "Try another filter, clear the search, or add a new habit.";
-    }
 }
