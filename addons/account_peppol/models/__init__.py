de80fe5363fd122f439a0e6d1ce3cbeec218533d
Paul Morelle <pmo@odoo.com>
2026-07-02 14:44:58 +0200

    [IMP] account_peppol: report KYC status as a KPI
    With this commit, the KYC status of a database will be reported along
    with the other KPIs, to be used by the databases application.
    
    The new KPI type `kyc_status` is introduced by odoo/enterprise#122510
    
    Task-id: 6348952

--- addons/account_peppol/models/__init__.py
+++ addons/account_peppol/models/__init__.py
@@ -5,6 +5,7 @@ from . import account_edi_xml_ubl_bis3
 from . import account_journal
 from . import account_move
 from . import account_move_send
+from . import kpi_provider
 from . import res_company
 from . import res_config_settings
 from . import res_partner
---
+++ addons/account_peppol/tests/test_kpi_provider.py
@@ -0,0 +1,154 @@
+from itertools import product
+
+from odoo.tests import tagged, TransactionCase
+from odoo.tests.common import decorator
+
+
+def main_company_countries(*country_codes):
+    """ Decorate a method to execute it once for each given country """
+    @decorator
+    def _main_company_countries(func, *args, **kwargs):
+        self = args[0]
+        old_country_id = self.main_company.partner_id.country_id
+        try:
+            # retrieve countries
+            Countries = self.env['res.country'].with_context(active_test=False)
+            countries = {
+                country.code: country
+                for country in Countries.search([('code', 'in', list(country_codes))])
+            }
+            for country_code, country_id in countries.items():
+                with self.subTest(country_code=country_code):
+                    self.main_company.partner_id.country_id = country_id
+                    func(*args, **kwargs)
+                self.env.invalidate_all()
+        finally:
+            self.main_company.partner_id.country_id = old_country_id
+
+    return _main_company_countries
+
+
+@tagged('post_install', '-at_install')
+class TestKpiProvider(TransactionCase):
+
+    def setUp(self):
+        super().setUp()
+
+        self.main_company = self.env.ref('base.main_company')
+        other_companies = self.env['res.company'].search([('id', '!=', self.main_company.id)])
+        self.env['res.users'].search([('company_id', 'in', other_companies.ids)]).active = False
+        other_companies.active = False
+
+        self.expected_value_by_peppol_proxy_state = {
+            'not_registered': 'not_done',
+            'rejected': 'not_done',
+            'sender': 'not_done',
+            'smp_registration': 'incomplete',
+            'receiver': 'done',
+        }
+
+    @main_company_countries('US', 'LU', 'IT', 'CN')
+    def test_default_kpi_summary(self):
+        self.assertCountEqual(self.env['kpi.provider'].get_account_peppol_kpi_summary(), [])
+
+    def test_check_all_possible_peppol_proxy_states_are_covered(self):
+        expected = {s[0] for s in self.env['res.company']._fields['account_peppol_proxy_state'].selection}
+        actual = set(self.expected_value_by_peppol_proxy_state)
+        self.assertEqual(actual, expected,
+                         f"Missing states: {expected - actual}; "
+                         f"Extra states: {actual - expected}")
+
+    def test_kpi_summary_with_multiple_companies(self):
+        other_company = self.env['res.company'].create({'name': 'Other LTD'})
+        self.main_company.partner_id.country_id = other_company.partner_id.country_id = self.env.ref('base.be')
+
+        table_of_truth = {
+            ('not_registered', 'not_registered'):       'not_done',
+            ('not_registered', 'rejected'):             'not_done',
+            ('not_registered', 'sender'):               'not_done',
+            ('not_registered', 'smp_registration'):     'incomplete',
+            ('not_registered', 'receiver'):             'incomplete',
+
+            ('rejected', 'not_registered'):             'not_done',
+            ('rejected', 'rejected'):                   'not_done',
+            ('rejected', 'sender'):                     'not_done',
+            ('rejected', 'smp_registration'):           'incomplete',
+            ('rejected', 'receiver'):                   'incomplete',
+
+            ('sender', 'not_registered'):               'not_done',
+            ('sender', 'rejected'):                     'not_done',
+            ('sender', 'sender'):                       'not_done',
+            ('sender', 'smp_registration'):             'incomplete',
+            ('sender', 'receiver'):                     'incomplete',
+
+            ('smp_registration', 'not_registered'):     'incomplete',
+            ('smp_registration', 'rejected'):           'incomplete',
+            ('smp_registration', 'sender'):             'incomplete',
+            ('smp_registration', 'smp_registration'):   'incomplete',
+            ('smp_registration', 'receiver'):           'incomplete',
+
+            ('receiver', 'not_registered'):             'incomplete',
+            ('receiver', 'rejected'):                   'incomplete',
+            ('receiver', 'sender'):                     'incomplete',
+            ('receiver', 'smp_registration'):           'incomplete',
+            ('receiver', 'receiver'):                   'done',
+        }
+
+        actual_keys = set(table_of_truth)
+        expected_keys = set(product((s[0] for s in self.env['res.company']._fields['account_peppol_proxy_state'].selection), repeat=2))
+        self.assertEqual(actual_keys, expected_keys,
+                         f"Missing table of truth keys: {expected_keys - actual_keys}; "
+                         f"Extra table of truth keys: {actual_keys - expected_keys}")
+
+        for (proxy_state1, proxy_state2), expected_value in table_of_truth.items():
+            with self.subTest(proxy_state1=proxy_state1, proxy_state2=proxy_state2):
+                self.main_company.account_peppol_proxy_state = proxy_state1
+                other_company.account_peppol_proxy_state = proxy_state2
+
+                self.assertCountEqual(self.env['kpi.provider'].get_account_peppol_kpi_summary(), [{
+                    'id': 'account_peppol.proxy_state',
+                    'name': 'KYC',
+                    'type': 'kyc_status',
+                    'value': expected_value,
+                }])
+
+    @main_company_countries('BE')
+    def test_kpi_summary_peppol_proxy_states_be(self):
+        for proxy_state, expected_value in self.expected_value_by_peppol_proxy_state.items():
+            with self.subTest(proxy_state=proxy_state):
+                self.main_company.account_peppol_proxy_state = proxy_state
+
+                self.assertCountEqual(self.env['kpi.provider'].get_account_peppol_kpi_summary(), [{
+                    'id': 'account_peppol.proxy_state',
+                    'name': 'KYC',
+                    'type': 'kyc_status',
+                    'value': expected_value,
+                }])
+
+    @main_company_countries('FR', 'GP', 'MQ', 'RE')
+    def test_kpi_summary_peppol_proxy_states_fr(self):
+        self.main_company.partner_id.peppol_eas = '0208'
+
+        for proxy_state, expected_value in self.expected_value_by_peppol_proxy_state.items():
+            with self.subTest(proxy_state=proxy_state):
+                self.main_company.account_peppol_proxy_state = proxy_state
+
+                self.assertCountEqual(self.env['kpi.provider'].get_account_peppol_kpi_summary(), [{
+                    'id': 'account_peppol.proxy_state',
+                    'name': 'KYC',
+                    'type': 'kyc_status',
+                    'value': 'not_done',
+                }])
+
+        self.main_company.partner_id.peppol_eas = '0225'
+
+        for proxy_state, expected_value in self.expected_value_by_peppol_proxy_state.items():
+            with self.subTest(proxy_state=proxy_state):
+                self.main_company.account_peppol_proxy_state = proxy_state
+
+                self.assertCountEqual(self.env['kpi.provider'].get_account_peppol_kpi_summary(), [{
+                    'id': 'account_peppol.proxy_state',
+                    'name': 'KYC',
+                    'type': 'kyc_status',
+                    'value': expected_value,
+                }])
