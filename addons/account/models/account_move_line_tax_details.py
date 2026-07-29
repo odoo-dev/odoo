051c17be55aa997dcaf7f8f723775daed9c98fe3
Josse Colpaert <jco@odoo.com>
2026-07-29 12:21:59 +0200

    [IMP] account: python hybrid approach

--- addons/account/models/account_move_line_tax_details.py
+++ addons/account/models/account_move_line_tax_details.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+
 from odoo import api, models
 from odoo.tools import SQL
 
@@ -10,8 +12,9 @@ class AccountMoveLine(models.Model):
     def _get_query_tax_details_simplified(self, table_references, search_condition):
         return SQL('''
             WITH filtered_aml AS MATERIALIZED (
-                SELECT account_move_line.*, account_move_line__move_id.move_type AS move_type
+                SELECT account_move_line.*, move.move_type AS move_type
                 FROM %(table_references)s
+                JOIN account_move move ON move.id = account_move_line.move_id
                 WHERE %(search_condition)s
             ),
             base_lines AS (
@@ -19,6 +22,11 @@ class AccountMoveLine(models.Model):
                 FROM filtered_aml f
                 JOIN account_move_line_account_tax_rel rel ON f.id = rel.account_move_line_id
                 WHERE f.tax_repartition_line_id IS NULL
+                OR EXISTS (
+                    SELECT 1
+                    FROM account_move_line_account_tax_rel tax_line_rel
+                    WHERE tax_line_rel.account_move_line_id = f.id
+                )
             ),
             tax_lines AS (
                 SELECT
@@ -34,16 +42,46 @@ class AccountMoveLine(models.Model):
             tax_data AS (
                 SELECT
                     lt.id AS tax_line_id, lt.balance AS tax_amount,
+                    lt.amount_currency AS tax_amount_currency,
                     aml.id AS base_line_id, aml.move_id,
-                    t.sequence, t.id AS tax_id,
-                    CASE WHEN t.amount_type <> 'fixed' THEN aml.balance ELSE aml.quantity END AS base_value
+                    lt.display_type,
+                    lt.tax_line_id AS tax_id,
+                    lt.group_tax_id,
+                    lt.tax_repartition_line_id,
+                    lt.account_id AS tax_account_id,
+                    aml.account_id AS base_account_id,
+                    t.sequence,
+                    CASE WHEN t.amount_type <> 'fixed' THEN aml.balance ELSE aml.quantity END AS base_value,
+                    aml.balance AS base_amount,
+                    CASE WHEN t.amount_type <> 'fixed' THEN aml.amount_currency ELSE aml.quantity END AS base_value_currency,
+                    aml.amount_currency AS base_amount_currency,
+                    curr.decimal_places AS curr_prec,
+                    comp_curr.decimal_places AS comp_curr_prec,
+                    (
+                        t.tax_exigibility != 'on_payment'
+                        OR move.tax_cash_basis_rec_id IS NOT NULL
+                        OR move.always_tax_exigible
+                    ) AS tax_exigible
                 FROM base_lines aml
+                JOIN account_move move ON move.id = aml.move_id
                 JOIN tax_lines lt
                 ON lt.move_id = aml.move_id
                 AND lt.currency_id = aml.currency_id
                 AND lt.partner_id IS NOT DISTINCT FROM aml.partner_id
-                AND lt.effective_tax_id = aml.applied_tax_id
+                AND (
+                    (
+                        aml.tax_repartition_line_id IS NULL
+                        AND lt.effective_tax_id = aml.applied_tax_id
+                    )
+                    OR (
+                        aml.tax_repartition_line_id IS NOT NULL
+                        AND lt.tax_line_id = aml.applied_tax_id
+                        AND lt.group_tax_id IS NOT DISTINCT FROM aml.group_tax_id
+                    )
+                )
                 JOIN account_tax t ON aml.applied_tax_id = t.id
+                JOIN res_currency curr ON curr.id = lt.currency_id
+                JOIN res_currency comp_curr ON comp_curr.id = lt.company_currency_id
                 WHERE (
                     aml.move_type != 'entry'
                     OR (t.tax_exigibility = 'on_payment' AND t.cash_basis_transition_account_id IS NOT NULL)
@@ -56,34 +94,260 @@ class AccountMoveLine(models.Model):
                     OR (aml.analytic_distribution IS NULL AND lt.analytic_distribution IS NULL)
                     OR aml.analytic_distribution = lt.analytic_distribution
                 )
-            ),
-            aggregated AS (
-                SELECT
-                    *,
-                    SUM(base_value) OVER (
-                        PARTITION BY tax_line_id, tax_id
-                        ORDER BY sequence, base_line_id
-                    ) AS base_cumul,
-                    SUM(base_value) OVER (PARTITION BY tax_line_id, tax_id) AS base
-                FROM tax_data
             )
             SELECT
+                tax_line_id || '-' || base_line_id || '-' || base_line_id AS id,
                 move_id,
                 tax_line_id,
                 base_line_id,
-                ROUND(tax_amount * base_cumul / NULLIF(base, 0), 2)
-                - LAG(ROUND(tax_amount * base_cumul / NULLIF(base, 0), 2), 1, 0.0)
-                    OVER (PARTITION BY tax_line_id, tax_id ORDER BY tax_line_id, base_line_id)
-                    AS tax_amount
-            FROM aggregated
+                base_line_id AS src_line_id,
+                display_type,
+                tax_id,
+                group_tax_id,
+                tax_exigible,
+                base_account_id,
+                tax_repartition_line_id,
+                base_amount,
+                tax_amount,
+                base_amount_currency,
+                tax_amount_currency,
+                base_value,
+                base_value_currency,
+                sequence
+            FROM tax_data
             ORDER BY tax_line_id, base_line_id;
             ''',
             table_references=table_references,
             search_condition=search_condition,
         )
 
+    def _postprocess_tax_details_simplified(self, tax_details):
+        if not tax_details:
+            return []
+
+        line_ids = {tax_detail['base_line_id'] for tax_detail in tax_details} | {tax_detail['tax_line_id'] for tax_detail in tax_details}
+        lines = self.browse(line_ids).exists()
+        lines_by_id = {line.id: line for line in lines}
+        tax_ids = {tax_detail['tax_id'] for tax_detail in tax_details}
+        taxes_by_id = {tax.id: tax for tax in self.env['account.tax'].browse(tax_ids).exists()}
+        context_tax_ids = set(tax_ids)
+        for line in lines:
+            context_tax_ids.update(line.tax_ids.ids)
+        parent_tax_ids_by_child = defaultdict(set)
+        for parent_tax in self.env['account.tax'].search([('children_tax_ids', 'in', list(context_tax_ids))]):
+            for child_tax in parent_tax.children_tax_ids:
+                parent_tax_ids_by_child[child_tax.id].add(parent_tax.id)
+
+        tax_details_by_base_tax = defaultdict(list)
+        for tax_detail in tax_details:
+            tax_details_by_base_tax[(tax_detail['base_line_id'], tax_detail['tax_id'])].append(tax_detail)
+
+        tax_details = []
+        for (base_line_id, tax_id), candidate_details in tax_details_by_base_tax.items():
+            base_line = lines_by_id[base_line_id]
+            context_candidates = [
+                tax_detail
+                for tax_detail in candidate_details
+                if lines_by_id[tax_detail['tax_line_id']].tax_ids
+            ]
+            if not context_candidates:
+                tax_details.extend(candidate_details)
+                continue
+
+            base_context_tax_ids = set(base_line.tax_ids.ids)
+            for tax in base_line.tax_ids:
+                base_context_tax_ids.update(tax.children_tax_ids.ids)
+            base_context_tax_ids.discard(tax_id)
+            for parent_tax_id in parent_tax_ids_by_child[tax_id]:
+                base_context_tax_ids.discard(parent_tax_id)
+
+            matching_candidates = []
+            for tax_detail in context_candidates:
+                tax_line = lines_by_id[tax_detail['tax_line_id']]
+                tax_line_context_tax_ids = set(tax_line.tax_ids.ids)
+                for tax in tax_line.tax_ids:
+                    tax_line_context_tax_ids.update(parent_tax_ids_by_child[tax.id])
+                if tax_line_context_tax_ids.issubset(base_context_tax_ids):
+                    matching_candidates.append(tax_detail)
+
+            if matching_candidates:
+                tax_details.append(max(
+                    matching_candidates,
+                    key=lambda tax_detail: (
+                        len(lines_by_id[tax_detail['tax_line_id']].tax_ids),
+                        -lines_by_id[tax_detail['tax_line_id']].id,
+                    ),
+                ))
+            else:
+                tax_details.append(min(
+                    candidate_details,
+                    key=lambda tax_detail: (
+                        bool(lines_by_id[tax_detail['tax_line_id']].tax_ids),
+                        lines_by_id[tax_detail['tax_line_id']].id,
+                    ),
+                ))
+
+        raw_tax_details_by_tax_line = defaultdict(list)
+        for tax_detail in tax_details:
+            raw_tax_details_by_tax_line[(tax_detail['tax_line_id'], tax_detail['tax_id'])].append(tax_detail)
+
+        tax_details = []
+        for (tax_line_id, _tax_id), raw_tax_details in raw_tax_details_by_tax_line.items():
+            tax_line = lines_by_id[tax_line_id]
+            company_currency = tax_line.company_currency_id
+            currency = tax_line.currency_id
+            total_base_value = sum(tax_detail['base_value'] for tax_detail in raw_tax_details)
+            total_base_value_currency = sum(tax_detail['base_value_currency'] for tax_detail in raw_tax_details)
+            total_tax_amount = raw_tax_details[0]['tax_amount']
+            total_tax_amount_currency = raw_tax_details[0]['tax_amount_currency']
+            cumulated_base_value = 0.0
+            cumulated_base_value_currency = 0.0
+            cumulated_tax_amount = 0.0
+            cumulated_tax_amount_currency = 0.0
+
+            for tax_detail in sorted(raw_tax_details, key=lambda tax_detail: (tax_detail['sequence'], tax_detail['base_line_id'])):
+                cumulated_base_value += tax_detail['base_value']
+                cumulated_base_value_currency += tax_detail['base_value_currency']
+                new_cumulated_tax_amount = company_currency.round(
+                    total_tax_amount * cumulated_base_value / total_base_value
+                ) if total_base_value else 0.0
+                new_cumulated_tax_amount_currency = currency.round(
+                    total_tax_amount_currency * cumulated_base_value_currency / total_base_value_currency
+                ) if total_base_value_currency else 0.0
+                tax_details.append({
+                    **tax_detail,
+                    'tax_amount': round(new_cumulated_tax_amount - cumulated_tax_amount, company_currency.decimal_places),
+                    'tax_amount_currency': round(new_cumulated_tax_amount_currency - cumulated_tax_amount_currency, currency.decimal_places),
+                })
+                cumulated_tax_amount = new_cumulated_tax_amount
+                cumulated_tax_amount_currency = new_cumulated_tax_amount_currency
+
+        direct_tax_details = []
+        tax_line_base_details = []
+        for tax_detail in tax_details:
+            if lines_by_id[tax_detail['base_line_id']].tax_repartition_line_id:
+                tax_line_base_details.append(tax_detail)
+            else:
+                direct_tax_details.append(tax_detail)
+
+        processed_tax_details = list(direct_tax_details)
+        tax_details_by_tax_line = defaultdict(list)
+        for tax_detail in processed_tax_details:
+            tax_details_by_tax_line[tax_detail['tax_line_id']].append(tax_detail)
+
+        tax_line_base_details.sort(key=lambda tax_detail: (
+            lines_by_id[tax_detail['base_line_id']].tax_line_id.sequence,
+            lines_by_id[tax_detail['base_line_id']].id,
+            lines_by_id[tax_detail['tax_line_id']].tax_line_id.sequence,
+            lines_by_id[tax_detail['tax_line_id']].id,
+        ))
+
+        for tax_line_base_detail in tax_line_base_details:
+            source_details = tax_details_by_tax_line.get(tax_line_base_detail['base_line_id'], [])
+            if not source_details:
+                continue
+
+            source_details_by_base_line = defaultdict(lambda: {
+                'base_amount': 0.0,
+                'base_amount_currency': 0.0,
+            })
+            for source_detail in source_details:
+                source_base_detail = source_details_by_base_line[source_detail['base_line_id']]
+                source_base_detail['base_amount'] += source_detail['tax_amount']
+                source_base_detail['base_amount_currency'] += source_detail['tax_amount_currency']
+
+            cumulated_tax_amount = 0.0
+            cumulated_tax_amount_currency = 0.0
+            total_base_amount = sum(source_detail['base_amount'] for source_detail in source_details_by_base_line.values())
+            total_base_amount_currency = sum(source_detail['base_amount_currency'] for source_detail in source_details_by_base_line.values())
+            base_line = lines_by_id[tax_line_base_detail['base_line_id']]
+            company_currency = base_line.company_currency_id
+            currency = base_line.currency_id
+
+            for base_line_id, source_detail in sorted(source_details_by_base_line.items()):
+                new_cumulated_tax_amount = company_currency.round(
+                    tax_line_base_detail['tax_amount'] * source_detail['base_amount'] / total_base_amount
+                ) if total_base_amount else 0.0
+                new_cumulated_tax_amount_currency = currency.round(
+                    tax_line_base_detail['tax_amount_currency'] * source_detail['base_amount_currency'] / total_base_amount_currency
+                ) if total_base_amount_currency else 0.0
+                new_tax_detail = {
+                    **tax_line_base_detail,
+                    'id': f"{tax_line_base_detail['tax_line_id']}-{base_line_id}-{tax_line_base_detail['base_line_id']}",
+                    'base_line_id': base_line_id,
+                    'src_line_id': tax_line_base_detail['base_line_id'],
+                    'base_amount': source_detail['base_amount'],
+                    'tax_amount': round(new_cumulated_tax_amount - cumulated_tax_amount, company_currency.decimal_places),
+                    'base_amount_currency': source_detail['base_amount_currency'],
+                    'tax_amount_currency': round(new_cumulated_tax_amount_currency - cumulated_tax_amount_currency, currency.decimal_places),
+                }
+                cumulated_tax_amount = new_cumulated_tax_amount
+                cumulated_tax_amount_currency = new_cumulated_tax_amount_currency
+                processed_tax_details.append(new_tax_detail)
+                tax_details_by_tax_line[new_tax_detail['tax_line_id']].append(new_tax_detail)
+
+        return processed_tax_details
+
+    def _get_query_postprocessed_tax_details_simplified(self, table_references, search_condition):
+        self.flush_model()
+        self.env.cr.execute(self._get_query_tax_details_simplified(table_references, search_condition))
+        tax_details = self.env.cr.dictfetchall()
+        tax_details = self._postprocess_tax_details_simplified(tax_details)
+
+        table_name = f"account_move_line_tax_details_simplified_{id(self.env.cr)}"
+        self.env.cr.execute(SQL('''
+            CREATE TEMPORARY TABLE IF NOT EXISTS %(table_name)s (
+                id text,
+                base_line_id integer,
+                tax_line_id integer,
+                display_type varchar,
+                src_line_id integer,
+                tax_id integer,
+                group_tax_id integer,
+                tax_exigible boolean,
+                base_account_id integer,
+                tax_repartition_line_id integer,
+                base_amount numeric,
+                tax_amount numeric,
+                base_amount_currency numeric,
+                tax_amount_currency numeric
+            ) ON COMMIT DROP
+            ''', table_name=SQL.identifier(table_name)))
+        self.env.cr.execute(SQL("TRUNCATE %(table_name)s", table_name=SQL.identifier(table_name)))
+        if tax_details:
+            self.env.cr.execute_values(
+                f'''
+                    INSERT INTO "{table_name}" (
+                        id, base_line_id, tax_line_id, display_type, src_line_id, tax_id, group_tax_id,
+                        tax_exigible, base_account_id, tax_repartition_line_id, base_amount, tax_amount,
+                        base_amount_currency, tax_amount_currency
+                    ) VALUES %s
+                    ''',
+                [(
+                    tax_detail['id'],
+                    tax_detail['base_line_id'],
+                    tax_detail['tax_line_id'],
+                    tax_detail['display_type'],
+                    tax_detail['src_line_id'],
+                    tax_detail['tax_id'],
+                    tax_detail['group_tax_id'],
+                    tax_detail['tax_exigible'],
+                    tax_detail['base_account_id'],
+                    tax_detail['tax_repartition_line_id'],
+                    tax_detail['base_amount'],
+                    tax_detail['tax_amount'],
+                    tax_detail['base_amount_currency'],
+                    tax_detail['tax_amount_currency'],
+                ) for tax_detail in tax_details],
+            )
+        return SQL('''
+            SELECT *
+            FROM %(table_name)s
+            ORDER BY base_line_id, ABS(base_amount), ABS(tax_amount), tax_line_id
+            ''', table_name=SQL.identifier(table_name))
+
     @api.model
-    def _get_query_tax_details_from_domain(self, domain, fallback=True) -> SQL:
+    def _get_query_tax_details_from_domain(self, domain, fallback=False) -> SQL:
         """ Create the tax details sub-query based on the orm domain passed as parameter.
 
         :param domain:      An orm domain on account.move.line.
@@ -91,6 +355,8 @@ class AccountMoveLine(models.Model):
         :return:            query as SQL object
         """
         query = self.env['account.move.line']._search(domain)
+        if not fallback:
+            return self._get_query_postprocessed_tax_details_simplified(query.from_clause, query.where_clause)
 
         return self._get_query_tax_details(query.from_clause, query.where_clause, fallback=fallback)
 
