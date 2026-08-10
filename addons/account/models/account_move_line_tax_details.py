112006e60b441b3baaf53b57f9d7f3fa3d515042
Josse Colpaert <jco@odoo.com>
2026-08-10 14:54:45 +0200

    [IMP] account: tax details simpler

--- addons/account/models/account_move_line_tax_details.py
+++ addons/account/models/account_move_line_tax_details.py
@@ -21,56 +21,23 @@ class AccountMoveLine(models.Model):
                 FROM %(table_references)s
                 WHERE %(search_condition)s
             ),
-            base_lines AS (
-                SELECT
-                    f.*,
-                    rel.account_tax_id AS applied_tax_id,
-                    ARRAY(
-                        SELECT COALESCE(child_tax.id, tax.id)
-                        FROM account_move_line_account_tax_rel rel2
-                        JOIN account_tax tax ON tax.id = rel2.account_tax_id
-                        LEFT JOIN account_tax_filiation_rel tax_filiation ON tax_filiation.parent_tax = tax.id
-                        LEFT JOIN account_tax child_tax ON child_tax.id = tax_filiation.child_tax
-                        WHERE rel2.account_move_line_id = f.id
-                        ORDER BY tax.sequence, tax.id, child_tax.sequence, child_tax.id
-                    ) AS applied_tax_ids
-                FROM filtered_aml f
-                JOIN account_move_line_account_tax_rel rel ON f.id = rel.account_move_line_id
-            ),
-            tax_lines AS (
-                SELECT
-                    f.*,
-                    tax_rep.account_id AS rep_account_id,
-                    tax_rep.factor_percent,
-                    tax_rep.use_in_tax_closing,
-                    COALESCE(f.group_tax_id, f.tax_line_id) AS applied_tax_id,
-                    ARRAY(
-                        SELECT rel.account_tax_id
-                        FROM account_move_line_account_tax_rel rel
-                        JOIN account_tax tax ON tax.id = rel.account_tax_id
-                        WHERE rel.account_move_line_id = f.id
-                        ORDER BY tax.sequence, tax.id
-                    ) AS applied_tax_ids
-                FROM filtered_aml f
-                JOIN account_tax_repartition_line tax_rep ON tax_rep.id = f.tax_repartition_line_id
-            ),
             tax_data AS (
                 SELECT
-                    account_move_line.id AS tax_line_id,
-                    account_move_line.balance AS tax_amount,
-                    account_move_line.amount_currency AS tax_amount_currency,
-                    base_line.id AS base_line_id,
-                    base_line.move_id,
-                    account_move_line.display_type,
-                    account_move_line.tax_line_id AS tax_id,
-                    account_move_line.group_tax_id,
-                    account_move_line.tax_repartition_line_id,
-                    base_line.account_id AS base_account_id,
+                    lt.id AS tax_line_id,
+                    t.id AS tax_id,
+                    lt.balance AS tax_amount,
+                    lt.amount_currency AS tax_amount_currency,
+                    account_move_line.id AS base_line_id,
+                    lt.move_id,
+                    lt.display_type,
+                    lt.group_tax_id,
+                    lt.tax_repartition_line_id,
+                    account_move_line.account_id AS base_account_id,
                     t.sequence,
-                    CASE WHEN t.amount_type <> 'fixed' THEN base_line.balance ELSE base_line.quantity END AS base_value,
-                    base_line.balance AS base_amount,
-                    CASE WHEN t.amount_type <> 'fixed' THEN base_line.amount_currency ELSE base_line.quantity END AS base_value_currency,
-                    base_line.amount_currency AS base_amount_currency,
+                    CASE WHEN t.amount_type <> 'fixed' THEN account_move_line.balance ELSE account_move_line.quantity END AS base_value,
+                    account_move_line.balance AS base_amount,
+                    CASE WHEN t.amount_type <> 'fixed' THEN account_move_line.amount_currency ELSE account_move_line.quantity END AS base_value_currency,
+                    account_move_line.amount_currency AS base_amount_currency,
                     curr.decimal_places AS curr_prec,
                     comp_curr.decimal_places AS comp_curr_prec,
                     (
@@ -78,39 +45,35 @@ class AccountMoveLine(models.Model):
                         OR move.tax_cash_basis_rec_id IS NOT NULL
                         OR move.always_tax_exigible
                     ) AS tax_exigible
-                FROM base_lines base_line
-                JOIN account_move move ON move.id = base_line.move_id
-                JOIN tax_lines account_move_line
-                    ON account_move_line.move_id = base_line.move_id
-                    AND account_move_line.currency_id = base_line.currency_id
-                    AND account_move_line.partner_id IS NOT DISTINCT FROM base_line.partner_id
-                    AND (
-                        account_move_line.applied_tax_id = base_line.applied_tax_id
-                        OR (
-                            base_line.tax_repartition_line_id IS NOT NULL
-                            AND account_move_line.tax_line_id = base_line.applied_tax_id
-                        )
-                    )
-                JOIN account_tax t ON account_move_line.tax_line_id = t.id
-                JOIN res_currency curr ON curr.id = account_move_line.currency_id
-                JOIN res_currency comp_curr ON comp_curr.id = account_move_line.company_currency_id
+                FROM filtered_aml account_move_line
+                JOIN account_move move ON move.id = account_move_line.move_id
+                JOIN account_move_line_account_tax_rel rel ON rel.account_move_line_id = account_move_line.id
+                JOIN account_tax t ON t.id = rel.account_tax_id
+                JOIN filtered_aml lt
+                    ON t.id = COALESCE(lt.group_tax_id, lt.tax_line_id)
+                    AND lt.move_id = account_move_line.move_id
+                    AND lt.currency_id = account_move_line.currency_id
+                    AND lt.partner_id IS NOT DISTINCT FROM account_move_line.partner_id
+                JOIN account_tax_repartition_line tax_rep ON tax_rep.id = lt.tax_repartition_line_id
+                JOIN res_currency curr ON curr.id = lt.currency_id
+                JOIN res_currency comp_curr ON comp_curr.id = lt.company_currency_id
                 WHERE (
-                        move.move_type != 'entry'
-                    OR (t.tax_exigibility = 'on_payment' AND t.cash_basis_transition_account_id IS NOT NULL)
-                    OR sign(base_line.balance) = sign(account_move_line.balance * t.amount * account_move_line.factor_percent)
+                    lt.account_id = account_move_line.account_id
+                    OR tax_rep.account_id IS NOT NULL
+                ) AND NOT EXISTS (
+                    SELECT 1
+                    FROM account_move_line_account_tax_rel ltr
+                    WHERE ltr.account_move_line_id = lt.id
+                    AND NOT EXISTS (
+                        SELECT 1
+                        FROM account_move_line_account_tax_rel br
+                        WHERE br.account_move_line_id = account_move_line.id
+                        AND br.account_tax_id = ltr.account_tax_id
+                    )
                 ) AND (
-                    NOT t.is_base_affected
-                    OR NOT t.include_base_amount
-                    OR base_line.applied_tax_ids[
-                        ARRAY_LENGTH(base_line.applied_tax_ids, 1) - COALESCE(ARRAY_LENGTH(account_move_line.applied_tax_ids, 1), 0):ARRAY_LENGTH(base_line.applied_tax_ids, 1)
-                    ] = ARRAY[account_move_line.tax_line_id] || account_move_line.applied_tax_ids
-                ) AND (
-                    (t.analytic IS NOT TRUE AND account_move_line.use_in_tax_closing IS TRUE)
-                    OR (base_line.analytic_distribution IS NULL AND account_move_line.analytic_distribution IS NULL)
-                    OR base_line.analytic_distribution = account_move_line.analytic_distribution
-                ) AND (
-                    account_move_line.rep_account_id IS NOT NULL
-                    OR base_line.account_id = account_move_line.account_id
+                    (t.analytic IS NOT TRUE AND tax_rep.use_in_tax_closing IS TRUE)
+                    OR (account_move_line.analytic_distribution IS NULL AND lt.analytic_distribution IS NULL)
+                    OR account_move_line.analytic_distribution = lt.analytic_distribution
                 )
                 %(extra_query_base_tax_line_mapping)s
             ),
