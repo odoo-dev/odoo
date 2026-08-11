4eacead4a7cb45114f2de1a91a9910845fc4b318
Josse Colpaert <jco@odoo.com>
2026-08-11 19:29:53 +0200

    [IMP] account: avoid double EXISTS

--- addons/account/models/account_move_line_tax_details.py
+++ addons/account/models/account_move_line_tax_details.py
@@ -93,19 +93,15 @@ class AccountMoveLine(models.Model):
                             base_line.tax_repartition_line_id IS NOT NULL
                             AND account_move_line.tax_line_id = base_line.applied_tax_id
                             AND (
-                                NOT EXISTS (
-                                    SELECT 1
-                                    FROM account_tax_filiation_rel tax_filiation
-                                    WHERE tax_filiation.parent_tax = account_move_line.group_tax_id
-                                    AND tax_filiation.child_tax = account_move_line.tax_line_id
+                                account_move_line.group_tax_id = base_line.group_tax_id
+                                OR (
+                                    NOT EXISTS (
+                                        SELECT 1
+                                        FROM account_tax_filiation_rel tax_filiation
+                                        WHERE tax_filiation.parent_tax IN (account_move_line.group_tax_id, base_line.group_tax_id)
+                                        AND tax_filiation.child_tax = account_move_line.tax_line_id
+                                    )
                                 )
-                                AND NOT EXISTS (
-                                    SELECT 1
-                                    FROM account_tax_filiation_rel tax_filiation
-                                    WHERE tax_filiation.parent_tax = base_line.group_tax_id
-                                      AND tax_filiation.child_tax = account_move_line.tax_line_id
-                                )
-                                OR account_move_line.group_tax_id = base_line.group_tax_id
                             )
                         )
                     )
