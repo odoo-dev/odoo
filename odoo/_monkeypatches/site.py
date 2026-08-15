fdead50caac7aeef9162ce9b992900cd75ec82dc
Chong Wang (cwg) <cwg@odoo.com>
2026-08-15 13:24:32 +0200

    [REM] _monkeypatches: drop psycopg2 gevent wait callback
    psycopg2 needed set_wait_callback so gevent could yield while
    libpq polls. From 3.1.14, psycopg3 does that itself: if select
    is already monkey-patched at import time, it skips wait_c.
    
    patch_evented() runs patch_all() before any Odoo psycopg import,
    so forcing wait_select is redundant (and would replace wait_poll
    on Linux). Drop the old callback and leave the builtin detection.

--- odoo/_monkeypatches/site.py
+++ odoo/_monkeypatches/site.py
@@ -29,28 +29,7 @@ def patch_evented():
         return
     sys.argv.remove('gevent')
     import gevent.monkey  # noqa: PLC0415
-    import psycopg2  # noqa: PLC0415
-    from gevent.socket import wait_read, wait_write  # noqa: PLC0415
     gevent.monkey.patch_all()
-
-    def gevent_wait_callback(conn, timeout=None):
-        """A wait callback useful to allow gevent to work with Psycopg."""
-        # Copyright (C) 2010-2012 Daniele Varrazzo <daniele.varrazzo@gmail.com>
-        # This function is borrowed from psycogreen module which is licensed
-        # under the BSD license (see in odoo/debian/copyright)
-        while 1:
-            state = conn.poll()
-            if state == psycopg2.extensions.POLL_OK:
-                break
-            elif state == psycopg2.extensions.POLL_READ:
-                wait_read(conn.fileno(), timeout=timeout)
-            elif state == psycopg2.extensions.POLL_WRITE:
-                wait_write(conn.fileno(), timeout=timeout)
-            else:
-                raise psycopg2.OperationalError(
-                    "Bad result from poll: %r" % state)
-
-    psycopg2.extensions.set_wait_callback(gevent_wait_callback)
     odoo.evented = True
 
 
