781a48326624b32f703a9ca1ec074dc731999856
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-08-28 13:11:17 +0000

    [IMP] account: fiscal position taxes
    test_fiscal_position_tax_mapping_with_inactive_tax

--- addons/account/models/partner.py
+++ addons/account/models/partner.py
@@ -53,6 +53,7 @@ class AccountFiscalPosition(models.Model):
         column1='account_fiscal_position_id',
         column2='account_tax_id',
         string='Taxes',
+        domain=[('active', '=', True)],
     )
     tax_map = fields.Json(compute='_compute_tax_map')
     note = fields.Html('Notes', translate=True, help="Legal mentions that have to be printed on the invoices.")
