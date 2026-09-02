6e31f8e7106e59094aa1bc280b033539294aba60
Sébastien Geelen <sge@odoo.com>
2026-09-02 16:02:05 +0200

    WIP

--- addons/html_editor/static/src/views/media_manager_ir_attachment_kanban/kanban_record.js
+++ addons/html_editor/static/src/views/media_manager_ir_attachment_kanban/kanban_record.js
@@ -2,8 +2,9 @@ import { KanbanRecord, kanbanRecordProps } from "@web/views/kanban/kanban_record
 // import { KanbanDropdownMenuWrapper } from "@web/views/kanban/kanban_dropdown_menu_wrapper";
 import { useService } from "@web/core/utils/hooks";
 
-import { onMounted, proxy, t, useProps } from "@odoo/owl";
+import { proxy, t, useProps } from "@odoo/owl";
 import { IMAGE_MIMETYPES } from "@html_editor/main/media/media_manager/helpers";
+import { KanbanDropdownMenuWrapper } from "@web/views/kanban/kanban_dropdown_menu_wrapper";
 
 // import {
 //     IMAGE_MIMETYPES,
@@ -13,15 +14,22 @@ import { IMAGE_MIMETYPES } from "@html_editor/main/media/media_manager/helpers";
 //     QUERRY_ORDERS_BY,
 // } from "@html_editor/main/media/media_manager/helpers";
 
-// export class MediaManagerKanbanDropdownMenuWrapper extends KanbanDropdownMenuWrapper {
-//     onClick(ev) {
-//         console.warn("MediaManagerKanbanDropdownMenuWrapper::onClick", ev);
-//         // Keep the dropdown open as we need the fileupload to remain in the dom
-//         if (!ev.target.tagName === "INPUT" && !ev.target.closest(".file_upload_kanban_action_a")) {
-//             super.onClick(ev);
-//         }
-//     }
-// }
+export class MediaManagerKanbanDropdownMenuWrapper extends KanbanDropdownMenuWrapper {
+    props = useProps({
+        slots: t.object(),
+    });
+
+    onClick(ev) {
+        console.warn("MediaManagerKanbanDropdownMenuWrapper::onClick");
+        console.log(ev.target);
+        console.log(this.props);
+        console.log(JSON.parse(JSON.stringify(this.props)));
+        // // Keep the dropdown open as we need the fileupload to remain in the dom
+        // if (!ev.target.tagName === "INPUT" && !ev.target.closest(".file_upload_kanban_action_a")) {
+        //     super.onClick(ev);
+        // }
+    }
+}
 
 const LS_IS_STARRED_KEY = "ir-attachment-record-is-starred";
 export const MediaManagerkanbanRecordProps = {
@@ -32,12 +40,14 @@ export const MediaManagerkanbanRecordProps = {
 
 export class MediaManagerKanbanRecord extends KanbanRecord {
     static template = "ir.attachment.MediaManagerKanbanRecord";
-    static menuTemplate = "web.KanbanMenu";
+    // static menuTemplate = "web.KanbanMenu"; // todo : unnecessary ?
+    // static menuTemplate = "ir.attachment.MediaManagerKanbanMenu";
     static components = {
         ...KanbanRecord.components,
-        //KanbanDropdownMenuWrapper: MediaManagerKanbanDropdownMenuWrapper,
+        // KanbanDropdownMenuWrapper: MediaManagerKanbanDropdownMenuWrapper,
     };
 
+    // menuRef = signal.ref();
     props = useProps(MediaManagerkanbanRecordProps);
 
     setup() {
@@ -55,11 +65,13 @@ export class MediaManagerKanbanRecord extends KanbanRecord {
 
         this.fileViewer = useService("fileViewer")();
         super.setup();
-        onMounted(() => {
-            console.log("              ====>   this.rootRef()", this.rootRef());
-            const previewMenuItem = this.rootRef().querySelector("[name=open_preview]");
-            console.log("              ====>   previewMenuItem", previewMenuItem);
-        });
+        // onMounted(() => {
+        //     console.log("------");
+        //     console.log(this);
+        //     console.log("  => menuRef \n", this.menuRef());
+        //     const previewMenuItem = this.menuRef()?.querySelector("[name=open_preview]");
+        //     console.log("  => previewMenuItem \n", previewMenuItem);
+        // });
     }
 
     getCardClasses() {
@@ -88,11 +100,16 @@ export class MediaManagerKanbanRecord extends KanbanRecord {
             return;
         }
         console.warn("MediaManagerKanbanRecord::onGlobalClick", ev);
+
+        this.toggleSelected();
         if (!this.props.canMultiSelect) {
             this.toggleSelected();
-        } else if (!ev.target.closest(".o_record_interactive")) {
-            this.previewRecord();
         }
+        // if (!this.props.canMultiSelect) {
+        //     this.toggleSelected();
+        // } else if (!ev.target.closest(".o_record_interactive")) {
+        //     this.previewRecord();
+        // }
 
         // const { forceGlobalClick, canOpenRecords, openAction, openRecord, record } = this.props;
         // if (!forceGlobalClick && openAction) {
