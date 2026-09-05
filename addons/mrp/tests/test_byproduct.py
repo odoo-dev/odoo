5178d5b5f1e7260ea28927590f456a12cf20500b
Raphael Collet <rco@odoo.com>
2026-09-05 12:09:16 +0200

    [FIX] mrp: checking byproducts in test
    The constraint is checked when writing on mrp.production field
    move_finished_ids.  However, this was tested by some idempotent write on
    the field.  Simply make the test not rely on idempotent writes.

--- addons/mrp/tests/test_byproduct.py
+++ addons/mrp/tests/test_byproduct.py
@@ -408,19 +408,18 @@ class TestMrpByProduct(common.TransactionCase):
 
         # Update byproduct has cost share > 100%
         with self.assertRaises(ValidationError):
-            byproduct_1.cost_share = 120
-            mo.write({'move_byproduct_ids': [(4, byproduct_1.id)]})
+            mo.write({'move_byproduct_ids': [Command.update(byproduct_1.id, {'cost_share': 120})]})
 
         # Update byproduct has cost share < 0%
         with self.assertRaises(ValidationError):
-            byproduct_1.cost_share = -10
-            mo.write({'move_byproduct_ids': [(4, byproduct_1.id)]})
+            mo.write({'move_byproduct_ids': [Command.update(byproduct_1.id, {'cost_share': -10})]})
 
         # Update byproducts have total cost share > 100%
         with self.assertRaises(ValidationError):
-            byproduct_1.cost_share = 60
-            byproduct_2.cost_share = 70
-            mo.write({'move_byproduct_ids': [(6, 0, [byproduct_1.id, byproduct_2.id])]})
+            mo.write({'move_byproduct_ids': [
+                Command.update(byproduct_1.id, {'cost_share': 60}),
+                Command.update(byproduct_2.id, {'cost_share': 70}),
+            ]})
 
     def test_check_byproducts_cost_share_02(self):
         """
