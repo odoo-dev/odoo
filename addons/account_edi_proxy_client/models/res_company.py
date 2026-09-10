2eafd84e325fe968898406100501d8c5a2e70980
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-09-10 22:34:18 +0000

    [IMP] account_peppol account_edi_proxy_client: fixup

--- addons/account_edi_proxy_client/models/res_company.py
+++ addons/account_edi_proxy_client/models/res_company.py
@@ -1,4 +1,3 @@
-# -*- coding:utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import fields, models
 
@@ -6,4 +5,4 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    account_edi_proxy_client_ids = fields.One2many('account_edi_proxy_client.user', inverse_name='company_id')
+    account_edi_proxy_client_ids = fields.One2many('account_edi_proxy_client.user', inverse_name='company_id', domain=[('active', '=', True)])
