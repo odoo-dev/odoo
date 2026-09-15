1de9d85e69d6641125ff3d9f5ab622abc2d10580
Kevin Gerard (kege) <kege@odoo.com>
2026-09-15 10:59:51 +0000

    [FIX] portal_discuss: fix portal users not able to access chat
    Before this commit, a user was not able to access a chat if not having
    access to the partner.
    
    Steps to reproduce:
    Log in as 'Admin' and create a chat with a portal user
    Send a message in the same chat
    Log in as the portal user > My Account > Discuss
    go to the 'chat' tab
    
    To fix this issue, we follow a similar approach as livechat and generate
    the avatar in that specific case.
    
    task-6455893

--- addons/portal_discuss/models/discuss_channel.py
+++ addons/portal_discuss/models/discuss_channel.py
@@ -6,6 +6,13 @@ from odoo import models
 class DiscussChannel(models.Model):
     _inherit = "discuss.channel"
 
+    def _generate_avatar(self):
+        if self.channel_type == "chat" and self.env.user.share:
+            # sudo: discuss.channel - only the correspondent's initials are shown to the portal
+            # user, like livechat does for visitors, so their real uploaded photo is never exposed
+            return super(DiscussChannel, self.sudo())._generate_avatar()
+        return super()._generate_avatar()
+
     def _get_invite_urls_by_emails(self, emails):
         urls = super()._get_invite_urls_by_emails(emails)
         portal_emails = self.env["res.users"].search_fetch([
