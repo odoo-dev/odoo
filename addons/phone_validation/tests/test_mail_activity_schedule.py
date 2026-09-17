191dd84e6ab40473a5f81fb0ab972cce3249e527
qsm-odoo <qsm@odoo.com>
2026-09-17 16:12:49 +0200

    forward

--- addons/phone_validation/tests/test_mail_activity_schedule.py
+++ addons/phone_validation/tests/test_mail_activity_schedule.py
@@ -10,18 +10,71 @@ from odoo.tests import tagged
 @tagged("phone_validation")
 class TestMailActivitySchedule(ActivityScheduleCase):
 
+    def _create_call_activity(self, record):
+        return self.env["mail.activity"].create({
+            "activity_type_id": self.activity_type_call.id,
+            "res_model_id": self.env["ir.model"]._get_id(record._name),
+            "res_id": record.id,
+            "user_id": self.env.user.id,
+        })
+
     def test_call_activity_phone(self):
         """Only Call activities copy the target's phone number."""
         partner = self.env["res.partner"].create({
-            "name": "Partner with phone",
-            "phone": "+1 202 555 0182",
+            "name": "Belgian partner",
+            "country_id": self.env.ref("base.be").id,
+            "phone": "0498707070",
         })
 
         call_activity = partner.activity_schedule("mail.mail_activity_data_call")
         todo_activity = partner.activity_schedule("mail.mail_activity_data_todo")
 
-        self.assertEqual(call_activity.phone, partner.phone)
+        self.assertEqual(partner.phone_formatted, "+32 498 70 70 70")
+        self.assertEqual(call_activity.phone, partner.phone_formatted)
         self.assertFalse(todo_activity.phone)
+
+    def test_call_activity_phone_falls_back_on_raw_phone(self):
+        partner = self.env["res.partner"].create({
+            "name": "Partner with an invalid phone",
+            "phone": "not a phone number",
+        })
+
+        activity = partner.activity_schedule("mail.mail_activity_data_call")
+
+        self.assertFalse(partner.phone_formatted)
+        self.assertEqual(activity.phone, partner.phone)
+
+    def test_call_activity_phone_uses_declared_phone_field(self):
+        record = self.env["mail.activity.type"].create({"name": "0498707070"})
+
+        with patch.object(
+            self.env.registry[record._name],
+            "_phone_get_number_fields",
+            return_value=["name"],
+        ):
+            activity = self._create_call_activity(record)
+
+        self.assertNotIn("phone", record)
+        self.assertEqual(activity.phone, record.name)
+
+    def test_call_activity_phone_falls_back_to_partner(self):
+        partner = self.env["res.partner"].create({
+            "name": "Belgian partner",
+            "country_id": self.env.ref("base.be").id,
+            "phone": "0498707070",
+        })
+        record = self.env["mail.activity.type"].create({"name": "Partner-only record"})
+
+        with patch.object(
+            self.env.registry[record._name],
+            "_mail_get_partners",
+            return_value={record.id: partner},
+        ):
+            activity = self._create_call_activity(record)
+
+        self.assertFalse(record._phone_get_number_fields())
+        self.assertEqual(partner.phone_formatted, "+32 498 70 70 70")
+        self.assertEqual(activity.phone, partner.phone_formatted)
 
     def test_call_activity_schedule_phone(self):
         """Call scheduling saves its phone and only fills an empty partner phone."""
@@ -30,16 +83,19 @@ class TestMailActivitySchedule(ActivityScheduleCase):
                 "name": "fill empty partner",
                 "input_partner_phone": False,
                 "input_activity_phone": "+1 202 555 0182",
-                "expected_partner_phone": "+1 202 555 0182",
+                "expected_activity_phone": "+1 202-555-0182",
+                "expected_partner_phone": "+1 202-555-0182",
             }, {
                 "name": "keep existing partner",
                 "input_partner_phone": "+1 202 555 0100",
                 "input_activity_phone": "+1 202 555 0182",
+                "expected_activity_phone": "+1 202-555-0182",
                 "expected_partner_phone": "+1 202 555 0100",
             }, {
                 "name": "clear activity phone",
                 "input_partner_phone": "+1 202 555 0100",
                 "input_activity_phone": False,
+                "expected_activity_phone": False,
                 "expected_partner_phone": "+1 202 555 0100",
             },
         ]
@@ -58,7 +114,7 @@ class TestMailActivitySchedule(ActivityScheduleCase):
                 wizard.phone = case["input_activity_phone"]
                 activity = wizard._action_schedule_activities()
 
-                self.assertEqual(activity.phone, case["input_activity_phone"])
+                self.assertEqual(activity.phone, case["expected_activity_phone"])
                 self.assertEqual(partner.phone, case["expected_partner_phone"])
 
     def test_call_activity_schedule_keeps_record_phones_in_batch(self):
@@ -77,7 +133,7 @@ class TestMailActivitySchedule(ActivityScheduleCase):
 
         self.assertEqual(
             set(activities.mapped("phone")),
-            {"+1 202 555 0101", "+1 202 555 0102"},
+            {"+1 202-555-0101", "+1 202-555-0102"},
         )
         self.assertEqual(
             set(partners.mapped("phone")),
@@ -102,7 +158,7 @@ class TestMailActivitySchedule(ActivityScheduleCase):
             wizard.phone = "+1 202 555 0182"
             activity = wizard._action_schedule_activities()
 
-        self.assertEqual(activity.phone, "+1 202 555 0182")
+        self.assertEqual(activity.phone, "+1 202-555-0182")
         self.assertFalse(any(partners.mapped("phone")))
 
     def test_call_activity_schedule_does_not_expose_inaccessible_phone(self):
@@ -143,3 +199,7 @@ class TestMailActivitySchedule(ActivityScheduleCase):
 
         self.assertEqual(wizard.phone, "6504193846")
         self.assertEqual(wizard.phone_formatted, "+1 650-419-3846")
+
+        activity = wizard._action_schedule_activities()
+
+        self.assertEqual(activity.phone, wizard.phone_formatted)
